fe491d1655e049ef021f574c21d87f17711529f4
Jae Kwon <jae@tendermint.com>
2021-05-26 19:38:45 -0700

    more complete selector_test.go

--- tests/selector_test.go
+++ tests/selector_test.go
@@ -31,11 +31,11 @@ type PS1 *struct {
 	F0 int
 }
 
-type S2 struct {
+type S7 struct {
 	S1
 }
 
-type S3 struct {
+type S9 struct {
 	*S1
 }
 
@@ -83,16 +83,16 @@ func TestSelectors(t *testing.T) {
 	// _printValue(x6.F0)       *PT>*DT(S1P)??? > *PT>*DT(S1)>*ST.F0
 	//                          +D  +1            +D  +1        F:0
 	//                          VPDerefField{depth:1,index:0}(WRONG!!!) > VPDerefField{depth:1,index:0} XXX ERROR
-	var x7 S2 = S2{S1{1}}
-	_printValue(x7.F0) //       *DT(S2)>*ST.S1 > *DT(S1)>*ST.F0
+	var x7 S7 = S7{S1{1}}
+	_printValue(x7.F0) //       *DT(S7)>*ST.S1 > *DT(S1)>*ST.F0
 	//                          +1        F:0    +1        F:0
 	//                          VPField{depth:1,index:0} > VPField{depth:1,index:0}
-	var x8 *S2 = &x7
-	_printValue(x8.F0) //       *PT>*DT(S2)>*ST.S1 > *DT(S1)>*ST.F0
+	var x8 *S7 = &x7
+	_printValue(x8.F0) //       *PT>*DT(S7)>*ST.S1 > *DT(S1)>*ST.F0
 	//                          +D  +1        F:0    +1        F:0
 	//                          VPDerefField{depth:1,index:0} > VPField{depth:1,index:0}
-	var x9 = S3{x5}
-	_printValue(x9.F0) //       *DT(S3)>*ST.S1 > *PT>*DT(S1)>*ST.F0
+	var x9 = S9{x5}
+	_printValue(x9.F0) //       *DT(S9)>*ST.S1 > *PT>*DT(S1)>*ST.F0
 	//                          +1        F:0    +D  +1        F:0
 	//                          VPField{depth:1,index:0} > VPDerefField{depth:1,index:0}
 	var x10 struct {
@@ -104,39 +104,59 @@ func TestSelectors(t *testing.T) {
 	_printValue(x10.Hello) //   *ST.S1 > *DT(S1).Hello
 	//                            F:0    +1    M:0
 	//                          VPField{depth:0,index:0} > VPValMethod{index:0}
-	//_printValue(x10.Byte)     *ST.S1 > *DT(S1).Bye
-	//                            F:0    +1   *M:1
-	//                          VPField{depth:0,index:0} > VPPtrField{index:1} XXX ERROR
-	x11 := &x10
-	_printValue(x11.F0) //      *PT>*ST.S1 > *DT(S1)>*ST.F0
+	_printValue(x10.Bye) //     (*PT>)*ST.S1 > *DT(S1).Bye
+	//                           +S     F:0    +1   *M:1
+	//                          VPSubrefField{depth:0,index:0} > VPDerefPtrField{index:1}
+	var x11 S7 = S7{S1{1}}
+	_printValue(x11.F0) //      *DT(S7)>*ST.S1 > *DT(S1)>*ST.F0 NOTE same as x7.
+	//                          +1        F:0    +1        F:0
+	//                          VPField{depth:1,index:0} > VPField{depth:1,index:0}
+	_printValue(x11.Hello) //   *DT(S7)>*ST.S1 > *DT(S1)>*ST.Hello
+	//                          +1        F:0    +1        M:0
+	//                          VPField{depth:1,index:0} > VPValMethod{index:0}
+	_printValue(x11.Bye) //     (*PT>)*DT(S7)>*ST.S1 > *DT(S1).Bye
+	//                           +S   +1        F:0    +1   *M:1
+	//                          VPSubrefField{depth:1,index:0} > VPDerefPtrMethod{index:1}
+	x10p := &x10
+	_printValue(x10p.F0) //     *PT>*ST.S1 > *DT(S1)>*ST.F0
 	//                          +D    F:0    +1        F:0
-	//                          VPDerefField{depth:0,index:0} > VPField{depth:1,index:0}
+	//                          VPDerefField{depth:1,index:0} > VPField{depth:1,index:0}
+	_printValue(x10p.Hello) //  *PT>*ST.S1 > *DT(S1).Hello
+	//                          +D    F:0    +1    M:0
+	//                          VPDerefField{depth:1,index:0} > VPValMethod{index:0}
+	_printValue(x10p.Bye) //    *PT>*ST.S1 > *DT(S1).Bye
+	//                          +D    F:0    +1   *M:1
+	//                          VPSubrefField{depth:1,index:0} > VPDerefPtrMethod{index:1}
+	var x11p *S7 = &S7{S1{1}}
+	_printValue(x11p.F0) //     *PT>*DT(S7)>*ST.S1 > *DT(S1)>*ST.F0
+	//                          +1            F:0    +1        F:0
+	//                          VPDerefField{depth:1,index:0} > VPField{depth:1,index:0}
+	_printValue(x11p.Hello) //  *PT>*DT(S7)>*ST.S1 > *DT(S1).Hello
+	//                          +1            F:0    +1    M:0
+	//                          VPDerefField{depth:1,index:0} > VPValMethod{index:0}
+	_printValue(x11p.Bye) //    *PT>*DT(S7)>*ST.S1 > *DT(S1).Bye
+	//                          +1            F:0    +1   *M:1
+	//                          VPSubrefField{depth:1,index:0} > VPDerefPtrMethod{index:1}
 	var x12 struct {
 		*S1
 	} = struct{ *S1 }{&S1{1}}
 	_printValue(x12.F0) //      *ST.S1 > *PT>*DT(S1)>*ST.F0
-	//                            F:0     +D  +1       F:0
+	//                            F:0    +D  +1        F:0
 	//                          VPField{depth:0,index:0} > VPDerefField{depth:1,index:0}
 	_printValue(x12.Hello) //   *ST.S1 > *PT>*DT(S1).Hello
-	//                            F:0     +D  +1   M:0
+	//                            F:0    +D  +1    M:0
 	//                          VPField{depth:0,index:0} > VPDerefValMethod{index:0}
 	_printValue(x12.Bye) //     *ST.S1 > *PT>*DT(S1).Bye
-	//                            F:0     +D  +1  *M:1
+	//                            F:0    +D  +1   *M:1
 	//                          VPField{depth:0,index:0} > VPDerefPtrMethod{index:1}
-	var x13 struct {
-		S1
-	} = struct{ S1 }{S1{1}}
-	_printValue(x13.F0) //      (*PT>)*ST.S1 &> *DT(S1)>*ST.F0
-	//                           +S     F:0
-	// XXX Remove this line
-	x14 := &x12
-	_printValue(x14.F0) //      *PT>*ST.S1 > *PT>*DT(S1)>*ST.F0
-	//                          +D    F:0     +D  +1        F:0
+	x13 := &x12
+	_printValue(x13.F0) //      *PT>*ST.S1 > *PT>*DT(S1)>*ST.F0
+	//                          +D    F:0    +D  +1        F:0
 	//                          VPDerefField{depth:0,index:0} > VPDerefField{depth:1,index:0}
-	_printValue(x14.Hello) //   *PT>*ST.S1 > *PT>*DT(S1)>*ST.Hello
-	//                           +D   F:0     +D  +1        M:0
+	_printValue(x13.Hello) //   *PT>*ST.S1 > *PT>*DT(S1).Hello
+	//                          +D    F:0    +D  +1    M:0
 	//                          VPDerefField{depth:0,index:0} > VPDerefValMethod{index:0}
-	_printValue(x14.Bye) //     *PT>*ST.S1 > *PT>*DT(S1)>*ST.Bye
-	//                           +D   F:0     +D  +1       *M:1
+	_printValue(x13.Bye) //     *PT>*ST.S1 > *PT>*DT(S1).Bye
+	//                          +D    F:0    +D  +1   *M:1
 	//                          VPDerefField{depth:0,index:0} > VPDerefPtrMethod{index:1}
 }
