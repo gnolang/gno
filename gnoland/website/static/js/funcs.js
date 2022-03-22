3502420be7ff90b4fdc244012539fd8656c9eccf
Jae Kwon <jae@tendermint.com>
2022-03-21 18:01:15 -0700

    fix shell quoting

--- gnoland/website/static/js/funcs.js
+++ gnoland/website/static/js/funcs.js
@@ -21,51 +21,36 @@ function updateCommand(x) {
 	shell.empty();
 
 	// command 1: construct tx.
-	var args = ["gnokey", "maketx", "call", "<KEYNAME>", 
-		"--pkgpath", realmPath, "--func", funcName,
+	var args = ["gnokey", "maketx", "call", "KEYNAME", 
+		"--pkgpath", shq(realmPath), "--func", shq(funcName),
 		"--gas-fee", "1gnot", "--gas-wanted", "2000000"];
 	vals.forEach(function(arg) {
 		args.push("--arg");
-		args.push(arg);
+		args.push(shq(arg));
 	});
-	var command = shellescape(args);
-	command = "> ./"+command+" > unsigned.tx";
+	var command = args.join(" ");
+	command = command+" > unsigned.tx";
 	shell.append(u("<span>").text(command)).append(u("<br>"));
 
 	// command 2: sign tx.
-	var args = ["gnokey", "sign", "<KEYNAME>",
+	var args = ["gnokey", "sign", "KEYNAME",
 		"--txpath", "unsigned.tx", "--chainid", "testchain",
-		"--number", "<ACCOUNTNUMBER>",
-		"--sequence", "<SEQUENCENUMBER>"];
-	var command = shellescape(args);
-	command = "> ./"+command+" > signed.tx";
+		"--number", "ACCOUNTNUMBER",
+		"--sequence", "SEQUENCENUMBER"];
+	var command = args.join(" ");
+	command = command+" > signed.tx";
 	shell.append(u("<span>").text(command)).append(u("<br>"));
 
 	// command 3: broadcast tx.
 	var args = ["gnokey", "broadcast", "signed.tx"];
-	var command = shellescape(args);
-	command = "> ./"+command;
+	var command = args.join(" ");
+	command = command;
 	shell.append(u("<span>").text(command)).append(u("<br>"));
 }
 
-// From https://github.com/xxorax/node-shell-escape/blob/master/shell-escape.js
-// MIT license.
-//
-// return a shell compatible format
-function shellescape(a) {
-	var ret = [];
-
-	a.forEach(function(s) {
-		if (/[^A-Za-z0-9_\/:=-]/.test(s)) {
-			s = "'"+s.replace(/'/g,"'\\''")+"'";
-			s = s.replace(/^(?:'')+/g, '') // unduplicate single-quote at the beginning
-				.replace(/\\'''/g, "\\'" ); // remove non-escaped single-quote if there are enclosed between 2 escaped
-		}
-		if (s == "") {
-			s = "''";
-		}
-		ret.push(s);
-	});
-
-	return ret.join(' ');
-}
+function shq(s) {
+	var s2 = String(s).replace(/\t/g, '\\t');
+	var s2 = String(s2).replace(/\n/g, '\\n');
+	var s2 = String(s2).replace(/([#!"$&'()*,:;<=>?@\[\\\]^`{|}])/g, '\\$1');
+	return '"'+s2+'"';
+};
