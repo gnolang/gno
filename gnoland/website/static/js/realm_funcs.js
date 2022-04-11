dce22649db4c61f4c39b42f915e388e51bd14c50
Jae Kwon <jae@tendermint.com>
2022-04-11 11:47:21 -0700

    improve funcs helper

--- gnoland/website/static/js/realm_funcs.js
+++ gnoland/website/static/js/realm_funcs.js
@@ -1,12 +1,38 @@
 function main() {
+	// init
+	var myAddr = getMyAddress()
+	u("#my_address").first().value = myAddr;
+	setMyAddress(myAddr);
+	// main renders
 	u("div.func_spec").each(function(x) {
 		updateCommand(u(x));
 	});
+	// main hooks
 	u("div.func_spec input").on("input", function(e) {
 		var x = u(e.currentTarget).closest("div.func_spec");
 		updateCommand(x);
 	});
+	// special case: when address changes.
+	u("#my_address").on("input", function(e) {
+		var value = u("#my_address").first().value;
+		setMyAddress(value)
+		u("div.func_spec").each(function(node, i){
+			updateCommand(u(node));
+		});
+	});
 };
+
+function setMyAddress(addr) {
+	localStorage.setItem("my_address", addr);
+}
+
+function getMyAddress() {
+	var myAddr = localStorage.getItem("my_address");
+	if (!myAddr){
+		return "ADDRESS";
+	}
+	return myAddr;
+}
 
 // x: the u("div.func_spec") element.
 function updateCommand(x) {
@@ -17,13 +43,37 @@ function updateCommand(x) {
 	ins.each(function(input) {
 		vals.push(input.value);
 	});
+	var myAddr = getMyAddress();
 	var shell = x.find(".shell_command");
 	shell.empty();
 
-	// command 1: construct tx.
-	var args = ["gnokey", "maketx", "call", "KEYNAME", 
+	// command Z: all in one.
+	shell.append(u("<span>").text("### INSECURE BUT QUICK ###")).append(u("<br>"));
+	var args = ["gnokey", "maketx", "call", myAddr,
 		"--pkgpath", shq(realmPath), "--func", shq(funcName),
-		"--gas-fee", "1gnot", "--gas-wanted", "2000000"];
+		"--gas-fee", "1gnot", "--gas-wanted", "2000000",
+		"--send", shq(""),
+		"--broadcast", "true", "--chainid", "testchain"];
+	vals.forEach(function(arg) {
+		args.push("--args");
+		args.push(shq(arg));
+	});
+	var command = args.join(" ");
+	shell.append(u("<span>").text(command)).append(u("<br>")).append(u("<br>"));
+
+	// or...
+	shell.append(u("<span>").text("### FULL SECURITY WITH AIRGAP ###")).append(u("<br>"));
+
+	// command 0: query account info.
+	var args = ["gnokey", "query", "auth/accounts/" + myAddr, "--remote", "gno.land:36657"];
+	var command = args.join(" ");
+	shell.append(u("<span>").text(command)).append(u("<br>"));
+
+	// command 1: construct tx.
+	var args = ["gnokey", "maketx", "call", myAddr,
+		"--pkgpath", shq(realmPath), "--func", shq(funcName),
+		"--gas-fee", "1gnot", "--gas-wanted", "2000000",
+		"--send", shq("")];
 	vals.forEach(function(arg) {
 		args.push("--args");
 		args.push(shq(arg));
@@ -33,7 +83,7 @@ function updateCommand(x) {
 	shell.append(u("<span>").text(command)).append(u("<br>"));
 
 	// command 2: sign tx.
-	var args = ["gnokey", "sign", "KEYNAME",
+	var args = ["gnokey", "sign", myAddr,
 		"--txpath", "unsigned.tx", "--chainid", "testchain",
 		"--number", "ACCOUNTNUMBER",
 		"--sequence", "SEQUENCENUMBER"];
