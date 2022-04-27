e030d7691c9a6a09e292d76223c4e23db3251102
myoung-su,shin <anarcher@gmail.com>
2022-04-28 07:01:07 +0900

    chore: --home default option: $HOME/.gno (#141)

--- pkgs/crypto/keys/client/options.go
+++ pkgs/crypto/keys/client/options.go
@@ -1,5 +1,10 @@
 package client
 
+import (
+	"fmt"
+	"os"
+)
+
 type BaseOptions struct {
 	Home   string `flag:"home" help:"home directory"`
 	Remote string `flag:"remote" help:"remote node URL (default 127.0.0.1:26657)"`
@@ -7,7 +12,15 @@ type BaseOptions struct {
 }
 
 var DefaultBaseOptions = BaseOptions{
-	Home:   "",
+	Home:   homeDir(),
 	Remote: "127.0.0.1:26657",
 	Quiet:  false,
+}
+
+func homeDir() string {
+	hd, err := os.UserHomeDir()
+	if err != nil {
+		panic(err)
+	}
+	return fmt.Sprintf("%s/.gno", hd)
 }
