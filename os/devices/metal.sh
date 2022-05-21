c7d868ddc2f6402d2a6d24ef919426f46e4f07d1
Jacob Gadikian <jacobgadikian@gmail.com>
2022-05-22 02:59:02 +0700

    Update metal.sh

--- os/devices/metal.sh
+++ os/devices/metal.sh
@@ -1,3 +1,7 @@
+#!/bin/bash
+
+# This script handles building on bare metal servers instead of in github actions
+
 wget -O rpi/Manjaro-ARM-aarch64-latest.tar.gz -N --progress=bar:force:noscroll https://osdn.net/projects/manjaro-arm/storage/.rootfs/Manjaro-ARM-aarch64-latest.tar.gz
 cp rpi/Manjaro-ARM-aarch64-latest.tar.gz c2/Manjaro-ARM-aarch64-latest.tar.gz
 cd c2
