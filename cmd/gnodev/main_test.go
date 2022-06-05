6b0289f4ce7810a86c1c9a3ad3d033b5ecdbe354
Manfred Touron <94029+moul@users.noreply.github.com>
2022-06-05 12:08:19 +0000

    test(gnodev): more integration tests
    Signed-off-by: Manfred Touron <94029+moul@users.noreply.github.com>

--- cmd/gnodev/main_test.go
+++ cmd/gnodev/main_test.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bytes"
+	"fmt"
 	"strings"
 	"testing"
 
@@ -15,26 +16,33 @@ func TestMain(t *testing.T) {
 		errShouldContains    string
 		stdoutShouldContains string
 		stderrShouldContains string
+		panicShouldContains  string
 	}{
 		// no args
-		{[]string{""}, "unknown command", "", ""},
-		{[]string{"test"}, "invalid args", "", "Usage: test [test flags] [packages]"},
-		{[]string{"build"}, "invalid args", "", "Usage: build [build flags] [packages]"},
-		{[]string{"precompile"}, "invalid args", "", "Usage: precompile [precompile flags] [packages]"},
+		{[]string{""}, "unknown command", "", "", ""},
+		{[]string{"test"}, "invalid args", "", "Usage: test [test flags] [packages]", ""},
+		{[]string{"build"}, "invalid args", "", "Usage: build [build flags] [packages]", ""},
+		{[]string{"precompile"}, "invalid args", "", "Usage: precompile [precompile flags] [packages]", ""},
 		// {[]string{"repl"}, "", "", ""},
 
 		// --help
-		{[]string{"build", "--help"}, "", "# buildOptions options\n-", ""},
-		{[]string{"test", "--help"}, "", "# testOptions options\n-", ""},
-		{[]string{"precompile", "--help"}, "", "# precompileOptions options\n-", ""},
-		{[]string{"repl", "--help"}, "", "# replOptions options\n-", ""},
+		{[]string{"build", "--help"}, "", "# buildOptions options\n-", "", ""},
+		{[]string{"test", "--help"}, "", "# testOptions options\n-", "", ""},
+		{[]string{"precompile", "--help"}, "", "# precompileOptions options\n-", "", ""},
+		{[]string{"repl", "--help"}, "", "# replOptions options\n-", "", ""},
 
 		// custom
-		{[]string{"test", "../../examples/gno.land/p/rand"}, "", "", "ok"},
+		{[]string{"test", "../../examples/gno.land/p/rand"}, "", "", "ok", ""},
+		{[]string{"test", "../../tests/integ/no-such-dir"}, "no such file or directory", "", "", ""},
+		{[]string{"test", "../../tests/integ/empty-dir"}, "", "", "", ""},
+		{[]string{"test", "../../tests/integ/empty-gno1"}, "", "", "no test files", ""},
+		{[]string{"test", "../../tests/integ/empty-gno2"}, "", "", "", "expected 'package', found 'EOF'"}, // FIXME: better error handling
+		{[]string{"test", "../../tests/integ/empty-gno3"}, "", "", "", "expected 'package', found 'EOF'"}, // FIXME: better error handling
 	}
 
 	for _, test := range tc {
 		name := strings.Join(test.args, " ")
+		name = strings.ReplaceAll(name, "/", "~")
 		t.Run(name, func(t *testing.T) {
 			cmd := command.NewMockCommand()
 			mockOut := bytes.NewBufferString("")
@@ -46,7 +54,16 @@ func TestMain(t *testing.T) {
 
 			require.NotNil(t, cmd)
 
-			err := runMain(cmd, "gnodev", test.args)
+			exec := "gnodev"
+			defer func() {
+				if r := recover(); r != nil {
+					output := fmt.Sprintf("%v", r)
+					require.Contains(t, output, test.panicShouldContains)
+				} else {
+					require.Empty(t, test.panicShouldContains)
+				}
+			}()
+			err := runMain(cmd, exec, test.args)
 
 			if test.errShouldContains == "" {
 				require.Nil(t, err)
