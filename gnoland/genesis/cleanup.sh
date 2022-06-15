8a305d4809c64c6b841a623a11f17cc2ec80befb
Manfred Touron <94029+moul@users.noreply.github.com>
2022-06-15 19:17:09 +0000

    chore: fixup
    Signed-off-by: Manfred Touron <94029+moul@users.noreply.github.com>

--- gnoland/genesis/cleanup.sh
+++ gnoland/genesis/cleanup.sh
@@ -5,7 +5,14 @@ TARGET=genesis_txs.txt
 
 
 > $TARGET
+
 # first, bank transfers, without faucet
 cat $SRC | grep /bank.MsgSend | grep -v g1f4v282mwyhu29afke4vq5r2xzcm6z3ftnugcnv >> $TARGET
+
+# then, r/users's invites
+cat $SRC | grep '"pkg_path":"gno.land/r/users","func":"Invite"' >> $TARGET
+
+# then, r/users's registers
+cat $SRC | grep '"pkg_path":"gno.land/r/users","func":"Register"' >> $TARGET
 
 wc -l $SRC $TARGET
