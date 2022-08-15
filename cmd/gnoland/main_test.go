e681d8cd0635bb7df4e527ac55017a1525b438d8
Manfred Touron <94029+moul@users.noreply.github.com>
2022-08-15 00:01:43 +0000

    chore: test broken genesis/configuration
    Signed-off-by: Manfred Touron <94029+moul@users.noreply.github.com>

--- cmd/gnoland/main_test.go
+++ cmd/gnoland/main_test.go
@@ -3,6 +3,7 @@ package main
 import (
 	"os"
 	"path/filepath"
+	"strings"
 	"testing"
 
 	"github.com/gnolang/gno/pkgs/testutils"
@@ -10,18 +11,31 @@ import (
 )
 
 func TestInitialize(t *testing.T) {
+	cases := []struct {
+		args []string
+	}{
+		// {[]string{"--skip-start", "--skip-failing-gensis-txs"}},
+		{[]string{"--skip-start"}},
+		// FIXME: test seems flappy as soon as we have multiple cases.
+	}
 	os.Chdir(filepath.Join("..", "..")) // go to repo's root dir
 
-	closer := testutils.CaptureStdoutAndStderr()
+	for _, tc := range cases {
+		name := strings.Join(tc.args, " ")
+		t.Run(name, func(t *testing.T) {
+			closer := testutils.CaptureStdoutAndStderr()
 
-	err := runMain([]string{"--skip-failing-genesis-txs", "--skip-start"})
-	stdouterr, bufErr := closer()
-	require.NoError(t, bufErr)
-	require.NoError(t, err)
+			err := runMain([]string{"--skip-failing-genesis-txs", "--skip-start"})
+			stdouterr, bufErr := closer()
+			require.NoError(t, bufErr)
+			require.NoError(t, err)
 
-	_ = stdouterr
-	require.Contains(t, stdouterr, "Node created.", "failed to create node")
-	require.Contains(t, stdouterr, "'--skip-start' is set. Exiting.", "not exited with skip-start")
+			_ = stdouterr
+			require.Contains(t, stdouterr, "Node created.", "failed to create node")
+			require.Contains(t, stdouterr, "'--skip-start' is set. Exiting.", "not exited with skip-start")
+			require.NotContains(t, stdouterr, "panic:")
+		})
+	}
 }
 
 // TODO: test various configuration files?
