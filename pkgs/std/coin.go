91aeb8f9b96c02799a04a767d6fbdb80234110ac
Manfred Touron <94029+moul@users.noreply.github.com>
2022-09-29 21:34:33 -0500

    feat: add r/faucet (#246)
    * feat: r/faucet, community-driven faucet realm
    
    Signed-off-by: Manfred Touron <94029+moul@users.noreply.github.com>
    
    * chore: add std.Coins.{AmountOf,Add} helpers
    
    Signed-off-by: Manfred Touron <94029+moul@users.noreply.github.com>
    
    * chore: new faucet
    
    Signed-off-by: Manfred Touron <94029+moul@users.noreply.github.com>
    
    * chore: implement pause
    
    Signed-off-by: Manfred Touron <94029+moul@users.noreply.github.com>
    
    * chore: fixup
    
    Signed-off-by: Manfred Touron <94029+moul@users.noreply.github.com>
    
    Signed-off-by: Manfred Touron <94029+moul@users.noreply.github.com>

--- pkgs/std/coin.go
+++ pkgs/std/coin.go
@@ -600,7 +600,7 @@ func removeZeroCoins(coins Coins) Coins {
 //-----------------------------------------------------------------------------
 // Sort interface
 
-//nolint
+// nolint
 func (coins Coins) Len() int           { return len(coins) }
 func (coins Coins) Less(i, j int) bool { return coins[i].Denom < coins[j].Denom }
 func (coins Coins) Swap(i, j int)      { coins[i], coins[j] = coins[j], coins[i] }
