fc53df228f85934d0d98e41d3d7679e9d65ecb54
piux2 <90544084+piux2@users.noreply.github.com>
2022-11-29 22:02:07 -0800

    fix: assign contract owner upon contract deployment (#403)
    * contract owner(admin) in init()
    
    * Update pkgs/sdk/vm/keeper_test.go
    
    Co-authored-by: Manfred Touron <94029+moul@users.noreply.github.com>
    
    * Update pkgs/sdk/vm/keeper_test.go
    
    Co-authored-by: Manfred Touron <94029+moul@users.noreply.github.com>
    
    * Update pkgs/sdk/vm/keeper_test.go
    
    Co-authored-by: Manfred Touron <94029+moul@users.noreply.github.com>
    
    * Update pkgs/sdk/vm/keeper_test.go
    
    Co-authored-by: Manfred Touron <94029+moul@users.noreply.github.com>
    
    * Update pkgs/sdk/vm/keeper_test.go
    
    Co-authored-by: Manfred Touron <94029+moul@users.noreply.github.com>
    
    * create a  testing case
    
    Co-authored-by: piux2 <>
    Co-authored-by: Manfred Touron <94029+moul@users.noreply.github.com>

--- pkgs/sdk/vm/keeper_test.go
+++ pkgs/sdk/vm/keeper_test.go
@@ -75,7 +75,10 @@ package test
 
 import "std"
 
+var admin std.Address
+
 func init() {
+     admin = 	std.GetOrigCaller()
 }
 
 func Echo(msg string) string {
@@ -85,7 +88,12 @@ func Echo(msg string) string {
 	banker := std.GetBanker(std.BankerTypeOrigSend)
 	banker.SendCoins(pkgAddr, addr, send) // send back
 	return "echo:"+msg
-}`},
+}
+
+func GetAdmin() string {
+	return admin.String()
+}
+`},
 	}
 	pkgPath := "gno.land/r/test"
 	msg1 := NewMsgAddPackage(addr, pkgPath, files)
@@ -232,4 +240,58 @@ func Echo(msg string) string {
 	// XXX change this into an error and make sure error message is descriptive.
 	_, err = env.vmk.Call(ctx, msg2)
 	assert.Error(t, err)
+}
+
+// Assign admin as OrigCaller on deploying the package.
+func TestVMKeeperOrigCallerInit(t *testing.T) {
+	env := setupTestEnv()
+	ctx := env.ctx
+
+	// Give "addr1" some gnots.
+	addr := crypto.AddressFromPreimage([]byte("addr1"))
+	acc := env.acck.NewAccountWithAddress(ctx, addr)
+	env.acck.SetAccount(ctx, acc)
+	env.bank.SetCoins(ctx, addr, std.MustParseCoins("10000000ugnot"))
+	assert.True(t, env.bank.GetCoins(ctx, addr).IsEqual(std.MustParseCoins("10000000ugnot")))
+
+	// Create test package.
+	files := []*std.MemFile{
+		{"init.gno", `
+package test
+
+import "std"
+
+var admin std.Address
+
+func init() {
+     admin = 	std.GetOrigCaller()
+}
+
+func Echo(msg string) string {
+	addr := std.GetOrigCaller()
+	pkgAddr := std.GetOrigPkgAddr()
+	send := std.GetOrigSend()
+	banker := std.GetBanker(std.BankerTypeOrigSend)
+	banker.SendCoins(pkgAddr, addr, send) // send back
+	return "echo:"+msg
+}
+
+func GetAdmin() string {
+	return admin.String()
+}
+
+`},
+	}
+	pkgPath := "gno.land/r/test"
+	msg1 := NewMsgAddPackage(addr, pkgPath, files)
+	err := env.vmk.AddPackage(ctx, msg1)
+	assert.NoError(t, err)
+
+	// Run GetAdmin()
+	coins := std.MustParseCoins("")
+	msg2 := NewMsgCall(addr, coins, pkgPath, "GetAdmin", []string{})
+	res, err := env.vmk.Call(ctx, msg2)
+	addrString := fmt.Sprintf("(\"%s\" string)", addr.String())
+	assert.NoError(t, err)
+	assert.Equal(t, res, addrString)
 }
