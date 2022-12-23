8f38d597abcf7a8deb72b7ae8fb1eee6f2d22c59
Milos Zivkovic <milos.zivkovic@tendermint.com>
2022-12-23 12:05:55 +0100

    Add unit test for key imports, clean up existing export test

--- pkgs/crypto/keys/client/export_test.go
+++ pkgs/crypto/keys/client/export_test.go
@@ -12,6 +12,97 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+// newTestKeybase generates a new test key-base
+// Returns the temporary key-base, and its path
+func newTestKeybase(t *testing.T) (keys.Keybase, string) {
+	t.Helper()
+
+	// Generate a temporary key-base directory
+	kbHome, kbCleanUp := testutils.NewTestCaseDir(t)
+
+	t.Cleanup(func() {
+		kbCleanUp()
+	})
+
+	kb, err := keys.NewKeyBaseFromDir(kbHome)
+	if err != nil {
+		t.Fatalf(
+			"unable to create a key base in directory %s, %v",
+			kbHome,
+			err,
+		)
+	}
+
+	return kb, kbHome
+}
+
+// addRandomKeyToKeybase adds a random key to the key-base
+func addRandomKeyToKeybase(
+	kb keys.Keybase,
+	keyName,
+	encryptPassword string,
+) (keys.Info, error) {
+	// Generate a random mnemonic
+	mnemonic, err := generateMnemonic(mnemonicEntropySize)
+	if err != nil {
+		return nil, fmt.Errorf(
+			"unable to generate a mnemonic phrase, %v",
+			err,
+		)
+	}
+
+	// Add the key to the key base
+	return kb.CreateAccount(
+		keyName,
+		mnemonic,
+		"",
+		encryptPassword,
+		0,
+		0,
+	)
+}
+
+type testCmdKeyOptsBase struct {
+	kbHome          string
+	keyName         string
+	decryptPassword string
+	encryptPassword string
+}
+
+type testExportKeyOpts struct {
+	testCmdKeyOptsBase
+
+	outputPath string
+}
+
+// exportKey runs the private key export command
+// using the provided options
+func exportKey(exportOpts testExportKeyOpts) error {
+	var (
+		cmd  = command.NewMockCommand()
+		opts = ExportOptions{
+			BaseOptions: BaseOptions{
+				Home: exportOpts.kbHome,
+			},
+			NameOrBech32: exportOpts.keyName,
+			OutputPath:   exportOpts.outputPath,
+		}
+	)
+
+	cmd.SetIn(
+		strings.NewReader(
+			fmt.Sprintf(
+				"%s\n%s\n%s\n",
+				exportOpts.decryptPassword,
+				exportOpts.encryptPassword,
+				exportOpts.encryptPassword,
+			),
+		),
+	)
+
+	return exportApp(cmd, nil, opts)
+}
+
 // TestExport_ExportKey makes sure the key can be exported correctly
 func TestExport_ExportKey(t *testing.T) {
 	t.Parallel()
@@ -28,43 +119,15 @@ func TestExport_ExportKey(t *testing.T) {
 	}
 
 	var (
-		keyName         = "key name"
-		encryptPassword = "encrypt password"
-
-		cmd = command.NewMockCommand()
+		keyName  = "key name"
+		password = "password"
 	)
 
 	// Generate a temporary key-base directory
-	kbHome, kbCleanUp := testutils.NewTestCaseDir(t)
-	defer kbCleanUp()
-
-	kb, err := keys.NewKeyBaseFromDir(kbHome)
-	if err != nil {
-		t.Fatalf(
-			"unable to create a key base in directory %s, %v",
-			kbHome,
-			err,
-		)
-	}
-
-	// Generate a random mnemonic
-	mnemonic, err := generateMnemonic(mnemonicEntropySize)
-	if err != nil {
-		t.Fatalf(
-			"unable to generate a mnemonic phrase, %v",
-			err,
-		)
-	}
+	kb, kbHome := newTestKeybase(t)
 
 	// Add an initial key to the key base
-	info, err := kb.CreateAccount(
-		keyName,
-		mnemonic,
-		"",
-		encryptPassword,
-		0,
-		0,
-	)
+	info, err := addRandomKeyToKeybase(kb, keyName, password)
 	if err != nil {
 		t.Fatalf(
 			"unable to create a key base account, %v",
@@ -75,30 +138,22 @@ func TestExport_ExportKey(t *testing.T) {
 	outputFile, outputCleanupFn := testutils.NewTestFile(t)
 	defer outputCleanupFn()
 
-	opts := ExportOptions{
-		BaseOptions: BaseOptions{
-			Home: kbHome,
-		},
-		NameOrBech32: info.GetName(),
-		OutputPath:   outputFile.Name(),
+	baseOpts := testCmdKeyOptsBase{
+		kbHome:          kbHome,
+		keyName:         info.GetName(),
+		decryptPassword: password,
+		encryptPassword: password,
 	}
-
-	// Prepend standard input
-	cmd.SetIn(
-		strings.NewReader(
-			fmt.Sprintf(
-				"%s\n%s\n%s\n",
-				encryptPassword,
-				encryptPassword,
-				encryptPassword,
-			),
-		),
-	)
 
 	// Make sure the command executes correctly
 	assert.NoError(
 		t,
-		exportApp(cmd, nil, opts),
+		exportKey(
+			testExportKeyOpts{
+				testCmdKeyOptsBase: baseOpts,
+				outputPath:         outputFile.Name(),
+			},
+		),
 	)
 
 	// Make sure the encrypted armor has been written to disk
