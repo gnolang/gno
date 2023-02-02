bd13cf76348a411d637dd1d25ba5ad972d520c04
Yassin <yassinouider@gmail.com>
2023-02-02 08:55:47 +0100

    chore: add support for duration format for timeout flag (#488)

---
+++ pkgs/command/flags_test.go
@@ -0,0 +1,23 @@
+package command
+
+import (
+	"reflect"
+	"testing"
+	"time"
+)
+
+func TestApplyFlagToFieldReflectString(t *testing.T) {
+	t.Parallel()
+
+	// Test 1: time.Duration
+	duration := time.Duration(0)
+	field := reflect.ValueOf(&duration)
+	flagValue := "10s"
+	err := applyFlagToFieldReflectString(field, flagValue)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if duration.String() != flagValue {
+		t.Errorf("Expected %s, got %s", flagValue, duration)
+	}
+}
