d29a354485364f6f0c75805f99180d907d9174db
Milos Zivkovic <milos.zivkovic@tendermint.com>
2023-02-17 16:55:39 +0100

    Add ffcli support for gnokey maketx addpkg

--- cmd/gnokey/main.go
+++ cmd/gnokey/main.go
@@ -14,7 +14,6 @@ import (
 	"github.com/gnolang/gno/pkgs/crypto"
 	"github.com/gnolang/gno/pkgs/crypto/keys"
 	"github.com/gnolang/gno/pkgs/errors"
-	gno "github.com/gnolang/gno/pkgs/gnolang"
 	"github.com/gnolang/gno/pkgs/sdk/bank"
 	"github.com/gnolang/gno/pkgs/sdk/vm"
 	"github.com/gnolang/gno/pkgs/std"
@@ -41,7 +40,6 @@ func main() {
 	)
 
 	cmd.AddSubCommands(
-		// TODO add
 		newAddCmd(cfg),
 		newDeleteCmd(cfg),
 		newGenerateCmd(cfg),
@@ -52,6 +50,7 @@ func main() {
 		newVerifyCmd(cfg),
 		newQueryCmd(cfg),
 		newBroadcastCmd(cfg),
+		newMakeTxCmd(cfg),
 	)
 
 	if err := cmd.ParseAndRun(context.Background(), os.Args[1:]); err != nil {
@@ -117,96 +116,6 @@ type SignBroadcastOptions struct {
 
 	Broadcast bool
 	ChainID   string
-}
-
-// ----------------------------------------
-// makeAddPackageTx
-
-type makeAddPackageTxOptions struct {
-	common.BaseOptions          // home,...
-	SignBroadcastOptions        // gas-wanted, gas-fee, memo, ...
-	PkgPath              string `flag:"pkgpath" help:"package path (required)"`
-	PkgDir               string `flag:"pkgdir" help:"path to package files (required)"`
-	Deposit              string `flag:"deposit" help:"deposit coins"`
-}
-
-var defaultMakeAddPackageTxOptions = makeAddPackageTxOptions{
-	BaseOptions: common.DefaultBaseOptions,
-	// SignBroadcastOptions: defaultSignBroadcastOptions,
-	PkgPath: "", // must override
-	PkgDir:  "", // must override
-	Deposit: "",
-}
-
-func makeAddPackageTxApp(cmd *command.Command, args []string, iopts interface{}) error {
-	opts := iopts.(makeAddPackageTxOptions)
-	if opts.PkgPath == "" {
-		return errors.New("pkgpath not specified")
-	}
-	if opts.PkgDir == "" {
-		return errors.New("pkgdir not specified")
-	}
-	if len(args) != 1 {
-		cmd.ErrPrintfln("Usage: addpkg <keyname or address>")
-		return errors.New("invalid args")
-	}
-
-	// read account pubkey.
-	nameOrBech32 := args[0]
-	kb, err := keys.NewKeyBaseFromDir(opts.Home)
-	if err != nil {
-		return err
-	}
-	info, err := kb.GetByNameOrAddress(nameOrBech32)
-	if err != nil {
-		return err
-	}
-	creator := info.GetAddress()
-	// info.GetPubKey()
-
-	// parse deposit.
-	deposit, err := std.ParseCoins(opts.Deposit)
-	if err != nil {
-		panic(err)
-	}
-
-	// open files in directory as MemPackage.
-	memPkg := gno.ReadMemPackage(opts.PkgDir, opts.PkgPath)
-
-	// precompile and validate syntax
-	err = gno.PrecompileAndCheckMempkg(memPkg)
-	if err != nil {
-		panic(err)
-	}
-
-	// parse gas wanted & fee.
-	gaswanted := opts.GasWanted
-	gasfee, err := std.ParseCoin(opts.GasFee)
-	if err != nil {
-		panic(err)
-	}
-	// construct msg & tx and marshal.
-	msg := vm.MsgAddPackage{
-		Creator: creator,
-		Package: memPkg,
-		Deposit: deposit,
-	}
-	tx := std.Tx{
-		Msgs:       []std.Msg{msg},
-		Fee:        std.NewFee(gaswanted, gasfee),
-		Signatures: nil,
-		Memo:       opts.Memo,
-	}
-
-	if opts.Broadcast {
-		// err := signAndBroadcast(cmd, args, tx, opts.BaseOptions, opts.SignBroadcastOptions)
-		// if err != nil {
-		// 	return err
-		// }
-	} else {
-		fmt.Println(string(amino.MustMarshalJSON(tx)))
-	}
-	return nil
 }
 
 // ----------------------------------------
@@ -303,81 +212,6 @@ func makeCallTxApp(cmd *command.Command, args []string, iopts interface{}) error
 	}
 	return nil
 }
-
-// func signAndBroadcast(cmd *command.Command, args []string, tx std.Tx, baseopts client.BaseOptions, txopts SignBroadcastOptions) error {
-// 	// query account
-// 	nameOrBech32 := args[0]
-// 	kb, err := keys.NewKeyBaseFromDir(baseopts.Home)
-// 	if err != nil {
-// 		return err
-// 	}
-// 	info, err := kb.GetByNameOrAddress(nameOrBech32)
-// 	if err != nil {
-// 		return err
-// 	}
-// 	accountAddr := info.GetAddress()
-//
-// 	qopts := client.QueryOptions{
-// 		Path: fmt.Sprintf("auth/accounts/%s", accountAddr),
-// 	}
-// 	qopts.Remote = baseopts.Remote
-// 	qres, err := client.QueryHandler(qopts)
-// 	if err != nil {
-// 		return errors.Wrap(err, "query account")
-// 	}
-// 	var qret struct{ BaseAccount std.BaseAccount }
-// 	err = amino.UnmarshalJSON(qres.Response.Data, &qret)
-// 	if err != nil {
-// 		return err
-// 	}
-//
-// 	// sign tx
-// 	accountNumber := qret.BaseAccount.AccountNumber
-// 	sequence := qret.BaseAccount.Sequence
-// 	sopts := client.SignOptions{
-// 		Sequence:      &sequence,
-// 		AccountNumber: &accountNumber,
-// 		ChainID:       txopts.ChainID,
-// 		NameOrBech32:  nameOrBech32,
-// 		TxJSON:        amino.MustMarshalJSON(tx),
-// 	}
-// 	sopts.Home = baseopts.Home
-// 	if baseopts.Quiet {
-// 		sopts.Pass, err = cmd.GetPassword("", baseopts.InsecurePasswordStdin)
-// 	} else {
-// 		sopts.Pass, err = cmd.GetPassword("Enter password.", baseopts.InsecurePasswordStdin)
-// 	}
-// 	if err != nil {
-// 		return err
-// 	}
-//
-// 	signedTx, err := client.SignHandler(sopts)
-// 	if err != nil {
-// 		return errors.Wrap(err, "sign tx")
-// 	}
-//
-// 	// broadcast signed tx
-// 	bopts := client.BroadcastOptions{
-// 		Tx: signedTx,
-// 	}
-// 	bopts.Remote = baseopts.Remote
-// 	bres, err := client.BroadcastHandler(bopts)
-// 	if err != nil {
-// 		return errors.Wrap(err, "broadcast tx")
-// 	}
-// 	if bres.CheckTx.IsErr() {
-// 		return errors.Wrap(bres.CheckTx.Error, "check transaction failed: log:%s", bres.CheckTx.Log)
-// 	}
-// 	if bres.DeliverTx.IsErr() {
-// 		return errors.Wrap(bres.DeliverTx.Error, "deliver transaction failed: log:%s", bres.DeliverTx.Log)
-// 	}
-// 	cmd.Println(string(bres.DeliverTx.Data))
-// 	cmd.Println("OK!")
-// 	cmd.Println("GAS WANTED:", bres.DeliverTx.GasWanted)
-// 	cmd.Println("GAS USED:  ", bres.DeliverTx.GasUsed)
-//
-// 	return nil
-// }
 
 // ----------------------------------------
 // makeSendTxApp
