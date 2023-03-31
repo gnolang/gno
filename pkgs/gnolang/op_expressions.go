dce95beb1e23cd53963d0a333b60b1e0a045abb0
ltzMaxwell <ltz.maxwell@gmail.com>
2023-03-31 12:10:22 +0800

    remove log

--- pkgs/gnolang/op_expressions.go
+++ pkgs/gnolang/op_expressions.go
@@ -571,16 +571,12 @@ func (m *Machine) doOpMapLit() {
 }
 
 func (m *Machine) doOpStructLit() {
-	// println("doOpStructList")
-	// assess performance TODO
 	x := m.PopExpr().(*CompositeLitExpr)
 	el := len(x.Elts) // may be incomplete
 	// peek struct type.
 	xt := m.PeekValue(1 + el).V.(TypeValue).Type
 	st := baseOf(xt).(*StructType)
 	nf := len(st.Fields)
-	// println("nf: ", nf)
-	// println("el: ", el)
 	fs := []TypedValue(nil)
 	// NOTE includes embedded fields.
 	if el == 0 {
@@ -625,7 +621,6 @@ func (m *Machine) doOpStructLit() {
 			}
 		}
 	} else {
-		// println("------------else branch of st lit---------------")
 		// field values are by name and may be out of order.
 		fs = defaultStructFields(m.Alloc, st)
 		ftvs := m.PopValues(el)
@@ -635,28 +630,10 @@ func (m *Machine) doOpStructLit() {
 			ftv := ftvs[i]
 			// convert
 			if dt, ok := fs[fnx.Path.Index].T.(*DeclaredType); ok {
-				// println("is dt, index, fs[index].T:", fnx.Path.Index, fs[fnx.Path.Index].T.String())
 				if checkSameTypes(dt.Base, ftvs[i].T) {
-					// println("same type")
-					// println("dt.Base", dt.Base.String())
-					// println("ftvs[i].T", ftvs[i].T.String())
-					// println("fs[fnx.Path.Index].T", fs[fnx.Path.Index].T.String())
-					// println("st.Fields[i].Type", st.Fields[i].Type.String())
 					ftv.T = fs[fnx.Path.Index].T // use defined type
 				}
-				// else {
-				// 	println("not same type")
-				// 	println("dt.Base", dt.Base.String())
-				// 	println("ftvs[fnx.Path.Index].T", ftvs[fnx.Path.Index].T.String())
-				// }
-				// println("convert done")
-				// println("ftv: ", ftv.String())
-			} else {
-				// println("----------------------")
-				// println("not dt, index, fs[index].T:", fnx.Path.Index, fs[fnx.Path.Index].String())
-				// println("ftv: ", ftv.String())
 			}
-
 			if debug {
 				if fnx.Path.Depth != 0 {
 					panic("unexpected struct composite lit key path generation value")
@@ -665,12 +642,9 @@ func (m *Machine) doOpStructLit() {
 					panic("should not happen")
 				}
 			}
-			// println("index: ", fnx.Path.Index)
 			fs[fnx.Path.Index] = ftv
 		}
 	}
-	// println("all done")
-	// println("----------------------")
 	// construct and push value.
 	m.PopValue() // baseOf() is st
 	sv := m.Alloc.NewStruct(fs)
