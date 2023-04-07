522d2dfc53b1de7c9a4393b8017709d443b6420a
peter <petar.dambovaliev@tendermint.com>
2023-04-07 13:41:54 +0200

    save

--- gnovm/pkg/gnolang/gc_test.go
+++ gnovm/pkg/gnolang/gc_test.go
@@ -7,21 +7,9 @@ import (
 )
 
 func TestGC_NotCollectUsedObjects(t *testing.T) {
-	obj1 := &GCObj{data: defaultStructValue(nil, &StructType{
-		PkgPath: "",
-		Fields:  nil,
-		typeid:  "",
-	})}
-	obj2 := &GCObj{data: defaultStructValue(nil, &StructType{
-		PkgPath: "",
-		Fields:  nil,
-		typeid:  "",
-	})}
-	obj3 := &GCObj{data: defaultStructValue(nil, &StructType{
-		PkgPath: "",
-		Fields:  nil,
-		typeid:  "",
-	})}
+	obj1 := &GCObj{id: ObjectIDFromPkgPath("obj1")}
+	obj2 := &GCObj{id: ObjectIDFromPkgPath("obj2")}
+	obj3 := &GCObj{id: ObjectIDFromPkgPath("obj3")}
 
 	// Link objects together
 	obj1.AddRef(obj2)
@@ -39,27 +27,15 @@ func TestGC_NotCollectUsedObjects(t *testing.T) {
 
 	// Collect garbage
 	gc.Collect()
-	assert.NotNil(t, gc.getObj(obj1.data.GetObjectID()))
-	assert.NotNil(t, gc.getObj(obj1.data.GetObjectID()))
-	assert.NotNil(t, gc.getObj(obj1.data.GetObjectID()))
+	assert.NotNil(t, gc.getObj(obj1.id))
+	assert.NotNil(t, gc.getObj(obj2.id))
+	assert.NotNil(t, gc.getObj(obj3.id))
 }
 
 func TestGC_CollectUnsedObjects(t *testing.T) {
-	obj1 := &GCObj{data: defaultStructValue(nil, &StructType{
-		PkgPath: "",
-		Fields:  nil,
-		typeid:  "",
-	})}
-	obj2 := &GCObj{data: defaultStructValue(nil, &StructType{
-		PkgPath: "",
-		Fields:  nil,
-		typeid:  "",
-	})}
-	obj3 := &GCObj{data: defaultStructValue(nil, &StructType{
-		PkgPath: "",
-		Fields:  nil,
-		typeid:  "",
-	})}
+	obj1 := &GCObj{id: ObjectIDFromPkgPath("obj1")}
+	obj2 := &GCObj{id: ObjectIDFromPkgPath("obj2")}
+	obj3 := &GCObj{id: ObjectIDFromPkgPath("obj3")}
 
 	// Link objects together
 	obj1.AddRef(obj2)
@@ -75,9 +51,9 @@ func TestGC_CollectUnsedObjects(t *testing.T) {
 
 	// Collect garbage
 	gc.Collect()
-	assert.Nil(t, gc.getObj(obj1.data.GetObjectID()))
-	assert.Nil(t, gc.getObj(obj1.data.GetObjectID()))
-	assert.Nil(t, gc.getObj(obj1.data.GetObjectID()))
+	assert.Nil(t, gc.getObj(obj1.id))
+	assert.Nil(t, gc.getObj(obj2.id))
+	assert.Nil(t, gc.getObj(obj3.id))
 	assert.Empty(t, gc.objs)
 	assert.Empty(t, gc.roots)
 }
