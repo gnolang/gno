5f356f5056090299dc98c5d2cef5521a9a56d170
Morgan Bazalgette <morgan@morganbaz.com>
2023-04-20 18:10:32 +0200

    handle file exist case

--- tm2/pkg/crypto/keys/client/common.go
+++ tm2/pkg/crypto/keys/client/common.go
@@ -49,7 +49,12 @@ func fixOldDefaultGnoHome(newDir string) {
 	if err != nil || !s.IsDir() {
 		return
 	}
-	if err = os.Rename(oldDir, newDir); err != nil && !os.IsExist(err) {
-		log.Printf("WARNING: attempted moving old default GNO_HOME (%q) to new (%q) but failed with error: %v", oldDir, newDir, err)
+	if err = os.Rename(oldDir, newDir); err != nil {
+		if os.IsExist(err) {
+			log.Printf("WARNING: attempted moving old default GNO_HOME (%q) to new (%q) but failed because directory exists.", oldDir, newDir)
+			log.Printf("You may need to move files from the old directory manually, or set the env var GNO_HOME to %q to retain the old directory.", oldDir)
+		} else {
+			log.Printf("WARNING: attempted moving old default GNO_HOME (%q) to new (%q) but failed with error: %v", oldDir, newDir, err)
+		}
 	}
 }
