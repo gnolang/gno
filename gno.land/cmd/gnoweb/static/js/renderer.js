fa685f22c4c1d5bfe95435b33083f4f25b08e4d2
alexiscolin <alexis@jaunebleu.co>
2023-04-26 10:29:43 +0200

    feat: add first ui component

--- gno.land/cmd/gnoweb/static/js/renderer.js
+++ gno.land/cmd/gnoweb/static/js/renderer.js
@@ -6,3 +6,42 @@
 function renderUsernames(raw) {
   return raw.replace(/( |\n)@([_a-z0-9]{5,16})/, "$1[@$2](/r/demo/users:$2)");
 }
+
+const components = [
+    {name: 'jumbotron', toRender: content => `<div class="comp-jumbotron">${content}</div>`},
+]
+
+const extensionBuilder = (comp) => {
+    const {name, toRender} = comp
+    const startReg = RegExp(`:::${name}`);
+    const tokenizerReg = RegExp(`^:::${name}\n([\\s\\S]*?)\n:::`);
+    return {
+        name: name,
+        level: 'block',
+        start(src) { return src.match(startReg)?.index; },
+        tokenizer(src, tokens) {
+          const match = tokenizerReg.exec(src);
+          if (match) {
+            const token = {
+              type: name,
+              raw: match[0],
+              text: match[1].trim(),
+              tokens: []
+            };
+            this.lexer.blockTokens(token.text, token.tokens);
+            return token;
+          }
+        },
+        renderer(token) {
+          return toRender(this.parser.parse(token.tokens));
+        }
+    };
+}
+
+function parseContent(source) {
+    marked.setOptions({ gfm: true });
+    components.forEach(comp => marked.use({ extensions: [extensionBuilder(comp)] }))
+    const doc = new DOMParser().parseFromString(source, "text/html");
+    const contents = doc.documentElement.textContent;
+    return marked.parse(contents);
+}
