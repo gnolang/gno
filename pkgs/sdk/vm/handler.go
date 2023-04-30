4faed149b256098c0dca92d32b7415a0cfbd73e0
nir1218 <eamy182214@gmail.com>
2023-04-30 19:55:23 +0300

    Revert "first draft"
    This reverts commit e5739d0de6bb361d1f7296c45a5265557593f497.
    
    removing handler changes
    i
    removing handler changes

--- pkgs/sdk/vm/handler.go
+++ pkgs/sdk/vm/handler.go
@@ -2,17 +2,12 @@ package vm
 
 import (
 	"fmt"
-	"strconv"
 	"strings"
 
 	abci "github.com/gnolang/gno/pkgs/bft/abci/types"
 	"github.com/gnolang/gno/pkgs/sdk"
 	"github.com/gnolang/gno/pkgs/sdk/auth"
 	"github.com/gnolang/gno/pkgs/std"
-)
-
-const (
-	RatePerByte int = 1
 )
 
 type vmHandler struct {
@@ -40,8 +35,7 @@ func (vh vmHandler) Process(ctx sdk.Context, msg std.Msg) sdk.Result {
 
 // Handle MsgAddPackage.
 func (vh vmHandler) handleMsgAddPackage(ctx sdk.Context, msg MsgAddPackage) sdk.Result {
-	totalCost := len(msg.GetSignBytes()) * RatePerByte
-	amount, err := std.ParseCoins(strconv.Itoa(totalCost) + "ugnot") // XXX calculate
+	amount, err := std.ParseCoins("1000000ugnot") // XXX calculate
 	if err != nil {
 		return abciResult(err)
 	}
@@ -58,8 +52,7 @@ func (vh vmHandler) handleMsgAddPackage(ctx sdk.Context, msg MsgAddPackage) sdk.
 
 // Handle MsgCall.
 func (vh vmHandler) handleMsgCall(ctx sdk.Context, msg MsgCall) (res sdk.Result) {
-	totalCost := len(msg.GetSignBytes()) * RatePerByte
-	amount, err := std.ParseCoins(strconv.Itoa(totalCost) + "ugnot") // XXX calculate
+	amount, err := std.ParseCoins("1000000ugnot") // XXX calculate
 	if err != nil {
 		return abciResult(err)
 	}
