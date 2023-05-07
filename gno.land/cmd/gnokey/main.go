96762c221a9308c75a9323cf997baa55be5ca033
Manfred Touron <94029+moul@users.noreply.github.com>
2023-05-07 23:02:32 +0200

    chore(gnokey): return print verbose errors in CLI
    Signed-off-by: Manfred Touron <94029+moul@users.noreply.github.com>

--- gno.land/cmd/gnokey/main.go
+++ gno.land/cmd/gnokey/main.go
@@ -12,7 +12,7 @@ func main() {
 	cmd := client.NewRootCmd()
 
 	if err := cmd.ParseAndRun(context.Background(), os.Args[1:]); err != nil {
-		_, _ = fmt.Fprintf(os.Stderr, "%+v", err)
+		_, _ = fmt.Fprintf(os.Stderr, "%#v", err)
 
 		os.Exit(1)
 	}
