9282dbab4fa6b45179302e41f8ab7ab64a4a3dbd
Hariom Verma <hariom.verma@tendermint.com>
2023-05-22 17:24:49 +0530

    Test `ListPkgs()`

--- gnovm/pkg/gnomod/pkg_test.go
+++ gnovm/pkg/gnomod/pkg_test.go
@@ -1,11 +1,188 @@
 package gnomod
 
 import (
+	"fmt"
+	"os"
+	"path/filepath"
 	"testing"
 
+	"github.com/gnolang/gno/tm2/pkg/testutils"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
+
+func TestListPkgs(t *testing.T) {
+	for _, tc := range []struct {
+		desc     string
+		pkg      []struct{ name, modfile string }
+		expected []string
+	}{
+		{
+			desc: "no packages",
+		},
+		{
+			desc: "no package depends on another package",
+			pkg: []struct{ name, modfile string }{
+				{
+					"foo",
+					`module foo`,
+				},
+				{
+					"bar",
+					`module bar`,
+				},
+				{
+					"baz",
+					`module baz`,
+				},
+			},
+			expected: []string{"foo", "bar", "baz"},
+		},
+		{
+			desc: "no package depends on draft package",
+			pkg: []struct{ name, modfile string }{
+				{
+					"foo",
+					`module foo`,
+				},
+				{
+					"bar",
+					`module bar
+
+					require foo v0.0.0`,
+				},
+				{
+					"baz",
+					`module baz`,
+				},
+				{
+					"qux",
+					`// Draft
+
+					module qux`,
+				},
+			},
+			expected: []string{"foo", "bar", "baz"},
+		},
+		{
+			desc: "package directly depends on draft package",
+			pkg: []struct{ name, modfile string }{
+				{
+					"foo",
+					`// Draft
+
+					module foo`,
+				},
+				{
+					"bar",
+					`module bar
+					require foo v0.0.0`,
+				},
+				{
+					"baz",
+					`module baz`,
+				},
+			},
+			expected: []string{"baz"},
+		},
+		{
+			desc: "package indirectly depends on draft package",
+			pkg: []struct{ name, modfile string }{
+				{
+					"foo",
+					`// Draft
+
+					module foo`,
+				},
+				{
+					"bar",
+					`module bar
+
+					require foo v0.0.0`,
+				},
+				{
+					"baz",
+					`module baz
+
+					require bar v0.0.0`,
+				},
+				{
+					"qux",
+					`module qux`,
+				},
+			},
+			expected: []string{"qux"},
+		},
+		{
+			desc: "package indirectly depends on draft package (multiple levels)",
+			pkg: []struct{ name, modfile string }{
+				{
+					"foo",
+					`// Draft
+
+					module foo`,
+				},
+				{
+					"bar",
+					`module bar
+
+					require foo v0.0.0`,
+				},
+				{
+					"baz",
+					`module baz
+
+					require bar v0.0.0`,
+				},
+				{
+					"qux",
+					`module qux
+					
+					require baz v0.0.0`,
+				},
+			},
+			expected: []string{},
+		},
+	} {
+		t.Run(tc.desc, func(t *testing.T) {
+			// Create test dir
+			dirPath, cleanUpFn := testutils.NewTestCaseDir(t)
+			require.NotNil(t, dirPath)
+			defer cleanUpFn()
+
+			// Create packages
+			for _, p := range tc.pkg {
+				createGnoModPkg(t, dirPath, p.name, p.modfile)
+			}
+
+			// List packages
+			pkgs, err := ListPkgs(dirPath)
+			require.NoError(t, err)
+
+			for _, p := range pkgs {
+				fmt.Println(p.name)
+			}
+
+			// Check output
+			assert.Equal(t, len(tc.expected), len(pkgs))
+			for _, p := range pkgs {
+				assert.Contains(t, tc.expected, p.name)
+			}
+		})
+	}
+}
+
+func createGnoModPkg(t *testing.T, dirPath, pkgName, modData string) {
+	t.Helper()
+
+	// Create package dir
+	pkgDirPath := filepath.Join(dirPath, pkgName)
+	err := os.MkdirAll(pkgDirPath, 0o755)
+	require.NoError(t, err)
+
+	// Create gno.mod
+	err = os.WriteFile(filepath.Join(pkgDirPath, "gno.mod"), []byte(modData), 0o644)
+}
 
 func TestSortPkgs(t *testing.T) {
 	for _, tc := range []struct {
