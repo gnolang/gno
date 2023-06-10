eb567b6337e32c87d903313eb93f4fa2347a20f9
Manfred Touron <94029+moul@users.noreply.github.com>
2023-06-10 12:03:30 +0200

    feat(repl): add imports, funcs, commands, etc
    Signed-off-by: Manfred Touron <94029+moul@users.noreply.github.com>

--- gnovm/cmd/gno/repl.go
+++ gnovm/cmd/gno/repl.go
@@ -7,6 +7,7 @@ import (
 	"fmt"
 	"io"
 	"os"
+	"strings"
 
 	gno "github.com/gnolang/gno/gnovm/pkg/gnolang"
 	"github.com/gnolang/gno/gnovm/tests"
@@ -15,8 +16,11 @@ import (
 )
 
 type replCfg struct {
-	verbose bool
-	rootDir string
+	verbose        bool
+	rootDir        string
+	initialImports string
+	initialCommand string
+	skipUsage      bool
 }
 
 func newReplCmd() *commands.Command {
@@ -49,6 +53,27 @@ func (c *replCfg) RegisterFlags(fs *flag.FlagSet) {
 		"",
 		"clone location of github.com/gnolang/gno (gnodev tries to guess it)",
 	)
+
+	fs.StringVar(
+		&c.initialImports,
+		"imports",
+		"gno.land/p/demo/avl,gno.land/p/demo/ufmt",
+		"initial imports, separated by a comma",
+	)
+
+	fs.StringVar(
+		&c.initialCommand,
+		"command",
+		"",
+		"initial command to run",
+	)
+
+	fs.BoolVar(
+		&c.skipUsage,
+		"skip-usage",
+		false,
+		"do not print usage",
+	)
 }
 
 func execRepl(cfg *replCfg, args []string) error {
@@ -60,17 +85,33 @@ func execRepl(cfg *replCfg, args []string) error {
 		cfg.rootDir = guessRootDir()
 	}
 
-	return runRepl(cfg.rootDir, cfg.verbose)
+	if !cfg.skipUsage {
+		fmt.Fprint(os.Stderr, `// Usage:
+//   gno:1> /import "gno.land/p/demo/avl"     // import the p/demo/avl package
+//   gno:2> /func a() string { return "a" }   // declare a new function named a
+//   gno:3> /src                              // print current generated source
+//   gno:4> println(a())                      // print the result of calling a()
+//   gno:5> /exit
+`)
+	}
+
+	return runRepl(cfg)
 }
 
-func runRepl(rootDir string, verbose bool) error {
+type state struct {
+	imports []string
+	funcs   []string
+	// TODO: vars
+}
+
+func runRepl(cfg *replCfg) error {
 	stdin := os.Stdin
 	stdout := os.Stdout
 	stderr := os.Stderr
 
 	// init store and machine
-	testStore := tests.TestStore(rootDir, "", stdin, stdout, stderr, tests.ImportModeStdlibsOnly)
-	if verbose {
+	testStore := tests.TestStore(cfg.rootDir, "", stdin, stdout, stderr, tests.ImportModeStdlibsOnly)
+	if cfg.verbose {
 		testStore.SetLogStoreOps(true)
 	}
 	m := gno.NewMachineWithOptions(gno.MachineOptions{
@@ -88,8 +129,13 @@ func runRepl(rootDir string, verbose bool) error {
 	}{os.Stdin, os.Stderr}
 	t := term.NewTerminal(rw, "")
 
+	state := state{
+		imports: make([]string, 0),
+		funcs:   make([]string, 0),
+	}
+
 	// main loop
-	for i := 1; ; i++ {
+	for i := 1; ; /* continue until break */ i++ {
 		// parse line and execute
 		t.SetPrompt(fmt.Sprintf("gno:%d> ", i))
 		oldState, err := term.MakeRaw(0)
@@ -103,15 +149,39 @@ func runRepl(rootDir string, verbose bool) error {
 		}
 		term.Restore(0, oldState)
 
+		if strings.TrimSpace(input) == "" {
+			i--
+			continue
+		}
+
 		funcName := fmt.Sprintf("repl_%d", i)
-		src := "package test\nfunc " + funcName + "() {\n" + input + "\n}"
 		// FIXME: support ";" as line separator?
 		// FIXME: gofmt as linter + formatter
 		// FIXME: support multiline when unclosed parenthesis, etc
 
+		imports := strings.Join(state.imports, "\n")
+		funcs := strings.Join(state.funcs, "\n")
+		src := "package test\n" + imports + "\n" + funcs + "\nfunc " + funcName + "() {\nINPUT\n}"
+
+		fields := strings.Fields(input)
+		switch {
+		case fields[0] == "/import":
+			state.imports = append(state.imports, input[1:])
+			continue
+		case fields[0] == "/func":
+			state.funcs = append(state.funcs, input[1:])
+			continue
+		case fields[0] == "/src":
+			println(src)
+		case fields[0] == "/exit":
+			break
+		}
+
+		src = strings.Replace(src, "INPUT", input, 0)
 		n := gno.MustParseFile(funcName+".gno", src)
 		m.RunFiles(n)
 		m.RunStatement(gno.S(gno.Call(gno.X(funcName))))
+
 	}
 
 	return nil
