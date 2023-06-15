6fa6c91c1358872fc948c5177d13ab5e019b8b08
Thomas Bruyelle <thomas.bruyelle@tendermint.com>
2023-06-15 15:59:54 +0200

    test: extract PrevRealm algorithm in tested function

--- gnovm/stdlibs/frame.go
+++ gnovm/stdlibs/frame.go
@@ -1,6 +1,9 @@
 package stdlibs
 
-import "github.com/gnolang/gno/tm2/pkg/crypto"
+import (
+	gno "github.com/gnolang/gno/gnovm/pkg/gnolang"
+	"github.com/gnolang/gno/tm2/pkg/crypto"
+)
 
 type Realm struct {
 	addr    crypto.Bech32Address
@@ -17,4 +20,40 @@ func (r Realm) PkgPath() string {
 
 func (r Realm) IsUser() bool {
 	return r.pkgPath == ""
+}
+
+func prevRealm(m *gno.Machine) (crypto.Bech32Address, string) {
+	var (
+		ctx = m.Context.(ExecContext)
+		// Default lastCaller is OrigCaller, the signer of the tx
+		lastCaller  = ctx.OrigCaller
+		lastPkgPath = ""
+	)
+
+	for i := m.NumFrames() - 1; i > 0; i-- {
+		fr := m.Frames[i]
+		if fr.LastPackage == nil || !fr.LastPackage.IsRealm() {
+			// Ignore non-realm frame
+			continue
+		}
+		pkgPath := fr.LastPackage.PkgPath
+		// The first realm we encounter will be the one calling
+		// this function; to get the calling realm determine the first frame
+		// where fr.LastPackage changes.
+		if lastPkgPath == "" {
+			lastPkgPath = pkgPath
+		} else if lastPkgPath == pkgPath {
+			continue
+		} else {
+			lastCaller = fr.LastPackage.GetPkgAddr().Bech32()
+			lastPkgPath = pkgPath
+			break
+		}
+	}
+
+	// Empty the pkgPath if we return a user
+	if ctx.OrigCaller == lastCaller {
+		lastPkgPath = ""
+	}
+	return lastCaller, lastPkgPath
 }
