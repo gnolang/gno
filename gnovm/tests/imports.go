450fcba050292f9378ee5c130e3d52d8d200224d
Thomas Bruyelle <thomas.bruyelle@tendermint.com>
2023-06-15 17:16:10 +0200

    use prevRealm in IsOriginCall/AssertOriginCall

--- gnovm/tests/imports.go
+++ gnovm/tests/imports.go
@@ -458,22 +458,6 @@ func TestStore(rootDir, filesPath string, stdin io.Reader, stdout, stderr io.Wri
 func testPackageInjector(store gno.Store, pn *gno.PackageNode) {
 	// Also inject stdlibs native functions.
 	stdlibs.InjectPackage(store, pn)
-	isOriginCall := func(m *gno.Machine) bool {
-		tname := m.Frames[0].Func.Name
-		switch tname {
-		case "main": // test is a _filetest
-			return len(m.Frames) == 3
-		case "runtest": // test is a _test
-			return len(m.Frames) == 7
-		}
-		// support init() in _filetest
-		// XXX do we need to distinguish from 'runtest'/_test?
-		// XXX pretty hacky even if not.
-		if strings.HasPrefix(string(tname), "init.") {
-			return len(m.Frames) == 3
-		}
-		panic("unable to determine if test is a _test or a _filetest")
-	}
 	// Test specific injections:
 	switch pn.PkgPath {
 	case "strconv":
@@ -484,34 +468,6 @@ func testPackageInjector(store gno.Store, pn *gno.PackageNode) {
 	case "std":
 		// NOTE: some of these are overrides.
 		// Also see stdlibs/InjectPackage.
-		pn.DefineNativeOverride("AssertOriginCall",
-			/*
-				gno.Flds( // params
-				),
-				gno.Flds( // results
-				),
-			*/
-			func(m *gno.Machine) {
-				if !isOriginCall(m) {
-					m.Panic(typedString("invalid non-origin call"))
-					return
-				}
-			},
-		)
-		pn.DefineNativeOverride("IsOriginCall",
-			/*
-				gno.Flds( // params
-				),
-				gno.Flds( // results
-					"isOrigin", "bool",
-				),
-			*/
-			func(m *gno.Machine) {
-				res0 := gno.TypedValue{T: gno.BoolType}
-				res0.SetBool(isOriginCall(m))
-				m.PushValue(res0)
-			},
-		)
 		pn.DefineNativeOverride("GetCallerAt",
 			/*
 				gno.Flds( // params
