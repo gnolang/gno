e890d6eb4ccd420f34ed47c79f7d1a643b2d0d73
Manfred Touron <94029+moul@users.noreply.github.com>
2023-07-20 15:30:43 +0200

    chore: fixup
    Signed-off-by: Manfred Touron <94029+moul@users.noreply.github.com>

--- gnovm/cmd/gno/lint.go
+++ gnovm/cmd/gno/lint.go
@@ -5,8 +5,10 @@ import (
 	"flag"
 	"fmt"
 	"os"
+	"path/filepath"
 
 	"github.com/gnolang/gno/tm2/pkg/commands"
+	osm "github.com/gnolang/gno/tm2/pkg/os"
 )
 
 type lintCfg struct {
@@ -58,23 +60,57 @@ func execLint(cfg *lintCfg, args []string, io *commands.IO) error {
 	}
 
 	hasError := false
+	addIssue := func(issue lintIssue) {
+		hasError = true
+		fmt.Fprint(io.Err, issue.String()+"\n")
+	}
+
 	for _, pkgPath := range pkgPaths {
 		if verbose {
 			fmt.Fprintf(io.Err, "Linting %q...\n", pkgPath)
 		}
-		// – setup in ci
-		// – add comment about what to do with syntax, also
-		// – gno.mod
-		// - update docs/
-		// - clear comments with fix suggestion
 
-		// 1. lint the package (gno.mod, etc)
-		// 2. lint the files individually
-		// 3. TODO: consider making `gno precompile; go lint *gen.go`
+		// 'gno.mod' exists?
+		gnoModPath := filepath.Join(pkgPath, "gno.mod")
+		if !osm.FileExists(gnoModPath) {
+			addIssue(lintIssue{
+				Code:       lintNoGnoMod,
+				Confidence: 1,
+				Location:   pkgPath,
+				Msg:        "missing 'gno.mod' file",
+			})
+		}
+
+		// TODO: 'gno mod' is valid?
+		// TODO: is gno source valid?
+		// TODO: are dependencies valid?
+		// TODO: is gno source using unsafe/discouraged features?
+		// TODO: consider making `gno precompile; go lint *gen.go`
 	}
 
 	if hasError {
 		os.Exit(cfg.setExitStatus)
 	}
 	return nil
+}
+
+type lintCode int
+
+const (
+	lintUnknown  lintCode = 0
+	lintNoGnoMod lintCode = iota
+	// TODO: add new linter codes here.
+)
+
+type lintIssue struct {
+	Code       lintCode
+	Msg        string
+	Confidence float64 // 1 is 100%
+	Location   string  // file:line, or equivalent
+	// TODO: consider writing fix suggestions
+}
+
+func (i lintIssue) String() string {
+	// TODO: consider crafting a doc URL based on Code.
+	return fmt.Sprintf("%s: %s (code=%d).", i.Location, i.Msg, i.Code)
 }
