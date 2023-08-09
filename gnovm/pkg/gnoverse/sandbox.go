c059e3cbea86e1aaf35e186375cce670b74e93b2
Manfred Touron <94029+moul@users.noreply.github.com>
2023-08-10 01:39:18 +0200

    chore: fixup
    Signed-off-by: Manfred Touron <94029+moul@users.noreply.github.com>

--- gnovm/pkg/gnoverse/sandbox.go
+++ gnovm/pkg/gnoverse/sandbox.go
@@ -2,14 +2,22 @@ package gnoverse
 
 import (
 	"fmt"
+	"path/filepath"
 
 	"github.com/gnolang/gno/tm2/pkg/db"
+	"github.com/gnolang/gno/tm2/pkg/sdk/auth"
+	"github.com/gnolang/gno/tm2/pkg/sdk/bank"
+	"github.com/gnolang/gno/tm2/pkg/sdk/vm"
+	"github.com/gnolang/gno/tm2/pkg/store"
 )
 
 type Sandbox struct {
-	db db.DB
-	// banker
-	// machine
+	db         db.DB
+	account    auth.AccountKeeper
+	bank       bank.BankKeeper
+	vm         vm.VMKeeper
+	stdlibsDir string
+	maxCycles  int64
 }
 
 type SandboxOpts struct {
@@ -27,9 +35,23 @@ func NewSandbox(opts SandboxOpts) (Sandbox, error) {
 	if err := opts.Validate(); err != nil {
 		return Sandbox{}, fmt.Errorf("invalid opts: %w", err)
 	}
-	// TODO: consider using a realm VMKeeper here, so we can use the existing handlers
+
+	baseKey := store.NewStoreKey("base")
+	iavlKey := store.NewStoreKey("main")
+	acctKpr := auth.NewAccountKeeper(iavlKey, ProtoGnoAccount)
+	bankKpr := bank.NewBankKeeper(acctKpr)
+	stdlibsDir := filepath.Join("..", "..", "stdlibs")
+	vm := vm.NewVMKeeper(
+		baseKey,
+		iavlKey,
+		acctKpr,
+		bankKpr,
+		stdlibsDir,
+		maxCycles,
+	)
 	box := Sandbox{
 		db: opts.DB,
+		vm: vm,
 	}
 	return box, nil
 }
