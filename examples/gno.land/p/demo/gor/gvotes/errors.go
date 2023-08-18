0434b949f1c447278f9b7023416a2e751844377a
ltzMaxwell <ltz.maxwell@gmail.com>
2023-08-18 11:49:45 +0800

    snapshot gov_token

--- examples/gno.land/p/demo/gor/gvotes/errors.go
+++
@@ -1,9 +0,0 @@
-package gvotes
-
-import "errors"
-
-var (
-	ErrInsufficientVotes    = errors.New("insufficient votes")
-	ErrInvalidAddress       = errors.New("invalid address")
-	ErrCannotTransferToSelf = errors.New("cannot send transfer to self")
-)
