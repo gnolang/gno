3560f428d10fb87b52ac24b5ef8e63a04d4a45b7
Morgan Bazalgette <morgan@morganbaz.com>
2023-08-23 14:29:53 +0200

    add native.go files as generated

--- gnovm/tests/file.go
+++ gnovm/tests/file.go
@@ -283,7 +283,7 @@ func RunFileTest(rootDir string, path string, opts ...RunFileTestOption) error {
 					if tv, ok := pnc.(*gno.TypedValue); ok {
 						panic(fmt.Sprintf("fail on %s: got unexpected error: %s", path, tv.Sprint(m)))
 					} else { // happens on 'unknown import path ...'
-						panic(fmt.Sprintf("fail on %s: got unexpected error: %#v", path, pnc))
+						panic(fmt.Sprintf("fail on %s: got unexpected error: %v", path, pnc))
 					}
 				}
 				// check result
