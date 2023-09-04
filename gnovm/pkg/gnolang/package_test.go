4b6891aee60300513e1e668373534fd59051c925
Thomas Bruyelle <thomas.bruyelle@tendermint.com>
2023-09-04 17:49:09 +0200

    fix(amino/json): use AminoMarshaler reprType info
    When checking if `info.IsJSONAnyValueType`, if this type is a
    AminoMarshaler, then also check `info.ReprType.IsJSONAnyValueType`.
    
    This prevents a panics that happens during the sanity check phase of
    `encodeReflectJSONInterface()` when the type `ConcreteInfo` doesn't match
    the JSON structure. For instance, before that fix, that used to happen
    with `BigintValue` which is a struct (so expected JSON is `{...}` and
    `IsJSONAnyValueType=false`), but is marshaled into a simple string (so
    expected JSON is only `"..."`, and `ReprType.IsJSONAnyValueType=true`).
    
    `decodeReflectJSONInterface` is also impacted.

---
+++ gnovm/pkg/gnolang/package_test.go
@@ -0,0 +1,41 @@
+package gnolang
+
+import (
+	"math/big"
+	"reflect"
+	"testing"
+
+	"github.com/jaekwon/testify/require"
+
+	"github.com/gnolang/gno/tm2/pkg/amino"
+)
+
+// Tries to reproduce the bug #1036 on all registered types
+func TestAminoJSONRegisteredTypes(t *testing.T) {
+	for _, typ := range Package.Types {
+		// Instanciate registered type
+		x := reflect.New(typ.Type).Interface()
+
+		// Call MarshalAmino directly on 'x'
+		_, err := amino.MarshalJSON(x)
+		require.NoError(t, err, "marshal type %s", typ.Type.Name())
+
+		// Call MarshalAmino on a struct that embeds 'x' in a field of type any
+		xx := struct {
+			X any
+		}{X: x}
+		_, err = amino.MarshalJSON(xx)
+		require.NoError(t, err, "marshal type %s from struct", typ.Type.Name())
+
+	}
+
+	// Check unmarshaling (can't reuse package.Types because some internal values
+	// must be filled properly
+	bi := BigintValue{V: big.NewInt(1)}
+	bz := amino.MustMarshalJSON(bi)
+	amino.MustUnmarshalJSON(bz, &bi)
+	// Check unmarshaling with an embedding struct
+	x := struct{ X any }{X: bi}
+	bz = amino.MustMarshalJSON(x)
+	amino.MustUnmarshalJSON(bz, &x)
+}
