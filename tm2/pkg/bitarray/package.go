905b9f2fb5328453a21323aa07af66e01b97c39f
gfanton <8671905+gfanton@users.noreply.github.com>
2023-09-24 14:27:02 +0200

    tmp: -- panic
    Signed-off-by: gfanton <8671905+gfanton@users.noreply.github.com>

--- tm2/pkg/bitarray/package.go
+++ tm2/pkg/bitarray/package.go
@@ -2,10 +2,16 @@ package bitarray
 
 import "github.com/gnolang/gno/tm2/pkg/amino"
 
+var dirname = amino.GetCallersDirname()
+
+func init() {
+	panic(dirname)
+}
+
 var Package = amino.RegisterPackage(amino.NewPackage(
 	"github.com/gnolang/gno/tm2/pkg/bitarray",
 	"tm",
-	amino.GetCallersDirname(),
+	dirname,
 ).WithDependencies().WithTypes(
 	BitArray{}, "BitArray",
 ))
