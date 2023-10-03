15335ef126351a3404e247d237aa1dbb3908c714
Manfred Touron <94029+moul@users.noreply.github.com>
2023-10-03 09:50:13 +0200

    add genesis doc

---
+++ gnosdk/pkg/genesis/doc.go
@@ -0,0 +1,23 @@
+// Package genesis provides tools and libraries for managing and manipulating
+// genesis files within the gnoland ecosystem.
+//
+// Objective:
+//
+// Genesis aims to enhance the development of testnets and local nets, evolving
+// from the basic genesis generation in gnoland main. The package enables creation,
+// revision, and amalgamation of various genesis files.
+//
+// Philosophy:
+//
+// While foundational projects like gno.land, gnovm, and tm2 emphasize minimalism,
+// genesis, along with `gnosdk`, caters to a wider range of advanced features.
+//
+// Anticipated features:
+//
+// - Gnotxsync Integration: Seamless integration with gnotxsync outputs.
+// - Data Management: Utilities for genesis data manipulation.
+// - Balance Management: Tools for balance-related tasks.
+// - Automatic Balance Updates: Auto-adjust balances via gnotxsync transactions.
+// - Local Development Funding: Ensures consistent funding during local dev sessions.
+// - Extensibility: Designed for future features, including potential proof-of-stake tools.
+package genesis
