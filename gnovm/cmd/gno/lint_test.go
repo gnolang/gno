3ff3b437eec2bdf322149778e5d47117c2fab722
gfanton <8671905+gfanton@users.noreply.github.com>
2023-10-07 11:29:07 +0200

    fix: add standard test
    Signed-off-by: gfanton <8671905+gfanton@users.noreply.github.com>

--- gnovm/cmd/gno/lint_test.go
+++ gnovm/cmd/gno/lint_test.go
@@ -14,12 +14,16 @@ func TestLintApp(t *testing.T) {
 			args:                []string{"lint", "--set_exit_status=0", "../../tests/integ/run-main/"},
 			stderrShouldContain: "./../../tests/integ/run-main: missing 'gno.mod' file (code=1).",
 		}, {
+			args:                []string{"lint", "--set_exit_status=0", "../../tests/integ/package-not-declared/main.gno"},
+			stderrShouldContain: "main.gno:4: name fmt not declared (code=2).",
+		}, {
 			args: []string{"lint", "--set_exit_status=0", "../../tests/integ/minimalist-gnomod/"},
 			// TODO: raise an error because there is a gno.mod, but no .gno files
 		}, {
 			args: []string{"lint", "--set_exit_status=0", "../../tests/integ/invalid-module-name/"},
 			// TODO: raise an error because gno.mod is invalid
 		},
+
 		// TODO: 'gno mod' is valid?
 		// TODO: is gno source valid?
 		// TODO: are dependencies valid?
