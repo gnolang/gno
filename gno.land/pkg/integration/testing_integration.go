010cda3c39153ba21c98a93bc0c8926b4bbb4ff3
gfanton <8671905+gfanton@users.noreply.github.com>
2023-10-10 17:16:15 +0200

    fix: use `FirstBlockReceived` signal in gnoland integration setup
    Signed-off-by: gfanton <8671905+gfanton@users.noreply.github.com>

--- gno.land/pkg/integration/testing_integration.go
+++ gno.land/pkg/integration/testing_integration.go
@@ -14,11 +14,9 @@ import (
 
 	"github.com/gnolang/gno/gno.land/pkg/gnoland"
 	"github.com/gnolang/gno/tm2/pkg/bft/node"
-	"github.com/gnolang/gno/tm2/pkg/bft/types"
 	"github.com/gnolang/gno/tm2/pkg/commands"
 	"github.com/gnolang/gno/tm2/pkg/crypto/keys"
 	"github.com/gnolang/gno/tm2/pkg/crypto/keys/client"
-	"github.com/gnolang/gno/tm2/pkg/events"
 	"github.com/gnolang/gno/tm2/pkg/log"
 	"github.com/rogpeppe/go-internal/testscript"
 )
@@ -141,28 +139,12 @@ func SetupGnolandTestScript(t *testing.T, txtarDir string) testscript.Params {
 						ts.Setenv("RPC_ADDR", laddr)
 						ts.Setenv("GNODATA", gnoDataDir)
 
-						const listenerID = "testing_listener"
-
-						// Wait for first block by waiting for `EventNewBlock` event.
-						nb := make(chan struct{}, 1)
-						node.EventSwitch().AddListener(listenerID, func(ev events.Event) {
-							if _, ok := ev.(types.EventNewBlock); ok {
-								select {
-								case nb <- struct{}{}:
-								default:
-								}
-							}
-						})
-
-						if node.BlockStore().Height() == 0 {
-							select {
-							case <-nb: // ok
-							case <-time.After(time.Second * 6):
-								ts.Fatalf("timeout while waiting for the node to start")
-							}
+						// Wait for first block.
+						select {
+						case <-time.After(time.Second * 6):
+							ts.Fatalf("timeout while waiting for the node to start")
+						case <-node.FirstBlockReceived(): // ready
 						}
-
-						node.EventSwitch().RemoveListener(listenerID)
 
 						fmt.Fprintln(ts.Stdout(), "node started successfully")
 					}
