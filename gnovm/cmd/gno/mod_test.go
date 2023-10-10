0767b53d109bb514bea256702e1e06c2e5e2b925
Hariom Verma <hariom.verma@tendermint.com>
2023-10-10 14:44:09 +0530

    TestGetGnoImports

--- gnovm/cmd/gno/mod_test.go
+++ gnovm/cmd/gno/mod_test.go
@@ -1,6 +1,13 @@
 package main
 
-import "testing"
+import (
+	"os"
+	"path/filepath"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
 
 func TestModApp(t *testing.T) {
 	tc := []testMainCase{
@@ -140,6 +147,12 @@ func TestModApp(t *testing.T) {
 
 		// test `gno mod tidy` with module name
 		{
+			args:                 []string{"mod", "tidy", "arg1"},
+			testDir:              "../../tests/integ/minimalist-gnomod",
+			simulateExternalRepo: true,
+			errShouldContain:     "flag: help requested",
+		},
+		{
 			args:                 []string{"mod", "tidy"},
 			testDir:              "../../tests/integ/empty-dir",
 			simulateExternalRepo: true,
@@ -174,4 +187,121 @@ func TestModApp(t *testing.T) {
 		},
 	}
 	testMainCaseRun(t, tc)
+}
+
+func TestGetGnoImports(t *testing.T) {
+	workingDir, err := os.Getwd()
+	require.NoError(t, err)
+
+	// create external dir
+	tmpDir, cleanUpFn := createTmpDir(t)
+	defer cleanUpFn()
+
+	// cd to tmp directory
+	os.Chdir(tmpDir)
+	defer os.Chdir(workingDir)
+
+	files := []struct {
+		name, data string
+	}{
+		{
+			name: "file1.gno",
+			data: `
+			package tmp
+
+			import (
+				"std"
+
+				"gno.land/p/demo/pkg1"
+			)
+			`,
+		},
+		{
+			name: "file2.gno",
+			data: `
+			package tmp
+
+			import (
+				"gno.land/p/demo/pkg1"
+				"gno.land/p/demo/pkg2"
+			)
+			`,
+		},
+		{
+			name: "file1_test.gno",
+			data: `
+			package tmp
+
+			import (
+				"testing"
+
+				"gno.land/p/demo/testpkg"
+			)
+			`,
+		},
+		{
+			name: "z_0_filetest.gno",
+			data: `
+			package main
+
+			import (
+				"gno.land/p/demo/filetestpkg"
+			)
+			`,
+		},
+
+		// subpkg files
+		{
+			name: filepath.Join("subtmp", "file1.gno"),
+			data: `
+			package subtmp
+	
+			import (
+				"std"
+			
+				"gno.land/p/demo/subpkg1"
+			)
+			`,
+		},
+		{
+			name: filepath.Join("subtmp", "file2.gno"),
+			data: `
+			package subtmp
+	
+			import (
+				"gno.land/p/demo/subpkg1"
+				"gno.land/p/demo/subpkg2"
+			)
+			`,
+		},
+	}
+
+	// Expected list of imports
+	// - ignore subdirs
+	// - ignore duplicate
+	// - ignore *_filetest.gno
+	// - should be sorted
+	expected := []string{
+		"gno.land/p/demo/pkg1",
+		"gno.land/p/demo/pkg2",
+		"gno.land/p/demo/testpkg",
+	}
+
+	// Create subpkg dir
+	err = os.Mkdir("subtmp", 0700)
+	require.NoError(t, err)
+
+	// Create files
+	for _, f := range files {
+		err = os.WriteFile(f.name, []byte(f.data), 0o644)
+		require.NoError(t, err)
+	}
+
+	imports, err := getGnoImports(tmpDir)
+	require.NoError(t, err)
+
+	require.Equal(t, len(expected), len(imports))
+	for i := range imports {
+		assert.Equal(t, expected[i], imports[i])
+	}
 }
