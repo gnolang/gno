9f372d53ec561f3ca71d02254ae0735f8c30c0fc
gfanton <8671905+gfanton@users.noreply.github.com>
2023-10-11 17:26:53 +0200

    fix: move away homedir from tm2
    Signed-off-by: gfanton <8671905+gfanton@users.noreply.github.com>

--- gno.land/cmd/gnokey/main.go
+++ gno.land/cmd/gnokey/main.go
@@ -5,12 +5,21 @@ import (
 	"fmt"
 	"os"
 
+	"github.com/gnolang/gno/gnovm/pkg/gnoenv"
 	"github.com/gnolang/gno/tm2/pkg/commands"
 	"github.com/gnolang/gno/tm2/pkg/crypto/keys/client"
 )
 
 func main() {
-	cmd := client.NewRootCmd(commands.NewDefaultIO())
+	baseCfg := client.BaseOptions{
+		Home:                  gnoenv.HomeDir(),
+		Remote:                "127.0.0.1:26657",
+		Quiet:                 false,
+		InsecurePasswordStdin: false,
+		Config:                "",
+	}
+
+	cmd := client.NewRootCmdWithBaseConfig(commands.NewDefaultIO(), baseCfg)
 
 	if err := cmd.ParseAndRun(context.Background(), os.Args[1:]); err != nil {
 		_, _ = fmt.Fprintf(os.Stderr, "%+v\n", err)
