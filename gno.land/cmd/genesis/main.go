50332326d5e97498663c622e86216c4b25bf7079
Milos Zivkovic <milos.zivkovic@tendermint.com>
2023-10-18 18:54:01 -0400

    Add genesis generate command

---
+++ gno.land/cmd/genesis/main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"os"
+
+	"github.com/gnolang/gno/tm2/pkg/commands"
+)
+
+func main() {
+	io := commands.NewDefaultIO()
+	cmd := newRootCmd(io)
+
+	if err := cmd.ParseAndRun(context.Background(), os.Args[1:]); err != nil {
+		_, _ = fmt.Fprintf(os.Stderr, "%+v\n", err)
+
+		os.Exit(1)
+	}
+}
+
+func newRootCmd(io *commands.IO) *commands.Command {
+	cmd := commands.NewCommand(
+		commands.Metadata{
+			ShortUsage: "<subcommand> [flags] [<arg>...]",
+			LongHelp:   "TM2 Genesis manipulation suite",
+		},
+		commands.NewEmptyConfig(),
+		commands.HelpExec,
+	)
+
+	cmd.AddSubCommands(
+		newGenerateCmd(io),
+	)
+
+	return cmd
+}
