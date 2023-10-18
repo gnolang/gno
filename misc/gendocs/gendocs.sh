553888108a1ed5b6b8e08a2207c65a8767c54e1e
Morgan Bazalgette <morgan@morganbaz.com>
2023-10-18 16:08:15 -0400

    docs: fix gendocs

--- misc/gendocs/gendocs.sh
+++ misc/gendocs/gendocs.sh
@@ -1,35 +1,38 @@
-#!/bin/sh
+#!/bin/bash
+set -u
 
-GODOC_PORT=${GODOC_PORT:-6060}
-GO_MODULE=${GO_MODULE:-github.com/gnolang/gno}
-GODOC_OUT=${GODOC_OUT:-godoc}
-URL=http://localhost:${GODOC_PORT}/pkg/github.com/gnolang/gno/
+DOC_DIR=godoc
+PKG=github.com/gnolang/gno
 
-echo "[+] Starting godoc server..."
-go run \
-   -modfile ../devdeps/go.mod \
-   golang.org/x/tools/cmd/godoc \
-     -http="localhost:${GODOC_PORT}" &
-PID=$!
-# Waiting for godoc server
-while ! curl --fail --silent "$URL" > /dev/null 2>&1; do
-    sleep 0.1
+# Run a godoc server which we will scrape. Clobber the GOPATH to include
+# only our dependencies.
+env -C ../.. pkgsite &
+DOC_PID=$!
+
+# Wait for the server to init
+while :
+do
+    curl -s "http://localhost:8080" > /dev/null
+    if [ $? -eq 0 ] # exit code is 0 if we connected
+    then
+        break
+    fi
 done
 
-echo "[+] Downloading godoc pages..."
-wget \
-    --recursive \
-    --no-verbose \
-    --convert-links \
-    --page-requisites \
-    --adjust-extension \
-    --execute=robots=off \
-    --include-directories="/lib,/pkg/$GO_MODULE,/src/$GO_MODULE" \
-    --exclude-directories="*" \
-    --directory-prefix="${GODOC_OUT}" \
-    --no-host-directories \
-    "$URL?m=all"
+# Scrape the pkg directory for the API docs. Scrap lib for the CSS/JS. Ignore everything else.
+wget -v -r -m -k -E -p -erobots=off --accept-regex='8080/((search|license-policy|about|)$|(static|images)/|github.com/gnolang/)' "http://localhost:8080/"
 
-echo "[+] Killing godoc server..."
-kill -9 "$PID"
+# Stop the godoc server
+kill -9 $DOC_PID
 
+# Delete the old directory or else mv will put the localhost dir into
+# the DOC_DIR if it already exists.
+rm -rf $DOC_DIR
+mv localhost\:8080 $DOC_DIR
+
+find godoc -type f -exec sed -ri 's#http://localhost:8080/files/[^"]*/github.com/gnolang/([^/"]+)/([^"]*)#https://github.com/gnolang/\1/blob/master/\2#g
+s#http://localhost:8080/[^"?]*\?tab=(importedby|versions)#\##g
+s#http://localhost:8080([^")]*)#https://pkg.go.dev\1#g
+s#/files/[^" ]*/(github.com/[^" ]*)/#\1#g' {} +
+
+echo "Docs can be found in $DOC_DIR"
