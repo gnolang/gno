505ed6057cda8bf6f4356bad883295f3c9317a66
Miloš Živković <milos.zivkovic@tendermint.com>
2023-10-25 11:32:02 -0400

    feat: add `genesis txs` subcommand (#1278)

---
+++ gno.land/cmd/genesis/txs.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"flag"
+
+	"github.com/gnolang/gno/tm2/pkg/commands"
+)
+
+type txsCfg struct {
+	genesisPath string
+}
+
+// newTxsCmd creates the genesis txs subcommand
+func newTxsCmd(io *commands.IO) *commands.Command {
+	cfg := &txsCfg{}
+
+	cmd := commands.NewCommand(
+		commands.Metadata{
+			Name:       "txs",
+			ShortUsage: "txs <subcommand> [flags]",
+			ShortHelp:  "Manages the initial genesis transactions",
+			LongHelp:   "Manages genesis transactions through input files",
+		},
+		cfg,
+		commands.HelpExec,
+	)
+
+	cmd.AddSubCommands(
+		newTxsAddCmd(cfg, io),
+		newTxsRemoveCmd(cfg, io),
+	)
+
+	return cmd
+}
+
+func (c *txsCfg) RegisterFlags(fs *flag.FlagSet) {
+	fs.StringVar(
+		&c.genesisPath,
+		"genesis-path",
+		"./genesis.json",
+		"the path to the genesis.json",
+	)
+}
