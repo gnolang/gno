3584d866de6aab2bb810e6c46ad1e119d49cacef
Guilhem Fanton <8671905+gfanton@users.noreply.github.com>
2023-10-26 08:45:32 -0400

    Update gno.land/cmd/gnoland/start.go
    Co-authored-by: Manfred Touron <94029+moul@users.noreply.github.com>

--- gno.land/cmd/gnoland/start.go
+++ gno.land/cmd/gnoland/start.go
@@ -60,7 +60,7 @@ func newStartCmd(io *commands.IO) *commands.Command {
 
 func (c *startCfg) RegisterFlags(fs *flag.FlagSet) {
 	gnoroot := gnoland.MustGuessGnoRootDir()
-	defaultGenesisBalancesFile := filepath.Join(gnoroot, "gno.land/genesis/genesis_balances.txt")
+	defaultGenesisBalancesFile := filepath.Join(gnoroot, "gno.land", "genesis", "genesis_balances.txt")
 	defaultGenesisTxsFile := filepath.Join(gnoroot, "gno.land/genesis/genesis_txs.txt")
 
 	fs.BoolVar(
