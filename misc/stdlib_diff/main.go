16e54a3852c78d0cce73a63c0d993e57fe7c69fc
Florian Richard <florian.richard@aloe-corp.com>
2023-12-08 11:18:53 +0100

    Update flag usage

--- misc/stdlib_diff/main.go
+++ misc/stdlib_diff/main.go
@@ -11,10 +11,10 @@ func main() {
 	var outDirectory string
 	var srcIsGno bool
 
-	flag.StringVar(&srcPath, "src", "", "Path to Gnoland standard libraries directory")
-	flag.StringVar(&dstPath, "dst", "", "Path to Goland standard libraries directory")
-	flag.StringVar(&outDirectory, "out", "", "Path to the directory where the report will be generated")
-	flag.BoolVar(&srcIsGno, "src_is_gno", false, "If true, indicates that the src parameter correponds to the gno standard libraries")
+	flag.StringVar(&srcPath, "src", "", "Directory containing packages that will be compared to destination")
+	flag.StringVar(&dstPath, "dst", "", "Directory containing packages; used to compare src packages")
+	flag.StringVar(&outDirectory, "out", "", "Directory where the report will be created")
+	flag.BoolVar(&srcIsGno, "src_is_gno", false, "If true, indicates that the src parameter corresponds to the gno standard libraries")
 	flag.Parse()
 
 	reportBuilder, err := NewReportBuilder(srcPath, dstPath, outDirectory, srcIsGno)
