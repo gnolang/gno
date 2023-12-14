e6478c38c70f64d89c4432fd4bdfa117966bf61c
gfanton <8671905+gfanton@users.noreply.github.com>
2023-12-14 16:01:05 +0100

    chore: fixup comment
    Signed-off-by: gfanton <8671905+gfanton@users.noreply.github.com>

--- tm2/pkg/crypto/keys/client/deprecated.go
+++ tm2/pkg/crypto/keys/client/deprecated.go
@@ -2,10 +2,18 @@ package client
 
 import "github.com/gnolang/gno/tm2/pkg/commands"
 
-// Deprecated: NewRootCmd is deprecated and no longer maintained. It has been
-// moved in `gno.land/pkg/keycli`. Please update your code to use the new
-// package. This function now only serves as a placeholder and will panic if
-// called to encourage migration.
+// Deprecated: NewRootCmd is deprecated and has been moved in
+// `gno.land/pkg/keycli`. Please update your code to use the new package. This
+// function now only serves as a placeholder and will panic if called to
+// encourage migration.
 func NewRootCmd(io commands.IO) *commands.Command {
 	panic("NewRootCmd: has been deprecated, use `gno.land/pkg/keycli` instead")
+}
+
+// Deprecated: NewRootCmdWithBaseConfig as has been moved in
+// `gno.land/pkg/keycli`. Please update your code to use the new package. This
+// function now only serves as a placeholder and will panic if called to
+// encourage migration.
+func NewRootCmdWithBaseConfig(io commands.IO, cfg interface{}) *commands.Command {
+	panic("NewRootCmdWithBaseConfig: has been deprecated, use `gno.land/pkg/keycli` instead")
 }
