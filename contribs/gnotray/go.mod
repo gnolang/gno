2f0bfaaaa44b2ae6479396670c44bc2de4d875cf
moul <94029+moul@users.noreply.github.com>
2023-12-16 16:58:09 +0100

    chore: fixup
    Signed-off-by: moul <94029+moul@users.noreply.github.com>

--- contribs/gnotray/go.mod
+++ contribs/gnotray/go.mod
@@ -5,10 +5,14 @@ go 1.20
 require (
 	github.com/getlantern/systray v1.2.2
 	github.com/gnolang/gno v0.0.0-00010101000000-000000000000
+	github.com/gnolang/gno/contribs/gnodev v0.0.0-00010101000000-000000000000
 	github.com/skratchdot/open-golang v0.0.0-20200116055534-eef842397966
 )
 
-replace github.com/gnolang/gno => ../..
+replace (
+	github.com/gnolang/gno => ../..
+	github.com/gnolang/gno/contribs/gnodev => ../gnodev
+)
 
 require (
 	github.com/btcsuite/btcd/btcec/v2 v2.3.2 // indirect
@@ -36,7 +40,11 @@ require (
 	github.com/golang/protobuf v1.5.3 // indirect
 	github.com/golang/snappy v0.0.4 // indirect
 	github.com/google/flatbuffers v1.12.1 // indirect
+	github.com/gorilla/mux v1.8.1 // indirect
+	github.com/gorilla/securecookie v1.1.1 // indirect
+	github.com/gorilla/sessions v1.2.1 // indirect
 	github.com/gorilla/websocket v1.5.1 // indirect
+	github.com/gotuna/gotuna v0.6.0 // indirect
 	github.com/jaekwon/testify v1.6.1 // indirect
 	github.com/jmhodges/levigo v1.0.0 // indirect
 	github.com/klauspost/compress v1.12.3 // indirect
@@ -53,13 +61,12 @@ require (
 	github.com/tecbot/gorocksdb v0.0.0-20191217155057-f0fad39f321c // indirect
 	go.etcd.io/bbolt v1.3.8 // indirect
 	go.opencensus.io v0.22.5 // indirect
-	go.uber.org/atomic v1.7.0 // indirect
-	go.uber.org/multierr v1.9.0 // indirect
+	go.uber.org/multierr v1.10.0 // indirect
 	golang.org/x/crypto v0.15.0 // indirect
 	golang.org/x/mod v0.14.0 // indirect
 	golang.org/x/net v0.17.0 // indirect
-	golang.org/x/sys v0.14.0 // indirect
-	golang.org/x/term v0.14.0 // indirect
+	golang.org/x/sys v0.15.0 // indirect
+	golang.org/x/term v0.15.0 // indirect
 	golang.org/x/tools v0.13.0 // indirect
 	google.golang.org/protobuf v1.31.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
--- contribs/gnotray/main.go
+++ contribs/gnotray/main.go
@@ -1,12 +1,24 @@
 package main
 
 import (
+	"context"
+	"fmt"
 	"log"
+	"net"
+	"net/http"
+	"os"
+	"path/filepath"
+	"time"
+
+	gnodev "github.com/gnolang/gno/contribs/gnodev/pkg/dev"
+	"github.com/gnolang/gno/gno.land/pkg/gnoweb"
+	"github.com/gnolang/gno/gno.land/pkg/integration"
+	"github.com/gnolang/gno/gnovm/pkg/gnoenv"
+	tmlog "github.com/gnolang/gno/tm2/pkg/log"
+	osm "github.com/gnolang/gno/tm2/pkg/os"
 
 	"github.com/getlantern/systray"
 	"github.com/skratchdot/open-golang/open"
-
-	"github.com/gnolang/gno/gno.land/pkg/integration"
 )
 
 func main() {
@@ -45,7 +57,69 @@ func onReady() {
 			select {
 			case <-mStartGnodev.ClickedCh:
 				mStartGnodev.SetTitle("Starting...")
-				log.Println("NOT IMPLEMENTED")
+				ctx, cancel := context.WithCancelCause(context.Background())
+				defer cancel(nil)
+				gnoroot := gnoenv.RootDir()
+				examplesDir := filepath.Join(gnoroot, "examples")
+				// pkgpaths, err := parseArgsPackages(args); if err
+				// pkgpaths = append(pkgpaths, examplesDir)
+				pkgpaths := []string{examplesDir}
+				osm.TrapSignal(func() {
+					cancel(nil)
+				})
+				nodeOut := os.Stdout
+				logger := tmlog.NewTMLogger(nodeOut)
+				logger.SetLevel(tmlog.LevelError)
+				node, err := gnodev.NewDevNode(ctx, logger, pkgpaths)
+				if err != nil {
+					panic(err)
+				}
+				defer node.Close()
+
+				log.Printf("Listener: %s\n", node.GetRemoteAddress())
+				log.Printf("Default Address: %s\n", gnodev.DefaultCreator.String())
+				log.Printf("Chain ID: %s\n", node.Config().ChainID())
+
+				gnodevListen := ":8000"
+
+				// TODO: auto-reload
+
+				// gnoweb
+				l, err := net.Listen("tcp", gnodevListen)
+				if err != nil {
+					panic(fmt.Errorf("unable to listen to %q: %w", gnodevListen, err))
+				}
+				defer l.Close()
+
+				serveGnoWebServer := func(l net.Listener, dnode *gnodev.Node) error {
+					var server http.Server
+
+					webConfig := gnoweb.NewDefaultConfig()
+					webConfig.RemoteAddr = dnode.GetRemoteAddress()
+
+					loggerweb := tmlog.NewTMLogger(os.Stdout)
+					loggerweb.SetLevel(tmlog.LevelDebug)
+
+					app := gnoweb.MakeApp(loggerweb, webConfig)
+
+					server.ReadHeaderTimeout = 60 * time.Second
+					server.Handler = app.Router
+
+					if err := server.Serve(l); err != nil {
+						return fmt.Errorf("unable to serve GnoWeb: %w", err)
+					}
+
+					return nil
+				}
+
+				// Run GnoWeb server
+				go func() {
+					cancel(serveGnoWebServer(l, node))
+				}()
+
+				log.Printf("Listener: http://%s\n", l.Addr())
+
+				mStartGnodev.SetTitle("Started...")
 			case <-mOpenGnolandRPC.ClickedCh:
 				// open.Run("http://127.0.0.1:XXX/")
 				log.Println("NOT IMPLEMENTED")
