68dc0917be2e21ba9019e64d8add87bbacc6e261
gfanton <8671905+gfanton@users.noreply.github.com>
2023-12-20 16:12:23 +0100

    fix: remove all method signature in `keys/client`
    Signed-off-by: gfanton <8671905+gfanton@users.noreply.github.com>

--- tm2/pkg/crypto/keys/client/deprecated.go
+++ tm2/pkg/crypto/keys/client/deprecated.go
@@ -1,21 +1,2 @@
-//go:build deprecated
-
+// Deprecated: Please use 'gno.land/pkg/keycli'
 package client
-
-import "github.com/gnolang/gno/tm2/pkg/commands"
-
-// Deprecated: NewRootCmd is deprecated and has been moved in
-// `gno.land/pkg/keycli`. Please update your code to use the new package. This
-// function now only serves as a placeholder and will panic if called to
-// encourage migration.
-func NewRootCmd(io commands.IO) *commands.Command {
-	panic("NewRootCmd: has been deprecated, use `gno.land/pkg/keycli` instead")
-}
-
-// Deprecated: NewRootCmdWithBaseConfig as has been moved in
-// `gno.land/pkg/keycli`. Please update your code to use the new package. This
-// function now only serves as a placeholder and will panic if called to
-// encourage migration.
-func NewRootCmdWithBaseConfig(io commands.IO, cfg interface{}) *commands.Command {
-	panic("NewRootCmdWithBaseConfig: has been deprecated, use `gno.land/pkg/keycli` instead")
-}
