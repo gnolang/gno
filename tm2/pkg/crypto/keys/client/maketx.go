489e4a419053c8dc71efef8a490e57fa92229ea7
gfanton <8671905+gfanton@users.noreply.github.com>
2023-12-21 23:11:57 +0100

    feat: add SignAndBroadcastHandler
    Signed-off-by: gfanton <8671905+gfanton@users.noreply.github.com>

--- tm2/pkg/crypto/keys/client/maketx.go
+++ tm2/pkg/crypto/keys/client/maketx.go
@@ -2,8 +2,14 @@ package client
 
 import (
 	"flag"
+	"fmt"
 
+	"github.com/gnolang/gno/tm2/pkg/amino"
+	types "github.com/gnolang/gno/tm2/pkg/bft/rpc/core/types"
 	"github.com/gnolang/gno/tm2/pkg/commands"
+	"github.com/gnolang/gno/tm2/pkg/crypto/keys"
+	"github.com/gnolang/gno/tm2/pkg/errors"
+	"github.com/gnolang/gno/tm2/pkg/std"
 )
 
 type MakeTxCfg struct {
@@ -77,4 +83,107 @@ func (c *MakeTxCfg) RegisterFlags(fs *flag.FlagSet) {
 		"dev",
 		"chainid to sign for (only useful if --broadcast)",
 	)
+}
+
+func SignAndBroadcastHandler(
+	cfg *MakeTxCfg,
+	nameOrBech32 string,
+	tx std.Tx,
+	pass string,
+) (*types.ResultBroadcastTxCommit, error) {
+	baseopts := cfg.RootCfg
+	txopts := cfg
+
+	kb, err := keys.NewKeyBaseFromDir(cfg.RootCfg.Config)
+	if err != nil {
+		return nil, err
+	}
+
+	info, err := kb.GetByNameOrAddress(nameOrBech32)
+	if err != nil {
+		return nil, err
+	}
+	accountAddr := info.GetAddress()
+
+	qopts := &QueryCfg{
+		RootCfg: baseopts,
+		Path:    fmt.Sprintf("auth/accounts/%s", accountAddr),
+	}
+	qres, err := QueryHandler(qopts)
+	if err != nil {
+		return nil, errors.Wrap(err, "query account")
+	}
+	var qret struct{ BaseAccount std.BaseAccount }
+	err = amino.UnmarshalJSON(qres.Response.Data, &qret)
+	if err != nil {
+		return nil, err
+	}
+
+	// sign tx
+	accountNumber := qret.BaseAccount.AccountNumber
+	sequence := qret.BaseAccount.Sequence
+	sopts := &SignCfg{
+		Pass:          pass,
+		RootCfg:       baseopts,
+		Sequence:      sequence,
+		AccountNumber: accountNumber,
+		ChainID:       txopts.ChainID,
+		NameOrBech32:  nameOrBech32,
+		TxJSON:        amino.MustMarshalJSON(tx),
+	}
+
+	signedTx, err := SignHandler(sopts)
+	if err != nil {
+		return nil, errors.Wrap(err, "sign tx")
+	}
+
+	// broadcast signed tx
+	bopts := &BroadcastCfg{
+		RootCfg: baseopts,
+		tx:      signedTx,
+	}
+
+	return BroadcastHandler(bopts)
+}
+
+func signAndBroadcast(
+	cfg *MakeTxCfg,
+	args []string,
+	tx std.Tx,
+	io commands.IO,
+) error {
+	baseopts := cfg.RootCfg
+
+	// query account
+	nameOrBech32 := args[0]
+
+	var err error
+	var pass string
+	if baseopts.Quiet {
+		pass, err = io.GetPassword("", baseopts.InsecurePasswordStdin)
+	} else {
+		pass, err = io.GetPassword("Enter password.", baseopts.InsecurePasswordStdin)
+	}
+
+	if err != nil {
+		return err
+	}
+
+	bres, err := SignAndBroadcastHandler(cfg, nameOrBech32, tx, pass)
+	if err != nil {
+		return errors.Wrap(err, "broadcast tx")
+	}
+	if bres.CheckTx.IsErr() {
+		return errors.Wrap(bres.CheckTx.Error, "check transaction failed: log:%s", bres.CheckTx.Log)
+	}
+	if bres.DeliverTx.IsErr() {
+		return errors.Wrap(bres.DeliverTx.Error, "deliver transaction failed: log:%s", bres.DeliverTx.Log)
+	}
+
+	io.Println(string(bres.DeliverTx.Data))
+	io.Println("OK!")
+	io.Println("GAS WANTED:", bres.DeliverTx.GasWanted)
+	io.Println("GAS USED:  ", bres.DeliverTx.GasUsed)
+
+	return nil
 }
