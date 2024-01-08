2fa6b600f17408fca2cf977cac5779317cca5aeb
gfanton <8671905+gfanton@users.noreply.github.com>
2024-01-08 12:04:54 +0100

    chore: rename and better comments
    Signed-off-by: gfanton <8671905+gfanton@users.noreply.github.com>

--- contribs/gnodev/pkg/watcher/watch.go
+++ contribs/gnodev/pkg/watcher/watch.go
@@ -19,12 +19,160 @@ type PackageWatcher struct {
 	PackagesUpdate <-chan PackageUpdateList
 	Errors         <-chan error
 
+	ctx  context.Context
+	stop context.CancelFunc
+
 	logger  log.Logger
 	watcher *fsnotify.Watcher
-	pkgs    []string
-	ctx     context.Context
-	stop    context.CancelFunc
+	pkgsDir []string
 	emitter events.Emitter
+}
+
+func NewPackageWatcher(logger log.Logger, emitter events.Emitter) (*PackageWatcher, error) {
+	watcher, err := fsnotify.NewWatcher()
+	if err != nil {
+		return nil, fmt.Errorf("unable to watch files: %w", err)
+	}
+
+	ctx, cancel := context.WithCancel(context.Background())
+	p := &PackageWatcher{
+		pkgsDir: []string{},
+		logger:  logger,
+		watcher: watcher,
+		ctx:     ctx,
+		stop:    cancel,
+		emitter: emitter,
+	}
+
+	p.startWatching()
+
+	return p, nil
+}
+
+func (p *PackageWatcher) Stop() {
+	p.stop()
+}
+
+// AddPackages adds new packages to the watcher.
+// Packages are sorted by their length in descending order to facilitate easier
+// and more efficient matching with corresponding paths. The longest paths are
+// compared first.
+func (p *PackageWatcher) AddPackages(pkgs ...gnomod.Pkg) error {
+	for _, pkg := range pkgs {
+		dir := pkg.Dir
+
+		abs, err := filepath.Abs(dir)
+		if err != nil {
+			return fmt.Errorf("unable to get absolute path of %q: %w", dir, err)
+		}
+
+		// Use binary search to find the correct insertion point
+		index := sort.Search(len(p.pkgsDir), func(i int) bool {
+			return len(p.pkgsDir[i]) <= len(dir) // Longest paths first
+		})
+
+		// Check for duplicates at the insertion point to avoid redundancy
+		if index < len(p.pkgsDir) && p.pkgsDir[index] == dir {
+			continue // Skip
+		}
+
+		// Add the package to the watcher and handle any errors
+		if err := p.watcher.Add(abs); err != nil {
+			return fmt.Errorf("unable to watch %q: %w", pkg.Dir, err)
+		}
+	}
+
+	return nil
+}
+
+func (p *PackageWatcher) startWatching() {
+	const timeout = time.Millisecond * 500 // Debounce interval
+
+	cErrors := make(chan error, 1)
+	cPkgUpdate := make(chan PackageUpdateList)
+
+	go func() {
+		defer close(cErrors)
+		defer close(cPkgUpdate)
+
+		var debounceTimer <-chan time.Time
+		var pathList = []string{}
+		var err error
+
+		for err == nil {
+			select {
+			case <-p.ctx.Done():
+				err = p.ctx.Err()
+			case watchErr := <-p.watcher.Errors:
+				err = fmt.Errorf("watch error: %w", watchErr)
+			case <-debounceTimer:
+				// Process and emit package updates after the debounce interval
+				updates := p.generatePackagesUpdateList(pathList)
+				for _, update := range updates {
+					p.logger.Error("packages update",
+						"pkg", update.Package,
+						"files", update.Files,
+					)
+				}
+
+				// Send updates
+				cPkgUpdate <- updates
+				p.emitter.Emit(events.NewPackagesUpdateEvent(updates))
+
+				// Reset the path list and debounce timer
+				pathList = []string{}
+				debounceTimer = nil
+			case evt := <-p.watcher.Events:
+				// Only handle write operations
+				if evt.Op != fsnotify.Write {
+					continue
+				}
+
+				pathList = append(pathList, evt.Name)
+
+				// Set up the debounce timer
+				debounceTimer = time.After(timeout)
+			}
+		}
+
+		cErrors <- err // Send any final error to the channel
+	}()
+
+	// Set update channels
+	p.PackagesUpdate = cPkgUpdate
+	p.Errors = cErrors
+}
+
+func (p *PackageWatcher) generatePackagesUpdateList(paths []string) PackageUpdateList {
+	pkgsUpdate := []events.PackageUpdate{}
+
+	mpkgs := map[string]*events.PackageUpdate{} // Pkg -> Update
+	for _, path := range paths {
+		for _, pkg := range p.pkgsDir {
+			// Check if the path is inside the package directory
+			if !strings.HasPrefix(pkg, path) {
+				continue
+			}
+
+			if len(pkg) == len(path) {
+				continue // Skip if pkg == path
+			}
+
+			// Accumulate file updates for each package
+			pkgu, ok := mpkgs[pkg]
+			if !ok {
+				pkgsUpdate = append(pkgsUpdate, events.PackageUpdate{
+					Package: pkg,
+					Files:   []string{},
+				})
+				pkgu = &pkgsUpdate[len(pkgsUpdate)-1]
+			}
+
+			pkgu.Files = append(pkgu.Files, path)
+		}
+	}
+
+	return pkgsUpdate
 }
 
 type PackageUpdateList []events.PackageUpdate
@@ -43,143 +191,4 @@ func (pkgsu PackageUpdateList) FilesPath() []string {
 		files = append(files, pkg.Files...)
 	}
 	return files
-}
-
-func NewPackageWatcher(logger log.Logger, emitter events.Emitter) (*PackageWatcher, error) {
-	watcher, err := fsnotify.NewWatcher()
-	if err != nil {
-		return nil, fmt.Errorf("unable to watch files: %w", err)
-	}
-
-	ctx, cancel := context.WithCancel(context.Background())
-	p := &PackageWatcher{
-		pkgs:    []string{},
-		logger:  logger,
-		watcher: watcher,
-		ctx:     ctx,
-		stop:    cancel,
-		emitter: emitter,
-	}
-
-	p.startWatching()
-
-	return p, nil
-}
-
-func (p *PackageWatcher) Stop() {
-	p.stop()
-}
-
-func (p *PackageWatcher) AddPackages(pkgs ...gnomod.Pkg) error {
-	for _, pkg := range pkgs {
-		dir := pkg.Dir
-
-		abs, err := filepath.Abs(dir)
-		if err != nil {
-			return fmt.Errorf("unable to get absolute path of %q: %w", dir, err)
-		}
-
-		// Find the correct insertion point using sorting search
-		index := sort.Search(len(p.pkgs), func(i int) bool {
-			return len(p.pkgs[i]) <= len(dir)
-		})
-
-		// Check if the string already exists at the insertion point
-		if index < len(p.pkgs) && (p.pkgs)[index] == dir {
-			continue // Skip as it's a duplicate
-		}
-
-		// Add the pakcage to the watcher
-		if err := p.watcher.Add(abs); err != nil {
-			return fmt.Errorf("unable to watch %q: %w", pkg.Dir, err)
-		}
-	}
-
-	return nil
-}
-
-func (p *PackageWatcher) startWatching() {
-	const timeout = time.Millisecond * 500
-
-	cerrs := make(chan error, 1)
-
-	cwatch := make(chan PackageUpdateList)
-
-	go func() {
-		defer close(cerrs)
-		defer close(cwatch)
-
-		var debounceTimer <-chan time.Time
-		var pathList = []string{}
-		var err error
-
-		for err == nil {
-			select {
-			case <-p.ctx.Done():
-				err = p.ctx.Err()
-			case watchErr := <-p.watcher.Errors:
-				err = fmt.Errorf("watch error: %w", watchErr)
-			case <-debounceTimer:
-				updates := p.generatePackagesUpdateList(pathList)
-				for _, update := range updates {
-					p.logger.Error("packages update",
-						"pkg", update.Package,
-						"files", update.Files,
-					)
-				}
-				panic(fmt.Sprintf("%+v", updates))
-
-				cwatch <- updates
-
-				// Notify that we have some packages update
-				p.emitter.Emit(events.NewPackagesUpdateEvent(updates))
-
-				// Reset pathList and debounceTimer
-				pathList = []string{}
-				debounceTimer = nil
-			case evt := <-p.watcher.Events:
-				if evt.Op != fsnotify.Write {
-					continue
-				}
-
-				pathList = append(pathList, evt.Name)
-				debounceTimer = time.After(timeout)
-			}
-		}
-
-		cerrs <- err
-	}()
-
-	p.PackagesUpdate = cwatch
-	p.Errors = cerrs
-}
-
-func (p *PackageWatcher) generatePackagesUpdateList(paths []string) PackageUpdateList {
-	pkgsUpdate := []events.PackageUpdate{}
-
-	mpkgs := map[string]*events.PackageUpdate{} // pkg -> update
-	for _, path := range paths {
-		for _, pkg := range p.pkgs {
-			if !strings.HasPrefix(pkg, path) {
-				continue
-			}
-
-			pkgu, ok := mpkgs[pkg]
-			if !ok {
-				pkgsUpdate = append(pkgsUpdate, events.PackageUpdate{
-					Package: pkg,
-					Files:   []string{},
-				})
-				pkgu = &pkgsUpdate[len(pkgsUpdate)-1]
-			}
-
-			if len(pkg) == len(path) {
-				continue
-			}
-
-			pkgu.Files = append(pkgu.Files, path)
-		}
-	}
-
-	return pkgsUpdate
 }
