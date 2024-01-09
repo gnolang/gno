aa62473aafb2d99a14fbb9d74c282a678193d42f
irreverentsimplicity <irreverentsimplicity@proton.me>
2024-01-09 05:24:02 -0500

    remove custom grc721 references from trunk

--- examples/flippandoDeploy.sh
+++ examples/flippandoDeploy.sh
@@ -2,32 +2,6 @@
 
 #/bin/bash
 
-# echo "Deploying math_rand to math_rand..."
-
-# gnokey maketx addpkg  \
-# -deposit="1ugnot" \
-# -gas-fee="1ugnot" \
-# -gas-wanted="6000000" \
-# -broadcast="true" \
-# -remote="localhost:26657" \
-# -chainid="dev" \
-# -pkgdir="gno.land/p/demo/math_rand" \
-# -pkgpath="gno.land/p/demo/math_rand" \
-# test
-
-echo "Deploying grc721f to grc721f..."
-
-gnokey maketx addpkg  \
--deposit="1ugnot" \
--gas-fee="1ugnot" \
--gas-wanted="6000000" \
--broadcast="true" \
--remote="localhost:26657" \
--chainid="dev" \
--pkgdir="gno.land/p/demo/grc/grc721f" \
--pkgpath="gno.land/p/demo/grc/grc721f" \
-test
-
 echo "Deploying flippandoserver..."
 
 gnokey maketx addpkg  \
