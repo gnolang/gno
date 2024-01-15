5ad12cd2b668607f291ffa4787f9184e59970ca9
Guilhem Fanton <8671905+gfanton@users.noreply.github.com>
2024-01-15 19:31:14 +0100

    feat: remove `gnovm` and `gno.land` dependencies from tm2 (#1483)
    This PR shares the same goal as #1438: to make `tm2` completely unaware
    of `gnovm` and `gno.land`. However, it adopts a different strategy to
    achieve this, following
    https://github.com/gnolang/gno/pull/1438#issuecomment-1866686822. It
    will:
    - Exposed almost everything from the `tm2/crypto/keys/client` package:
    - `NewCommandXXX`, along with their specific structure configurations
    and fields, are now exposed to allow the creation of composable CLI
    tools.
    - `XXXHandler` (including Sign, Verify, Broadcast, etc.) are now exposed
    to enable external packages to reuse some logic from the `keys/client`
    package.
    - Moved specific `gnovm`/`gno.land` commands to the `gnokey` package:
    `run`, `call`, and `addpkg`.
    - In an effort to avoid duplicate code, an `ExecSignAndBroadcast` method
    has been exposed. However, it appears too specific, and I am considering
    duplicating it regardless. I would appreciate thoughts on this.
    
    
    
    
    <details><summary>Contributors' checklist...</summary>
    
    - [ ] Added new tests, or not needed, or not feasible
    - [ ] Provided an example (e.g. screenshot) to aid review or the PR is
    self-explanatory
    - [ ] Updated the official documentation or not needed
    - [ ] No breaking changes were made, or a `BREAKING CHANGE: xxx` message
    was included in the description
    - [ ] Added references to related issues and PRs
    - [ ] Provided any useful hints for running manual tests
    - [ ] Added new benchmarks to [generated
    graphs](https://gnoland.github.io/benchmarks), if any. More info
    [here](https://github.com/gnolang/gno/blob/master/.benchmarks/README.md).
    </details>
    
    ---------
    
    Signed-off-by: gfanton <8671905+gfanton@users.noreply.github.com>

--- tm2/pkg/crypto/keys/client/maketx.go
+++ tm2/pkg/crypto/keys/client/maketx.go
@@ -2,24 +2,30 @@ package client
 
 import (
 	"flag"
+	"fmt"
 
+	"github.com/gnolang/gno/tm2/pkg/amino"
+	types "github.com/gnolang/gno/tm2/pkg/bft/rpc/core/types"
 	"github.com/gnolang/gno/tm2/pkg/commands"
+	"github.com/gnolang/gno/tm2/pkg/crypto/keys"
+	"github.com/gnolang/gno/tm2/pkg/errors"
+	"github.com/gnolang/gno/tm2/pkg/std"
 )
 
-type makeTxCfg struct {
-	rootCfg *baseCfg
+type MakeTxCfg struct {
+	RootCfg *BaseCfg
 
-	gasWanted int64
-	gasFee    string
-	memo      string
+	GasWanted int64
+	GasFee    string
+	Memo      string
 
-	broadcast bool
-	chainID   string
+	Broadcast bool
+	ChainID   string
 }
 
-func newMakeTxCmd(rootCfg *baseCfg, io commands.IO) *commands.Command {
-	cfg := &makeTxCfg{
-		rootCfg: rootCfg,
+func NewMakeTxCmd(rootCfg *BaseCfg, io commands.IO) *commands.Command {
+	cfg := &MakeTxCfg{
+		RootCfg: rootCfg,
 	}
 
 	cmd := commands.NewCommand(
@@ -33,48 +39,148 @@ func newMakeTxCmd(rootCfg *baseCfg, io commands.IO) *commands.Command {
 	)
 
 	cmd.AddSubCommands(
-		newAddPkgCmd(cfg, io),
-		newSendCmd(cfg, io),
-		newCallCmd(cfg, io),
-		newRunCmd(cfg, io),
+		NewMakeSendCmd(cfg, io),
 	)
 
 	return cmd
 }
 
-func (c *makeTxCfg) RegisterFlags(fs *flag.FlagSet) {
+func (c *MakeTxCfg) RegisterFlags(fs *flag.FlagSet) {
 	fs.Int64Var(
-		&c.gasWanted,
+		&c.GasWanted,
 		"gas-wanted",
 		0,
 		"gas requested for tx",
 	)
 
 	fs.StringVar(
-		&c.gasFee,
+		&c.GasFee,
 		"gas-fee",
 		"",
 		"gas payment fee",
 	)
 
 	fs.StringVar(
-		&c.memo,
+		&c.Memo,
 		"memo",
 		"",
 		"any descriptive text",
 	)
 
 	fs.BoolVar(
-		&c.broadcast,
+		&c.Broadcast,
 		"broadcast",
 		false,
 		"sign and broadcast",
 	)
 
 	fs.StringVar(
-		&c.chainID,
+		&c.ChainID,
 		"chainid",
 		"dev",
 		"chainid to sign for (only useful if --broadcast)",
 	)
+}
+
+func SignAndBroadcastHandler(
+	cfg *MakeTxCfg,
+	nameOrBech32 string,
+	tx std.Tx,
+	pass string,
+) (*types.ResultBroadcastTxCommit, error) {
+	baseopts := cfg.RootCfg
+	txopts := cfg
+
+	kb, err := keys.NewKeyBaseFromDir(cfg.RootCfg.Home)
+	if err != nil {
+		return nil, err
+	}
+
+	info, err := kb.GetByNameOrAddress(nameOrBech32)
+	if err != nil {
+		return nil, err
+	}
+	accountAddr := info.GetAddress()
+
+	qopts := &QueryCfg{
+		RootCfg: baseopts,
+		Path:    fmt.Sprintf("auth/accounts/%s", accountAddr),
+	}
+	qres, err := QueryHandler(qopts)
+	if err != nil {
+		return nil, errors.Wrap(err, "query account")
+	}
+	var qret struct{ BaseAccount std.BaseAccount }
+	err = amino.UnmarshalJSON(qres.Response.Data, &qret)
+	if err != nil {
+		return nil, err
+	}
+
+	// sign tx
+	accountNumber := qret.BaseAccount.AccountNumber
+	sequence := qret.BaseAccount.Sequence
+	sopts := &SignCfg{
+		Pass:          pass,
+		RootCfg:       baseopts,
+		Sequence:      sequence,
+		AccountNumber: accountNumber,
+		ChainID:       txopts.ChainID,
+		NameOrBech32:  nameOrBech32,
+		TxJSON:        amino.MustMarshalJSON(tx),
+	}
+
+	signedTx, err := SignHandler(sopts)
+	if err != nil {
+		return nil, errors.Wrap(err, "sign tx")
+	}
+
+	// broadcast signed tx
+	bopts := &BroadcastCfg{
+		RootCfg: baseopts,
+		tx:      signedTx,
+	}
+
+	return BroadcastHandler(bopts)
+}
+
+func ExecSignAndBroadcast(
+	cfg *MakeTxCfg,
+	args []string,
+	tx std.Tx,
+	io commands.IO,
+) error {
+	baseopts := cfg.RootCfg
+
+	// query account
+	nameOrBech32 := args[0]
+
+	var err error
+	var pass string
+	if baseopts.Quiet {
+		pass, err = io.GetPassword("", baseopts.InsecurePasswordStdin)
+	} else {
+		pass, err = io.GetPassword("Enter password.", baseopts.InsecurePasswordStdin)
+	}
+
+	if err != nil {
+		return err
+	}
+
+	bres, err := SignAndBroadcastHandler(cfg, nameOrBech32, tx, pass)
+	if err != nil {
+		return errors.Wrap(err, "broadcast tx")
+	}
+	if bres.CheckTx.IsErr() {
+		return errors.Wrap(bres.CheckTx.Error, "check transaction failed: log:%s", bres.CheckTx.Log)
+	}
+	if bres.DeliverTx.IsErr() {
+		return errors.Wrap(bres.DeliverTx.Error, "deliver transaction failed: log:%s", bres.DeliverTx.Log)
+	}
+
+	io.Println(string(bres.DeliverTx.Data))
+	io.Println("OK!")
+	io.Println("GAS WANTED:", bres.DeliverTx.GasWanted)
+	io.Println("GAS USED:  ", bres.DeliverTx.GasUsed)
+
+	return nil
 }
