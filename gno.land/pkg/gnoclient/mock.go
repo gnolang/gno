23fb5b4685239a83a9f2f0aed8eab818ee2a3d19
leohhhn <hudakleon@gmail.com>
2024-01-31 17:09:09 +0100

    add full rpcclient mock infra

--- gno.land/pkg/gnoclient/mock.go
+++ gno.land/pkg/gnoclient/mock.go
@@ -71,22 +71,57 @@ func (m mockKeysInfo) GetPath() (*hd.BIP44Params, error) {
 	return nil, nil
 }
 
-// RPC Client
+// RPC Client mock
 type (
-	mockBroadcastTxCommit func(tx types.Tx) (*ctypes.ResultBroadcastTxCommit, error)
-	mockABCIQuery         func(path string, data []byte) (*ctypes.ResultABCIQuery, error)
+	mockBroadcastTxCommit    func(tx types.Tx) (*ctypes.ResultBroadcastTxCommit, error)
+	mockABCIQuery            func(path string, data []byte) (*ctypes.ResultABCIQuery, error)
+	mockABCIInfo             func() (*ctypes.ResultABCIInfo, error)
+	mockABCIQueryWithOptions func(path string, data []byte, opts client.ABCIQueryOptions) (*ctypes.ResultABCIQuery, error)
+	mockBroadcastTxAsync     func(tx types.Tx) (*ctypes.ResultBroadcastTx, error)
+	mockBroadcastTxSync      func(tx types.Tx) (*ctypes.ResultBroadcastTx, error)
+	mockGenesis              func() (*ctypes.ResultGenesis, error)
+	mockBlockchainInfo       func(minHeight, maxHeight int64) (*ctypes.ResultBlockchainInfo, error)
+	mockNetInfo              func() (*ctypes.ResultNetInfo, error)
+	mockDumpConsensusState   func() (*ctypes.ResultDumpConsensusState, error)
+	mockConsensusState       func() (*ctypes.ResultConsensusState, error)
+	mockConsensusParams      func(height *int64) (*ctypes.ResultConsensusParams, error)
+	mockHealth               func() (*ctypes.ResultHealth, error)
+	mockBlock                func(height *int64) (*ctypes.ResultBlock, error)
+	mockBlockResults         func(height *int64) (*ctypes.ResultBlockResults, error)
+	mockCommit               func(height *int64) (*ctypes.ResultCommit, error)
+	mockValidators           func(height *int64) (*ctypes.ResultValidators, error)
+	mockStatus               func() (*ctypes.ResultStatus, error)
+	mockUnconfirmedTxs       func(limit int) (*ctypes.ResultUnconfirmedTxs, error)
+	mockNumUnconfirmedTxs    func() (*ctypes.ResultUnconfirmedTxs, error)
 )
 
 type mockRPCClient struct {
-	broadcastTxCommit mockBroadcastTxCommit
-	abciQuery         mockABCIQuery
+	broadcastTxCommit    mockBroadcastTxCommit
+	abciQuery            mockABCIQuery
+	abciInfo             mockABCIInfo
+	abciQueryWithOptions mockABCIQueryWithOptions
+	broadcastTxAsync     mockBroadcastTxAsync
+	broadcastTxSync      mockBroadcastTxSync
+	genesis              mockGenesis
+	blockchainInfo       mockBlockchainInfo
+	netInfo              mockNetInfo
+	dumpConsensusState   mockDumpConsensusState
+	consensusState       mockConsensusState
+	consensusParams      mockConsensusParams
+	health               mockHealth
+	block                mockBlock
+	blockResults         mockBlockResults
+	commit               mockCommit
+	validators           mockValidators
+	status               mockStatus
+	unconfirmedTxs       mockUnconfirmedTxs
+	numUnconfirmedTxs    mockNumUnconfirmedTxs
 }
 
 func (m mockRPCClient) BroadcastTxCommit(tx types.Tx) (*ctypes.ResultBroadcastTxCommit, error) {
 	if m.broadcastTxCommit != nil {
 		return m.broadcastTxCommit(tx)
 	}
-
 	return nil, nil
 }
 
@@ -97,76 +132,128 @@ func (m mockRPCClient) ABCIQuery(path string, data []byte) (*ctypes.ResultABCIQu
 	return nil, nil
 }
 
-// Unused RPC Client functions
-
 func (m mockRPCClient) ABCIInfo() (*ctypes.ResultABCIInfo, error) {
-	panic("implement me")
+	if m.abciInfo != nil {
+		return m.ABCIInfo()
+	}
+	return nil, nil
 }
 
 func (m mockRPCClient) ABCIQueryWithOptions(path string, data []byte, opts client.ABCIQueryOptions) (*ctypes.ResultABCIQuery, error) {
-	panic("implement me")
+	if m.abciQueryWithOptions != nil {
+		return m.abciQueryWithOptions(path, data, opts)
+	}
+	return nil, nil
 }
 
 func (m mockRPCClient) BroadcastTxAsync(tx types.Tx) (*ctypes.ResultBroadcastTx, error) {
-	panic("implement me")
+	if m.broadcastTxAsync != nil {
+		return m.broadcastTxAsync(tx)
+	}
+	return nil, nil
 }
 
 func (m mockRPCClient) BroadcastTxSync(tx types.Tx) (*ctypes.ResultBroadcastTx, error) {
-	panic("implement me")
+	if m.broadcastTxSync != nil {
+		return m.broadcastTxSync(tx)
+	}
+	return nil, nil
 }
 
 func (m mockRPCClient) Genesis() (*ctypes.ResultGenesis, error) {
-	panic("implement me")
+	if m.genesis != nil {
+		return m.genesis()
+	}
+	return nil, nil
 }
 
 func (m mockRPCClient) BlockchainInfo(minHeight, maxHeight int64) (*ctypes.ResultBlockchainInfo, error) {
-	panic("implement me")
+	if m.blockchainInfo != nil {
+		return m.blockchainInfo(minHeight, maxHeight)
+	}
+	return nil, nil
 }
 
 func (m mockRPCClient) NetInfo() (*ctypes.ResultNetInfo, error) {
-	panic("implement me")
+	if m.netInfo != nil {
+		return m.netInfo()
+	}
+	return nil, nil
 }
 
 func (m mockRPCClient) DumpConsensusState() (*ctypes.ResultDumpConsensusState, error) {
-	panic("implement me")
+	if m.dumpConsensusState != nil {
+		return m.dumpConsensusState()
+	}
+	return nil, nil
 }
 
 func (m mockRPCClient) ConsensusState() (*ctypes.ResultConsensusState, error) {
-	panic("implement me")
+	if m.consensusState != nil {
+		return m.consensusState()
+	}
+	return nil, nil
 }
 
 func (m mockRPCClient) ConsensusParams(height *int64) (*ctypes.ResultConsensusParams, error) {
-	panic("implement me")
+	if m.consensusParams != nil {
+		return m.consensusParams(height)
+	}
+	return nil, nil
 }
 
 func (m mockRPCClient) Health() (*ctypes.ResultHealth, error) {
-	panic("implement me")
+	if m.health != nil {
+		return m.health()
+	}
+	return nil, nil
 }
 
 func (m mockRPCClient) Block(height *int64) (*ctypes.ResultBlock, error) {
-	panic("implement me")
+	if m.block != nil {
+		return m.block(height)
+	}
+	return nil, nil
 }
 
 func (m mockRPCClient) BlockResults(height *int64) (*ctypes.ResultBlockResults, error) {
-	panic("implement me")
+	if m.blockResults != nil {
+		return m.blockResults(height)
+	}
+	return nil, nil
 }
 
 func (m mockRPCClient) Commit(height *int64) (*ctypes.ResultCommit, error) {
-	panic("implement me")
+	if m.commit != nil {
+		return m.commit(height)
+	}
+	return nil, nil
 }
 
 func (m mockRPCClient) Validators(height *int64) (*ctypes.ResultValidators, error) {
-	panic("implement me")
+	if m.validators != nil {
+		return m.validators(height)
+	}
+	return nil, nil
 }
 
 func (m mockRPCClient) Status() (*ctypes.ResultStatus, error) {
-	panic("implement me")
+	if m.status != nil {
+		return m.status()
+	}
+	return nil, nil
 }
 
 func (m mockRPCClient) UnconfirmedTxs(limit int) (*ctypes.ResultUnconfirmedTxs, error) {
-	panic("implement me")
+	if m.unconfirmedTxs != nil {
+		return m.unconfirmedTxs(limit)
+	}
+	return nil, nil
 }
 
 func (m mockRPCClient) NumUnconfirmedTxs() (*ctypes.ResultUnconfirmedTxs, error) {
-	panic("implement me")
+	if m.numUnconfirmedTxs != nil {
+		return m.numUnconfirmedTxs()
+	}
+	return nil, nil
 }
