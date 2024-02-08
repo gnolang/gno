1ea0510da392b05ca9d01d9911e3646c2a1fe3fe
leohhhn <hudakleon@gmail.com>
2024-02-08 22:37:06 +0100

    make fmt

--- gno.land/pkg/gnoclient/integration_test.go
+++ gno.land/pkg/gnoclient/integration_test.go
@@ -1,8 +1,9 @@
 package gnoclient
 
 import (
-	"github.com/gnolang/gno/tm2/pkg/std"
 	"testing"
+
+	"github.com/gnolang/gno/tm2/pkg/std"
 
 	"github.com/gnolang/gno/gno.land/pkg/integration"
 	"github.com/gnolang/gno/gnovm/pkg/gnoenv"
