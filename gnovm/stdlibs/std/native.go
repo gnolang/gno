072c9a824569cf3e47fee3bceea78715da67493c
Morgan Bazalgette <morgan@morganbaz.com>
2024-02-22 21:09:00 +0100

    begin removing support for linked identifiers

--- gnovm/stdlibs/std/native.go
+++ gnovm/stdlibs/std/native.go
@@ -1,12 +1,9 @@
 package std
 
 import (
-	"reflect"
-
 	gno "github.com/gnolang/gno/gnovm/pkg/gnolang"
 	"github.com/gnolang/gno/tm2/pkg/bech32"
 	"github.com/gnolang/gno/tm2/pkg/crypto"
-	"github.com/gnolang/gno/tm2/pkg/std"
 )
 
 func AssertOriginCall(m *gno.Machine) {
@@ -34,82 +31,26 @@ func GetHeight(m *gno.Machine) int64 {
 	return m.Context.(ExecContext).Height
 }
 
-func GetOrigSend(m *gno.Machine) std.Coins {
-	return m.Context.(ExecContext).OrigSend
+func X_origSend(m *gno.Machine) (denoms []string, amounts []int64) {
+	os := m.Context.(ExecContext).OrigSend
+	denoms = make([]string, len(os))
+	amounts = make([]int64, len(os))
+	for i, coin := range os {
+		denoms[i] = coin.Denom
+		amounts[i] = coin.Amount
+	}
+	return denoms, amounts
 }
 
-func GetOrigCaller(m *gno.Machine) crypto.Bech32Address {
-	return m.Context.(ExecContext).OrigCaller
+func X_origCaller(m *gno.Machine) string {
+	return string(m.Context.(ExecContext).OrigCaller)
 }
 
-func CurrentRealm(m *gno.Machine) Realm {
-	var (
-		ctx = m.Context.(ExecContext)
-		// Default lastCaller is OrigCaller, the signer of the tx
-		lastCaller  = ctx.OrigCaller
-		lastPkgPath = ""
-	)
-
-	for i := m.NumFrames() - 1; i > 0; i-- {
-		fr := m.Frames[i]
-		if fr.LastPackage != nil && fr.LastPackage.IsRealm() {
-			lastCaller = fr.LastPackage.GetPkgAddr().Bech32()
-			lastPkgPath = fr.LastPackage.PkgPath
-			break
-		}
-	}
-
-	return Realm{
-		addr:    lastCaller,
-		pkgPath: lastPkgPath,
-	}
+func X_origPkgAddr(m *gno.Machine) string {
+	return string(m.Context.(ExecContext).OrigPkgAddr)
 }
 
-func PrevRealm(m *gno.Machine) Realm {
-	var (
-		ctx = m.Context.(ExecContext)
-		// Default lastCaller is OrigCaller, the signer of the tx
-		lastCaller  = ctx.OrigCaller
-		lastPkgPath = ""
-	)
-
-	for i := m.NumFrames() - 1; i > 0; i-- {
-		fr := m.Frames[i]
-		if fr.LastPackage == nil || !fr.LastPackage.IsRealm() {
-			// Ignore non-realm frame
-			continue
-		}
-		pkgPath := fr.LastPackage.PkgPath
-		// The first realm we encounter will be the one calling
-		// this function; to get the calling realm determine the first frame
-		// where fr.LastPackage changes.
-		if lastPkgPath == "" {
-			lastPkgPath = pkgPath
-		} else if lastPkgPath == pkgPath {
-			continue
-		} else {
-			lastCaller = fr.LastPackage.GetPkgAddr().Bech32()
-			lastPkgPath = pkgPath
-			break
-		}
-	}
-
-	// Empty the pkgPath if we return a user
-	if ctx.OrigCaller == lastCaller {
-		lastPkgPath = ""
-	}
-
-	return Realm{
-		addr:    lastCaller,
-		pkgPath: lastPkgPath,
-	}
-}
-
-func GetOrigPkgAddr(m *gno.Machine) crypto.Bech32Address {
-	return m.Context.(ExecContext).OrigPkgAddr
-}
-
-func GetCallerAt(m *gno.Machine, n int) crypto.Bech32Address {
+func X_callerAt(m *gno.Machine, n int) string {
 	if n <= 0 {
 		m.Panic(typedString("GetCallerAt requires positive arg"))
 		return ""
@@ -124,52 +65,57 @@ func GetCallerAt(m *gno.Machine, n int) crypto.Bech32Address {
 	if n == m.NumFrames() {
 		// This makes it consistent with GetOrigCaller.
 		ctx := m.Context.(ExecContext)
-		return ctx.OrigCaller
+		return string(ctx.OrigCaller)
 	}
-	return m.LastCallFrame(n).LastPackage.GetPkgAddr().Bech32()
+	return string(m.LastCallFrame(n).LastPackage.GetPkgAddr().Bech32())
 }
 
-func GetBanker(m *gno.Machine, bankerType BankerType) gno.TypedValue {
-	ctx := m.Context.(ExecContext)
-	banker := ctx.Banker
-	switch bankerType {
-	case BankerTypeReadonly:
-		banker = NewReadonlyBanker(banker)
-	case BankerTypeOrigSend:
-		banker = NewOrigSendBanker(banker, ctx.OrigPkgAddr, ctx.OrigSend, ctx.OrigSendSpent)
-	case BankerTypeRealmSend:
-		banker = NewRealmSendBanker(banker, ctx.OrigPkgAddr)
-	case BankerTypeRealmIssue:
-		banker = banker
-	default:
-		panic("should not happen") // defensive
+func X_getRealm(m *gno.Machine, height int) (address string, pkgPath string) {
+	var (
+		ctx           = m.Context.(ExecContext)
+		currentCaller crypto.Bech32Address
+		// Keeps track of the number of times currentCaller
+		// has changed.
+		changes int
+	)
+
+	for i := m.NumFrames() - 1; i > 0; i-- {
+		fr := m.Frames[i]
+		if fr.LastPackage == nil || !fr.LastPackage.IsRealm() {
+			continue
+		}
+
+		// LastPackage is a realm. Get caller and pkgPath, and compare against
+		// current* values.
+		caller := fr.LastPackage.GetPkgAddr().Bech32()
+		pkgPath := fr.LastPackage.PkgPath
+		if caller != currentCaller {
+			if changes == height {
+				return string(caller), pkgPath
+			}
+			currentCaller = caller
+			changes++
+		}
 	}
-	m.Alloc.AllocateStruct()         // defensive; native space not allocated.
-	m.Alloc.AllocateStructFields(10) // defensive 10; native space not allocated.
 
-	// make gno bankAdapter{rv}
-	btv := gno.Go2GnoNativeValue(m.Alloc, reflect.ValueOf(banker))
-	bsv := m.Alloc.NewStructWithFields(btv)
-	bankAdapterType := m.Store.GetType(gno.DeclaredTypeID("std", "bankAdapter"))
-	res0 := gno.TypedValue{T: bankAdapterType, V: bsv}
-
-	return res0
+	// Fallback case: return OrigCaller.
+	return string(ctx.OrigCaller), ""
 }
 
-func EncodeBech32(prefix string, bytes [20]byte) crypto.Bech32Address {
+func X_derivePkgAddr(pkgPath string) string {
+	return string(gno.DerivePkgAddr(pkgPath).Bech32())
+}
+
+func X_encodeBech32(prefix string, bytes [20]byte) string {
 	b32, err := bech32.ConvertAndEncode(prefix, bytes[:])
 	if err != nil {
 		panic(err) // should not happen
 	}
-	return crypto.Bech32Address(b32)
+	return b32
 }
 
-func DerivePkgAddr(pkgPath string) crypto.Bech32Address {
-	return gno.DerivePkgAddr(pkgPath).Bech32()
-}
-
-func DecodeBech32(addr crypto.Bech32Address) (prefix string, bytes [20]byte, ok bool) {
-	prefix, bz, err := bech32.Decode(string(addr))
+func X_decodeBech32(addr string) (prefix string, bytes [20]byte, ok bool) {
+	prefix, bz, err := bech32.Decode(addr)
 	if err != nil || len(bz) != 20 {
 		return "", [20]byte{}, false
 	}
