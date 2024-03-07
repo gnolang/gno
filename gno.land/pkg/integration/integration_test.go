758977211dc9e9658a32da9d63db22c13b000155
gfanton <8671905+gfanton@users.noreply.github.com>
2024-03-07 11:23:10 +0100

    chore: duplicate testify import
    Signed-off-by: gfanton <8671905+gfanton@users.noreply.github.com>

--- gno.land/pkg/integration/integration_test.go
+++ gno.land/pkg/integration/integration_test.go
@@ -4,8 +4,8 @@ import (
 	"strings"
 	"testing"
 
-	"github.com/jaekwon/testify/require"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
 func TestTestdata(t *testing.T) {
