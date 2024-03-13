a69768ee4601a244f2698111ba14c3d44ac277ee
Morgan Bazalgette <morgan@morganbaz.com>
2024-03-13 19:34:31 +0100

    add maxwell tests

--- gnovm/tests/file_test.go
+++ gnovm/tests/file_test.go
@@ -32,6 +32,13 @@ func TestFiles(t *testing.T) {
 	runFileTests(t, baseDir, []string{"*_native*"})
 }
 
+func TestDebug(t *testing.T) {
+	// baseDir := filepath.Join(".", "debug2")
+	baseDir := filepath.Join(".", "debug")
+	runFileTests(t, baseDir, []string{"*_native*"})
+	// runFileTests(t, baseDir, []string{"*_stdlibs*"}, WithNativeLibs())
+}
+
 func TestChallenges(t *testing.T) {
 	baseDir := filepath.Join(".", "challenges")
 	runFileTests(t, baseDir, nil)
