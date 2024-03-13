e3162e6d3d6d1bed196839ac66e77b625449541d
Morgan Bazalgette <morgan@morganbaz.com>
2024-03-13 22:26:00 +0100

    remove go2gnoMap

--- gnovm/pkg/gnolang/store.go
+++ gnovm/pkg/gnolang/store.go
@@ -75,7 +75,6 @@ type defaultStore struct {
 	iavlStore        store.Store           // for escaped object hashes
 	pkgInjector      PackageInjector       // for injecting natives
 	nativeStore      NativeStore           // for injecting natives
-	go2gnoMap        map[string]string     // go pkgpath.name -> gno pkgpath.name
 	go2gnoStrict     bool                  // if true, native->gno type conversion must be registered.
 
 	// transient
@@ -93,7 +92,6 @@ func NewStore(alloc *Allocator, baseStore, iavlStore store.Store) *defaultStore 
 		cacheNativeTypes: make(map[reflect.Type]Type),
 		baseStore:        baseStore,
 		iavlStore:        iavlStore,
-		go2gnoMap:        make(map[string]string),
 		go2gnoStrict:     true,
 		current:          make(map[string]struct{}),
 	}
@@ -607,7 +605,6 @@ func (ds *defaultStore) Fork() Store {
 		iavlStore:        ds.iavlStore,
 		pkgInjector:      ds.pkgInjector,
 		nativeStore:      ds.nativeStore,
-		go2gnoMap:        ds.go2gnoMap,
 		go2gnoStrict:     ds.go2gnoStrict,
 		opslog:           nil, // new ops log.
 		current:          make(map[string]struct{}),
