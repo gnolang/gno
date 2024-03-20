7da7f3874e65ad1a49db4560056254e5a32e6404
Morgan Bazalgette <morgan@morganbaz.com>
2024-03-20 18:23:48 +0100

    use ff options instead of manually writing out env vars

--- misc/loop/cmd/cmd_backup.go
+++ misc/loop/cmd/cmd_backup.go
@@ -14,33 +14,20 @@ type backupCfg struct {
 	traefikGnoFile string
 	backupDir      string
 	hostPWD        string
+	promAddr       string
 }
 
 func (c *backupCfg) RegisterFlags(fs *flag.FlagSet) {
-	if os.Getenv("HOST_PWD") == "" {
-		os.Setenv("HOST_PWD", os.Getenv("PWD"))
+	wd, err := os.Getwd()
+	if err != nil {
+		panic(err)
 	}
 
-	if os.Getenv("BACKUP_DIR") == "" {
-		os.Setenv("BACKUP_DIR", "./backups")
-	}
-
-	if os.Getenv("RPC_URL") == "" {
-		os.Setenv("RPC_URL", "http://rpc.portal.gno.local:26657")
-	}
-
-	if os.Getenv("PROM_ADDR") == "" {
-		os.Setenv("PROM_ADDR", ":9090")
-	}
-
-	if os.Getenv("TRAEFIK_GNO_FILE") == "" {
-		os.Setenv("TRAEFIK_GNO_FILE", "./traefik/gno.yml")
-	}
-
-	fs.StringVar(&c.rpcAddr, "rpc", os.Getenv("RPC_URL"), "tendermint rpc url")
-	fs.StringVar(&c.traefikGnoFile, "traefik-gno-file", os.Getenv("TRAEFIK_GNO_FILE"), "traefik gno file")
-	fs.StringVar(&c.backupDir, "backup-dir", os.Getenv("BACKUP_DIR"), "backup directory")
-	fs.StringVar(&c.hostPWD, "pwd", os.Getenv("HOST_PWD"), "host pwd (for docker usage)")
+	fs.StringVar(&c.rpcAddr, "rpc-url", "http://rpc.portal.gno.local:26657", "tendermint rpc url")
+	fs.StringVar(&c.traefikGnoFile, "traefik-gno-file", "./traefik/gno.yml", "traefik gno file")
+	fs.StringVar(&c.backupDir, "backup-dir", "./backups", "backup directory")
+	fs.StringVar(&c.hostPWD, "host-pwd", wd, "host pwd (for docker usage)")
+	fs.StringVar(&c.promAddr, "prom-addr", ":9090", "listening address for prometheus exporter")
 }
 
 func newBackupCmd(io commands.IO) *commands.Command {
