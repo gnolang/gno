ceab4a4ca090efa5579b5539e56baad2523b0c62
Hariom Verma <hariom18599@gmail.com>
2024-03-22 14:46:24 +0530

    Test rePkgOrRlmPath

--- tm2/pkg/std/memfile_test.go
+++ tm2/pkg/std/memfile_test.go
@@ -51,3 +51,29 @@ func TestMemPackage_Validate(t *testing.T) {
 		})
 	}
 }
+
+func TestRePkgOrRlmPath(t *testing.T) {
+	t.Parallel()
+
+	for _, tc := range []struct {
+		desc, in string
+		expected bool
+	}{
+		{"Valid p", "gno.land/p/path/path", true},
+		{"Valid r", "gno.land/r/path/path", true},
+		{"Invalid x", "gno.land/x/path/path", false},
+		{"Special Character 1", "gno.land/p/p@th/abc/def", false},   // fails
+		{"Special Character 2", "gno.land/p/p&th/abc/def", false},   // fails
+		{"Special Character 3", "gno.land/p/p&%$#h/abc/def", false}, // fails
+		{"Leading Number", "gno.land/p/1Path/abc/def", false},
+		{"Uppercase Letters", "gno.land/p/PaTh/abc/def", false},
+		{"Empty Path Part", "gno.land/p/path//def", false},     // fails
+		{"Trailing Slash", "gno.land/p/path/abc/def/", false},  // fails
+		{"Extra Slash(s)", "gno.land/p/path///abc/def", false}, // fails
+	} {
+		t.Run(tc.desc, func(t *testing.T) {
+			res := rePkgOrRlmPath.MatchString(tc.in)
+			assert.Equal(t, res, tc.expected)
+		})
+	}
+}
