cc634e6eabedc96ee1503367f0b3234f339e55e1
n3wbie <r3v4@onbloc.xyz>
2024-03-26 13:02:43 +0900

    fix: typo

--- tm2/pkg/std/coin.go
+++ tm2/pkg/std/coin.go
@@ -616,7 +616,7 @@ func (coins Coins) Sort() Coins {
 // Parsing
 
 var (
-	reDnmString = `[a-z][a-z0-9/:._-]{2,127}` // cosmos regex(https://github.com/cosmos/cosmos-sdk/blob/b2c26cdc4c3d2f5b7cb28e5153e2b458c61ee8fa/types/coin.go#L852) without captial letters
+	reDnmString = `[a-z][a-z0-9/:._-]{2,127}` // cosmos regex(https://github.com/cosmos/cosmos-sdk/blob/b2c26cdc4c3d2f5b7cb28e5153e2b458c61ee8fa/types/coin.go#L852) without capital letters
 	reAmt       = `[[:digit:]]+`
 	reSpc       = `[[:space:]]*`
 	reDnm       = regexp.MustCompile(fmt.Sprintf(`^%s$`, reDnmString))
