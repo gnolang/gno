aadd296827541d3c45d44c4eaf6c8adf706ec5df
Milos Zivkovic <milos.zivkovic@tendermint.com>
2024-04-01 10:44:53 +0200

    Add unit tests for bech32 addition

--- tm2/pkg/crypto/keys/client/add_test.go
+++ tm2/pkg/crypto/keys/client/add_test.go
@@ -2,15 +2,15 @@ package client
 
 import (
 	"bytes"
+	"context"
 	"fmt"
 	"strings"
 	"testing"
+	"time"
 
 	"github.com/gnolang/gno/tm2/pkg/commands"
 	"github.com/gnolang/gno/tm2/pkg/crypto/bip39"
 	"github.com/gnolang/gno/tm2/pkg/crypto/keys"
-	"github.com/gnolang/gno/tm2/pkg/crypto/secp256k1"
-	"github.com/gnolang/gno/tm2/pkg/testutils"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
@@ -34,19 +34,192 @@ func TestAdd_Base_Add(t *testing.T) {
 	t.Run("valid key addition, generated mnemonic", func(t *testing.T) {
 		t.Parallel()
 
-		// TODO
+		var (
+			kbHome      = t.TempDir()
+			baseOptions = BaseOptions{
+				InsecurePasswordStdin: true,
+				Home:                  kbHome,
+			}
+
+			keyName = "key-name"
+		)
+
+		ctx, cancelFn := context.WithTimeout(context.Background(), 5*time.Second)
+		defer cancelFn()
+
+		io := commands.NewTestIO()
+		io.SetIn(strings.NewReader("test1234\ntest1234\n"))
+
+		// Create the command
+		cmd := NewRootCmdWithBaseConfig(io, baseOptions)
+
+		args := []string{
+			"add",
+			"--insecure-password-stdin",
+			"--home",
+			kbHome,
+			keyName,
+		}
+
+		require.NoError(t, cmd.ParseAndRun(ctx, args))
+
+		// Check the keybase
+		kb, err := keys.NewKeyBaseFromDir(kbHome)
+		require.NoError(t, err)
+
+		original, err := kb.GetByName(keyName)
+		require.NoError(t, err)
+		require.NotNil(t, original)
+	})
+
+	t.Run("valid key addition, overwrite", func(t *testing.T) {
+		t.Parallel()
+
+		var (
+			kbHome      = t.TempDir()
+			baseOptions = BaseOptions{
+				InsecurePasswordStdin: true,
+				Home:                  kbHome,
+			}
+
+			keyName = "key-name"
+		)
+
+		ctx, cancelFn := context.WithTimeout(context.Background(), 5*time.Second)
+		defer cancelFn()
+
+		io := commands.NewTestIO()
+		io.SetIn(strings.NewReader("test1234\ntest1234\n"))
+
+		// Create the command
+		cmd := NewRootCmdWithBaseConfig(io, baseOptions)
+
+		args := []string{
+			"add",
+			"--insecure-password-stdin",
+			"--home",
+			kbHome,
+			keyName,
+		}
+
+		require.NoError(t, cmd.ParseAndRun(ctx, args))
+
+		// Check the keybase
+		kb, err := keys.NewKeyBaseFromDir(kbHome)
+		require.NoError(t, err)
+
+		original, err := kb.GetByName(keyName)
+		require.NoError(t, err)
+
+		io.SetIn(strings.NewReader("y\ntest1234\ntest1234\n"))
+
+		cmd = NewRootCmdWithBaseConfig(io, baseOptions)
+		require.NoError(t, cmd.ParseAndRun(ctx, args))
+
+		newKey, err := kb.GetByName(keyName)
+		require.NoError(t, err)
+
+		// Make sure the different key is generated and overwritten
+		assert.NotEqual(t, original.GetAddress(), newKey.GetAddress())
 	})
 
 	t.Run("valid key addition, provided mnemonic", func(t *testing.T) {
 		t.Parallel()
 
-		// TODO
+		var (
+			kbHome      = t.TempDir()
+			mnemonic    = generateTestMnemonic(t)
+			baseOptions = BaseOptions{
+				InsecurePasswordStdin: true,
+				Home:                  kbHome,
+			}
+
+			keyName = "key-name"
+		)
+
+		ctx, cancelFn := context.WithTimeout(context.Background(), 5*time.Second)
+		defer cancelFn()
+
+		io := commands.NewTestIO()
+		io.SetIn(strings.NewReader("test1234" + "\n" + "test1234" + "\n" + mnemonic + "\n"))
+
+		// Create the command
+		cmd := NewRootCmdWithBaseConfig(io, baseOptions)
+
+		args := []string{
+			"add",
+			"--insecure-password-stdin",
+			"--home",
+			kbHome,
+			"--recover",
+			keyName,
+		}
+
+		require.NoError(t, cmd.ParseAndRun(ctx, args))
+		// Check the keybase
+		kb, err := keys.NewKeyBaseFromDir(kbHome)
+		require.NoError(t, err)
+
+		key, err := kb.GetByName(keyName)
+		require.NoError(t, err)
+		require.NotNil(t, key)
+
+		// Get the account
+		accounts := generateAccounts(mnemonic, []string{"44'/118'/0'/0/0"})
+
+		assert.Equal(t, accounts[0].String(), key.GetAddress().String())
 	})
 
 	t.Run("no overwrite permission", func(t *testing.T) {
 		t.Parallel()
 
-		// TODO
+		var (
+			kbHome      = t.TempDir()
+			baseOptions = BaseOptions{
+				InsecurePasswordStdin: true,
+				Home:                  kbHome,
+			}
+
+			keyName = "key-name"
+		)
+
+		ctx, cancelFn := context.WithTimeout(context.Background(), 5*time.Second)
+		defer cancelFn()
+
+		io := commands.NewTestIO()
+		io.SetIn(strings.NewReader("test1234\ntest1234\n"))
+
+		// Create the command
+		cmd := NewRootCmdWithBaseConfig(io, baseOptions)
+
+		args := []string{
+			"add",
+			"--insecure-password-stdin",
+			"--home",
+			kbHome,
+			keyName,
+		}
+
+		require.NoError(t, cmd.ParseAndRun(ctx, args))
+
+		// Check the keybase
+		kb, err := keys.NewKeyBaseFromDir(kbHome)
+		require.NoError(t, err)
+
+		original, err := kb.GetByName(keyName)
+		require.NoError(t, err)
+
+		io.SetIn(strings.NewReader("n\ntest1234\ntest1234\n"))
+
+		// Confirm overwrite
+		cmd = NewRootCmdWithBaseConfig(io, baseOptions)
+		require.ErrorIs(t, cmd.ParseAndRun(ctx, args), errOverwriteAborted)
+
+		newKey, err := kb.GetByName(keyName)
+		require.NoError(t, err)
+
+		// Make sure the key is not overwritten
+		assert.Equal(t, original.GetAddress(), newKey.GetAddress())
 	})
 }
 
@@ -67,26 +240,20 @@ func TestAdd_Derive(t *testing.T) {
 		t.Parallel()
 
 		var (
+			kbHome   = t.TempDir()
 			mnemonic = generateTestMnemonic(t)
 			paths    = generateDerivationPaths(10)
+
+			baseOptions = BaseOptions{
+				InsecurePasswordStdin: true,
+				Home:                  kbHome,
+			}
 
 			dummyPass = "dummy-pass"
 		)
 
-		kbHome, kbCleanUp := testutils.NewTestCaseDir(t)
-		require.NotNil(t, kbHome)
-		t.Cleanup(kbCleanUp)
-
-		cfg := &AddCfg{
-			RootCfg: &BaseCfg{
-				BaseOptions: BaseOptions{
-					InsecurePasswordStdin: true,
-					Home:                  kbHome,
-				},
-			},
-			Recover:        true,
-			DerivationPath: paths,
-		}
+		ctx, cancelFn := context.WithTimeout(context.Background(), 5*time.Second)
+		defer cancelFn()
 
 		mockOut := bytes.NewBufferString("")
 
@@ -94,15 +261,28 @@ func TestAdd_Derive(t *testing.T) {
 		io.SetIn(strings.NewReader(dummyPass + "\n" + dummyPass + "\n" + mnemonic + "\n"))
 		io.SetOut(commands.WriteNopCloser(mockOut))
 
-		require.NoError(t,
-			execAdd(
-				cfg,
-				[]string{
-					"example-key",
-				},
-				io,
-			),
-		)
+		// Create the command
+		cmd := NewRootCmdWithBaseConfig(io, baseOptions)
+
+		args := []string{
+			"add",
+			"--insecure-password-stdin",
+			"--home",
+			kbHome,
+			"--recover",
+			"example-key",
+		}
+
+		for _, path := range paths {
+			args = append(
+				args, []string{
+					"--derivation-path",
+					path,
+				}...,
+			)
+		}
+
+		require.NoError(t, cmd.ParseAndRun(ctx, args))
 
 		// Verify the addresses are derived correctly
 		expectedAccounts := generateAccounts(
@@ -122,26 +302,17 @@ func TestAdd_Derive(t *testing.T) {
 		t.Parallel()
 
 		var (
-			mnemonic  = generateTestMnemonic(t)
-			dummyPass = "dummy-pass"
+			kbHome      = t.TempDir()
+			mnemonic    = generateTestMnemonic(t)
+			dummyPass   = "dummy-pass"
+			baseOptions = BaseOptions{
+				InsecurePasswordStdin: true,
+				Home:                  kbHome,
+			}
 		)
 
-		kbHome, kbCleanUp := testutils.NewTestCaseDir(t)
-		require.NotNil(t, kbHome)
-		t.Cleanup(kbCleanUp)
-
-		cfg := &AddCfg{
-			RootCfg: &BaseCfg{
-				BaseOptions: BaseOptions{
-					InsecurePasswordStdin: true,
-					Home:                  kbHome,
-				},
-			},
-			Recover: true,
-			DerivationPath: []string{
-				"malformed path",
-			},
-		}
+		ctx, cancelFn := context.WithTimeout(context.Background(), 5*time.Second)
+		defer cancelFn()
 
 		mockOut := bytes.NewBufferString("")
 
@@ -149,42 +320,38 @@ func TestAdd_Derive(t *testing.T) {
 		io.SetIn(strings.NewReader(dummyPass + "\n" + dummyPass + "\n" + mnemonic + "\n"))
 		io.SetOut(commands.WriteNopCloser(mockOut))
 
-		require.ErrorIs(
-			t, execAdd(
-				cfg,
-				[]string{
-					"example-key",
-				},
-				io,
-			),
-			errInvalidDerivationPath,
-		)
+		// Create the command
+		cmd := NewRootCmdWithBaseConfig(io, baseOptions)
+
+		args := []string{
+			"add",
+			"--insecure-password-stdin",
+			"--home",
+			kbHome,
+			"--recover",
+			"example-key",
+			"--derivation-path",
+			"malformed path",
+		}
+
+		require.ErrorIs(t, cmd.ParseAndRun(ctx, args), errInvalidDerivationPath)
 	})
 
 	t.Run("invalid derivation path", func(t *testing.T) {
 		t.Parallel()
 
 		var (
-			mnemonic  = generateTestMnemonic(t)
-			dummyPass = "dummy-pass"
+			kbHome      = t.TempDir()
+			mnemonic    = generateTestMnemonic(t)
+			dummyPass   = "dummy-pass"
+			baseOptions = BaseOptions{
+				InsecurePasswordStdin: true,
+				Home:                  kbHome,
+			}
 		)
 
-		kbHome, kbCleanUp := testutils.NewTestCaseDir(t)
-		require.NotNil(t, kbHome)
-		t.Cleanup(kbCleanUp)
-
-		cfg := &AddCfg{
-			RootCfg: &BaseCfg{
-				BaseOptions: BaseOptions{
-					InsecurePasswordStdin: true,
-					Home:                  kbHome,
-				},
-			},
-			Recover: true,
-			DerivationPath: []string{
-				"44'/500'/0'/0/0", // invalid coin type
-			},
-		}
+		ctx, cancelFn := context.WithTimeout(context.Background(), 5*time.Second)
+		defer cancelFn()
 
 		mockOut := bytes.NewBufferString("")
 
@@ -192,120 +359,20 @@ func TestAdd_Derive(t *testing.T) {
 		io.SetIn(strings.NewReader(dummyPass + "\n" + dummyPass + "\n" + mnemonic + "\n"))
 		io.SetOut(commands.WriteNopCloser(mockOut))
 
-		require.ErrorIs(
-			t, execAdd(
-				cfg,
-				[]string{
-					"example-key",
-				},
-				io,
-			),
-			errInvalidDerivationPath,
-		)
+		// Create the command
+		cmd := NewRootCmdWithBaseConfig(io, baseOptions)
+
+		args := []string{
+			"add",
+			"--insecure-password-stdin",
+			"--home",
+			kbHome,
+			"--recover",
+			"example-key",
+			"--derivation-path",
+			"44'/500'/0'/0/0", // invalid coin type
+		}
+
+		require.ErrorIs(t, cmd.ParseAndRun(ctx, args), errInvalidDerivationPath)
 	})
-}
-
-func Test_execAddBasic(t *testing.T) {
-	t.Parallel()
-
-	// make new test dir
-	kbHome, kbCleanUp := testutils.NewTestCaseDir(t)
-	assert.NotNil(t, kbHome)
-	defer kbCleanUp()
-
-	cfg := &AddCfg{
-		RootCfg: &BaseCfg{
-			BaseOptions: BaseOptions{
-				InsecurePasswordStdin: true,
-				Home:                  kbHome,
-			},
-		},
-	}
-
-	keyName := "keyname1"
-
-	io := commands.NewTestIO()
-	io.SetIn(strings.NewReader("test1234\ntest1234\n"))
-
-	// Create a new key
-	if err := execAdd(cfg, []string{keyName}, io); err != nil {
-		t.Fatalf("unable to execute add cmd, %v", err)
-	}
-
-	io.SetIn(strings.NewReader("y\ntest1234\ntest1234\n"))
-
-	// Confirm overwrite
-	if err := execAdd(cfg, []string{keyName}, io); err != nil {
-		t.Fatalf("unable to execute add cmd, %v", err)
-	}
-}
-
-var (
-	test2Mnemonic     = "hair stove window more scrap patient endorse left early pear lawn school loud divide vibrant family still bulk lyrics firm plate media critic dove"
-	test2PubkeyBech32 = "gpub1pgfj7ard9eg82cjtv4u4xetrwqer2dntxyfzxz3pqg5y7u93gpzug38k2p8s8322zpdm96t0ch87ax88sre4vnclz2jcy8uyhst"
-)
-
-func Test_execAddPublicKey(t *testing.T) {
-	t.Parallel()
-
-	kbHome, kbCleanUp := testutils.NewTestCaseDir(t)
-	assert.NotNil(t, kbHome)
-	defer kbCleanUp()
-
-	cfg := &AddBech32Cfg{
-		RootCfg: &AddCfg{
-			RootCfg: &BaseCfg{
-				BaseOptions: BaseOptions{
-					Home: kbHome,
-				},
-			},
-		},
-		PublicKey: test2PubkeyBech32, // test2 account
-	}
-
-	if err := execAddBech32(cfg, []string{"test2"}, nil); err != nil {
-		t.Fatalf("unable to execute add cmd, %v", err)
-	}
-}
-
-func Test_execAddRecover(t *testing.T) {
-	t.Parallel()
-
-	kbHome, kbCleanUp := testutils.NewTestCaseDir(t)
-	assert.NotNil(t, kbHome)
-	defer kbCleanUp()
-
-	cfg := &AddCfg{
-		RootCfg: &BaseCfg{
-			BaseOptions: BaseOptions{
-				InsecurePasswordStdin: true,
-				Home:                  kbHome,
-			},
-		},
-		Recover: true, // init test2 account
-	}
-
-	test2Name := "test2"
-	test2Passphrase := "gn0rocks!"
-
-	io := commands.NewTestIO()
-	io.SetIn(strings.NewReader(test2Passphrase + "\n" + test2Passphrase + "\n" + test2Mnemonic + "\n"))
-
-	if err := execAdd(cfg, []string{test2Name}, io); err != nil {
-		t.Fatalf("unable to execute add cmd, %v", err)
-	}
-
-	kb, err2 := keys.NewKeyBaseFromDir(kbHome)
-	assert.NoError(t, err2)
-
-	infos, err3 := kb.List()
-	assert.NoError(t, err3)
-
-	info := infos[0]
-
-	keypub := info.GetPubKey()
-	keypub = keypub.(secp256k1.PubKeySecp256k1)
-
-	s := fmt.Sprintf("%s", keypub)
-	assert.Equal(t, s, test2PubkeyBech32)
 }
