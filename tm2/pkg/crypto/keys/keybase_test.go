9a4c9f86cffe39b0b563385138c1023378c9145c
Milos Zivkovic <milos.zivkovic@tendermint.com>
2024-04-16 16:55:42 +0200

    Wrap up skipped tests

--- tm2/pkg/crypto/keys/keybase_test.go
+++ tm2/pkg/crypto/keys/keybase_test.go
@@ -24,38 +24,6 @@ func TestCreateAccountInvalidMnemonic(t *testing.T) {
 	assert.Equal(t, "invalid mnemonic", err.Error())
 }
 
-func TestCreateLedgerUnsupportedAlgo(t *testing.T) {
-	t.Parallel()
-
-	kb := NewInMemory()
-	_, err := kb.CreateLedger("some_account", Ed25519, "cosmos", 0, 1)
-	assert.Error(t, err)
-	assert.Equal(t, "unsupported signing algo: only secp256k1 is supported", err.Error())
-}
-
-func TestCreateLedger(t *testing.T) {
-	t.Parallel()
-
-	kb := NewInMemory()
-
-	// test_cover and test_unit will result in different answers
-	// test_cover does not compile some dependencies so ledger is disabled
-	// test_unit may add a ledger mock
-	// both cases are acceptable
-	_, err := kb.CreateLedger("some_account", Secp256k1, "cosmos", 3, 1)
-	require.NoError(t, err)
-
-	// Check that restoring the key gets the same results
-	restoredKey, err := kb.GetByName("some_account")
-	assert.NotNil(t, restoredKey)
-	assert.Equal(t, "some_account", restoredKey.GetName())
-	assert.Equal(t, TypeLedger, restoredKey.GetType())
-
-	path, err := restoredKey.GetPath()
-	assert.NoError(t, err)
-	assert.Equal(t, "44'/118'/3'/0/1", path.String())
-}
-
 // TestKeyManagement makes sure we can manipulate these keys well
 func TestKeyManagement(t *testing.T) {
 	t.Parallel()
