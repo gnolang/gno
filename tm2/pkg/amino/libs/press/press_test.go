f26634bd575eb462b9225cd23413f881fcdc7554
Hariom Verma <hariom.verma@tendermint.com>
2024-04-18 22:20:51 +0530

    refactor: use `stretchr/testify` throughout codebase (#1931)
    Currently, we use 2 implementations of testify in our codebase -
    `jaekwon/testify` and `stretchr/testify`
    
    To streamline our practices and minimize confusion, it's a good idea to
    opt for a single implementation.
    
    After discussion, we all agreed on using `stretchr/testify` throughout
    our codebase.
    
    Closes #777

--- tm2/pkg/amino/libs/press/press_test.go
+++ tm2/pkg/amino/libs/press/press_test.go
@@ -3,14 +3,14 @@ package press
 import (
 	"testing"
 
-	"github.com/jaekwon/testify/assert"
+	"github.com/stretchr/testify/assert"
 )
 
 func TestEmpty(t *testing.T) {
 	t.Parallel()
 
 	p := NewPress()
-	assert.Equal(t, p.Print(), "")
+	assert.Equal(t, "", p.Print())
 }
 
 func TestBasic(t *testing.T) {
@@ -20,7 +20,7 @@ func TestBasic(t *testing.T) {
 	p.P("this ")
 	p.P("is ")
 	p.P("a test")
-	assert.Equal(t, p.Print(), "this is a test")
+	assert.Equal(t, "this is a test", p.Print())
 }
 
 func TestBasicLn(t *testing.T) {
@@ -30,7 +30,7 @@ func TestBasicLn(t *testing.T) {
 	p.P("this ")
 	p.P("is ")
 	p.Pl("a test")
-	assert.Equal(t, p.Print(), "this is a test\n")
+	assert.Equal(t, "this is a test\n", p.Print())
 }
 
 func TestNewlineStr(t *testing.T) {
@@ -42,7 +42,7 @@ func TestNewlineStr(t *testing.T) {
 	p.Pl("a test")
 	p.Pl("a test")
 	p.Pl("a test")
-	assert.Equal(t, p.Print(), "this is a test\r\na test\r\na test\r\n")
+	assert.Equal(t, "this is a test\r\na test\r\na test\r\n", p.Print())
 }
 
 func TestIndent(t *testing.T) {
@@ -54,10 +54,10 @@ func TestIndent(t *testing.T) {
 		p.Pl("second line")
 		p.Pl("third line")
 	}).P("}")
-	assert.Equal(t, p.Print(), `first line {
+	assert.Equal(t, `first line {
 	second line
 	third line
-}`)
+}`, p.Print())
 }
 
 func TestIndent2(t *testing.T) {
@@ -72,9 +72,9 @@ func TestIndent2(t *testing.T) {
 		// the indented lines terminate with newlineDelim before
 		// the next unindented line.
 	}).P("}")
-	assert.Equal(t, p.Print(), `first line {
+	assert.Equal(t, `first line {
 	second line
-}`)
+}`, p.Print())
 }
 
 func TestIndent3(t *testing.T) {
@@ -86,9 +86,9 @@ func TestIndent3(t *testing.T) {
 		p.P("second ")
 		p.Pl("line")
 	}).P("}")
-	assert.Equal(t, p.Print(), `first line {
+	assert.Equal(t, `first line {
 	second line
-}`)
+}`, p.Print())
 }
 
 func TestIndentLn(t *testing.T) {
@@ -100,11 +100,11 @@ func TestIndentLn(t *testing.T) {
 		p.Pl("second line")
 		p.Pl("third line")
 	}).Pl("}")
-	assert.Equal(t, p.Print(), `first line {
+	assert.Equal(t, `first line {
 	second line
 	third line
 }
-`)
+`, p.Print())
 }
 
 func TestNestedIndent(t *testing.T) {
@@ -120,11 +120,11 @@ func TestNestedIndent(t *testing.T) {
 			p.Pl("fifth line")
 		})
 	}).Pl("}")
-	assert.Equal(t, p.Print(), `first line {
+	assert.Equal(t, `first line {
 	second line
 	third line
 		fourth line
 		fifth line
 }
-`)
+`, p.Print())
 }
