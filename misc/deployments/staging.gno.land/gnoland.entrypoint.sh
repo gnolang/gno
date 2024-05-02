dfbf5806fff68cf82ab4a9b27fcea040b8b96a06
albttx <contact@albttx.tech>
2024-05-02 11:52:19 +0100

    chore: simplify staging

---
+++ misc/deployments/staging.gno.land/gnoland.entrypoint.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env sh
+
+MONIKER=${MONIKER:-"gnode"}
+P2P_LADDR=${P2P_LADDR:-"tcp://0.0.0.0:26656"}
+RPC_LADDR=${RPC_LADDR:-"tcp://0.0.0.0:26657"}
+
+CHAIN_ID=${CHAIN_ID:-"dev"}
+
+gnoland start \
+    --chainid="${CHAIN_ID}" \
+    --skip-start=true \
+    --skip-failing-genesis-txs
+
+sed -i "s#^moniker = \".*\"#moniker = \"${MONIKER}\"#" ./gnoland-data/config/config.toml
+sed -i "s#laddr = \".*:26656\"#laddr = \"${P2P_LADDR}\"#" ./gnoland-data/config/config.toml
+sed -i "s#laddr = \".*:26657\"#laddr = \"${RPC_LADDR}\"#" ./gnoland-data/config/config.toml
+
+exec gnoland start --skip-failing-genesis-txs
