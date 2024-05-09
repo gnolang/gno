90ca68e4be7f487e56dbabb7635c809608e0d926
Milos Zivkovic <milos.zivkovic@tendermint.com>
2024-05-09 11:05:14 +0200

    Add VM telemetry

--- gno.land/pkg/sdk/vm/handler.go
+++ gno.land/pkg/sdk/vm/handler.go
@@ -1,12 +1,15 @@
 package vm
 
 import (
+	"context"
 	"fmt"
 	"strings"
 
 	abci "github.com/gnolang/gno/tm2/pkg/bft/abci/types"
 	"github.com/gnolang/gno/tm2/pkg/sdk"
 	"github.com/gnolang/gno/tm2/pkg/std"
+	"github.com/gnolang/gno/tm2/pkg/telemetry"
+	"github.com/gnolang/gno/tm2/pkg/telemetry/metrics"
 )
 
 type vmHandler struct {
@@ -51,14 +54,6 @@ func (vh vmHandler) handleMsgCall(ctx sdk.Context, msg MsgCall) (res sdk.Result)
 	}
 	res.Data = []byte(resstr)
 	return
-	/* TODO handle events.
-	ctx.EventManager().EmitEvent(
-		sdk.NewEvent(
-			sdk.EventTypeMessage,
-			sdk.NewAttribute(sdk.AttributeKeyXXX, types.AttributeValueXXX),
-		),
-	)
-	*/
 }
 
 // Handle MsgRun.
@@ -71,7 +66,7 @@ func (vh vmHandler) handleMsgRun(ctx sdk.Context, msg MsgRun) (res sdk.Result) {
 	return
 }
 
-//----------------------------------------
+// ----------------------------------------
 // Query
 
 // query paths
@@ -84,26 +79,62 @@ const (
 	QueryFile    = "qfile"
 )
 
-func (vh vmHandler) Query(ctx sdk.Context, req abci.RequestQuery) (res abci.ResponseQuery) {
-	switch secondPart(req.Path) {
+func (vh vmHandler) Query(ctx sdk.Context, req abci.RequestQuery) abci.ResponseQuery {
+	var (
+		res  abci.ResponseQuery
+		path = secondPart(req.Path)
+	)
+
+	switch path {
 	case QueryPackage:
-		return vh.queryPackage(ctx, req)
+		res = vh.queryPackage(ctx, req)
 	case QueryStore:
-		return vh.queryStore(ctx, req)
+		res = vh.queryStore(ctx, req)
 	case QueryRender:
-		return vh.queryRender(ctx, req)
+		res = vh.queryRender(ctx, req)
 	case QueryFuncs:
-		return vh.queryFuncs(ctx, req)
+		res = vh.queryFuncs(ctx, req)
 	case QueryEval:
-		return vh.queryEval(ctx, req)
+		res = vh.queryEval(ctx, req)
 	case QueryFile:
-		return vh.queryFile(ctx, req)
+		res = vh.queryFile(ctx, req)
 	default:
-		res = sdk.ABCIResponseQueryFromError(
+		return sdk.ABCIResponseQueryFromError(
 			std.ErrUnknownRequest(fmt.Sprintf(
 				"unknown vm query endpoint %s in %s",
 				secondPart(req.Path), req.Path)))
+	}
+
+	// Log the telemetry
+	vh.logQueryTelemetry(path, res.IsErr())
+
+	return res
+}
+
+// logQueryTelemetry logs the relevant VM query telemetry
+func (vh vmHandler) logQueryTelemetry(path string, isErr bool) {
+	if !telemetry.MetricsEnabled() {
 		return
+	}
+
+	switch path {
+	case QueryPackage:
+		metrics.VMQueryPackageCalls.Add(context.Background(), 1)
+	case QueryStore:
+		metrics.VMQueryStoreCalls.Add(context.Background(), 1)
+	case QueryRender:
+		metrics.VMQueryRenderCalls.Add(context.Background(), 1)
+	case QueryFuncs:
+		metrics.VMQueryFuncsCalls.Add(context.Background(), 1)
+	case QueryEval:
+		metrics.VMQueryEvalCalls.Add(context.Background(), 1)
+	case QueryFile:
+		metrics.VMQueryFileCalls.Add(context.Background(), 1)
+	default:
+	}
+
+	if isErr {
+		metrics.VMQueryErrors.Add(context.Background(), 1)
 	}
 }
 
@@ -187,7 +218,7 @@ func (vh vmHandler) queryFile(ctx sdk.Context, req abci.RequestQuery) (res abci.
 	return
 }
 
-//----------------------------------------
+// ----------------------------------------
 // misc
 
 func abciResult(err error) sdk.Result {
--- tm2/pkg/telemetry/metrics/metrics.go
+++ tm2/pkg/telemetry/metrics/metrics.go
@@ -23,14 +23,26 @@ const (
 
 	numMempoolTxsKey = "num_mempool_txs_gauge"
 	numCachedTxsKey  = "num_cached_txs_gauge"
+
+	vmQueryPackageCallsKey = "vm_query_package_calls_counter"
+	vmQueryStoreCallsKey   = "vm_query_store_calls_counter"
+	vmQueryRenderCallsKey  = "vm_query_render_calls_counter"
+	vmQueryFuncsCallsKey   = "vm_query_funcs_calls_counter"
+	vmQueryEvalCallsKey    = "vm_query_eval_calls_counter"
+	vmQueryFileCallsKey    = "vm_query_file_calls_counter"
+	vmQueryErrorsKey       = "vm_query_errors_counter"
 )
 
 var (
+	// Misc //
+
 	// BroadcastTxTimer measures the transaction broadcast duration
 	BroadcastTxTimer metric.Int64Histogram
 
 	// BuildBlockTimer measures the block build duration
 	BuildBlockTimer metric.Int64Histogram
+
+	// Networking //
 
 	// InboundPeers measures the active number of inbound peers
 	InboundPeers *Int64Gauge
@@ -41,11 +53,36 @@ var (
 	// DialingPeers measures the active number of peers in the dialing state
 	DialingPeers *Int64Gauge
 
+	// Mempool //
+
 	// NumMempoolTxs measures the number of transaction inside the mempool
 	NumMempoolTxs *Int64Gauge
 
 	// NumCachedTxs measures the number of transaction inside the mempool cache
 	NumCachedTxs *Int64Gauge
+
+	// Runtime //
+
+	// VMQueryPackageCalls measures the frequency of VM query package calls
+	VMQueryPackageCalls metric.Int64Counter
+
+	// VMQueryStoreCalls measures the frequency of VM query store calls
+	VMQueryStoreCalls metric.Int64Counter
+
+	// VMQueryRenderCalls measures the frequency of VM query render calls
+	VMQueryRenderCalls metric.Int64Counter
+
+	// VMQueryFuncsCalls measures the frequency of VM query funcs calls
+	VMQueryFuncsCalls metric.Int64Counter
+
+	// VMQueryEvalCalls measures the frequency of VM query eval calls
+	VMQueryEvalCalls metric.Int64Counter
+
+	// VMQueryFileCalls measures the frequency of VM query file calls
+	VMQueryFileCalls metric.Int64Counter
+
+	// VMQueryErrors measures the frequency of VM query errors
+	VMQueryErrors metric.Int64Counter
 )
 
 func Init(config config.Config) error {
@@ -130,6 +167,56 @@ func Init(config config.Config) error {
 		meter,
 	); err != nil {
 		return fmt.Errorf("unable to create gauge, %w", err)
+	}
+
+	// Runtime //
+	if VMQueryPackageCalls, err = meter.Int64Counter(
+		vmQueryPackageCallsKey,
+		metric.WithDescription("vm query package call frequency"),
+	); err != nil {
+		return fmt.Errorf("unable to create counter, %w", err)
+	}
+
+	if VMQueryStoreCalls, err = meter.Int64Counter(
+		vmQueryStoreCallsKey,
+		metric.WithDescription("vm query store call frequency"),
+	); err != nil {
+		return fmt.Errorf("unable to create counter, %w", err)
+	}
+
+	if VMQueryRenderCalls, err = meter.Int64Counter(
+		vmQueryRenderCallsKey,
+		metric.WithDescription("vm query render call frequency"),
+	); err != nil {
+		return fmt.Errorf("unable to create counter, %w", err)
+	}
+
+	if VMQueryFuncsCalls, err = meter.Int64Counter(
+		vmQueryFuncsCallsKey,
+		metric.WithDescription("vm query funcs call frequency"),
+	); err != nil {
+		return fmt.Errorf("unable to create counter, %w", err)
+	}
+
+	if VMQueryEvalCalls, err = meter.Int64Counter(
+		vmQueryEvalCallsKey,
+		metric.WithDescription("vm query eval call frequency"),
+	); err != nil {
+		return fmt.Errorf("unable to create counter, %w", err)
+	}
+
+	if VMQueryFileCalls, err = meter.Int64Counter(
+		vmQueryFileCallsKey,
+		metric.WithDescription("vm query file call frequency"),
+	); err != nil {
+		return fmt.Errorf("unable to create counter, %w", err)
+	}
+
+	if VMQueryErrors, err = meter.Int64Counter(
+		vmQueryErrorsKey,
+		metric.WithDescription("vm query errors call frequency"),
+	); err != nil {
+		return fmt.Errorf("unable to create counter, %w", err)
 	}
 
 	return nil
