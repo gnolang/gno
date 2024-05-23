d4438cd05e5b89a67ac794d626c4e4581eae70ca
albttx <contact@albttx.tech>
2024-05-23 15:17:05 +0100

    fix: with latest update

--- misc/deployments/staging.gno.land/gnoland.entrypoint.sh
+++ misc/deployments/staging.gno.land/gnoland.entrypoint.sh
@@ -6,7 +6,7 @@ RPC_LADDR=${RPC_LADDR:-"tcp://0.0.0.0:26657"}
 
 CHAIN_ID=${CHAIN_ID:-"dev"}
 
-gnoland start \
+/gnoland start \
     --chainid="${CHAIN_ID}" \
     --skip-start=true \
     --skip-failing-genesis-txs
@@ -15,4 +15,4 @@ sed -i "s#^moniker = \".*\"#moniker = \"${MONIKER}\"#" ./gnoland-data/config/con
 sed -i "s#laddr = \".*:26656\"#laddr = \"${P2P_LADDR}\"#" ./gnoland-data/config/config.toml
 sed -i "s#laddr = \".*:26657\"#laddr = \"${RPC_LADDR}\"#" ./gnoland-data/config/config.toml
 
-exec gnoland start --skip-failing-genesis-txs
+exec /gnoland start --skip-failing-genesis-txs
