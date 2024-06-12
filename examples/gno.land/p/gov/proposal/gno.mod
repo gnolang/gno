gnolang/gno

commit 145f612f36e3a7c52701e14b27cac98d0448af21

feat(PoC,gov): initial sys/validators and gov/dao contracts (#1945)
This PR showcases a model that combines multiple contracts with defined
goals and constraints. The aim is to make everything in `sys/*` usable
by the chain (tm2 powered) efficiently, with minimal need for updates
while maintaining flexibility in usage.

The `sys/` contracts focus on defining data types and helpers to ensure
that received callbacks meet minimal constraints, like GovDAO approval.
They do not handle DAO logic or state due to complexity and
upgradability requirements for DAOs.

I won't include complete DAO examples in this PR. Imagine having these
sections once everything is done:
-  `{p,r}/sys: minimal interface with the chain`
-  `{p,r}/gov: simple DAO frameworks`
- `{p,r}/*`: where users will develop permissionless logic and propose
it to `gov` for approval using `sys` to trigger the chain.

Personal note -> try to introduce and document the notion of "pausable
threads". Related with #1974.

---

TODO:
- [x] pseudo-code for proof of conribution's valset management.
- [x] proposal example.
- [ ] pseudo-code for gnosdk v0 to catch the event and apply the change
from tm2. cc @gfanton
- [ ] add unit-tests, to illustrate the expected usage.

depends on std.Emit (#575).
depends on #1948 (need rebase).

---------

Signed-off-by: moul <94029+moul@users.noreply.github.com>
Signed-off-by: gfanton <8671905+gfanton@users.noreply.github.com>
Co-authored-by: Milos Zivkovic <milos.zivkovic@tendermint.com>
Co-authored-by: gfanton <8671905+gfanton@users.noreply.github.com>

examples/gno.land/p/gov/proposal/gno.mod
@@ -1,0 +1,1 @@
+module proposal

examples/gno.land/r/gnoland/valopers/gno.mod
@@ -1,0 +1,6 @@
+module valopers
+
+require (
+	gno.land/p/demo/ownable v0.0.0-latest
+	gno.land/r/gov/dao v0.0.0-latest
+)

examples/gno.land/r/gov/dao/gno.mod
@@ -1,3 +1,3 @@
-module gno.land/r/sys/validators
+module govdao
 
 require gno.land/p/gov/proposal v0.0.0-latest

examples/gno.land/r/gov/proposals/prop1/gno.mod
@@ -1,0 +1,6 @@
+module prop1
+
+require (
+	gno.land/r/gov/dao v0.0.0-latest
+	gno.land/r/sys/validators v0.0.0-latest
+)

contribs/Makefile
@@ -3,6 +3,8 @@ help:
 	@echo "Available make commands:"
 	@cat Makefile | grep '^[a-z][^:]*:' | cut -d: -f1 | sort | sed 's/^/  /'
 
+programs=gnodev gnofaucet
+
 # command to run dependency utilities, like goimports.
 rundep=go run -modfile ../misc/devdeps/go.mod
 
@@ -25,15 +27,31 @@ GOTEST_FLAGS ?= -v -p 1 -timeout=30m
 ########################################
 # Dev tools
 .PHONY: install
-install: install.gnomd install.gnodev install.gnofaucet
+install:
+	@echo 'To install a tool, go to the subdirectory, then run `make install`.'
+	@echo 'To do a full installation, run `make install_all`.'
 
-install.gnomd:;  cd gnomd && go install .
-install.gnodev:; $(MAKE) -C ./gnodev install
-install.gnofaucet:; $(MAKE) -C ./gnofaucet install
+install_all: $(addsuffix .install,$(programs))
+%.install:
+	@echo "[+] make -C $(basename $@) install"
+	$(MAKE) --no-print-directory -C $(basename $@) install
+.PHONY: install_all
 
-.PHONY: clean
-clean:
-	rm -rf build
+########################################
+# Test suite
+test: $(addsuffix .test,$(programs))
+%.test:
+	@echo "[+] make -C $(basename $@) install"
+	$(MAKE) --no-print-directory -C $(basename $@) test
+.PHONY: test 
+
+########################################
+# Lint
+.PHONY: lint
+lint: $(addsuffix .lint,$(programs))
+%.lint:
+	@echo "[+] make -C $(basename $@) install"
+	$(MAKE) --no-print-directory -C $(basename $@) lint
 
 ########################################
 # Dev tools
@@ -45,23 +63,4 @@ fmt:
 
 .PHONY: tidy
 tidy:
-	@for gomod in `find . -name go.mod`; do ( \
-    dir=`dirname $$gomod`; \
-	  set -xe; \
-	  cd $$dir; \
-	  go mod tidy -v; \
-	); done
-
-########################################
-# Test suite
-.PHONY: test
-test: test.gnodev
-test.gnodev:
-	$(MAKE) -C ./gnodev test
-
-########################################
-# Lint
-.PHONY: test
-lint: lint.gnodev
-lint.gnodev:
-	$(MAKE) -C ./gnodev test
+	find . -name go.mod -execdir go mod tidy -v \;

examples/Makefile
@@ -27,6 +27,7 @@ OFFICIAL_PACKAGES = ./gno.land/p
 OFFICIAL_PACKAGES += ./gno.land/r/demo
 OFFICIAL_PACKAGES += ./gno.land/r/gnoland
 OFFICIAL_PACKAGES += ./gno.land/r/sys
+OFFICIAL_PACKAGES += ./gno.land/r/gov
 
 ########################################
 # Dev tools

examples/gno.land/p/demo/ownable/ownable.gno
@@ -12,8 +12,12 @@ type Ownable struct {
 
 func New() *Ownable {
 	return &Ownable{
-		owner: std.GetOrigCaller(),
+		owner: std.PrevRealm().Addr(),
 	}
+}
+
+func NewWithAddress(addr std.Address) *Ownable {
+	return &Ownable{owner: addr}
 }
 
 // TransferOwnership transfers ownership of the Ownable struct to a new address
@@ -44,14 +48,20 @@ func (o *Ownable) DropOwnership() error {
 	return nil
 }
 
+func (o Ownable) Owner() std.Address {
+	return o.owner
+}
+
 // CallerIsOwner checks if the caller of the function is the Realm's owner
-func (o *Ownable) CallerIsOwner() error {
-	if std.GetOrigCaller() == o.owner {
+func (o Ownable) CallerIsOwner() error {
+	if std.PrevRealm().Addr() == o.owner {
 		return nil
 	}
 	return ErrUnauthorized
 }
 
-func (o *Ownable) Owner() std.Address {
-	return o.owner
+func (o Ownable) AssertCallerIsOwner() {
+	if std.PrevRealm().Addr() != o.owner {
+		panic(ErrUnauthorized)
+	}
 }

examples/gno.land/p/demo/ownable/ownable_test.gno
@@ -11,28 +11,39 @@ var (
 )
 
 func TestNew(t *testing.T) {
-	std.TestSetOrigCaller(firstCaller)
+	std.TestSetRealm(std.NewUserRealm(firstCaller))
+	std.TestSetOrigCaller(firstCaller) // TODO(bug): should not be needed
 
-	result := New()
-	if firstCaller != result.owner {
-		t.Fatalf("Expected %s, got: %s\n", firstCaller, result.owner)
+	o := New()
+	got := o.Owner()
+	if firstCaller != got {
+		t.Fatalf("Expected %s, got: %s", firstCaller, got)
+	}
+}
+
+func TestNewWithAddress(t *testing.T) {
+	o := NewWithAddress(firstCaller)
+
+	got := o.Owner()
+	if firstCaller != got {
+		t.Fatalf("Expected %s, got: %s", firstCaller, got)
 	}
 }
 
 func TestOwner(t *testing.T) {
-	std.TestSetOrigCaller(firstCaller)
+	std.TestSetRealm(std.NewUserRealm(firstCaller))
 
-	result := New()
-	resultOwner := result.Owner()
-
+	o := New()
 	expected := firstCaller
-	if resultOwner != expected {
-		t.Fatalf("Expected %s, got: %s\n", expected, result)
+	got := o.Owner()
+	if expected != got {
+		t.Fatalf("Expected %s, got: %s", expected, got)
 	}
 }
 
 func TestTransferOwnership(t *testing.T) {
-	std.TestSetOrigCaller(firstCaller)
+	std.TestSetRealm(std.NewUserRealm(firstCaller))
+
 	o := New()
 
 	err := o.TransferOwnership(secondCaller)
@@ -40,28 +51,29 @@ func TestTransferOwnership(t *testing.T) {
 		t.Fatalf("TransferOwnership failed, %v", err)
 	}
 
-	result := o.Owner()
-	if secondCaller != result {
-		t.Fatalf("Expected: %s, got: %s\n", secondCaller, result)
+	got := o.Owner()
+	if secondCaller != got {
+		t.Fatalf("Expected: %s, got: %s", secondCaller, got)
 	}
 }
 
 func TestCallerIsOwner(t *testing.T) {
-	std.TestSetOrigCaller(firstCaller)
+	std.TestSetRealm(std.NewUserRealm(firstCaller))
 
 	o := New()
 	unauthorizedCaller := secondCaller
 
-	std.TestSetOrigCaller(unauthorizedCaller)
+	std.TestSetRealm(std.NewUserRealm(unauthorizedCaller))
+	std.TestSetOrigCaller(unauthorizedCaller) // TODO(bug): should not be needed
 
 	err := o.CallerIsOwner()
 	if err == nil {
-		t.Fatalf("Expected %s to not be owner\n", unauthorizedCaller)
+		t.Fatalf("Expected %s to not be owner", unauthorizedCaller)
 	}
 }
 
 func TestDropOwnership(t *testing.T) {
-	std.TestSetOrigCaller(firstCaller)
+	std.TestSetRealm(std.NewUserRealm(firstCaller))
 
 	o := New()
 
@@ -72,18 +84,20 @@ func TestDropOwnership(t *testing.T) {
 
 	owner := o.Owner()
 	if owner != "" {
-		t.Fatalf("Expected owner to be empty, not %s\n", owner)
+		t.Fatalf("Expected owner to be empty, not %s", owner)
 	}
 }
 
 // Errors
 
 func TestErrUnauthorized(t *testing.T) {
-	std.TestSetOrigCaller(firstCaller)
+	std.TestSetRealm(std.NewUserRealm(firstCaller))
+	std.TestSetOrigCaller(firstCaller) // TODO(bug): should not be needed
 
 	o := New()
 
-	std.TestSetOrigCaller(secondCaller)
+	std.TestSetRealm(std.NewUserRealm(secondCaller))
+	std.TestSetOrigCaller(secondCaller) // TODO(bug): should not be needed
 
 	err := o.TransferOwnership(firstCaller)
 	if err != ErrUnauthorized {
@@ -97,7 +111,7 @@ func TestErrUnauthorized(t *testing.T) {
 }
 
 func TestErrInvalidAddress(t *testing.T) {
-	std.TestSetOrigCaller(firstCaller)
+	std.TestSetRealm(std.NewUserRealm(firstCaller))
 
 	o := New()
 

commit 719ee9205de9eb81c1e1d6596611e95d8257e455

fix: bad module naming in gov package (#2339)
This PR addresses the issue of poor module naming introduced in the
recent merge #1945.

<details><summary>Contributors' checklist...</summary>

- [ ] Added new tests, or not needed, or not feasible
- [ ] Provided an example (e.g. screenshot) to aid review or the PR is
self-explanatory
- [ ] Updated the official documentation or not needed
- [ ] No breaking changes were made, or a `BREAKING CHANGE: xxx` message
was included in the description
- [ ] Added references to related issues and PRs
- [ ] Provided any useful hints for running manual tests
- [ ] Added new benchmarks to [generated
graphs](https://gnoland.github.io/benchmarks), if any. More info
[here](https://github.com/gnolang/gno/blob/master/.benchmarks/README.md).
</details>

Signed-off-by: gfanton <8671905+gfanton@users.noreply.github.com>

examples/gno.land/p/gov/proposal/gno.mod
@@ -1,1 +1,1 @@
-module proposal
+module gno.land/p/gov/proposal

examples/gno.land/r/gnoland/valopers/gno.mod
@@ -1,4 +1,4 @@
-module valopers
+module gno.land/r/gnoland/valopers
 
 require (
 	gno.land/p/demo/ownable v0.0.0-latest

examples/gno.land/r/gov/dao/gno.mod
@@ -1,3 +1,3 @@
-module govdao
+module gno.land/r/gov/dao
 
 require gno.land/p/gov/proposal v0.0.0-latest

examples/gno.land/r/gov/proposals/prop1/gno.mod
@@ -1,4 +1,4 @@
-module prop1
+module gno.land/r/gov/proposals/prop1
 
 require (
 	gno.land/r/gov/dao v0.0.0-latest