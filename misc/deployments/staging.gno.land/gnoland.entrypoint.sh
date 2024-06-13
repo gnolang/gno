bb2b042900f0968283f045d544617f3a3c3724e3
albttx <contact@albttx.tech>
2024-06-13 11:24:03 +0200

    chore: update docs + chain_id

--- misc/deployments/staging.gno.land/gnoland.entrypoint.sh
+++ misc/deployments/staging.gno.land/gnoland.entrypoint.sh
@@ -6,7 +6,7 @@ MONIKER=${MONIKER:-"gnode"}
 P2P_LADDR=${P2P_LADDR:-"tcp://0.0.0.0:26656"}
 RPC_LADDR=${RPC_LADDR:-"tcp://0.0.0.0:26657"}
 
-CHAIN_ID=${CHAIN_ID:-"dev"}
+CHAIN_ID=${CHAIN_ID:-"staging"}
 
 gnoland config  init
 gnoland secrets init
