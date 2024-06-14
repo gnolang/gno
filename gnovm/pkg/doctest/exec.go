f60cb1c857f2746a172a2abdf92dc35098724048
Lee ByeongJun <lbj199874@gmail.com>
2024-06-14 18:40:33 +0900

    generate hash key based on the parsed code

--- gnovm/pkg/doctest/exec.go
+++ gnovm/pkg/doctest/exec.go
@@ -1,16 +1,27 @@
 package doctest
 
 import (
+	"crypto/sha256"
+	"encoding/hex"
 	"fmt"
+	"strings"
 
 	gno "github.com/gnolang/gno/gnovm/pkg/gnolang"
 )
 
-var execResult map[string]string
+// Global variable to store execution results
+var codeResults map[string]string
 
+func init() {
+	codeResults = make(map[string]string)
+}
+
+// executeCodeBLock executes a code block using gnoVM and caching the result.
 func executeCodeBlock(c CodeBlock) error {
-	if c.T != "go" || c.T != "gno" {
+	if c.T != "go" && c.T != "gno" {
 		return fmt.Errorf("unsupported language: %s", c.T)
+	} else {
+		c.T = "gno"
 	}
 
 	m := gno.NewMachine("runMD", nil)
@@ -23,7 +34,17 @@ func executeCodeBlock(c CodeBlock) error {
 	m.RunMain()
 	res := m.PopValue().V.String()
 
-	execResult[fmt.Sprintf("%d.%s", c.Index, c.T)] = res
+
+	// ignore the whitespace in the source code
+	key := generateCacheKey([]byte(strings.ReplaceAll(c.Content, " ", "")))
+	codeResults[key] = res
 
 	return nil
+}
+
+// generateCacheKey creates a SHA-256 hah of the source code to be used as a cache key
+// to avoid re-executing the same code block.
+func generateCacheKey(data []byte) string {
+	hash := sha256.Sum256(data)
+	return hex.EncodeToString(hash[:])
 }
