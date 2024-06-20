d6bafd82ac0059d9dfca6a3487c5eeaf78ef3686
leohhhn <hudakleon@gmail.com>
2024-06-20 19:42:27 +0200

    save

--- misc/docs-linter/main_test.go
+++ misc/docs-linter/main_test.go
@@ -25,52 +25,6 @@ func TestEmptyPathError(t *testing.T) {
 	assert.ErrorIs(t, execLint(cfg, ctx), errEmptyPath)
 }
 
-func TestExtractLinks(t *testing.T) {
-	// todo rewrite
-	t.Parallel()
-
-	// Generate temporary source dir
-	sourceDir, err := os.MkdirTemp(".", "sourceDir")
-	require.NoError(t, err)
-	t.Cleanup(removeDir(t, sourceDir))
-
-	// Create mock files with random links
-	mockFiles := map[string]string{
-		"file1.md": "This is a test file with a link: https://example.com.\nAnother link: http://example.org.",
-		"file2.md": "Markdown content with a link: https://example.com/page.",
-		"file3.md": "Links in a list:\n- https://example.com/item1\n- https://example.org/item2",
-	}
-
-	for fileName, content := range mockFiles {
-		filePath := filepath.Join(sourceDir, fileName)
-		err := os.WriteFile(filePath, []byte(content), 0644)
-		require.NoError(t, err)
-	}
-
-	// Expected URLs and their corresponding files
-	expectedUrls := map[string]string{
-		"https://example.com":       filepath.Join(sourceDir, "file1.md"),
-		"http://example.org":        filepath.Join(sourceDir, "file1.md"),
-		"https://example.com/page":  filepath.Join(sourceDir, "file2.md"),
-		"https://example.com/item1": filepath.Join(sourceDir, "file3.md"),
-		"https://example.org/item2": filepath.Join(sourceDir, "file3.md"),
-	}
-
-	// Extract URLs from each file in the sourceDir
-	for fileName := range mockFiles {
-		filePath := filepath.Join(sourceDir, fileName)
-		extractedUrls, err := extractUrls(filePath)
-		require.NoError(t, err)
-
-		// Verify that the extracted URLs match the expected URLs
-		for url, expectedFile := range expectedUrls {
-			if expectedFile == filePath {
-				require.Equal(t, expectedFile, extractedUrls[url], "URL: %s not correctly mapped to file: %s", url, expectedFile)
-			}
-		}
-	}
-}
-
 func TestFindFilePaths(t *testing.T) {
 	t.Parallel()
 
