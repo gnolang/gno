281c9ccf7c943723276070f58412bcd5cf71691c
gfanton <8671905+gfanton@users.noreply.github.com>
2024-06-24 11:48:36 +0200

    chore: use continue in gno process files utility methods
    Signed-off-by: gfanton <8671905+gfanton@users.noreply.github.com>

--- gnovm/cmd/gno/util.go
+++ gnovm/cmd/gno/util.go
@@ -35,22 +35,24 @@ func gnoFilesFromArgsRecursively(args []string) ([]string, error) {
 		if !info.IsDir() {
 			curpath := arg
 			paths = append(paths, curpath)
-		} else {
-			err = filepath.WalkDir(arg, func(curpath string, f fs.DirEntry, err error) error {
-				if err != nil {
-					return fmt.Errorf("%s: walk dir: %w", arg, err)
-				}
-
-				if !isGnoFile(f) {
-					return nil // skip
-				}
-				paths = append(paths, curpath)
-				return nil
-			})
-			if err != nil {
-				return nil, err
-			}
+			continue
 		}
+
+		err = filepath.WalkDir(arg, func(curpath string, f fs.DirEntry, err error) error {
+			if err != nil {
+				return fmt.Errorf("%s: walk dir: %w", arg, err)
+			}
+
+			if !isGnoFile(f) {
+				return nil // skip
+			}
+			paths = append(paths, curpath)
+			return nil
+		})
+		if err != nil {
+			return nil, err
+		}
+
 	}
 	return paths, nil
 }
@@ -89,45 +91,49 @@ func gnoPackagesFromArgsRecursively(args []string) ([]string, error) {
 		if err != nil {
 			return nil, fmt.Errorf("invalid file or package path: %w", err)
 		}
+
 		if !info.IsDir() {
 			paths = append(paths, arg)
-		} else {
-			// if the passed arg is a dir, then we'll recursively walk the dir
-			// and look for directories containing at least one .gno file.
-
-			visited := map[string]bool{} // used to run the builder only once per folder.
-			err = filepath.WalkDir(arg, func(curpath string, f fs.DirEntry, err error) error {
-				if err != nil {
-					return fmt.Errorf("%s: walk dir: %w", arg, err)
-				}
-				if f.IsDir() {
-					return nil // skip
-				}
-				if !isGnoFile(f) {
-					return nil // skip
-				}
-
-				parentDir := filepath.Dir(curpath)
-				if _, found := visited[parentDir]; found {
-					return nil
-				}
-				visited[parentDir] = true
-
-				pkg := parentDir
-				if !filepath.IsAbs(parentDir) {
-					// cannot use path.Join or filepath.Join, because we need
-					// to ensure that ./ is the prefix to pass to go build.
-					// if not absolute.
-					pkg = "./" + parentDir
-				}
-
-				paths = append(paths, pkg)
-				return nil
-			})
-			if err != nil {
-				return nil, err
-			}
+			continue
 		}
+
+		// if the passed arg is a dir, then we'll recursively walk the dir
+		// and look for directories containing at least one .gno file.
+
+		visited := map[string]bool{} // used to run the builder only once per folder.
+		err = filepath.WalkDir(arg, func(curpath string, f fs.DirEntry, err error) error {
+			if err != nil {
+				return fmt.Errorf("%s: walk dir: %w", arg, err)
+			}
+			if f.IsDir() {
+				return nil // skip
+			}
+			if !isGnoFile(f) {
+				return nil // skip
+			}
+
+			parentDir := filepath.Dir(curpath)
+			if _, found := visited[parentDir]; found {
+				return nil
+			}
+			visited[parentDir] = true
+
+			pkg := parentDir
+			if !filepath.IsAbs(parentDir) {
+				// cannot use path.Join or filepath.Join, because we need
+				// to ensure that ./ is the prefix to pass to go build.
+				// if not absolute.
+				pkg = "./" + parentDir
+			}
+
+			paths = append(paths, pkg)
+			return nil
+		})
+
+		if err != nil {
+			return nil, err
+		}
+
 	}
 	return paths, nil
 }
