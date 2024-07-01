581681df750d7fe27595fcb7fdde4b6eb0dc63f0
moul <94029+moul@users.noreply.github.com>
2024-07-01 17:20:17 +0200

    chore: fixup
    Signed-off-by: moul <94029+moul@users.noreply.github.com>

--- gno.land/pkg/gnoland/genesis_test.go
+++ gno.land/pkg/gnoland/genesis_test.go
@@ -1,7 +1,30 @@
 package gnoland
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+
+	bft "github.com/gnolang/gno/tm2/pkg/bft/types"
+	"github.com/gnolang/gno/tm2/pkg/std"
+)
 
 func TestLoadPackagesFromDir(t *testing.T) {
-	println("$$$$$$$$$")
+	var (
+		memPkg = &std.MemPackage{
+			Name: "foo",
+			Path: "gno.land/r/demo/foo",
+			Files: []*std.MemFile{
+				{Name: "empty.gno", Body: "package foo"},
+				{Name: "generated.gno.tpl", Body: `package foo
+
+/* {{.}} */
+`},
+			},
+		}
+		creator = bft.Address{}
+		fee     = std.Fee{}
+		deposit = std.Coins{}
+	)
+	tx, err := LoadPackage(memPkg, creator, fee, deposit)
+	fmt.Printf("$$$$$$$$$ tx=%v, err=%v\n", tx, err)
 }
