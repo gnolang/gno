38392ee1d1aaf859dc4a08b66af5acff29328134
grepsuzette <grepsuzette@users.noreply.github.com>
2024-07-04 13:32:24 +0800

    chore(gnodev): show link to docs upon 'h' key

--- contribs/gnodev/cmd/gnodev/main.go
+++ contribs/gnodev/cmd/gnodev/main.go
@@ -311,7 +311,7 @@ func execDev(cfg *devCfg, args []string, io commands.IO) (err error) {
 
 var helper string = `
 A           Accounts - Display known accounts and balances
-H           Help - Display this message
+H           Help - Also https://github.com/gnolang/gno/blob/master/docs/gno-tooling/cli/gnodev.md
 R           Reload - Reload all packages to take change into account.
 Ctrl+R      Reset - Reset application state.
 Ctrl+C      Exit - Exit the application
