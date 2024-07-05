cc546151f7e2f08f09361954c14c6b484efbbf10
Norman Meier <norman@samourai.coop>
2024-07-05 14:19:30 +0200

    chore: improve test
    Signed-off-by: Norman Meier <norman@samourai.coop>

--- gno.land/pkg/sdk/vm/convert_test.go
+++ gno.land/pkg/sdk/vm/convert_test.go
@@ -10,27 +10,28 @@ import (
 
 func TestConvertEmptyNumbers(t *testing.T) {
 	tests := []struct {
-		arg         string
 		argT        gnolang.Type
 		expectedErr string
 	}{
-		{"", gnolang.Uint64Type, `error parsing uint64 "": strconv.ParseUint: parsing "": invalid syntax`},
-		{"", gnolang.Uint32Type, `error parsing uint32 "": strconv.ParseUint: parsing "": invalid syntax`},
-		{"", gnolang.Uint16Type, `error parsing uint16 "": strconv.ParseUint: parsing "": invalid syntax`},
-		{"", gnolang.Uint8Type, `error parsing uint8 "": strconv.ParseUint: parsing "": invalid syntax`},
-		{"", gnolang.Int64Type, `error parsing int64 "": strconv.ParseInt: parsing "": invalid syntax`},
-		{"", gnolang.Int32Type, `error parsing int32 "": strconv.ParseInt: parsing "": invalid syntax`},
-		{"", gnolang.Int16Type, `error parsing int16 "": strconv.ParseInt: parsing "": invalid syntax`},
-		{"", gnolang.Int8Type, `error parsing int8 "": strconv.ParseInt: parsing "": invalid syntax`},
-		{"", gnolang.Float64Type, `error parsing float64 "": parse mantissa: `},
-		{"", gnolang.Float32Type, `error parsing float32 "": parse mantissa: `},
+		{gnolang.UintType, `error parsing uint "": strconv.ParseUint: parsing "": invalid syntax`},
+		{gnolang.Uint64Type, `error parsing uint64 "": strconv.ParseUint: parsing "": invalid syntax`},
+		{gnolang.Uint32Type, `error parsing uint32 "": strconv.ParseUint: parsing "": invalid syntax`},
+		{gnolang.Uint16Type, `error parsing uint16 "": strconv.ParseUint: parsing "": invalid syntax`},
+		{gnolang.Uint8Type, `error parsing uint8 "": strconv.ParseUint: parsing "": invalid syntax`},
+		{gnolang.IntType, `error parsing int "": strconv.ParseInt: parsing "": invalid syntax`},
+		{gnolang.Int64Type, `error parsing int64 "": strconv.ParseInt: parsing "": invalid syntax`},
+		{gnolang.Int32Type, `error parsing int32 "": strconv.ParseInt: parsing "": invalid syntax`},
+		{gnolang.Int16Type, `error parsing int16 "": strconv.ParseInt: parsing "": invalid syntax`},
+		{gnolang.Int8Type, `error parsing int8 "": strconv.ParseInt: parsing "": invalid syntax`},
+		{gnolang.Float64Type, `error parsing float64 "": parse mantissa: `},
+		{gnolang.Float32Type, `error parsing float32 "": parse mantissa: `},
 	}
 
 	for _, tt := range tests {
-		testname := fmt.Sprintf("%q,%v", tt.arg, tt.argT)
+		testname := fmt.Sprintf("%v", tt.argT)
 		t.Run(testname, func(t *testing.T) {
 			run := func() {
-				_ = convertArgToGno(tt.arg, tt.argT)
+				_ = convertArgToGno("", tt.argT)
 			}
 			assert.PanicsWithValue(t, tt.expectedErr, run)
 		})
