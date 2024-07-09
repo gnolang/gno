2578356d0609fd0b5bdb746d28ad7e2eff6bde01
gfanton <8671905+gfanton@users.noreply.github.com>
2024-07-09 13:50:01 +0200

    chore: update gas deliver
    Signed-off-by: gfanton <8671905+gfanton@users.noreply.github.com>

--- gno.land/pkg/sdk/vm/gas_test.go
+++ gno.land/pkg/sdk/vm/gas_test.go
@@ -69,7 +69,7 @@ func TestAddPkgDeliverTx(t *testing.T) {
 	assert.True(t, res.IsOK())
 
 	// NOTE: let's try to keep this bellow 100_000 :)
-	assert.Equal(t, int64(91825), gasDeliver)
+	assert.Equal(t, int64(92825), gasDeliver)
 }
 
 // Enough gas for a failed transaction.
