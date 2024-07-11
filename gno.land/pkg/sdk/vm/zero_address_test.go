1a0f1de4d6ee5411f87031ba04a05f5d3f4d63e2
ThinhNX <thinhnx@var-meta.com>
2024-07-11 11:11:04 +0700

    update test files

--- gno.land/pkg/sdk/vm/zero_address_test.go
+++ gno.land/pkg/sdk/vm/zero_address_test.go
@@ -8,5 +8,5 @@ import (
 
 func Test_ZAccount(t *testing.T) {
 	zA := ZeroAddress()
-	assert.Exactly(t, zA.String(), "g100000000000000000000000000000000dnmcnx")
+	assert.Exactly(t, zA.String(), "g1qqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqluuxe")
 }
