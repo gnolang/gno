gnolang/gno

commit 5ad12cd2b668607f291ffa4787f9184e59970ca9

feat: remove `gnovm` and `gno.land` dependencies from tm2 (#1483)
This PR shares the same goal as #1438: to make `tm2` completely unaware
of `gnovm` and `gno.land`. However, it adopts a different strategy to
achieve this, following
https://github.com/gnolang/gno/pull/1438#issuecomment-1866686822. It
will:
- Exposed almost everything from the `tm2/crypto/keys/client` package:
- `NewCommandXXX`, along with their specific structure configurations
and fields, are now exposed to allow the creation of composable CLI
tools.
- `XXXHandler` (including Sign, Verify, Broadcast, etc.) are now exposed
to enable external packages to reuse some logic from the `keys/client`
package.
- Moved specific `gnovm`/`gno.land` commands to the `gnokey` package:
`run`, `call`, and `addpkg`.
- In an effort to avoid duplicate code, an `ExecSignAndBroadcast` method
has been exposed. However, it appears too specific, and I am considering
duplicating it regardless. I would appreciate thoughts on this.




<details><summary>Contributors' checklist...</summary>

- [ ] Added new tests, or not needed, or not feasible
- [ ] Provided an example (e.g. screenshot) to aid review or the PR is
self-explanatory
- [ ] Updated the official documentation or not needed
- [ ] No breaking changes were made, or a `BREAKING CHANGE: xxx` message
was included in the description
- [ ] Added references to related issues and PRs
- [ ] Provided any useful hints for running manual tests
- [ ] Added new benchmarks to [generated
graphs](https://gnoland.github.io/benchmarks), if any. More info
[here](https://github.com/gnolang/gno/blob/master/.benchmarks/README.md).
</details>

---------

Signed-off-by: gfanton <8671905+gfanton@users.noreply.github.com>

contribs/gnokeykc/go.mod
@@ -15,9 +15,7 @@ require (
 	github.com/btcsuite/btcd/btcutil v1.1.3 // indirect
 	github.com/cespare/xxhash v1.1.0 // indirect
 	github.com/cespare/xxhash/v2 v2.1.1 // indirect
-	github.com/cockroachdb/apd/v3 v3.2.1 // indirect
 	github.com/danieljoos/wincred v1.2.0 // indirect
-	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/decred/dcrd/dcrec/secp256k1/v4 v4.2.0 // indirect
 	github.com/dgraph-io/badger/v3 v3.2103.4 // indirect
 	github.com/dgraph-io/ristretto v0.1.1 // indirect
@@ -43,13 +41,10 @@ require (
 	github.com/tecbot/gorocksdb v0.0.0-20191217155057-f0fad39f321c // indirect
 	go.etcd.io/bbolt v1.3.8 // indirect
 	go.opencensus.io v0.22.5 // indirect
-	go.uber.org/atomic v1.7.0 // indirect
-	go.uber.org/multierr v1.9.0 // indirect
 	golang.org/x/crypto v0.15.0 // indirect
 	golang.org/x/mod v0.14.0 // indirect
 	golang.org/x/net v0.17.0 // indirect
 	golang.org/x/sys v0.14.0 // indirect
 	golang.org/x/term v0.14.0 // indirect
-	golang.org/x/tools v0.13.0 // indirect
 	google.golang.org/protobuf v1.31.0 // indirect
 )

contribs/gnokeykc/go.sum
@@ -35,8 +35,6 @@ github.com/cespare/xxhash v1.1.0/go.mod h1:XrSqR1VqqWfGrhpAt58auRo0WTKS1nRRg3ghf
 github.com/cespare/xxhash/v2 v2.1.1 h1:6MnRN8NT7+YBpUIWxHtefFZOKTAPgGjpQSxqLNn0+qY=
 github.com/cespare/xxhash/v2 v2.1.1/go.mod h1:VGX0DQ3Q6kWi7AoAeZDth3/j3BFtOZR5XLFGgcrjCOs=
 github.com/client9/misspell v0.3.4/go.mod h1:qj6jICC3Q7zFZvVWo7KLAzC3yx5G7kyvSDkc90ppPyw=
-github.com/cockroachdb/apd/v3 v3.2.1 h1:U+8j7t0axsIgvQUqthuNm82HIrYXodOV2iWLWtEaIwg=
-github.com/cockroachdb/apd/v3 v3.2.1/go.mod h1:klXJcjp+FffLTHlhIG69tezTDvdP065naDsHzKhYSqc=
 github.com/coreos/etcd v3.3.10+incompatible/go.mod h1:uF7uidLiAD3TWHmW31ZFd/JWoc32PjwdhPthX9715RE=
 github.com/coreos/go-etcd v2.0.0+incompatible/go.mod h1:Jez6KQU2B/sWsbdaef3ED8NzMklzPG4d5KIOhIy30Tk=
 github.com/coreos/go-semver v0.2.0/go.mod h1:nnelYz7RCh+5ahJtPPxZlU+153eP4D4r3EedlOD2RNk=
@@ -123,7 +121,6 @@ github.com/klauspost/compress v1.12.3/go.mod h1:8dP1Hq4DHOhN9w426knH3Rhby4rFm6D8
 github.com/kr/pretty v0.1.0/go.mod h1:dAy3ld7l9f0ibDNOQOHHMYYIIbhfbHSm3C4ZsoJORNo=
 github.com/kr/pty v1.1.1/go.mod h1:pFQYn66WHrOpPYNljwOMqo10TkYh1fy3cYio2l3bCsQ=
 github.com/kr/text v0.1.0/go.mod h1:4Jbv+DJW3UT/LiOwJeYQe1efqtUx/iVham/4vfdArNI=
-github.com/lib/pq v1.10.7 h1:p7ZhMD+KsSRozJr34udlUrhboJwWAgCg34+/ZZNvZZw=
 github.com/libp2p/go-buffer-pool v0.1.0 h1:oK4mSFcQz7cTQIfqbe4MIj9gLW+mnanjyFtc6cdF0Y8=
 github.com/libp2p/go-buffer-pool v0.1.0/go.mod h1:N+vh8gMqimBzdKkSMVuydVDq+UV5QTWy5HSiZacSbPg=
 github.com/linxGnu/grocksdb v1.8.5 h1:Okfk5B1h0ikCYdDM7Tc5yJUS8LTwAmMBq5IPWTmOLPs=
@@ -167,7 +164,6 @@ github.com/spf13/viper v1.3.2/go.mod h1:ZiWeW+zYFKm7srdB9IoDzzZXaJaI5eL9QjNiN/DM
 github.com/stretchr/objx v0.1.0/go.mod h1:HFkY916IF+rwdDfMAkV7OtwuqBVzrE8GR6GFx+wExME=
 github.com/stretchr/objx v0.5.0 h1:1zr/of2m5FGMsad5YfcqgdqdWrIhu+EBEJRhR1U7z/c=
 github.com/stretchr/testify v1.2.2/go.mod h1:a8OnRcib4nhh0OaRAV+Yts87kKdq0PP7pXfy6kDkUVs=
-github.com/stretchr/testify v1.3.0/go.mod h1:M5WIy9Dh21IEIfnGCwXGc5bZfKNJtfHm1UVUgZn+9EI=
 github.com/stretchr/testify v1.4.0/go.mod h1:j7eGeouHqKxXV5pUuKE4zz7dFj8WfuZ+81PSLYec5m4=
 github.com/stretchr/testify v1.7.0/go.mod h1:6Fq8oRcR53rry900zMqJjRRixrwX3KX962/h/Wwjteg=
 github.com/stretchr/testify v1.8.4 h1:CcVxjf3Q8PM0mHUKJCdn+eZZtm5yQwehR5yeSVQQcUk=
@@ -184,10 +180,6 @@ go.etcd.io/bbolt v1.3.8 h1:xs88BrvEv273UsB79e0hcVrlUWmS0a8upikMFhSyAtA=
 go.etcd.io/bbolt v1.3.8/go.mod h1:N9Mkw9X8x5fupy0IKsmuqVtoGDyxsaDlbk4Rd05IAQw=
 go.opencensus.io v0.22.5 h1:dntmOdLpSpHlVqbW5Eay97DelsZHe+55D+xC6i0dDS0=
 go.opencensus.io v0.22.5/go.mod h1:5pWMHQbX5EPX2/62yrJeAkowc+lfs/XD7Uxpq3pI6kk=
-go.uber.org/atomic v1.7.0 h1:ADUqmZGgLDDfbSL9ZmPxKTybcoEYHgpYfELNoN+7hsw=
-go.uber.org/atomic v1.7.0/go.mod h1:fEN4uk6kAWBTFdckzkM89CLk9XfWZrxpCo0nPH17wJc=
-go.uber.org/multierr v1.9.0 h1:7fIwc/ZtS0q++VgcfqFDxSBZVv/Xo49/SYnDFupUwlI=
-go.uber.org/multierr v1.9.0/go.mod h1:X2jQV1h+kxSjClGpnseKVIxpmcjrj7MNnI0bnlfKTVQ=
 golang.org/x/crypto v0.0.0-20170930174604-9419663f5a44/go.mod h1:6SG95UA2DQfeDnfUPMdvaQW0Q7yPrPDi9nlGo2tz2b4=
 golang.org/x/crypto v0.0.0-20181203042331-505ab145d0a9/go.mod h1:6SG95UA2DQfeDnfUPMdvaQW0Q7yPrPDi9nlGo2tz2b4=
 golang.org/x/crypto v0.0.0-20190308221718-c2843e01d9a2/go.mod h1:djNgcEr1/C05ACkg1iLfiJU5Ep61QUkGW8qpdssI0+w=
@@ -253,8 +245,6 @@ golang.org/x/tools v0.0.0-20190311212946-11955173bddd/go.mod h1:LCzVGOaR6xXOjkQ3
 golang.org/x/tools v0.0.0-20191119224855-298f0cb1881e/go.mod h1:b+2E5dAYhXwXZwtnZ6UAqBI28+e2cm9otk0dWdXHAEo=
 golang.org/x/tools v0.0.0-20200619180055-7c47624df98f/go.mod h1:EkVYQZoAsY45+roYkvgYkIh4xh/qjgUK9TdY2XT94GE=
 golang.org/x/tools v0.0.0-20210106214847-113979e3529a/go.mod h1:emZCQorbCU4vsT4fOWvOPXz4eW1wZW4PmDk9uLelYpA=
-golang.org/x/tools v0.13.0 h1:Iey4qkscZuv0VvIt8E0neZjtPVQFSc870HQ448QgEmQ=
-golang.org/x/tools v0.13.0/go.mod h1:HvlwmtVNQAhOuCjW7xxvovg8wbNq7LwfXh/k7wXUl58=
 golang.org/x/xerrors v0.0.0-20190717185122-a985d3407aa7/go.mod h1:I/5z698sn9Ka8TeJc9MKroUUfqBBauWjQqLJ2OPfmY0=
 golang.org/x/xerrors v0.0.0-20191011141410-1b5146add898/go.mod h1:I/5z698sn9Ka8TeJc9MKroUUfqBBauWjQqLJ2OPfmY0=
 golang.org/x/xerrors v0.0.0-20191204190536-9bdfabe68543/go.mod h1:I/5z698sn9Ka8TeJc9MKroUUfqBBauWjQqLJ2OPfmY0=

gno.land/cmd/gnokey/main.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"os"
 
+	"github.com/gnolang/gno/gno.land/pkg/keyscli"
 	"github.com/gnolang/gno/gnovm/pkg/gnoenv"
 	"github.com/gnolang/gno/tm2/pkg/commands"
 	"github.com/gnolang/gno/tm2/pkg/crypto/keys/client"
@@ -15,6 +16,6 @@ func main() {
 		Remote: "127.0.0.1:26657",
 	}
 
-	cmd := client.NewRootCmdWithBaseConfig(commands.NewDefaultIO(), baseCfg)
+	cmd := keyscli.NewRootCmd(commands.NewDefaultIO(), baseCfg)
 	cmd.Execute(context.Background(), os.Args[1:])
 }

gno.land/pkg/integration/testing_integration.go
@@ -12,6 +12,7 @@ import (
 	"testing"
 
 	"github.com/gnolang/gno/gno.land/pkg/gnoland"
+	"github.com/gnolang/gno/gno.land/pkg/keyscli"
 	"github.com/gnolang/gno/gnovm/pkg/gnoenv"
 	"github.com/gnolang/gno/tm2/pkg/bft/node"
 	"github.com/gnolang/gno/tm2/pkg/commands"
@@ -214,7 +215,7 @@ func setupGnolandTestScript(t *testing.T, txtarDir string) testscript.Params {
 				io := commands.NewTestIO()
 				io.SetOut(commands.WriteNopCloser(ts.Stdout()))
 				io.SetErr(commands.WriteNopCloser(ts.Stderr()))
-				cmd := client.NewRootCmd(io)
+				cmd := keyscli.NewRootCmd(io, client.DefaultBaseOptions)
 
 				io.SetIn(strings.NewReader("\n")) // Inject empty password to stdin.
 				defaultArgs := []string{

gno.land/pkg/keyscli/addpkg.go
@@ -1,0 +1,138 @@
+package keyscli
+
+import (
+	"context"
+	"flag"
+	"fmt"
+
+	"github.com/gnolang/gno/gno.land/pkg/sdk/vm"
+	gno "github.com/gnolang/gno/gnovm/pkg/gnolang"
+	"github.com/gnolang/gno/tm2/pkg/amino"
+	"github.com/gnolang/gno/tm2/pkg/commands"
+	"github.com/gnolang/gno/tm2/pkg/crypto/keys"
+	"github.com/gnolang/gno/tm2/pkg/crypto/keys/client"
+	"github.com/gnolang/gno/tm2/pkg/errors"
+	"github.com/gnolang/gno/tm2/pkg/std"
+)
+
+type MakeAddPkgCfg struct {
+	RootCfg *client.MakeTxCfg
+
+	PkgPath string
+	PkgDir  string
+	Deposit string
+}
+
+func NewMakeAddPkgCmd(rootCfg *client.MakeTxCfg, io commands.IO) *commands.Command {
+	cfg := &MakeAddPkgCfg{
+		RootCfg: rootCfg,
+	}
+
+	return commands.NewCommand(
+		commands.Metadata{
+			Name:       "addpkg",
+			ShortUsage: "addpkg [flags] <key-name>",
+			ShortHelp:  "Uploads a new package",
+		},
+		cfg,
+		func(_ context.Context, args []string) error {
+			return execMakeAddPkg(cfg, args, io)
+		},
+	)
+}
+
+func (c *MakeAddPkgCfg) RegisterFlags(fs *flag.FlagSet) {
+	fs.StringVar(
+		&c.PkgPath,
+		"pkgpath",
+		"",
+		"package path (required)",
+	)
+
+	fs.StringVar(
+		&c.PkgDir,
+		"pkgdir",
+		"",
+		"path to package files (required)",
+	)
+
+	fs.StringVar(
+		&c.Deposit,
+		"deposit",
+		"",
+		"deposit coins",
+	)
+}
+
+func execMakeAddPkg(cfg *MakeAddPkgCfg, args []string, io commands.IO) error {
+	if cfg.PkgPath == "" {
+		return errors.New("pkgpath not specified")
+	}
+	if cfg.PkgDir == "" {
+		return errors.New("pkgdir not specified")
+	}
+
+	if len(args) != 1 {
+		return flag.ErrHelp
+	}
+
+	// read account pubkey.
+	nameOrBech32 := args[0]
+	kb, err := keys.NewKeyBaseFromDir(cfg.RootCfg.RootCfg.Home)
+	if err != nil {
+		return err
+	}
+	info, err := kb.GetByNameOrAddress(nameOrBech32)
+	if err != nil {
+		return err
+	}
+	creator := info.GetAddress()
+	// info.GetPubKey()
+
+	// parse deposit.
+	deposit, err := std.ParseCoins(cfg.Deposit)
+	if err != nil {
+		panic(err)
+	}
+
+	// open files in directory as MemPackage.
+	memPkg := gno.ReadMemPackage(cfg.PkgDir, cfg.PkgPath)
+	if memPkg.IsEmpty() {
+		panic(fmt.Sprintf("found an empty package %q", cfg.PkgPath))
+	}
+
+	// precompile and validate syntax
+	err = gno.PrecompileAndCheckMempkg(memPkg)
+	if err != nil {
+		panic(err)
+	}
+
+	// parse gas wanted & fee.
+	gaswanted := cfg.RootCfg.GasWanted
+	gasfee, err := std.ParseCoin(cfg.RootCfg.GasFee)
+	if err != nil {
+		panic(err)
+	}
+	// construct msg & tx and marshal.
+	msg := vm.MsgAddPackage{
+		Creator: creator,
+		Package: memPkg,
+		Deposit: deposit,
+	}
+	tx := std.Tx{
+		Msgs:       []std.Msg{msg},
+		Fee:        std.NewFee(gaswanted, gasfee),
+		Signatures: nil,
+		Memo:       cfg.RootCfg.Memo,
+	}
+
+	if cfg.RootCfg.Broadcast {
+		err := client.ExecSignAndBroadcast(cfg.RootCfg, args, tx, io)
+		if err != nil {
+			return err
+		}
+	} else {
+		fmt.Println(string(amino.MustMarshalJSON(tx)))
+	}
+	return nil
+}

gno.land/pkg/keyscli/call.go
@@ -1,4 +1,4 @@
-package client
+package keyscli
 
 import (
 	"context"
@@ -9,22 +9,23 @@ import (
 	"github.com/gnolang/gno/tm2/pkg/amino"
 	"github.com/gnolang/gno/tm2/pkg/commands"
 	"github.com/gnolang/gno/tm2/pkg/crypto/keys"
+	"github.com/gnolang/gno/tm2/pkg/crypto/keys/client"
 	"github.com/gnolang/gno/tm2/pkg/errors"
 	"github.com/gnolang/gno/tm2/pkg/std"
 )
 
-type callCfg struct {
-	rootCfg *makeTxCfg
+type MakeCallCfg struct {
+	RootCfg *client.MakeTxCfg
 
-	send     string
-	pkgPath  string
-	funcName string
-	args     commands.StringArr
+	Send     string
+	PkgPath  string
+	FuncName string
+	Args     commands.StringArr
 }
 
-func newCallCmd(rootCfg *makeTxCfg, io commands.IO) *commands.Command {
-	cfg := &callCfg{
-		rootCfg: rootCfg,
+func NewMakeCallCmd(rootCfg *client.MakeTxCfg, io commands.IO) *commands.Command {
+	cfg := &MakeCallCfg{
+		RootCfg: rootCfg,
 	}
 
 	return commands.NewCommand(
@@ -35,63 +36,63 @@ func newCallCmd(rootCfg *makeTxCfg, io commands.IO) *commands.Command {
 		},
 		cfg,
 		func(_ context.Context, args []string) error {
-			return execCall(cfg, args, io)
+			return execMakeCall(cfg, args, io)
 		},
 	)
 }
 
-func (c *callCfg) RegisterFlags(fs *flag.FlagSet) {
+func (c *MakeCallCfg) RegisterFlags(fs *flag.FlagSet) {
 	fs.StringVar(
-		&c.send,
+		&c.Send,
 		"send",
 		"",
 		"send amount",
 	)
 
 	fs.StringVar(
-		&c.pkgPath,
+		&c.PkgPath,
 		"pkgpath",
 		"",
 		"package path (required)",
 	)
 
 	fs.StringVar(
-		&c.funcName,
+		&c.FuncName,
 		"func",
 		"",
 		"contract to call (required)",
 	)
 
 	fs.Var(
-		&c.args,
+		&c.Args,
 		"args",
 		"arguments to contract",
 	)
 }
 
-func execCall(cfg *callCfg, args []string, io commands.IO) error {
-	if cfg.pkgPath == "" {
+func execMakeCall(cfg *MakeCallCfg, args []string, io commands.IO) error {
+	if cfg.PkgPath == "" {
 		return errors.New("pkgpath not specified")
 	}
-	if cfg.funcName == "" {
+	if cfg.FuncName == "" {
 		return errors.New("func not specified")
 	}
 	if len(args) != 1 {
 		return flag.ErrHelp
 	}
-	if cfg.rootCfg.gasWanted == 0 {
+	if cfg.RootCfg.GasWanted == 0 {
 		return errors.New("gas-wanted not specified")
 	}
-	if cfg.rootCfg.gasFee == "" {
+	if cfg.RootCfg.GasFee == "" {
 		return errors.New("gas-fee not specified")
 	}
 
 	// read statement.
-	fnc := cfg.funcName
+	fnc := cfg.FuncName
 
 	// read account pubkey.
 	nameOrBech32 := args[0]
-	kb, err := keys.NewKeyBaseFromDir(cfg.rootCfg.rootCfg.Home)
+	kb, err := keys.NewKeyBaseFromDir(cfg.RootCfg.RootCfg.Home)
 	if err != nil {
 		return err
 	}
@@ -103,14 +104,14 @@ func execCall(cfg *callCfg, args []string, io commands.IO) error {
 	// info.GetPubKey()
 
 	// Parse send amount.
-	send, err := std.ParseCoins(cfg.send)
+	send, err := std.ParseCoins(cfg.Send)
 	if err != nil {
 		return errors.Wrap(err, "parsing send coins")
 	}
 
 	// parse gas wanted & fee.
-	gaswanted := cfg.rootCfg.gasWanted
-	gasfee, err := std.ParseCoin(cfg.rootCfg.gasFee)
+	gaswanted := cfg.RootCfg.GasWanted
+	gasfee, err := std.ParseCoin(cfg.RootCfg.GasFee)
 	if err != nil {
 		return errors.Wrap(err, "parsing gas fee coin")
 	}
@@ -119,19 +120,19 @@ func execCall(cfg *callCfg, args []string, io commands.IO) error {
 	msg := vm.MsgCall{
 		Caller:  caller,
 		Send:    send,
-		PkgPath: cfg.pkgPath,
+		PkgPath: cfg.PkgPath,
 		Func:    fnc,
-		Args:    cfg.args,
+		Args:    cfg.Args,
 	}
 	tx := std.Tx{
 		Msgs:       []std.Msg{msg},
 		Fee:        std.NewFee(gaswanted, gasfee),
 		Signatures: nil,
-		Memo:       cfg.rootCfg.memo,
+		Memo:       cfg.RootCfg.Memo,
 	}
 
-	if cfg.rootCfg.broadcast {
-		err := signAndBroadcast(cfg.rootCfg, args, tx, io)
+	if cfg.RootCfg.Broadcast {
+		err := client.ExecSignAndBroadcast(cfg.RootCfg, args, tx, io)
 		if err != nil {
 			return err
 		}

gno.land/pkg/keyscli/maketx.go
@@ -1,0 +1,83 @@
+package keyscli
+
+import (
+	"flag"
+
+	"github.com/gnolang/gno/tm2/pkg/commands"
+	"github.com/gnolang/gno/tm2/pkg/crypto/keys/client"
+)
+
+type MakeTxCfg struct {
+	RootCfg *client.BaseCfg
+
+	GasWanted int64
+	GasFee    string
+	Memo      string
+
+	Broadcast bool
+	ChainID   string
+}
+
+func NewMakeTxCmd(rootCfg *client.BaseCfg, io commands.IO) *commands.Command {
+	cfg := &client.MakeTxCfg{
+		RootCfg: rootCfg,
+	}
+
+	cmd := commands.NewCommand(
+		commands.Metadata{
+			Name:       "maketx",
+			ShortUsage: "<subcommand> [flags] [<arg>...]",
+			ShortHelp:  "Composes a tx document to sign",
+		},
+		cfg,
+		commands.HelpExec,
+	)
+
+	cmd.AddSubCommands(
+		client.NewMakeSendCmd(cfg, io),
+
+		// custom commands
+		NewMakeAddPkgCmd(cfg, io),
+		NewMakeCallCmd(cfg, io),
+		NewMakeRunCmd(cfg, io),
+	)
+
+	return cmd
+}
+
+func (c *MakeTxCfg) RegisterFlags(fs *flag.FlagSet) {
+	fs.Int64Var(
+		&c.GasWanted,
+		"gas-wanted",
+		0,
+		"gas requested for tx",
+	)
+
+	fs.StringVar(
+		&c.GasFee,
+		"gas-fee",
+		"",
+		"gas payment fee",
+	)
+
+	fs.StringVar(
+		&c.Memo,
+		"memo",
+		"",
+		"any descriptive text",
+	)
+
+	fs.BoolVar(
+		&c.Broadcast,
+		"broadcast",
+		false,
+		"sign and broadcast",
+	)
+
+	fs.StringVar(
+		&c.ChainID,
+		"chainid",
+		"dev",
+		"chainid to sign for (only useful if --broadcast)",
+	)
+}

gno.land/pkg/keyscli/root.go
@@ -1,0 +1,47 @@
+// Dedicated to my love, Lexi.
+package keyscli
+
+import (
+	"github.com/gnolang/gno/tm2/pkg/commands"
+	"github.com/gnolang/gno/tm2/pkg/crypto/keys/client"
+
+	"github.com/peterbourgon/ff/v3"
+	"github.com/peterbourgon/ff/v3/fftoml"
+)
+
+func NewRootCmd(io commands.IO, base client.BaseOptions) *commands.Command {
+	cfg := &client.BaseCfg{
+		BaseOptions: base,
+	}
+
+	cmd := commands.NewCommand(
+		commands.Metadata{
+			ShortUsage: "<subcommand> [flags] [<arg>...]",
+			LongHelp:   "Manages private keys for the node",
+			Options: []ff.Option{
+				ff.WithConfigFileFlag("config"),
+				ff.WithConfigFileParser(fftoml.Parser),
+			},
+		},
+		cfg,
+		commands.HelpExec,
+	)
+
+	cmd.AddSubCommands(
+		client.NewAddCmd(cfg, io),
+		client.NewDeleteCmd(cfg, io),
+		client.NewGenerateCmd(cfg, io),
+		client.NewExportCmd(cfg, io),
+		client.NewImportCmd(cfg, io),
+		client.NewListCmd(cfg, io),
+		client.NewSignCmd(cfg, io),
+		client.NewVerifyCmd(cfg, io),
+		client.NewQueryCmd(cfg, io),
+		client.NewBroadcastCmd(cfg, io),
+
+		// Custom MakeTX command
+		NewMakeTxCmd(cfg, io),
+	)
+
+	return cmd
+}

commit 4ff6bc76ff998f75b3747aa178acaf6e7b4ee186

fix(gnokeykc): gnokeykc maketx {addpkg,call,run} missing because of tm2 separation (fixes #2373) (#2375)
Fixes #2373. 

Seems `gnokeykc` was indirectly broken in #1483 (when tm2 was made
completely independent from gno messages as addpkg).

`gnokey` now makes use of `github.com/gnolang/gno/gno.land/pkg/keyscli`
and this PR proposes to use the same root cmd.


https://github.com/gnolang/gno/blob/5fdbce0e1fe533165c0c2009e93695ece677b2bd/gno.land/pkg/keyscli/README.md?plain=1#L1-L12

contribs/gnokeykc/go.mod
@@ -16,6 +16,7 @@ require (
 	github.com/btcsuite/btcd/btcec/v2 v2.3.3 // indirect
 	github.com/btcsuite/btcd/btcutil v1.1.5 // indirect
 	github.com/cenkalti/backoff/v4 v4.3.0 // indirect
+	github.com/cockroachdb/apd/v3 v3.2.1 // indirect
 	github.com/cosmos/ledger-cosmos-go v0.13.3 // indirect
 	github.com/danieljoos/wincred v1.2.0 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
@@ -47,6 +48,7 @@ require (
 	go.opentelemetry.io/otel/sdk/metric v1.28.0 // indirect
 	go.opentelemetry.io/otel/trace v1.28.0 // indirect
 	go.opentelemetry.io/proto/otlp v1.3.1 // indirect
+	go.uber.org/multierr v1.11.0 // indirect
 	golang.org/x/crypto v0.25.0 // indirect
 	golang.org/x/exp v0.0.0-20240613232115-7f521ea00fb8 // indirect
 	golang.org/x/mod v0.19.0 // indirect

contribs/gnokeykc/go.sum
@@ -30,6 +30,8 @@ github.com/btcsuite/websocket v0.0.0-20150119174127-31079b680792/go.mod h1:ghJtE
 github.com/btcsuite/winsvc v1.0.0/go.mod h1:jsenWakMcC0zFBFurPLEAyrnc/teJEM1O46fmI40EZs=
 github.com/cenkalti/backoff/v4 v4.3.0 h1:MyRJ/UdXutAwSAT+s3wNd7MfTIcy71VQueUuFK343L8=
 github.com/cenkalti/backoff/v4 v4.3.0/go.mod h1:Y3VNntkOUPxTVeUxJ/G5vcM//AlwfmyYozVcomhLiZE=
+github.com/cockroachdb/apd/v3 v3.2.1 h1:U+8j7t0axsIgvQUqthuNm82HIrYXodOV2iWLWtEaIwg=
+github.com/cockroachdb/apd/v3 v3.2.1/go.mod h1:klXJcjp+FffLTHlhIG69tezTDvdP065naDsHzKhYSqc=
 github.com/cosmos/ledger-cosmos-go v0.13.3 h1:7ehuBGuyIytsXbd4MP43mLeoN2LTOEnk5nvue4rK+yM=
 github.com/cosmos/ledger-cosmos-go v0.13.3/go.mod h1:HENcEP+VtahZFw38HZ3+LS3Iv5XV6svsnkk9vdJtLr8=
 github.com/danieljoos/wincred v1.2.0 h1:ozqKHaLK0W/ii4KVbbvluM91W2H3Sh0BncbUNPS7jLE=
@@ -93,6 +95,8 @@ github.com/kr/pretty v0.3.1 h1:flRD4NNwYAUpkphVc1HcthR4KEIFJ65n8Mw5qdRn3LE=
 github.com/kr/pretty v0.3.1/go.mod h1:hoEshYVHaxMs3cyo3Yncou5ZscifuDolrwPKZanG3xk=
 github.com/kr/text v0.2.0 h1:5Nx0Ya0ZqY2ygV366QzturHI13Jq95ApcVaJBhpS+AY=
 github.com/kr/text v0.2.0/go.mod h1:eLer722TekiGuMkidMxC/pM04lWEeraHUUmBw8l2grE=
+github.com/lib/pq v1.10.7 h1:p7ZhMD+KsSRozJr34udlUrhboJwWAgCg34+/ZZNvZZw=
+github.com/lib/pq v1.10.7/go.mod h1:AlVN5x4E4T544tWzH6hKfbfQvm3HdbOxrmggDNAPY9o=
 github.com/libp2p/go-buffer-pool v0.1.0 h1:oK4mSFcQz7cTQIfqbe4MIj9gLW+mnanjyFtc6cdF0Y8=
 github.com/libp2p/go-buffer-pool v0.1.0/go.mod h1:N+vh8gMqimBzdKkSMVuydVDq+UV5QTWy5HSiZacSbPg=
 github.com/nxadm/tail v1.4.4/go.mod h1:kenIhsEOeOJmVchQTgglprH7qJGnHDVpk1VPCcaMI8A=
@@ -152,6 +156,8 @@ go.opentelemetry.io/otel/trace v1.28.0 h1:GhQ9cUuQGmNDd5BTCP2dAvv75RdMxEfTmYejp+
 go.opentelemetry.io/otel/trace v1.28.0/go.mod h1:jPyXzNPg6da9+38HEwElrQiHlVMTnVfM3/yv2OlIHaI=
 go.opentelemetry.io/proto/otlp v1.3.1 h1:TrMUixzpM0yuc/znrFTP9MMRh8trP93mkCiDVeXrui0=
 go.opentelemetry.io/proto/otlp v1.3.1/go.mod h1:0X1WI4de4ZsLrrJNLAQbFeLCm3T7yBkR0XqQ7niQU+8=
+go.uber.org/multierr v1.11.0 h1:blXXJkSxSSfBVBlC76pxqeO+LN3aDfLQo+309xJstO0=
+go.uber.org/multierr v1.11.0/go.mod h1:20+QtiLqy0Nd6FdQB9TLXag12DsQkrbs3htMFfDN80Y=
 golang.org/x/crypto v0.0.0-20170930174604-9419663f5a44/go.mod h1:6SG95UA2DQfeDnfUPMdvaQW0Q7yPrPDi9nlGo2tz2b4=
 golang.org/x/crypto v0.0.0-20190308221718-c2843e01d9a2/go.mod h1:djNgcEr1/C05ACkg1iLfiJU5Ep61QUkGW8qpdssI0+w=
 golang.org/x/crypto v0.0.0-20200622213623-75b288015ac9/go.mod h1:LzIPMQfyMNhhGPhUkYOs5KpL4U8rLKemX1yGLhDgUto=

contribs/gnokeykc/main.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"os"
 
+	"github.com/gnolang/gno/gno.land/pkg/keyscli"
 	"github.com/gnolang/gno/gnovm/pkg/gnoenv"
 	"github.com/gnolang/gno/tm2/pkg/commands"
 	"github.com/gnolang/gno/tm2/pkg/crypto/keys/client"
@@ -15,7 +16,7 @@ func main() {
 	wrappedio := &wrappedIO{IO: stdio}
 	baseCfg := client.DefaultBaseOptions
 	baseCfg.Home = gnoenv.HomeDir()
-	cmd := client.NewRootCmdWithBaseConfig(wrappedio, baseCfg)
+	cmd := keyscli.NewRootCmd(wrappedio, baseCfg)
 	cmd.AddSubCommands(newKcCmd(stdio))
 
 	cmd.Execute(context.Background(), os.Args[1:])