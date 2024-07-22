e649de519ea1fd7ec60399c7ab4df0b85c7e8cde
n0izn0iz <n0izn0iz@users.noreply.github.com>
2024-07-22 23:28:17 +0200

    chore: SaveABCIResponses internal usage note
    Co-authored-by: Morgan <git@howl.moe>

--- tm2/pkg/bft/state/store.go
+++ tm2/pkg/bft/state/store.go
@@ -180,6 +180,7 @@ func LoadABCIResponses(db dbm.DB, height int64) (*ABCIResponses, error) {
 // SaveABCIResponses persists the ABCIResponses to the database.
 // This is useful in case we crash after app.Commit and before s.Save().
 // Responses are indexed by height so they can also be loaded later to produce Merkle proofs.
+// NOTE: this should only be used internally by the bft package and subpackages.
 func SaveABCIResponses(db dbm.DB, height int64, abciResponses *ABCIResponses) {
 	db.Set(CalcABCIResponsesKey(height), abciResponses.Bytes())
 }
