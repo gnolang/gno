86d658f43c16151e366c7e7cb755d87ed299354e
deelawn <dboltz03@gmail.com>
2024-07-30 18:08:31 -0700

    fix max names in block check

--- gnovm/pkg/gnolang/nodes.go
+++ gnovm/pkg/gnolang/nodes.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"go/parser"
 	"go/token"
+	"math"
 	"os"
 	"path/filepath"
 	"reflect"
@@ -1790,7 +1791,7 @@ func (sb *StaticBlock) Define2(isConst bool, n Name, st Type, tv TypedValue) {
 	if int(sb.NumNames) != len(sb.Names) {
 		panic("StaticBlock.NumNames and len(.Names) mismatch")
 	}
-	if (1<<16 - 1) < sb.NumNames {
+	if sb.NumNames == math.MaxUint16 {
 		panic("too many variables in block")
 	}
 	if tv.T == nil && tv.V != nil {
