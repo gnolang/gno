8737f115d31d04fa6f7dffeb2adfe23e433a2d15
irreverentsimplicity <irreverentsimplicity@proton.me>
2024-07-31 02:38:13 -0400

    cleanup

--- examples/gno.land/r/demo/zentasktic_core/addZentaskticCoreRealm.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-gnokey maketx addpkg --pkgpath "gno.land/r/demo/zentasktic_core" --pkgdir "./" --gas-fee 10000000ugnot --gas-wanted 20000000 --broadcast --chainid dev --remote localhost:26657 test
