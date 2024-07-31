6dde5b0acd449831580bb270715939ab4a23a258
irreverentsimplicity <irreverentsimplicity@proton.me>
2024-07-31 02:39:09 -0400

    cleanup

--- examples/gno.land/p/demo/zentasktic/addZentasktic.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-gnokey maketx addpkg --pkgpath "gno.land/p/demo/zentasktic" --pkgdir "./" --gas-fee 10000000ugnot --gas-wanted 20000000 --broadcast --chainid dev --remote localhost:26657 test
