d82b8527afbb6fa9d68c0000c68e5cb6353be79f
deelawn <dboltz03@gmail.com>
2024-08-19 13:51:10 -0700

    basic global params store

---
+++ tm2/pkg/sdk/params/keeper.go
@@ -0,0 +1,62 @@
+package params
+
+import (
+	"log/slog"
+
+	"github.com/gnolang/gno/tm2/pkg/amino"
+	"github.com/gnolang/gno/tm2/pkg/sdk"
+	"github.com/gnolang/gno/tm2/pkg/store"
+)
+
+const (
+	ModuleName = "params"
+
+	StoreKey = "params"
+
+	// ValueStorePrevfix is "/pv/" for param value.
+	ValueStoreKeyPrefix = "/pv/"
+)
+
+func ValueStoreKey(key string) []byte {
+	return append([]byte(ValueStoreKeyPrefix), []byte(key)...)
+}
+
+// Keeper of the global param store.
+type Keeper struct {
+	key store.StoreKey
+}
+
+// NewKeeper constructs a params keeper
+func NewKeeper(key store.StoreKey) Keeper {
+	return Keeper{
+		key: key,
+	}
+}
+
+// Logger returns a module-specific logger.
+func (k Keeper) Logger(ctx sdk.Context) *slog.Logger {
+	return ctx.Logger().With("module", ModuleName)
+}
+
+// GetParam gets a param value from the global param store.
+func (k Keeper) GetParam(ctx sdk.Context, key string, target interface{}) (bool, error) {
+	stor := ctx.Store(k.key)
+	bz := stor.Get(ValueStoreKey(key))
+	if bz == nil {
+		return false, nil
+	}
+
+	return true, amino.Unmarshal(bz, target)
+}
+
+// SetParam sets a param value to the global param store.
+func (k Keeper) SetParam(ctx sdk.Context, key string, param interface{}) error {
+	stor := ctx.Store(k.key)
+	bz, err := amino.Marshal(param)
+	if err != nil {
+		return err
+	}
+
+	stor.Set(ValueStoreKey(key), bz)
+	return nil
+}
