22f973f97d5825455ed2eebd57aec01d802ec8ff
ltzMaxwell <ltz.maxwell@gmail.com>
2024-08-27 10:02:11 +0800

    revert un-intentional change

--- gnovm/pkg/gnolang/uverse.go
+++ gnovm/pkg/gnolang/uverse.go
@@ -65,11 +65,9 @@ var (
 
 const uversePkgPath = ".uverse"
 
-var done bool
-
 // Always returns a new copy from the latest state of source.
 func Uverse() *PackageValue {
-	if uverseValue == nil || !done {
+	if uverseValue == nil {
 		pn := UverseNode()
 		uverseValue = pn.NewPackage()
 	}
@@ -1019,7 +1017,6 @@ func UverseNode() *PackageNode {
 			m.Exceptions = nil
 		},
 	)
-	done = true
 	return uverseNode
 }
 
