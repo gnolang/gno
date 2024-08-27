cba62bf17ab96ec088192721460d50afd1f04ef7
aeddi <antoine.e.b@gmail.com>
2024-08-27 15:25:02 +0200

    refactor(gnostats): remove config struct / add option

--- contribs/gnostats/agent/agent.go
+++ contribs/gnostats/agent/agent.go
@@ -8,21 +8,26 @@ import (
 	"github.com/gnolang/gnostats/proto"
 )
 
-// config holds both the injected Gno node RPC client and the Hub gRPC client.
-type config struct {
+// angent holds both the injected Gno node RPC client and the Hub gRPC client.
+type agent struct {
 	hClient      proto.HubClient
 	rClient      rpcClient
 	pollInterval time.Duration // Minimum time interval between two data points
 }
 
-type agent struct {
-	cfg config
+type Option func(*agent)
+
+// WithPollInterval sets the agent poll interval between two data points
+func WithPollInterval(interval time.Duration) Option {
+	return func(c *agent) {
+		c.pollInterval = interval
+	}
 }
 
 // Start registers with the Hub using Gno node static info, then pushes dynamic
 // info from the Gno node to the Hub at intervals specified by pollInterval
 func (a *agent) Start(ctx context.Context) error {
-	collector := NewCollector(a.cfg.rClient)
+	collector := NewCollector(a.rClient)
 
 	// Get static info from the Gno node
 	staticInfo, err := collector.CollectStatic(ctx)
@@ -31,12 +36,12 @@ func (a *agent) Start(ctx context.Context) error {
 	}
 
 	// Register with the Hub using static info
-	if _, err = a.cfg.hClient.Register(ctx, staticInfo); err != nil {
+	if _, err = a.hClient.Register(ctx, staticInfo); err != nil {
 		return fmt.Errorf("unable to register with hub: %w", err)
 	}
 
 	// Get the Hub data point stream
-	stream, err := a.cfg.hClient.PushData(ctx)
+	stream, err := a.hClient.PushData(ctx)
 	if err != nil {
 		return fmt.Errorf("unable to get data stream: %w", err)
 	}
@@ -55,7 +60,7 @@ func (a *agent) Start(ctx context.Context) error {
 		}
 
 		select {
-		case <-time.After(a.cfg.pollInterval):
+		case <-time.After(a.pollInterval):
 			// Wait for the specified interval before pushing a new data point
 		case <-ctx.Done():
 			return nil
@@ -63,9 +68,19 @@ func (a *agent) Start(ctx context.Context) error {
 	}
 }
 
-// NewAgent creates a new agent using the provided config
-func NewAgent(cfg config) *agent {
-	return &agent{
-		cfg: cfg,
+// NewAgent creates a new agent using the provided clients and options
+func NewAgent(hClient proto.HubClient, rClient rpcClient, options ...Option) *agent {
+	const defaultInverval = time.Second
+
+	a := &agent{
+		hClient:      hClient,
+		rClient:      rClient,
+		pollInterval: defaultInverval,
 	}
+
+	for _, opt := range options {
+		opt(a)
+	}
+
+	return a
 }
