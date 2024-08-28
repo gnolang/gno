441be4e1a8f1bc94d1ec220a576794d3fe684b49
Lee ByeongJun <lbj199874@gmail.com>
2024-08-28 22:54:12 +0900

    lint

--- gnovm/pkg/gnolang/preprocess_test.go
+++ gnovm/pkg/gnolang/preprocess_test.go
@@ -114,7 +114,6 @@ func main() {
 }
 
 func TestAssignmentsToBlankIdentifier(t *testing.T) {
-	t.Parallel()
 	tests := []struct {
 		name string
 		src  string
@@ -182,6 +181,7 @@ func main() {
 	for _, tt := range tests {
 		tt := tt
 		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
 			pn := NewPackageNode("main", "main", nil)
 			pv := pn.NewPackage()
 			store := gonativeTestStore(pn, pv)
