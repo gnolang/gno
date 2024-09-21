056ab0a3f5edb3e54e0735a3b9ed7846fe6f32b6
moul <94029+moul@users.noreply.github.com>
2024-09-21 21:23:40 +0200

    chore: fixup
    Signed-off-by: moul <94029+moul@users.noreply.github.com>

--- gno.land/pkg/sdk/vm/common_test.go
+++ gno.land/pkg/sdk/vm/common_test.go
@@ -61,6 +61,7 @@ func _setupTestEnv(cacheStdlibs bool) testEnv {
 	}
 	vmk.CommitGnoTransactionStore(stdlibCtx)
 	mcw.MultiWrite()
+	vmh := NewHandler(vmk)
 
 	return testEnv{ctx: ctx, vmk: vmk, bank: bank, acck: acck, vmh: vmh}
 }
