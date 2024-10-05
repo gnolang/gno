ec39ada2e6808d939c791f053869ed4f150f0d45
moul <94029+moul@users.noreply.github.com>
2024-10-05 13:55:11 +0200

    chore: fixup
    Signed-off-by: moul <94029+moul@users.noreply.github.com>

--- gnovm/pkg/gnolang/helpers.go
+++ gnovm/pkg/gnolang/helpers.go
@@ -10,19 +10,18 @@ import (
 // ----------------------------------------
 // Functions centralizing definitions
 
-// RealmPathPrefix is the prefix used to identify pkgpaths which are meant to
+// RealmPathPrefix is the regex used to identify pkgpaths which are meant to
 // be realms and as such to have their state persisted. This is used by [IsRealmPath].
-const RealmPathPrefix = "gno.land/r/"
+var RealmPathPrefix = regexp.MustCompile(`^([a-zA-Z0-9-]+\.)*[a-zA-Z0-9-]+\.[a-zA-Z]{2,}/r/`)
 
 // ReGnoRunPath is the path used for realms executed in maketx run.
 // These are not considered realms, as an exception to the RealmPathPrefix rule.
-var ReGnoRunPath = regexp.MustCompile(`^gno\.land/r/g[a-z0-9]+/run$`)
+var ReGnoRunPath = regexp.MustCompile(`^([a-zA-Z0-9-]+\.)*[a-zA-Z0-9-]+\.[a-zA-Z]{2,}/r/g[a-z0-9]+/run$`)
 
 // IsRealmPath determines whether the given pkgpath is for a realm, and as such
 // should persist the global state.
 func IsRealmPath(pkgPath string) bool {
-	return strings.HasPrefix(pkgPath, RealmPathPrefix) &&
-		// MsgRun pkgPath aren't realms
+	return RealmPathPrefix.MatchString(pkgPath) &&
 		!ReGnoRunPath.MatchString(pkgPath)
 }
 
