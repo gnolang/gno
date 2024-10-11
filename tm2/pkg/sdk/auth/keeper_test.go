6da36bba8b3e5853409a134be246b736559f4bce
piux2 <90544084+piux2@users.noreply.github.com>
2024-10-10 17:48:54 -0700

    more testing coverage

--- tm2/pkg/sdk/auth/keeper_test.go
+++ tm2/pkg/sdk/auth/keeper_test.go
@@ -1,6 +1,7 @@
 package auth
 
 import (
+	"math/big"
 	"testing"
 
 	"github.com/stretchr/testify/require"
@@ -96,4 +97,82 @@ func TestGasPrice(t *testing.T) {
 	env.gk.setGasPrice(env.ctx, gp)
 	gp2 := env.gk.LastGasPrice(env.ctx)
 	require.True(t, gp == gp2)
+}
+
+func TestMax(t *testing.T) {
+	tests := []struct {
+		name     string
+		x, y     *big.Int
+		expected *big.Int
+	}{
+		{
+			name:     "X is less than Y",
+			x:        big.NewInt(5),
+			y:        big.NewInt(10),
+			expected: big.NewInt(10),
+		},
+		{
+			name:     "X is greater than Y",
+			x:        big.NewInt(15),
+			y:        big.NewInt(10),
+			expected: big.NewInt(15),
+		},
+		{
+			name:     "X is equal to Y",
+			x:        big.NewInt(10),
+			y:        big.NewInt(10),
+			expected: big.NewInt(10),
+		},
+	}
+
+	for _, tc := range tests {
+		t.Run(tc.name, func(t *testing.T) {
+			result := max(tc.x, tc.y)
+			require.Equal(t, tc.expected, result)
+		})
+	}
+}
+
+func TestCalcBlockGasPrice(t *testing.T) {
+	gk := GasPriceKeeper{}
+
+	lastGasPrice := std.GasPrice{
+		Price: std.Coin{
+			Amount: 100,
+			Denom:  "atom",
+		},
+	}
+	gasUsed := int64(5000)
+	maxGas := int64(10000)
+	params := Params{
+		TargetGasRatio:            50,
+		GasPricesChangeCompressor: 2,
+	}
+
+	// Test with normal parameters
+	newGasPrice := gk.calcBlockGasPrice(lastGasPrice, gasUsed, maxGas, params)
+	expectedAmount := big.NewInt(100)
+	num := big.NewInt(gasUsed - maxGas*params.TargetGasRatio/100)
+	num.Mul(num, expectedAmount)
+	num.Div(num, big.NewInt(maxGas*params.TargetGasRatio/100))
+	num.Div(num, big.NewInt(params.GasPricesChangeCompressor))
+	expectedAmount.Add(expectedAmount, num)
+	require.Equal(t, expectedAmount.Int64(), newGasPrice.Price.Amount)
+
+	// Test with lastGasPrice amount as 0
+	lastGasPrice.Price.Amount = 0
+	newGasPrice = gk.calcBlockGasPrice(lastGasPrice, gasUsed, maxGas, params)
+	require.Equal(t, int64(0), newGasPrice.Price.Amount)
+
+	// Test with TargetGasRatio as 0 (should not change the last price)
+	params.TargetGasRatio = 0
+	newGasPrice = gk.calcBlockGasPrice(lastGasPrice, gasUsed, maxGas, params)
+	require.Equal(t, int64(0), newGasPrice.Price.Amount)
+
+	// Test with gasUsed as 0 (should not change the last price)
+	params.TargetGasRatio = 50
+	lastGasPrice.Price.Amount = 100
+	gasUsed = 0
+	newGasPrice = gk.calcBlockGasPrice(lastGasPrice, gasUsed, maxGas, params)
+	require.Equal(t, int64(100), newGasPrice.Price.Amount)
 }
