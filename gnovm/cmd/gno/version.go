ccf789b6122452f5449910e7edc1c2aa49f05407
Kazai777 <zakaria.chaikhi.pro@gmail.com>
2024-10-29 17:25:29 +0100

    fix: fix CI, makefile modification and use [branch].[N]+[hash]

--- gnovm/cmd/gno/version.go
+++ gnovm/cmd/gno/version.go
@@ -2,13 +2,13 @@ package main
 
 import (
 	"context"
-	"fmt"
-	"os"
-	"os/exec"
-	"path/filepath"
-	"strings"
 
 	"github.com/gnolang/gno/tm2/pkg/commands"
+)
+
+var (
+	buildVersion string
+	commitHash   string
 )
 
 // newVersionCmd creates a new version command
@@ -21,63 +21,16 @@ func newVersionCmd(io commands.IO) *commands.Command {
 		},
 		nil,
 		func(_ context.Context, args []string) error {
-			version, err := getGnoVersion()
-			if err != nil {
-				io.ErrPrintln("error retrieving version:", err)
-				return err
-			}
+			version := getGnoVersion()
 			io.Println("gno version:", version)
 			return nil
 		},
 	)
 }
 
-// getGnoVersion attempts to retrieve the Gno version using different methods
-func getGnoVersion() (string, error) {
-	if version, err := getGoPathVersion(); err == nil {
-		return version, nil
+func getGnoVersion() string {
+	if buildVersion != "" {
+		return buildVersion
 	}
-	if version, err := getGitVersion(); err == nil {
-		return version, nil
-	}
-	return "unknown", fmt.Errorf("unable to determine gno version")
-}
-
-// getGoPathVersion looks for the version in GOPATH
-func getGoPathVersion() (string, error) {
-	gopath := os.Getenv("GOPATH")
-	if gopath == "" {
-		return "", fmt.Errorf("GOPATH not set")
-	}
-
-	gnoPath := filepath.Join(gopath, "pkg", "mod", "github.com", "gnolang", "gno@*")
-	matches, err := filepath.Glob(gnoPath)
-	if err != nil || len(matches) == 0 {
-		return "", fmt.Errorf("gno version not found in GOPATH")
-	}
-
-	version := filepath.Base(matches[len(matches)-1])
-	version = strings.TrimPrefix(version, "gno@")
-	return version, nil
-}
-
-// getGitVersion retrieves the version from git repository
-func getGitVersion() (string, error) {
-	if _, err := os.Stat(".git"); os.IsNotExist(err) {
-		return "", fmt.Errorf("git repository not found")
-	}
-
-	cmd := exec.Command("git", "describe", "--tags", "--abbrev=0")
-	output, err := cmd.Output()
-	if err == nil {
-		return strings.TrimSpace(string(output)), nil
-	}
-
-	cmd = exec.Command("git", "rev-parse", "--short", "HEAD")
-	output, err = cmd.Output()
-	if err != nil {
-		return "", err
-	}
-
-	return fmt.Sprintf("git-%s", strings.TrimSpace(string(output))), nil
+	return "unknown version"
 }
--- gnovm/cmd/gno/version_test.go
+++ gnovm/cmd/gno/version_test.go
@@ -1,22 +1,52 @@
 package main
 
 import (
+	"os"
+	"strings"
 	"testing"
 )
 
+var (
+	testBuildVersion string
+	testCommitHash   = "e872fa"
+)
+
+func init() {
+	if isTestTaggedVersion() {
+		testBuildVersion = "chain/test4.2"
+	} else {
+		testBuildVersion = "master.387+e872fa"
+	}
+	buildVersion = testBuildVersion
+	commitHash = testCommitHash
+}
+
+func isTestTaggedVersion() bool {
+	testDir := os.Getenv("TEST_CASE_DIR")
+	return strings.Contains(testDir, "tagged_version")
+}
+
 func TestVersionApp(t *testing.T) {
-	tc := []testMainCase{
+	testCases := []testMainCase{
 		{
 			args:                []string{"version"},
-			testDir:             "testdata/gno_version/gopath_version.txtar",
-			stdoutShouldContain: "gno version: v0.2.0",
+			testDir:             "testdata/gno_version/branch_commit_version.txtar",
+			stdoutShouldContain: "gno version: master.387+e872fa",
 		},
 		{
 			args:                []string{"version"},
-			testDir:             "testdata/gno_version/git_version.txtar",
-			stdoutShouldContain: "gno version: v0.2.0",
+			testDir:             "testdata/gno_version/tagged_version.txtar",
+			stdoutShouldContain: "gno version: chain/test4.2",
 		},
 	}
 
-	testMainCaseRun(t, tc)
+	for _, testCase := range testCases {
+		os.Setenv("TEST_CASE_DIR", testCase.testDir)
+		if isTestTaggedVersion() {
+			buildVersion = "chain/test4.2"
+		} else {
+			buildVersion = "master.387+e872fa"
+		}
+		testMainCaseRun(t, []testMainCase{testCase})
+	}
 }
