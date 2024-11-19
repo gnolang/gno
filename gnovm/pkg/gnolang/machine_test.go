3f9abe1b5fba3e54a9875199da5dffb45358028f
Morgan Bazalgette <morgan@morganbaz.com>
2024-11-19 01:37:56 +0100

    remove useless test

--- gnovm/pkg/gnolang/machine_test.go
+++ gnovm/pkg/gnolang/machine_test.go
@@ -2,13 +2,10 @@ package gnolang_test
 
 import (
 	"fmt"
-	"os"
-	"path/filepath"
 	"testing"
 
 	"github.com/gnolang/gno/gnovm"
 	gno "github.com/gnolang/gno/gnovm/pkg/gnolang"
-	"github.com/gnolang/gno/gnovm/pkg/test"
 	"github.com/gnolang/gno/tm2/pkg/db/memdb"
 	"github.com/gnolang/gno/tm2/pkg/store/dbadapter"
 	"github.com/gnolang/gno/tm2/pkg/store/iavl"
@@ -59,61 +56,4 @@ func TestRunMemPackageWithOverrides_revertToOld(t *testing.T) {
 	assert.NotNil(t, v)
 	assert.Equal(t, gno.StringKind, v.T.Kind())
 	assert.Equal(t, gno.StringValue("1"), v.V)
-}
-
-func TestMachineTestMemPackage(t *testing.T) {
-	matchFunc := func(pat, str string) (bool, error) { return true, nil }
-
-	tests := []struct {
-		name          string
-		path          string
-		shouldSucceed bool
-	}{
-		{
-			name:          "TestSuccess",
-			path:          "testdata/TestMemPackage/success",
-			shouldSucceed: true,
-		},
-		{
-			name:          "TestFail",
-			path:          "testdata/TestMemPackage/fail",
-			shouldSucceed: false,
-		},
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			// NOTE: Because the purpose of this test is to ensure testing.T.Failed()
-			// returns true if a gno test is failing, and because we don't want this
-			// to affect the current testing.T, we are creating an other one thanks
-			// to testing.RunTests() function.
-			testing.RunTests(matchFunc, []testing.InternalTest{
-				{
-					Name: tt.name,
-					F: func(t2 *testing.T) { //nolint:thelper
-						rootDir := filepath.Join("..", "..", "..")
-						_, store := test.Store(rootDir, false, os.Stdin, os.Stdout, os.Stderr)
-						store.SetLogStoreOps(true)
-						m := gno.NewMachineWithOptions(gno.MachineOptions{
-							PkgPath: "test",
-							Output:  os.Stdout,
-							Store:   store,
-							Context: nil,
-						})
-						memPkg := gno.ReadMemPackage(tt.path, "test")
-
-						_, _ = memPkg, m
-						panic("RE-IMPLEMENT")
-						// XXX TODO
-						// m.TestMemPackage(t2, memPkg)
-
-						if tt.shouldSucceed {
-							assert.False(t, t2.Failed(), "test %q should have succeed", tt.name)
-						} else {
-							assert.True(t, t2.Failed(), "test %q should have failed", tt.name)
-						}
-					},
-				},
-			})
-		})
-	}
 }
