ba9c95adf5152e8cea07c9a53f13b2c4921f065e
Norman Meier <norman@samourai.coop>
2024-11-20 13:49:39 +0100

    chore: punctuation
    Signed-off-by: Norman Meier <norman@samourai.coop>

--- gnovm/stdlibs/embedded.go
+++ gnovm/stdlibs/embedded.go
@@ -3,12 +3,12 @@ package stdlibs
 import "embed"
 
 // embeddedSources embeds the stdlibs.
-// Be careful to remove transpile artifacts before building release binaries or they will be included
+// Be careful to remove transpile artifacts before building release binaries or they will be included.
 //
 //go:embed */*
 var embeddedSources embed.FS
 
-// EmbeddedSources returns embedded stdlibs sources
+// EmbeddedSources returns embedded stdlibs sources.
 func EmbeddedSources() embed.FS {
 	return embeddedSources
 }
