ed89ff6d595310fcbdb5a776facee3610a53f494
Norman Meier <norman@samourai.coop>
2024-11-22 15:19:57 +0100

    chore: remove helper present in std
    Signed-off-by: Norman Meier <norman@samourai.coop>

--- gnovm/pkg/gnoimports/imports_test.go
+++ gnovm/pkg/gnoimports/imports_test.go
@@ -13,8 +13,7 @@ func TestGetGnoImports(t *testing.T) {
 	require.NoError(t, err)
 
 	// create external dir
-	tmpDir, cleanUpFn := createTmpDir(t)
-	defer cleanUpFn()
+	tmpDir := t.TempDir()
 
 	// cd to tmp directory
 	os.Chdir(tmpDir)
@@ -122,22 +121,4 @@ func TestGetGnoImports(t *testing.T) {
 	require.NoError(t, err)
 
 	require.Equal(t, expected, imports)
-}
-
-func createTmpDir(t *testing.T) (string, func()) {
-	t.Helper()
-
-	tmpDir, err := os.MkdirTemp("", "gno-mod-test")
-	if err != nil {
-		t.Error("Failed to create tmp dir for mod:", err)
-	}
-
-	cleanUpFn := func() {
-		err := os.RemoveAll(tmpDir)
-		if err != nil {
-			t.Logf("Failed to clean up test %s: %v", t.Name(), err)
-		}
-	}
-
-	return tmpDir, cleanUpFn
 }
