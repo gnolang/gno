e46c332382dbdce5011869ff1e9413e28906da2a
MikaelVallenet <mikael.vallenetpro@gmail.com>
2024-11-25 15:27:46 +0100

    feat: include chain tz in exec context of gno machine

--- gnovm/stdlibs/time/timezoneinfo.go
+++ gnovm/stdlibs/time/timezoneinfo.go
@@ -2,12 +2,13 @@ package time
 
 import (
 	gno "github.com/gnolang/gno/gnovm/pkg/gnolang"
+	"github.com/gnolang/gno/gnovm/stdlibs/std"
 )
 
-func X_GetChainTz(m *gno.Machine) string {
+func GetChainTz(m *gno.Machine) string {
 	if m == nil || m.Context == nil {
 		return "UTC"
 	}
 
-	return GetContext(m).ChainTz
+	return std.GetContext(m).ChainTz
 }
