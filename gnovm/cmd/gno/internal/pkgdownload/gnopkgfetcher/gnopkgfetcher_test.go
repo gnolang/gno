5ff7644ed237a5b50440e6c79342b616c18421a8
Norman Meier <norman@samourai.coop>
2024-11-29 15:56:52 +0100

    fix: push fail
    Signed-off-by: Norman Meier <norman@samourai.coop>

---
+++ gnovm/cmd/gno/internal/pkgdownload/gnopkgfetcher/gnopkgfetcher_test.go
@@ -0,0 +1,54 @@
+package gnopkgfetcher
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestRpcURLFromPkgPath(t *testing.T) {
+	cases := []struct {
+		name          string
+		pkgPath       string
+		overrides     map[string]string
+		result        string
+		errorContains string
+	}{
+		{
+			name:    "happy path simple",
+			pkgPath: "gno.land/p/demo/avl",
+			result:  "https://rpc.gno.land:443",
+		},
+		{
+			name:      "happy path override",
+			pkgPath:   "gno.land/p/demo/avl",
+			overrides: map[string]string{"gno.land": "https://example.com/rpc:42"},
+			result:    "https://example.com/rpc:42",
+		},
+		{
+			name:      "happy path override no effect",
+			pkgPath:   "gno.land/p/demo/avl",
+			overrides: map[string]string{"some.chain": "https://example.com/rpc:42"},
+			result:    "https://rpc.gno.land:443",
+		},
+		{
+			name:          "error bad pkg path",
+			pkgPath:       "std",
+			result:        "",
+			errorContains: fmt.Sprintf("bad pkg path %q", "std"),
+		},
+	}
+
+	for _, c := range cases {
+		t.Run(c.name, func(t *testing.T) {
+			res, err := rpcURLFromPkgPath(c.pkgPath, c.overrides)
+			if len(c.errorContains) == 0 {
+				require.NoError(t, err)
+			} else if err != nil {
+				require.ErrorContains(t, err, c.errorContains)
+			}
+			require.Equal(t, c.result, res)
+		})
+	}
+}
