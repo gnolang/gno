58d9efcdcb9f66158d271bdd9dc9fc62ce9a3c17
alexiscolin <alexis@jaunebleu.co>
2024-12-07 19:36:55 +0900

    feat: merge ts files

--- gno.land/pkg/gnoweb/frontend/js/copy.ts
+++ gno.land/pkg/gnoweb/frontend/js/copy.ts
@@ -1,76 +1,101 @@
-(() => {
-  class Copy {
-    private DOM: {
-      el: HTMLElement | null;
+class Copy {
+  private DOM: {
+    el: HTMLElement | null;
+  };
+  private static FEEDBACK_DELAY = 1500;
+
+  private btnClicked: HTMLElement | null = null;
+  private btnClickedIcons: HTMLElement[] = [];
+
+  private static SELECTORS = {
+    button: "[data-copy-btn]",
+    icon: `[data-copy-icon] > use`,
+    content: (id: string) => `[data-copy-content="${id}"]`,
+  };
+
+  constructor() {
+    this.DOM = {
+      el: document.querySelector<HTMLElement>("main"),
     };
-    btnClicked: HTMLElement | null = null;
-    btnClickedIcons: HTMLElement[] = [];
 
-    constructor() {
-      this.DOM = {
-        el: document.querySelector("main"),
-      };
-
-      if (this.DOM.el) this.init();
-    }
-
-    private init() {
-      this.bindEvents();
-    }
-
-    private bindEvents() {
-      this.DOM.el?.addEventListener("click", this.handleClick.bind(this));
-    }
-
-    private handleClick(event: Event) {
-      const target = event.target as HTMLElement;
-      const button = target.closest<HTMLElement>("[data-copy-btn]");
-
-      if (!button) return;
-
-      this.btnClicked = button;
-      this.btnClickedIcons = Array.from(button.querySelectorAll<HTMLElement>(`[data-copy-icon] > use`));
-
-      const contentId = button.getAttribute("data-copy-btn");
-      if (!contentId) return;
-
-      const codeBlock = this.DOM.el?.querySelector<HTMLElement>(`[data-copy-content="${contentId}"]`);
-      if (codeBlock) this.copyToClipboard(codeBlock);
-    }
-
-    private sanitizeContent(codeBlock: HTMLElement): string {
-      const html = codeBlock.innerHTML.replace(/<span class="chroma-ln">.*?<\/span>/g, "");
-      const tempDiv = document.createElement("div");
-      tempDiv.innerHTML = html;
-      return tempDiv.textContent?.trim() || "";
-    }
-
-    private toggleIcons() {
-      this.btnClickedIcons.forEach((icon) => {
-        icon.classList.toggle("hidden");
-      });
-    }
-
-    private showFeedback() {
-      if (!this.btnClicked) return;
-
-      this.toggleIcons();
-      window.setTimeout(() => {
-        this.toggleIcons();
-      }, 1500);
-    }
-
-    async copyToClipboard(codeBlock: HTMLElement) {
-      const sanitizedText = this.sanitizeContent(codeBlock);
-      try {
-        await navigator.clipboard.writeText(sanitizedText);
-        this.showFeedback();
-      } catch (err) {
-        console.error("Copy error: ", err);
-        this.showFeedback();
-      }
+    if (this.DOM.el) {
+      this.init();
+    } else {
+      console.warn("Copy: Main container not found.");
     }
   }
 
-  document.addEventListener("DOMContentLoaded", () => new Copy());
-})();
+  private init(): void {
+    this.bindEvents();
+  }
+
+  private bindEvents(): void {
+    this.DOM.el?.addEventListener("click", this.handleClick.bind(this));
+  }
+
+  private handleClick(event: Event): void {
+    const target = event.target as HTMLElement;
+    const button = target.closest<HTMLElement>(Copy.SELECTORS.button);
+
+    if (!button) return;
+
+    this.btnClicked = button;
+    this.btnClickedIcons = Array.from(button.querySelectorAll<HTMLElement>(Copy.SELECTORS.icon));
+
+    const contentId = button.getAttribute("data-copy-btn");
+    if (!contentId) {
+      console.warn("Copy: No content ID found on the button.");
+      return;
+    }
+
+    const codeBlock = this.DOM.el?.querySelector<HTMLElement>(Copy.SELECTORS.content(contentId));
+    if (codeBlock) {
+      this.copyToClipboard(codeBlock);
+    } else {
+      console.warn(`Copy: No content found for ID "${contentId}".`);
+    }
+  }
+
+  private sanitizeContent(codeBlock: HTMLElement): string {
+    const html = codeBlock.innerHTML.replace(/<span class="chroma-ln">.*?<\/span>/g, "");
+    const tempDiv = document.createElement("div");
+    tempDiv.innerHTML = html;
+    return tempDiv.textContent?.trim() || "";
+  }
+
+  private toggleIcons(): void {
+    this.btnClickedIcons.forEach((icon) => {
+      icon.classList.toggle("hidden");
+    });
+  }
+
+  private showFeedback(): void {
+    if (!this.btnClicked) return;
+
+    this.toggleIcons();
+    window.setTimeout(() => {
+      this.toggleIcons();
+    }, Copy.FEEDBACK_DELAY);
+  }
+
+  private async copyToClipboard(codeBlock: HTMLElement): Promise<void> {
+    const sanitizedText = this.sanitizeContent(codeBlock);
+
+    if (!navigator.clipboard) {
+      console.error("Copy: Clipboard API is not supported in this browser.");
+      this.showFeedback();
+      return;
+    }
+
+    try {
+      await navigator.clipboard.writeText(sanitizedText);
+      console.info("Copy: Text copied successfully.");
+      this.showFeedback();
+    } catch (err) {
+      console.error("Copy: Error while copying text.", err);
+      this.showFeedback();
+    }
+  }
+}
+
+export default () => new Copy();
--- gno.land/pkg/gnoweb/frontend/js/realmhelp.ts
+++ gno.land/pkg/gnoweb/frontend/js/realmhelp.ts
@@ -1,105 +1,124 @@
-(() => {
-  class Help {
-    private DOM: {
-      el: HTMLElement | null;
-      funcs: HTMLElement[];
-      addressInput: HTMLInputElement | null;
-      cmdModeSelect: HTMLSelectElement | null;
+class Help {
+  private DOM: {
+    el: HTMLElement | null;
+    funcs: HTMLElement[];
+    addressInput: HTMLInputElement | null;
+    cmdModeSelect: HTMLSelectElement | null;
+  };
+
+  private funcList: HelpFunc[];
+
+  private static SELECTORS = {
+    container: "#data",
+    func: "[data-func]",
+    addressInput: "[data-role='help-input-addr']",
+    cmdModeSelect: "[data-role='help-select-mode']",
+  };
+
+  constructor() {
+    this.DOM = {
+      el: document.querySelector<HTMLElement>(Help.SELECTORS.container),
+      funcs: [],
+      addressInput: null,
+      cmdModeSelect: null,
     };
 
-    private funcList: HelpFunc[];
+    this.funcList = [];
 
-    constructor() {
-      this.DOM = {
-        el: document.querySelector("#data"),
-        funcs: [],
-        addressInput: null,
-        cmdModeSelect: null,
-      };
-
-      this.funcList = [];
-
-      if (this.DOM.el) this.init();
-    }
-
-    private init() {
-      const { el } = this.DOM;
-      if (!el) return;
-
-      this.DOM.funcs = Array.from(el.querySelectorAll<HTMLElement>("[data-func]"));
-      this.DOM.addressInput = el.querySelector<HTMLInputElement>("[data-role='help-input-addr']");
-      this.DOM.cmdModeSelect = el.querySelector<HTMLSelectElement>("[data-role='help-select-mode']");
-
-      this.funcList = this.DOM.funcs.map((funcEl) => new HelpFunc(funcEl));
-
-      if (this.DOM.addressInput) this.bindEvents();
-    }
-
-    private bindEvents() {
-      const { addressInput, cmdModeSelect } = this.DOM;
-
-      addressInput?.addEventListener("input", () => this.funcList.forEach((func) => func.updateAddr(addressInput.value)));
-
-      cmdModeSelect?.addEventListener("change", (e) => {
-        const target = e.target as HTMLSelectElement;
-        this.funcList.forEach((func) => func.updateMode(target.value));
-      });
+    if (this.DOM.el) {
+      this.init();
+    } else {
+      console.warn("Help: Main container not found.");
     }
   }
 
-  class HelpFunc {
-    private DOM: {
-      el: HTMLElement;
-      addrs: HTMLElement[];
-      args: HTMLElement[];
-      modes: HTMLElement[];
-    };
+  private init(): void {
+    const { el } = this.DOM;
+    if (!el) return;
 
-    private funcName: null | string = null;
+    this.DOM.funcs = Array.from(el.querySelectorAll<HTMLElement>(Help.SELECTORS.func));
+    this.DOM.addressInput = el.querySelector<HTMLInputElement>(Help.SELECTORS.addressInput);
+    this.DOM.cmdModeSelect = el.querySelector<HTMLSelectElement>(Help.SELECTORS.cmdModeSelect);
 
-    constructor(el: HTMLElement) {
-      this.DOM = {
-        el,
-        addrs: Array.from(el.querySelectorAll<HTMLElement>("[data-role='help-code-address']")),
-        args: Array.from(el.querySelectorAll<HTMLElement>("[data-role='help-code-args']")),
-        modes: Array.from(el.querySelectorAll<HTMLElement>("[data-code-mode]")),
-      };
+    this.funcList = this.DOM.funcs.map((funcEl) => new HelpFunc(funcEl));
 
-      this.funcName = this.DOM.el.dataset.func || "";
-
-      this.bindEvents();
-    }
-
-    private bindEvents() {
-      this.DOM.el.addEventListener("input", (e) => {
-        const target = e.target as HTMLInputElement;
-        if (target.dataset.role === "help-param-input") {
-          this.updateArg(target.dataset.param || "", target.value);
-        }
-      });
-    }
-
-    public updateArg(paramName: string, paramValue: string) {
-      this.DOM.args
-        .filter((arg) => arg.dataset.arg === paramName)
-        .forEach((arg) => {
-          arg.textContent = paramValue.trim() || "";
-        });
-    }
-
-    public updateAddr(addr: string) {
-      this.DOM.addrs.forEach((DOMaddr) => {
-        DOMaddr.textContent = addr.trim() || "ADDRESS";
-      });
-    }
-
-    public updateMode(mode: string) {
-      this.DOM.modes.forEach((cmd) => {
-        cmd.className = cmd.dataset.codeMode === mode ? "inline" : "hidden";
-        cmd.dataset.copyContent = cmd.dataset.codeMode === mode ? `help-cmd-${this.funcName}` : "";
-      });
-    }
+    this.bindEvents();
   }
 
-  document.addEventListener("DOMContentLoaded", () => new Help());
-})();
+  private bindEvents(): void {
+    const { addressInput, cmdModeSelect } = this.DOM;
+
+    addressInput?.addEventListener("input", () => {
+      this.funcList.forEach((func) => func.updateAddr(addressInput.value));
+    });
+
+    cmdModeSelect?.addEventListener("change", (e) => {
+      const target = e.target as HTMLSelectElement;
+      this.funcList.forEach((func) => func.updateMode(target.value));
+    });
+  }
+}
+
+class HelpFunc {
+  private DOM: {
+    el: HTMLElement;
+    addrs: HTMLElement[];
+    args: HTMLElement[];
+    modes: HTMLElement[];
+  };
+
+  private funcName: string | null;
+
+  private static SELECTORS = {
+    address: "[data-role='help-code-address']",
+    args: "[data-role='help-code-args']",
+    mode: "[data-code-mode]",
+    paramInput: "[data-role='help-param-input']",
+  };
+
+  constructor(el: HTMLElement) {
+    this.DOM = {
+      el,
+      addrs: Array.from(el.querySelectorAll<HTMLElement>(HelpFunc.SELECTORS.address)),
+      args: Array.from(el.querySelectorAll<HTMLElement>(HelpFunc.SELECTORS.args)),
+      modes: Array.from(el.querySelectorAll<HTMLElement>(HelpFunc.SELECTORS.mode)),
+    };
+
+    this.funcName = el.dataset.func || null;
+
+    this.bindEvents();
+  }
+
+  private bindEvents(): void {
+    this.DOM.el.addEventListener("input", (e) => {
+      const target = e.target as HTMLInputElement;
+      if (target.dataset.role === "help-param-input") {
+        this.updateArg(target.dataset.param || "", target.value);
+      }
+    });
+  }
+
+  public updateArg(paramName: string, paramValue: string): void {
+    this.DOM.args
+      .filter((arg) => arg.dataset.arg === paramName)
+      .forEach((arg) => {
+        arg.textContent = paramValue.trim() || "";
+      });
+  }
+
+  public updateAddr(addr: string): void {
+    this.DOM.addrs.forEach((DOMaddr) => {
+      DOMaddr.textContent = addr.trim() || "ADDRESS";
+    });
+  }
+
+  public updateMode(mode: string): void {
+    this.DOM.modes.forEach((cmd) => {
+      const isVisible = cmd.dataset.codeMode === mode;
+      cmd.className = isVisible ? "inline" : "hidden";
+      cmd.dataset.copyContent = isVisible ? `help-cmd-${this.funcName}` : "";
+    });
+  }
+}
+
+export default () => new Help();
