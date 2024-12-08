4f7b0b80c6e726806a473556b02444fded707254
aeddi <antoine.e.b@gmail.com>
2024-12-08 17:33:38 +0100

    feat: handle pull_request_review events

--- contribs/github-bot/internal/utils/github_const.go
+++ contribs/github-bot/internal/utils/github_const.go
@@ -5,6 +5,7 @@ const (
 	// GitHub Actions Event Names.
 	EventIssueComment      = "issue_comment"
 	EventPullRequest       = "pull_request"
+	EventPullRequestReview = "pull_request_review"
 	EventPullRequestTarget = "pull_request_target"
 	EventWorkflowDispatch  = "workflow_dispatch"
 
