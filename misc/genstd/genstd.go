d9f4d3af653043df177091b916badae6adeb2896
Norman Meier <norman@samourai.coop>
2024-12-10 15:28:28 +0100

    chore: make genstd consider test files
    Signed-off-by: Norman Meier <norman@samourai.coop>

--- misc/genstd/genstd.go
+++ misc/genstd/genstd.go
@@ -121,9 +121,7 @@ func walkStdlibs(stdlibsPath string) ([]*pkgData, error) {
 
 		// skip non-source and test files.
 		ext := filepath.Ext(fpath)
-		noExt := fpath[:len(fpath)-len(ext)]
 		if (ext != ".go" && ext != ".gno") ||
-			strings.HasSuffix(noExt, "_test") ||
 			strings.HasSuffix(fpath, ".gen.go") {
 			return nil
 		}
