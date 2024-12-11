45caa10acd172fc9d5f9da7b08404358d7554cd7
Morgan Bazalgette <morgan@morganbaz.com>
2024-12-11 16:24:04 +0100

    memdb with btree

--- tm2/pkg/db/memdb/mem_db.go
+++ tm2/pkg/db/memdb/mem_db.go
@@ -1,13 +1,14 @@
 package memdb
 
 import (
+	"bytes"
 	"fmt"
-	"sort"
 	"sync"
 
 	"github.com/gnolang/gno/tm2/pkg/colors"
 	dbm "github.com/gnolang/gno/tm2/pkg/db"
 	"github.com/gnolang/gno/tm2/pkg/db/internal"
+	"github.com/tidwall/btree"
 )
 
 func init() {
@@ -20,12 +21,12 @@ var _ dbm.DB = (*MemDB)(nil)
 
 type MemDB struct {
 	mtx sync.Mutex
-	db  map[string][]byte
+	db  *btree.Map[string, []byte]
 }
 
 func NewMemDB() *MemDB {
 	database := &MemDB{
-		db: make(map[string][]byte),
+		db: btree.NewMap[string, []byte](0),
 	}
 	return database
 }
@@ -41,7 +42,7 @@ func (db *MemDB) Get(key []byte) []byte {
 	defer db.mtx.Unlock()
 	key = internal.NonNilBytes(key)
 
-	value := db.db[string(key)]
+	value, _ := db.db.Get(string(key))
 	return value
 }
 
@@ -51,7 +52,7 @@ func (db *MemDB) Has(key []byte) bool {
 	defer db.mtx.Unlock()
 	key = internal.NonNilBytes(key)
 
-	_, ok := db.db[string(key)]
+	_, ok := db.db.Get(string(key))
 	return ok
 }
 
@@ -78,10 +79,9 @@ func (db *MemDB) SetNoLock(key []byte, value []byte) {
 
 // Implements internal.AtomicSetDeleter.
 func (db *MemDB) SetNoLockSync(key []byte, value []byte) {
-	key = internal.NonNilBytes(key)
 	value = internal.NonNilBytes(value)
 
-	db.db[string(key)] = value
+	db.db.Set(string(key), internal.NonNilBytes(value))
 }
 
 // Implements DB.
@@ -109,7 +109,7 @@ func (db *MemDB) DeleteNoLock(key []byte) {
 func (db *MemDB) DeleteNoLockSync(key []byte) {
 	key = internal.NonNilBytes(key)
 
-	delete(db.db, string(key))
+	db.db.Delete(string(key))
 }
 
 // Implements DB.
@@ -126,12 +126,12 @@ func (db *MemDB) Print() {
 	db.mtx.Lock()
 	defer db.mtx.Unlock()
 
-	for key, value := range db.db {
-		var keystr, valstr string
-		keystr = colors.DefaultColoredBytesN([]byte(key), 50)
-		valstr = colors.DefaultColoredBytesN(value, 100)
+	db.db.Scan(func(key string, value []byte) bool {
+		keystr := colors.DefaultColoredBytesN([]byte(key), 50)
+		valstr := colors.DefaultColoredBytesN(value, 100)
 		fmt.Printf("%s: %s\n", keystr, valstr)
-	}
+		return true
+	})
 }
 
 // Implements DB.
@@ -141,7 +141,7 @@ func (db *MemDB) Stats() map[string]string {
 
 	stats := make(map[string]string)
 	stats["database.type"] = "memDB"
-	stats["database.size"] = fmt.Sprintf("%d", len(db.db))
+	stats["database.size"] = fmt.Sprintf("%d", db.db.Len())
 	return stats
 }
 
@@ -158,41 +158,129 @@ func (db *MemDB) NewBatch() dbm.Batch {
 
 // Implements DB.
 func (db *MemDB) Iterator(start, end []byte) dbm.Iterator {
+	if len(start) > 0 && len(end) > 0 &&
+		bytes.Compare(start, end) != -1 {
+		panic("cannot create iterator with start >= end: " + fmt.Sprintf("%q < %q", start, end))
+	}
+
 	db.mtx.Lock()
 	defer db.mtx.Unlock()
 
-	keys := db.getSortedKeys(start, end, false)
-	return internal.NewMemIterator(db, keys, start, end)
+	res := &iterator{
+		it:    db.db.Iter(),
+		start: string(start),
+		end:   string(end),
+	}
+	if len(start) == 0 {
+		if !res.it.First() {
+			res.invalid = true
+		}
+	} else {
+		if !res.it.Seek(string(start)) {
+			res.invalid = true
+		}
+	}
+	return res
 }
 
 // Implements DB.
 func (db *MemDB) ReverseIterator(start, end []byte) dbm.Iterator {
+	if len(start) != 0 && len(end) != 0 &&
+		bytes.Compare(start, end) != -1 {
+		panic("cannot create iterator with start >= end: " + fmt.Sprintf("%q < %q", start, end))
+	}
+
 	db.mtx.Lock()
 	defer db.mtx.Unlock()
 
-	keys := db.getSortedKeys(start, end, true)
-	return internal.NewMemIterator(db, keys, start, end)
-}
-
-// ----------------------------------------
-// Misc.
-
-func (db *MemDB) getSortedKeys(start, end []byte, reverse bool) []string {
-	keys := []string{}
-	for key := range db.db {
-		inDomain := dbm.IsKeyInDomain([]byte(key), start, end)
-		if inDomain {
-			keys = append(keys, key)
+	res := &iterator{
+		it:      db.db.Iter(),
+		start:   string(start),
+		end:     string(end),
+		reverse: true,
+	}
+	if len(end) == 0 {
+		if !res.it.Last() {
+			res.invalid = true
+		}
+	} else {
+		if res.it.Seek(string(end)) {
+			if !res.it.Prev() || res.it.Key() < res.start {
+				res.invalid = true
+			}
+		} else {
+			// if nothing > end is found, use the last item in the map.
+			if !res.it.Last() {
+				res.invalid = true
+			}
 		}
 	}
-	sort.Strings(keys)
-	if reverse {
-		nkeys := len(keys)
-		for i := 0; i < nkeys/2; i++ {
-			temp := keys[i]
-			keys[i] = keys[nkeys-i-1]
-			keys[nkeys-i-1] = temp
+	return res
+}
+
+type iterator struct {
+	it         btree.MapIter[string, []byte]
+	start, end string
+	invalid    bool
+	reverse    bool
+}
+
+// The start & end (exclusive) limits to iterate over.
+// If end < start, then the Iterator goes in reverse order.
+//
+// A domain of ([]byte{12, 13}, []byte{12, 14}) will iterate
+// over anything with the prefix []byte{12, 13}.
+//
+// The smallest key is the empty byte array []byte{} - see BeginningKey().
+// The largest key is the nil byte array []byte(nil) - see EndingKey().
+// CONTRACT: start, end readonly []byte
+func (i *iterator) Domain() (start []byte, end []byte) {
+	if i.reverse {
+		return []byte(i.end), []byte(i.start)
+	}
+	return []byte(i.start), []byte(i.end)
+}
+
+// Valid returns whether the current position is valid.
+// Once invalid, an Iterator is forever invalid.
+func (i *iterator) Valid() bool {
+	return !i.invalid
+}
+
+// Next moves the iterator to the next sequential key in the database, as
+// defined by order of iteration.
+//
+// If Valid returns false, this method will panic.
+func (i *iterator) Next() {
+	if i.invalid {
+		panic("invalid iterator")
+	}
+	if i.reverse {
+		if !i.it.Prev() ||
+			i.it.Key() < i.start {
+			i.invalid = true
+		}
+	} else {
+		if !i.it.Next() ||
+			i.it.Key() >= i.end {
+			i.invalid = true
 		}
 	}
-	return keys
 }
+
+// Key returns the key of the cursor.
+// If Valid returns false, this method will panic.
+// CONTRACT: key readonly []byte
+func (i *iterator) Key() []byte {
+	return []byte(i.it.Key())
+}
+
+// Value returns the value of the cursor.
+// If Valid returns false, this method will panic.
+// CONTRACT: value readonly []byte
+func (i *iterator) Value() []byte {
+	return i.it.Value()
+}
+
+// Close releases the Iterator.
+func (i *iterator) Close() {}
---
+++ tm2/pkg/db/memdb/mem_db_test.go
@@ -0,0 +1,174 @@
+package memdb
+
+import (
+	"bytes"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestIterator(t *testing.T) {
+	tests := []struct {
+		name       string
+		setupDB    func(*MemDB)
+		start      []byte
+		end        []byte
+		wantKeys   []string
+		wantValues []string
+		wantPanic  bool
+	}{
+		{
+			name:    "empty database",
+			setupDB: func(db *MemDB) {},
+			start:   nil,
+			end:     nil,
+		},
+		{
+			name: "single key",
+			setupDB: func(db *MemDB) {
+				db.Set([]byte("key1"), []byte("value1"))
+			},
+			start:      nil,
+			end:        nil,
+			wantKeys:   []string{"key1"},
+			wantValues: []string{"value1"},
+		},
+		{
+			name: "multiple keys in range",
+			setupDB: func(db *MemDB) {
+				db.Set([]byte("key1"), []byte("value1"))
+				db.Set([]byte("key2"), []byte("value2"))
+				db.Set([]byte("key3"), []byte("value3"))
+			},
+			start:      []byte("key1"),
+			end:        []byte("key3"),
+			wantKeys:   []string{"key1", "key2"},
+			wantValues: []string{"value1", "value2"},
+		},
+		{
+			name: "prefix iteration",
+			setupDB: func(db *MemDB) {
+				db.Set([]byte("prefix1_a"), []byte("value1"))
+				db.Set([]byte("prefix1_b"), []byte("value2"))
+				db.Set([]byte("prefix2_a"), []byte("value3"))
+			},
+			start:      []byte("prefix1_"),
+			end:        []byte("prefix1_\xff"),
+			wantKeys:   []string{"prefix1_a", "prefix1_b"},
+			wantValues: []string{"value1", "value2"},
+		},
+		{
+			name: "invalid range should panic",
+			setupDB: func(db *MemDB) {
+				db.Set([]byte("key1"), []byte("value1"))
+			},
+			start:     []byte("z"),
+			end:       []byte("a"),
+			wantPanic: true,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			db := NewMemDB()
+			tt.setupDB(db)
+
+			if tt.wantPanic {
+				assert.Panics(t, func() {
+					db.Iterator(tt.start, tt.end)
+				})
+				return
+			}
+			iter := db.Iterator(tt.start, tt.end)
+			defer iter.Close()
+
+			var gotKeys []string
+			var gotValues []string
+
+			for ; iter.Valid(); iter.Next() {
+				gotKeys = append(gotKeys, string(iter.Key()))
+				gotValues = append(gotValues, string(iter.Value()))
+			}
+
+			assert.Equal(t, tt.wantKeys, gotKeys)
+			assert.Equal(t, tt.wantValues, gotValues)
+		})
+	}
+}
+
+func TestIterator_Domain(t *testing.T) {
+	db := NewMemDB()
+	start := []byte("0")
+	end := []byte("9")
+
+	iter := db.Iterator(start, end)
+	gotStart, gotEnd := iter.Domain()
+
+	if !bytes.Equal(gotStart, start) {
+		t.Errorf("Domain start: got %v, want %v", gotStart, start)
+	}
+	if !bytes.Equal(gotEnd, end) {
+		t.Errorf("Domain end: got %v, want %v", gotEnd, end)
+	}
+}
+
+func TestIterator_InvalidOperation(t *testing.T) {
+	db := NewMemDB()
+	db.Set([]byte("key"), []byte("value"))
+
+	iter := db.Iterator(nil, nil)
+	iter.Next() // Move past the only key
+
+	defer func() {
+		if r := recover(); r == nil {
+			t.Error("Expected panic on Next() after iterator becomes invalid")
+		}
+	}()
+
+	iter.Next() // Should panic
+}
+
+func TestReverseIterator(t *testing.T) {
+	tests := []struct {
+		name       string
+		setupDB    func(*MemDB)
+		start      []byte
+		end        []byte
+		wantKeys   []string
+		wantValues []string
+	}{
+		{
+			name: "reverse multiple keys",
+			setupDB: func(db *MemDB) {
+				db.Set([]byte("key1"), []byte("value1"))
+				db.Set([]byte("key2"), []byte("value2"))
+				db.Set([]byte("key3"), []byte("value3"))
+			},
+			start:      []byte("key1"),
+			end:        []byte("key3"),
+			wantKeys:   []string{"key2", "key1"},
+			wantValues: []string{"value2", "value1"},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			db := NewMemDB()
+			tt.setupDB(db)
+
+			iter := db.ReverseIterator(tt.start, tt.end)
+			defer iter.Close()
+
+			var gotKeys []string
+			var gotValues []string
+
+			for ; iter.Valid(); iter.Next() {
+				gotKeys = append(gotKeys, string(iter.Key()))
+				gotValues = append(gotValues, string(iter.Value()))
+			}
+
+			assert.Equal(t, tt.wantKeys, gotKeys)
+			assert.Equal(t, tt.wantValues, gotValues)
+		})
+	}
+}
