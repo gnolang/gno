e95b41074f4a26221da631c75358c8c1a675665f
alexiscolin <alexis@jaunebleu.co>
2024-12-18 17:34:47 +0900

    chore: add this to debounce

--- gno.land/pkg/gnoweb/public/js/utils.js
+++ gno.land/pkg/gnoweb/public/js/utils.js
@@ -1,1 +1,1 @@
-function d(n,r=300){let e;return(...t)=>{e!==void 0&&clearTimeout(e),e=window.setTimeout(()=>{n(...t)},r)}}export{d as debounce};
+function r(t,n=250){let e;return function(...i){e!==void 0&&clearTimeout(e),e=setTimeout(()=>{t.apply(this,i)},n)}}export{r as debounce};
