de5a919d7eb0f6182581e8f7ac734cf7e46f0a40
Marc Vertes <mvertes@free.fr>
2025-01-14 09:04:14 +0100

    chore: revert "feat(gnovm): implement overflow checking at VM level (#3250)"
    This reverts commit 68aff6464dfba782903cdb5e3b318a9b233a479e.
    
    This PR was merged before discussions were complete. It was finally
    rejected because it does not comply with Go specification, which
    stipulates that overflow on signed integers do not trigger runtime
    panic. See https://go.dev/ref/spec#Integer_overflow

--- tm2/pkg/overflow/overflow_test.go
+++ tm2/pkg/overflow/overflow_test.go
@@ -28,7 +28,8 @@ func TestAlgorithms(t *testing.T) {
 				// now the verification
 				result, ok := Add8(a8, b8)
 				if ok && int64(result) != r64 {
-					t.Errorf("failed to fail on %v + %v = %v instead of %v\n", a8, b8, result, r64)
+					t.Errorf("failed to fail on %v + %v = %v instead of %v\n",
+						a8, b8, result, r64)
 					errors++
 				}
 				if !ok && int64(result) == r64 {
@@ -44,7 +45,8 @@ func TestAlgorithms(t *testing.T) {
 				// now the verification
 				result, ok := Sub8(a8, b8)
 				if ok && int64(result) != r64 {
-					t.Errorf("failed to fail on %v - %v = %v instead of %v\n", a8, b8, result, r64)
+					t.Errorf("failed to fail on %v - %v = %v instead of %v\n",
+						a8, b8, result, r64)
 				}
 				if !ok && int64(result) == r64 {
 					t.Fail()
@@ -59,7 +61,8 @@ func TestAlgorithms(t *testing.T) {
 				// now the verification
 				result, ok := Mul8(a8, b8)
 				if ok && int64(result) != r64 {
-					t.Errorf("failed to fail on %v * %v = %v instead of %v\n", a8, b8, result, r64)
+					t.Errorf("failed to fail on %v * %v = %v instead of %v\n",
+						a8, b8, result, r64)
 					errors++
 				}
 				if !ok && int64(result) == r64 {
@@ -75,10 +78,11 @@ func TestAlgorithms(t *testing.T) {
 				// now the verification
 				result, _, ok := Quotient8(a8, b8)
 				if ok && int64(result) != r64 {
-					t.Errorf("failed to fail on %v / %v = %v instead of %v\n", a8, b8, result, r64)
+					t.Errorf("failed to fail on %v / %v = %v instead of %v\n",
+						a8, b8, result, r64)
 					errors++
 				}
-				if !ok && int64(result) == r64 {
+				if !ok && result != 0 && int64(result) == r64 {
 					t.Fail()
 					errors++
 				}
