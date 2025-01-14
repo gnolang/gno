4b192eb36841dc1dbc8a6972e4523eeb1e0207b4
Lee ByeongJun <lbj199874@gmail.com>
2025-01-14 10:24:27 +0900

    fix

--- gnovm/cmd/gno/lint_test.go
+++ gnovm/cmd/gno/lint_test.go
@@ -28,46 +28,49 @@ func TestLintApp(t *testing.T) {
 		},
 		{
 			args:                []string{"lint", "../../tests/integ/several_lint_errors/main.gno"},
-			stderrShouldContain: "../../tests/integ/several_lint_errors/main.gno:5:5: expected ';', found example (code=2).\n../../tests/integ/several_lint_errors/main.gno:6:2: expected '}', found 'EOF' (code=2).\n",
-			errShouldBe:         "exit code: 1",
-		},
-		{
-			args:                []string{"lint", "../../tests/integ/several_files_multiple_errors/main.gno"},
-			stderrShouldContain: "../../tests/integ/several_files_multiple_errors/file2.gno:3:5: expected 'IDENT', found '{' (code=2).\n../../tests/integ/several_files_multiple_errors/file2.gno:5:1: expected type, found '}' (code=2).\n../../tests/integ/several_files_multiple_errors/main.gno:5:5: expected ';', found example (code=2).\n../../tests/integ/several_files_multiple_errors/main.gno:6:2: expected '}', found 'EOF' (code=2).\n",
+			stderrShouldContain: "../../tests/integ/several_lint_errors/main.gno:5:5: expected ';', found example (code=2)\n../../tests/integ/several_lint_errors/main.gno:6:2: expected '}', found 'EOF' (code=2)\n",
 			errShouldBe:         "exit code: 1",
 		},
 		{
 			args:                []string{"lint", "../../tests/integ/run_main/"},
-			stderrShouldContain: "./../../tests/integ/run_main: missing 'gno.mod' file (code=1).",
+			stderrShouldContain: "./../../tests/integ/run_main: parsing gno.mod at ./../../tests/integ/run_main: gno.mod file not found in current or any parent directory (code=1)\n",
 			errShouldBe:         "exit code: 1",
-		}, {
-			args:                []string{"lint", "../../tests/integ/several-lint-errors/main.gno"},
-			stderrShouldContain: "../../tests/integ/several-lint-errors/main.gno:5:5: expected ';', found example (code=2)\n../../tests/integ/several-lint-errors/main.gno:6",
-			errShouldBe:         "exit code: 1",
-		}, {
-			args: []string{"lint", "../../tests/integ/several-files-multiple-errors/main.gno"},
+		},
+		{
+			args: []string{"lint", "../../tests/integ/several_lint_errors/main.gno"},
+			stderrShouldContain: strings.Join([]string{
+				"../../tests/integ/several_lint_errors/main.gno:5:5: expected ';', found example (code=2)",
+				"../../tests/integ/several_lint_errors/main.gno:6:2: expected '}', found 'EOF' (code=2)",
+			}, "\n") + "\n",
+			errShouldBe: "exit code: 1",
+		},
+		{
+			args: []string{"lint", "../../tests/integ/several_files_multiple_errors/main.gno"},
 			stderrShouldContain: func() string {
 				lines := []string{
-					"../../tests/integ/several-files-multiple-errors/file2.gno:3:5: expected 'IDENT', found '{' (code=2)",
-					"../../tests/integ/several-files-multiple-errors/file2.gno:5:1: expected type, found '}' (code=2)",
-					"../../tests/integ/several-files-multiple-errors/main.gno:5:5: expected ';', found example (code=2)",
-					"../../tests/integ/several-files-multiple-errors/main.gno:6:2: expected '}', found 'EOF' (code=2)",
+					"../../tests/integ/several_files_multiple_errors/file2.gno:3:5: expected 'IDENT', found '{' (code=2)",
+					"../../tests/integ/several_files_multiple_errors/file2.gno:5:1: expected type, found '}' (code=2)",
+					"../../tests/integ/several_files_multiple_errors/main.gno:5:5: expected ';', found example (code=2)",
+					"../../tests/integ/several_files_multiple_errors/main.gno:6:2: expected '}', found 'EOF' (code=2)",
 				}
 				return strings.Join(lines, "\n") + "\n"
 			}(),
 			errShouldBe: "exit code: 1",
-		}, {
+		},
+		{
 			args: []string{"lint", "../../tests/integ/minimalist_gnomod/"},
 			// TODO: raise an error because there is a gno.mod, but no .gno files
 		},
 		{
 			args: []string{"lint", "../../tests/integ/invalid_module_name/"},
 			// TODO: raise an error because gno.mod is invalid
-		}, {
+		},
+		{
 			args:                []string{"lint", "../../tests/integ/invalid_gno_file/"},
 			stderrShouldContain: "../../tests/integ/invalid_gno_file/invalid.gno:1:1: expected 'package', found packag (code=2)",
 			errShouldBe:         "exit code: 1",
-		}, {
+		},
+		{
 			args:                []string{"lint", "../../tests/integ/typecheck_missing_return/"},
 			stderrShouldContain: "../../tests/integ/typecheck_missing_return/main.gno:5:1: missing return (code=4)",
 			errShouldBe:         "exit code: 1",
