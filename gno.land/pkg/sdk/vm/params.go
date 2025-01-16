4c6b55a7225407361ea934b823f82b32faa9564a
leohhhn <hudakleon@gmail.com>
2025-01-17 00:59:56 +0100

    modify keeper to use r/sys/names instead of r/sys/users

--- gno.land/pkg/sdk/vm/params.go
+++ gno.land/pkg/sdk/vm/params.go
@@ -3,7 +3,7 @@ package vm
 import "github.com/gnolang/gno/tm2/pkg/sdk"
 
 const (
-	sysUsersPkgParamPath = "gno.land/r/sys/params.sys.users_pkgpath.string"
+	sysNamesPkgParamPath = "gno.land/r/sys/params.sys.names_pkgpath.string"
 	chainDomainParamPath = "gno.land/r/sys/params.chain_domain.string"
 )
 
@@ -13,8 +13,8 @@ func (vm *VMKeeper) getChainDomainParam(ctx sdk.Context) string {
 	return chainDomain
 }
 
-func (vm *VMKeeper) getSysUsersPkgParam(ctx sdk.Context) string {
-	var sysUsersPkg string
-	vm.prmk.GetString(ctx, sysUsersPkgParamPath, &sysUsersPkg)
-	return sysUsersPkg
+func (vm *VMKeeper) getSysNamesPkgParam(ctx sdk.Context) string {
+	var sysNamesPkg string
+	vm.prmk.GetString(ctx, sysNamesPkgParamPath, &sysNamesPkg)
+	return sysNamesPkg
 }
