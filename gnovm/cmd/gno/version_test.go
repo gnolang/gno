9bd44d816aa7ccd61199fb46d72da7641efa5a2e
Kazai777 <zakaria.chaikhi.pro@gmail.com>
2025-01-20 20:55:09 +0100

    refactor version_test.go file

--- gnovm/cmd/gno/version_test.go
+++ gnovm/cmd/gno/version_test.go
@@ -1,40 +1,32 @@
 package main
 
 import (
-	"os"
 	"testing"
 
 	"github.com/gnolang/gno/gnovm/pkg/version"
 )
 
 func TestVersionApp(t *testing.T) {
-	testCases := []testMainCase{
-		{
+	originalVersion := version.Version
+
+	t.Cleanup(func() {
+		version.Version = originalVersion
+	})
+
+	versionValues := []string{"chain/test4.2", "develop", "master"}
+
+	testCases := make([]testMainCase, len(versionValues))
+	for i, v := range versionValues {
+		testCases[i] = testMainCase{
 			args:                []string{"version"},
-			testDir:             "testdata/gno_version/branch_commit_version.txtar",
-			stdoutShouldContain: "gno version: master.387+e872fa",
-		},
-		{
-			args:                []string{"version"},
-			testDir:             "testdata/gno_version/tagged_version.txtar",
-			stdoutShouldContain: "gno version: chain/test4.2",
-		},
-		{
-			args:                []string{"version"},
-			testDir:             "testdata/gno_version/develop_version.txtar",
-			stdoutShouldContain: "gno version: develop",
-		},
+			stdoutShouldContain: "gno version: " + v,
+		}
 	}
 
-	for _, testCase := range testCases {
-		os.Setenv("TEST_CASE_DIR", testCase.testDir)
-		if isTestTaggedVersion() {
-			version.Version = "chain/test4.2"
-		} else if isTestDevelopVersion() {
-			version.Version = "develop"
-		} else {
-			version.Version = "master.387+e872fa"
-		}
-		testMainCaseRun(t, []testMainCase{testCase})
+	for i, testCase := range testCases {
+		t.Run(versionValues[i], func(t *testing.T) {
+			version.Version = versionValues[i]
+			testMainCaseRun(t, []testMainCase{testCase})
+		})
 	}
 }
