5c6fbcead415b3a13c6ef048bec19460eb740b4c
Marc Vertes <mvertes@free.fr>
2025-02-13 16:21:18 +0100

    fix generate

--- tm2/pkg/overflow/overflow_template.sh
+++ tm2/pkg/overflow/overflow_template.sh
@@ -91,7 +91,7 @@ func Quotient${SIZE}(a, b int${SIZE}) (int${SIZE}, int${SIZE}, bool) {
 	}
 	c := a / b
 	status := (c < 0) == ((a < 0) != (b < 0)) || (c == 0) // no sign check for 0 quotient
-	return c, a%b, status
+	return c, a % b, status
 }
 "
 done
