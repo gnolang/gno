93325017aac6272c6f6a5eb5edd2f343518dfa9b
aeddi <antoine.e.b@gmail.com>
2025-02-18 11:11:24 +0100

    wip

--- tm2/pkg/bft/privval/doc.go
+++ tm2/pkg/bft/privval/doc.go
@@ -2,43 +2,44 @@
 * PrivValidator
 *
 * Package privval implements the BFT validator interface defined in tm2/pkg/bft/types.PrivValidator.
-* The BFT validator role is to sign votes and proposals for the consensus protocol and to ensure that
-* it never double signs, even in the case of a crash during the signing process or a malicious attack.
+* The validator role is to sign votes and proposals for the consensus protocol, ensuring that it never
+* double-signs, even in the case of a crash during the signing process or a malicious attack.
 *
 * To achieve this, the PrivValidator relies on two components:
-* - a signer which generates cryptographic signatures for arbitrary byte slice without any checks.
-* - a state which both stores and checks the last signature and signed data to prevent double signing.
+* - a signer that generates cryptographic signatures for arbitrary byte slices without any checks.
+* - a state that both stores and verifies the last signature and signed data to prevent double-signing.
 *
 *
-* Signer:
+* Signer
 *
 * The signer implements the BFT signer interface defined in tm2/pkg/bft/types.Signer. Two implementations
 * are provided in this package:
-* - a local signer which uses a keypair encoded with amino and persisted to disk (default for Gnoland nodes).
-* - a remote signer which uses a client that sends signing requests to a remote signer server.
+* - a local signer that uses a keypair encoded with amino and persisted to disk (default for gnoland nodes).
+* - a remote signer that uses a client sending signing requests to a remote signer server.
 *
 * Both the remote signer client and server are provided in tm2/pkg/bft/privval/signer/remote. The current
-* implementation of these remote signer client/server supports TCP and UNIX socket connections.
+* implementation supports TCP and UNIX socket connections.
 *
 * TCP connections are secured using the cryptographic handshake defined in tm2/pkg/p2p/conn.MakeSecretConnection
 * which is an implementation of the STS protocol described in this whitepaper:
 * https://github.com/tendermint/tendermint/blob/0.1/docs/sts-final.pdf
-* TCP connections can optionally be mutually authenticated using a whitelist of allowed public keys for both the
-* client and the server.
+* TCP connections can optionally be mutually authenticated using a whitelist of authorized public keys for both
+* the client and the server.
 *
 * By default, the remote signer client will indefinitely try to connect to the remote signer server for each
-* request it sends. A node using a private validator with a remote signer will therefore not fail in case of
-* a temporary network failure or a crash of the remote signer server.
+* request it sends. Consequently, a node using a private validator with a remote signer will not fail due to
+* temporary network issues or a crash of the remote signer server.
 *
-* The remote signer server provided by this package is just a generic bridge that take any types.Signer as
-* parameter and proxy the client requests to it. gnokms is a cli tool available in contribs/gnokms that aims to
-* provide a remote signer server along with a set of backend signers among which a gnokey based signer.
+* The remote signer server provided by this package is a generic bridge that take any types.Signer as a
+* parameter and proxies the client requests to it. Additionally, gnokms is a CLI tool available in
+* contribs/gnokms that aims to provide a remote signer server along with a set of backend signers, including
+* one based on gnokey.
 *
 *
-* State:
+* State
 *
-* The state manager defined in tm2/pkg/bft/privval/state does not implement any interface. It basically keep
-* track of the last signature and signed data to prevent double signing. The state is persisted to disk in a
-* file encoded with amino and all the checks are done locally.
+* The state manager defined in tm2/pkg/bft/privval/state does not implement any interface. It basically keeps
+* track of the last signature and signed data to prevent double-signing. The state is persisted to disk in a
+* file encoded with amino and all checks are performed locally.
  */
 package privval
