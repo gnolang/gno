c1fee3e156e92a72d747cb138cf833134c8745d2
Emmanuel T Odeke <emmanuel@orijtech.com>
2025-03-01 01:15:22 +0200

    Another one

--- gnovm/pkg/transpiler/fuzz_test.go
+++ gnovm/pkg/transpiler/fuzz_test.go
@@ -198,6 +198,8 @@ func FuzzTranspiling(f *testing.F) {
 				strings.Contains(sr, "expression in go must be function call"),
 				strings.Contains(sr, "unexpected newline, expecting"),
 				strings.Contains(sr, "imports must appear before"),
+				strings.Contains(sr, "missing condition in"),
+				strings.Contains(sr, "method has multiple receivers"),
 				strings.Contains(sr, "cannot parenthesize embedded type"),
 				strings.Contains(sr, "method must have no type parameters"),
 				strings.Contains(sr, "missing ',' before newline in type argument list"),
