c1174966a945a7e153607c4cbbd2614a3e4a44ca
Emmanuel T Odeke <emmanuel@orijtech.com>
2025-03-01 16:54:28 +0200

    More cases

--- gnovm/pkg/transpiler/fuzz_test.go
+++ gnovm/pkg/transpiler/fuzz_test.go
@@ -202,6 +202,7 @@ func FuzzTranspiling(f *testing.F) {
 				strings.Contains(sr, "method has multiple receivers"),
 				strings.Contains(sr, "expected 'chan', found"),
 				strings.Contains(sr, "missing type constraint"),
+				strings.Contains(sr, "expected boolean expression, found"),
 				strings.Contains(sr, "cannot parenthesize embedded type"),
 				strings.Contains(sr, "method must have no type parameters"),
 				strings.Contains(sr, "missing ',' before newline in type argument list"),
