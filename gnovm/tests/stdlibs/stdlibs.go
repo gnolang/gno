58d387261a56df38fa3b1e58f55b9b88f98960dd
Norman <norman@samourai.coop>
2025-03-04 05:50:10 +0100

    chore: remove deadcode
    Signed-off-by: Norman <norman@samourai.coop>

--- gnovm/tests/stdlibs/stdlibs.go
+++ gnovm/tests/stdlibs/stdlibs.go
@@ -2,8 +2,6 @@
 package stdlibs
 
 import (
-	"slices"
-
 	gno "github.com/gnolang/gno/gnovm/pkg/gnolang"
 	"github.com/gnolang/gno/gnovm/stdlibs"
 )
@@ -17,10 +15,4 @@ func NativeResolver(pkgPath string, name gno.Name) func(*gno.Machine) {
 		}
 	}
 	return stdlibs.NativeResolver(pkgPath, name)
-}
-
-var injectedGoLibs = []string{"fmt", "os", "encoding/json"}
-
-func IsInjectedGoLib(pkgPath string) bool {
-	return slices.Contains(injectedGoLibs, pkgPath)
 }
