5907de49b1c8de695a91c8665e771ca89b63d1ee
Sergio Maria Matone <sw360cab@gmail.com>
2025-03-05 00:22:59 +0100

    generalized command

--- misc/loop/cmd/cmd/cmd_serve.go
+++ misc/loop/cmd/cmd/cmd_serve.go
@@ -4,10 +4,7 @@ import (
 	"context"
 	"loop/cmd/cfg"
 	"loop/cmd/portalloop"
-	"os"
-	"os/signal"
 	"sync"
-	"syscall"
 	"time"
 
 	"github.com/gnolang/gno/tm2/pkg/commands"
@@ -29,38 +26,25 @@ func NewServeCmd(_ commands.IO) *commands.Command {
 	)
 }
 
-func execServe(ctx context.Context, cfg *cfg.CmdCfg) error {
-	var wg sync.WaitGroup
-	logger := logrus.New()
-	portalLoopHandler, err := portalloop.NewPortalLoopHandler(cfg)
-	if err != nil {
-		return err
-	}
+func execServe(ctx context.Context, cfg_ *cfg.CmdCfg) error {
+	return ExecAll(
+		ctx,
+		cfg_,
+		func(ctx context.Context, cfg *cfg.CmdCfg, portalLoopHandler *portalloop.PortalLoopHandler) error {
+			var wg sync.WaitGroup
+			logger := logrus.New()
 
-	sigs := make(chan os.Signal, 1)
-	signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM)
-	//  Wait for termination signal
-	go func() {
-		sig := <-sigs // Wait for SIGTERM or SIGINT
-		logger.Info("Received termination signal, gracefully killing all the existing portal loop instances")
-		portalLoopHandler.ProxyRemoveContainers(ctx)
-
-		exitCode := 1
-		if s, ok := sig.(syscall.Signal); ok {
-			exitCode = int(s)
-		}
-		os.Exit(exitCode)
-	}()
-
-	for {
-		wg.Add(1)
-		go func() {
-			defer wg.Done()
-			portalloop.StartPortalLoop(ctx, *portalLoopHandler, false)
-			// Wait for a new round
-			logger.Info("Waiting 3 min before new loop attempt")
-			time.Sleep(3 * time.Minute)
-		}()
-		wg.Wait()
-	}
+			for {
+				wg.Add(1)
+				go func() {
+					defer wg.Done()
+					portalloop.StartPortalLoop(ctx, *portalLoopHandler, false)
+					// Wait for a new round
+					logger.Info("Waiting 3 min before new loop attempt")
+					time.Sleep(3 * time.Minute)
+				}()
+				wg.Wait()
+			}
+		},
+	)
 }
