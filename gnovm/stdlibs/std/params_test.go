f6c248242e49c4b9e980e1673bd88ef891d341ae
jaekwon <jae@tendermint.com>
2025-03-05 00:57:41 -0800

    update params_test

--- gnovm/stdlibs/std/params_test.go
+++ gnovm/stdlibs/std/params_test.go
@@ -4,37 +4,53 @@ import (
 	"testing"
 )
 
+// XXX move elsewhere in the gno.land/ dir.
+// this test doesn't run with make test. (why not?)
+
 func TestValidate(t *testing.T) {
 	tests := []struct {
-		prefix  string
-		key     string
-		kind    string
-		wantErr bool
+		module    string
+		submodule string
+		name      string
+		type_     string
+		wantErr   bool
 	}{
 		// Valid cases
-		{"module", "valid_key.string", "string", false},
-		{"prefix1", "validKey.int64", "int64", false},
-		{"p_", "_valid123.bool", "bool", false},
+		{"module", "p", "valid_key", "string", false},
+		{"module", "p", "valid_key.string", "string", false}, // is a string
+		{"module1", "p", "validKey", "int64", false},
+		{"module", "p", "1invalid", "string", false}, // Starts with a number (see IsASCII)
+		{"p_", "p", "_valid123", "bool", false},
+		{"module", "_", "valid_key", "string", false},                         // Underscore as submodule
+		{"module", "gno.land/r/myuser/myrealm", "valid_key", "string", false}, // Realm path as submodule
+		{"1module", "valid_key.string", "string", false},                      // Module starts with a number
 
 		// Invalid key cases
-		{"module", "invalidKey", "string", true},         // Missing ".kind" suffix
-		{"module", "", "string", true},                   // Empty key
-		{"module", "1invalid.string", "string", true},    // Starts with a number
-		{"module", "-invalid.string", "string", true},    // Starts with an invalid character
-		{"module", "invalid-123.string", "string", true}, // Contains invalid character (-)
-		{"module", "valid/path.key.bool", "bool", true},  // Contains invalid character (/)
+		{"module", "p", "", "string", true},             // Empty key
+		{"module", "p", "-invalid", "string", true},     // Starts with an invalid character
+		{"module", "p", "invalid-123", "string", true},  // Contains invalid character (-)
+		{"module", "p", "valid/path.key", "bool", true}, // Contains invalid character (/)
+		{"module", "p", "invalid.string", 123, true},    // Not a string
+		{"module", "p", "valid:key", "string", true},    // ":" in name
+		{"module", "p", "valid:", "string", true},       // ":" in name
 
-		// Invalid prefix cases
-		{"1prefix", "valid_key.string", "string", true},           // Prefix starts with a number
-		{"-prefix", "valid_key.string", "string", true},           // Prefix starts with an invalid character
-		{"prefix!", "valid_key.string", "string", true},           // Prefix contains invalid character (!)
-		{"module/submodule", "valid/path.key.bool", "bool", true}, // Prefix contains invalid character (/)
+		// Invalid submodule cases
+		{"module", "", "valid_key", "string", true},    // Empty submodule
+		{"module", "p:q", "valid_key", "string", true}, // ":" in submodule
+		{"module", "p:", "valid_key", "string", true},  // ":" in submodule
+
+		// Invalid module cases
+		{"module!", "p", "valid_key", "string", true},          // Module contains invalid character (!)
+		{"-prefix", "p", "valid_key", "string", true},          // Module starts with an invalid character
+		{"module/submodule", "p", "valid_key", "string", true}, // Module contains invalid character (/)
+		{"module:submodule", "p", "valid_key", "string", true}, // Module contains invalid character (/)
 	}
 
 	for _, tt := range tests {
-		err := validate(tt.prefix, tt.key, tt.kind)
+		// XXX where did the validate functino go?
+		err := validate(tt.module, tt.submodule, tt.name, tt.type_)
 		if (err != nil) != tt.wantErr {
-			t.Errorf("validate(%q, %q, %q) = %v, wantErr %v", tt.prefix, tt.key, tt.kind, err, tt.wantErr)
+			t.Errorf("validate(%q, %q, %q, %q) = %v, wantErr %v", tt.module, tt.submodule, tt.name, tt.type_, err, tt.wantErr)
 		}
 	}
 }
