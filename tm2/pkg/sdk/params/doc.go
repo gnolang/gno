fedc6b62ff5171afc484bbfedbd383ccaa8c8dd7
jaekwon <jae@tendermint.com>
2025-03-05 04:42:56 -0800

    update tm2 param docs

--- tm2/pkg/sdk/params/doc.go
+++ tm2/pkg/sdk/params/doc.go
@@ -7,52 +7,31 @@
 // The Params Module provides functionalities for caching and persistent access
 // to parameters across the entire chain.
 //
-// It manages both module parameters and arbitrary parameters.
+// Keys are generally of the format <module>:<submodule>:<name>.
+// Parameters are stored in the underlying store with key format
+// /pv/<module>:<submodule>:<name>.
+//
+//  * 'module' must be an alphanumeric ASCII string.
+//  * 'submodule' can be anything but cannot contain a ':'
+//  * 'submodule' is set to 'p' for keeper param structs.
+//  * The VM keeper of gno.land uses the submodule for realm paths.
+//
 // Module parameters are sourced from all other keepers, such as AuthKeeper,
-// BankKeeper, and VMKeeper.Each keeper registers its keeper keys with ParamKeeper
-// using the ParamfulKeeper interface.WillSetParam() is called whenever module
-// parameters need to be updated.
+// BankKeeper, and VMKeeper which must be registered with .Register().
+// The ParamsKeeper ensures that the <module> is registered,
+// but otherwise doesn't enforce much else about the key format.
 //
+// Before params are written ParamfulKeeper.WillSetParam() is called, allowing
+// any custom caching to happen if needed. Any caching must be stored in 'ctx',
+// and keepers must otherwise be immutable otherwise the checktx (mempool) and
+// delivertx (blockchain) states would trample each other.
 //
-// NOTE: important to not use local cached fields unless they are synchronously
-// stored to the underlying store. This optimization generally only belongs in prmk.GetParams().
-// users of prmk.GetParams() generally should not cache anything and
-// instead rely on the efficiency of prmk.GetParams().
+// ParamKeeper.SetStruct(module, "p", k, v) is used by each registered module
+// keeper to set the module parameter struct (for type-safety).
+// ParamKeeper.SetParamXXX() is used to set arbitrary primitive parameters.
 //
-// In other words, ParamKeeper is the only component responsible for caching and storing parameters.
-// Other keepers should neither cache nor maintain these parameters as state variables.
-// While store access is synchronized, keeper access is not.
-//
-// Here is the interval key specs that is stored
-
-// /pv/<module>:(<realm>".")?<key>
-//
-//
-// ParamKeeper.SetParams(module_prefix, k, v) is used by each registered module keeper to set the
-// module parameter struct.
-// ParamKeeper.SetParamXXX() is used to set arbitrary parameters as single primitive values.
-//
-// A prefix, ValueStoreKeyPrefix (/pv/), is added to each key before it is stored as the internal key:
-// Module parameter keys follow this format:
-// /pv/<module>:<key>
-
-// For arbitrary parameter keys that set from realm.
-// /pv/vm:<realm>.<key>
-
 // The method for querying parameters follows this pattern:
-// To query module parameters:
-// gnokey query params/<module_prefix>:<params_key>
-//
-// Since a module parameter is a struct, a simple key "p" is used by the each module
-// For example, to query the Auth module's parameters:
-// gnokey query params/auth:p
-//
-// To query arbitrary parameters:
-// gnokey query params/<params_key>
-//
-// For example:
-// gnokey query params/gno.land/r/myrealm.foo
+// gnokey query params/<module>:<submodule>:<name>.
+// For example, gnokey query params/vm:gno.land/r/myuser/myrealm:foo.
 
-// XXX: removes isAlphaNum validation for keys.
-// (isAlphaNum for request router validation not sure we want to change it)
 package params
