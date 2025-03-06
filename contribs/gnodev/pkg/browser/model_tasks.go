43b9fcafa40bbe5d7b485ff697affd475abc79a5
hthieu1110 <tronghieu.ha@gmail.com>
2025-03-06 16:55:13 +0700

    fix: not expose expandNative

--- contribs/gnodev/pkg/browser/model_tasks.go
+++ contribs/gnodev/pkg/browser/model_tasks.go
@@ -50,7 +50,6 @@ func (m LoaderModel) Update(msg tea.Msg) (LoaderModel, tea.Cmd) {
 
 func (m LoaderModel) tick() tea.Cmd {
 	return tea.Tick(m.spinner.FPS, func(t time.Time) tea.Msg {
-		t.Unix()
 		return SpinnerTickMsg(t)
 	})
 }
