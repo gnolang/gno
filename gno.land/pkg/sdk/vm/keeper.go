b0eaaf48d9613081f0bed089054180c05ae0b83a
Leon Hudak <33522493+leohhhn@users.noreply.github.com>
2025-03-08 12:25:15 +0100

    refactor: `r/demo/users` (#3166)
    ## Description
    
    Addresses: #2827 
    
    This PR refactors the current `r/demo/users` & `r/sys/users` system in
    accordance to the issue mentioned above. Below is the high-level
    overview of the new system.
    
    It also makes a small change to the `p/demo/releases` lib.
    
    ## `r/gnoland/users/v1`
    
    - Fully replaces `r/demo/users`
    - Is the main entry point for users looking to register a name, create
    an alias (rename themselves), or delete their username
    - Implements allowed username pattern checks as per the issue #2827
    - Is pausable by GovDAO, contains no other administration functionality
    - Contains a nice UI/render for users (please suggest improvements) -
    see bottom
    - Uses `r/demo/profile` for displaying extra information about a user
    (to be moved to `r/nt/profiles` or `r/gnoland/profiles`)
    - @jaekwon's request: add payment in GNOT for registration ✅
    
    ## `r/sys/users`
    
    - Namespace check moved to `r/sys/names`
    - Contains the address/name <> user stores, to which only whitelisted
    realms can write
    - Contains resolver functions & other useful getters
    - Is callable only by whitelisted realms, such as `r/gnoland/users/v1`,
    etc.
    -  Whitelist is fully managed by GovDAO (after #3523 was resolved)
    
    ## `r/sys/names`
    - Namespace checking functionality moved here (from `r/sys/users`),
    added tests, as discussed with @moul
    - ~Verify function now only upgradeable with a GovDAO prop instead of
    previously being ownable~ verify func upgrateability removed, as we'll
    just deploy a `r/sys/names/vX` when we need to, as discussed with @moul
    - ~Verify functionality is now not pausable and on by default, as per
    discussions with @moul. This means Portal Loop will need to be patched
    upon merging this PR, as many txs will fail due to not having proper
    namespace permissions.~ The namespace verify functionality is off by
    default, but should be enabled in genesis via `r/sys/names.Enable()`.
    The Portal Loop will indeed need to be patched if we enable this
    functionality.
    
    ## `Keeper` & genesis params
    - Updated the VM keeper to call `r/sys/names` for the namespace check
    instead of `r/sys/users`
    - Updated params to load `r/sys/names`, which the keeper will call
    
    ### `r/gnoland/users/v1`
    
    ![Screenshot 2025-01-17 at 01 10
    56](https://github.com/user-attachments/assets/b824c2aa-29b3-4497-99bf-689b7f49cb4a)
    
    ## `r/gnoland/users`
    
    Currently this is the "releases" page for the user registration systems
    (example data):
    
    <img width="1287" alt="Screenshot 2025-02-03 at 12 19 28"
    src="https://github.com/user-attachments/assets/0407b21d-4457-48af-80f4-b0ef10649337"
    />
    
    <details><summary>Contributors' checklist...</summary>
    
    - [x] Added new tests, or not needed, or not feasible
    - [x] Provided an example (e.g. screenshot) to aid review or the PR is
    self-explanatory
    - [x] Updated the official documentation or not needed
    - [x] No breaking changes were made, or a `BREAKING CHANGE: xxx` message
    was included in the description
    - [x] Added references to related issues and PRs
    - [x] Provided any useful hints for running manual tests
    </details>
    
    ---------
    
    Co-authored-by: Morgan <morgan@morganbaz.com>
    Co-authored-by: Manfred Touron <94029+moul@users.noreply.github.com>

--- gno.land/pkg/sdk/vm/keeper.go
+++ gno.land/pkg/sdk/vm/keeper.go
@@ -239,8 +239,8 @@ var reNamespace = regexp.MustCompile(`^[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}/(?:r|p)/([\.
 
 // checkNamespacePermission check if the user as given has correct permssion to on the given pkg path
 func (vm *VMKeeper) checkNamespacePermission(ctx sdk.Context, creator crypto.Address, pkgPath string) error {
-	sysUsersPkg := vm.getSysUsersPkgParam(ctx)
-	if sysUsersPkg == "" {
+	sysNamesPkg := vm.getSysNamesPkgParam(ctx)
+	if sysNamesPkg == "" {
 		return nil
 	}
 	chainDomain := vm.getChainDomainParam(ctx)
@@ -259,10 +259,10 @@ func (vm *VMKeeper) checkNamespacePermission(ctx sdk.Context, creator crypto.Add
 	default:
 		panic("invalid pattern while matching pkgpath")
 	}
-	username := match[1]
+	namespace := match[1]
 
 	// if `sysUsersPkg` does not exist -> skip validation.
-	usersPkg := store.GetPackage(sysUsersPkg, false)
+	usersPkg := store.GetPackage(sysNamesPkg, false)
 	if usersPkg == nil {
 		return nil
 	}
@@ -294,15 +294,15 @@ func (vm *VMKeeper) checkNamespacePermission(ctx sdk.Context, creator crypto.Add
 		})
 	defer m.Release()
 
-	// call $sysUsersPkg.IsAuthorizedAddressForName("<user>")
-	// We only need to check by name here, as address have already been check
+	// call sysNamesPkg.IsAuthorizedAddressForName("<user>")
+	// We only need to check by name here, as addresses have already been checked
 	mpv := gno.NewPackageNode("main", "main", nil).NewPackage()
 	m.SetActivePackage(mpv)
-	m.RunDeclaration(gno.ImportD("users", sysUsersPkg))
+	m.RunDeclaration(gno.ImportD("names", sysNamesPkg))
 	x := gno.Call(
-		gno.Sel(gno.Nx("users"), "IsAuthorizedAddressForName"),
+		gno.Sel(gno.Nx("names"), "IsAuthorizedAddressForNamespace"),
 		gno.Str(creator.String()),
-		gno.Str(username),
+		gno.Str(namespace),
 	)
 
 	ret := m.Eval(x)
@@ -316,7 +316,11 @@ func (vm *VMKeeper) checkNamespacePermission(ctx sdk.Context, creator crypto.Add
 	}
 
 	if isAuthorized := useraddress.GetBool(); !isAuthorized {
-		return ErrUnauthorizedUser(username)
+		return ErrUnauthorizedUser(
+			fmt.Sprintf("%s is not authorized to deploy packages to namespace `%s`",
+				creator.String(),
+				namespace,
+			))
 	}
 
 	return nil
