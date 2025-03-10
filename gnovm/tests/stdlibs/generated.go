adf5fc22cebcd8ba1e3b2ae53cc4bb264ec0521f
Morgan Bazalgette <morgan@morganbaz.com>
2025-03-10 22:49:20 +0100

    generated

--- gnovm/tests/stdlibs/generated.go
+++ gnovm/tests/stdlibs/generated.go
@@ -636,6 +636,7 @@ var nativeFuncs = [...]NativeFunc{
 		},
 		[]gno.FieldTypeExpr{
 			{Name: gno.N("r0"), Type: gno.X("bool")},
+			{Name: gno.N("r1"), Type: gno.X("string")},
 		},
 		false,
 		func(m *gno.Machine) {
@@ -654,42 +655,17 @@ var nativeFuncs = [...]NativeFunc{
 			tv1.DeepFill(m.Store)
 			gno.Gno2GoValue(tv1, rp1)
 
-			r0 := testlibs_testing.X_matchString(p0, p1)
+			r0, r1 := testlibs_testing.X_matchString(p0, p1)
 
 			m.PushValue(gno.Go2GnoValue(
 				m.Alloc,
 				m.Store,
 				reflect.ValueOf(&r0).Elem(),
 			))
-		},
-	},
-	{
-		"testing",
-		"verifyRegex",
-		[]gno.FieldTypeExpr{
-			{Name: gno.N("p0"), Type: gno.X("string")},
-		},
-		[]gno.FieldTypeExpr{
-			{Name: gno.N("r0"), Type: gno.X("string")},
-		},
-		false,
-		func(m *gno.Machine) {
-			b := m.LastBlock()
-			var (
-				p0  string
-				rp0 = reflect.ValueOf(&p0).Elem()
-			)
-
-			tv0 := b.GetPointerTo(nil, gno.NewValuePathBlock(1, 0, "")).TV
-			tv0.DeepFill(m.Store)
-			gno.Gno2GoValue(tv0, rp0)
-
-			r0 := testlibs_testing.X_verifyRegex(p0)
-
 			m.PushValue(gno.Go2GnoValue(
 				m.Alloc,
 				m.Store,
-				reflect.ValueOf(&r0).Elem(),
+				reflect.ValueOf(&r1).Elem(),
 			))
 		},
 	},
