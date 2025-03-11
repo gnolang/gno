a693cd5146591261f90956e1899f5460d39535bc
Jeff Thompson <jeff@thefirst.org>
2025-03-11 14:37:25 +0100

    fix: In vm/qdoc, need to include Consts and Vars of the Type
    Signed-off-by: Jeff Thompson <jeff@thefirst.org>

--- gnovm/pkg/doc/json_doc.go
+++ gnovm/pkg/doc/json_doc.go
@@ -127,6 +127,24 @@ func (d *Documentable) WriteJSONDocumentation() (*JSONDocumentation, error) {
 			Doc:       string(pkg.Markdown(typ.Doc)),
 		})
 
+		// values of this type
+		for _, c := range typ.Consts {
+			jsonDoc.Values = append(jsonDoc.Values, &JSONValueDecl{
+				Signature: mustFormatNode(d.pkgData.fset, c.Decl),
+				Const:     true,
+				Values:    d.extractValueSpecs(pkg, c.Decl.Specs),
+				Doc:       string(pkg.Markdown(c.Doc)),
+			})
+		}
+		for _, v := range typ.Vars {
+			jsonDoc.Values = append(jsonDoc.Values, &JSONValueDecl{
+				Signature: mustFormatNode(d.pkgData.fset, v.Decl),
+				Const:     false,
+				Values:    d.extractValueSpecs(pkg, v.Decl.Specs),
+				Doc:       string(pkg.Markdown(v.Doc)),
+			})
+		}
+
 		// constructors for this type
 		for _, fun := range typ.Funcs {
 			jsonDoc.Funcs = append(jsonDoc.Funcs, &JSONFunc{
