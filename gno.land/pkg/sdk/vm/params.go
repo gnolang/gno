294638a4bd0b2ac142287c0117d961352a5332e5
Miloš Živković <milos.zivkovic@tendermint.com>
2025-03-20 19:48:59 +0100

    Push partial local changes. Some funky things are happening, need to investigate

--- gno.land/pkg/sdk/vm/params.go
+++ gno.land/pkg/sdk/vm/params.go
@@ -3,10 +3,12 @@ package vm
 import (
 	"fmt"
 	"regexp"
+	"strconv"
 	"strings"
 
 	gno "github.com/gnolang/gno/gnovm/pkg/gnolang"
 	"github.com/gnolang/gno/tm2/pkg/amino"
+	"github.com/gnolang/gno/tm2/pkg/crypto"
 	"github.com/gnolang/gno/tm2/pkg/sdk"
 )
 
@@ -75,8 +77,16 @@ func (vm *VMKeeper) GetParams(ctx sdk.Context) Params {
 }
 
 const (
-	sysUsersPkgParamPath = "vm:p:sysnames_pkgpath"
-	chainDomainParamPath = "vm:p:chain_domain"
+	moduleParamPrefix = "vm"
+
+	// Param declarations
+	sysUsersPkgParam   = "p:sysnames_pkgpath"
+	chainDomainParam   = "p:chain_domain"
+	valsetUpdatesParam = "p:valset_updates"
+
+	// Full param paths (for lookups)
+	sysUsersPkgParamPath = moduleParamPrefix + ":" + sysUsersPkgParam
+	chainDomainParamPath = moduleParamPrefix + ":" + chainDomainParam
 )
 
 func (vm *VMKeeper) getChainDomainParam(ctx sdk.Context) string {
@@ -91,6 +101,74 @@ func (vm *VMKeeper) getSysNamesPkgParam(ctx sdk.Context) string {
 	return sysNamesPkg
 }
 
-func (vm *VMKeeper) WillSetParam(ctx sdk.Context, key string, value interface{}) {
-	// XXX validate input?
+func (vm *VMKeeper) WillSetParam(_ sdk.Context, key string, value any) {
+	switch {
+	case strings.HasPrefix(key, valsetUpdatesParam):
+		// Validate the proposed valset changes
+		changes, ok := value.([]string)
+		if !ok {
+			// This panic is explicit, because the issue is otherwise
+			// unclearly propagated in the GnoVM to the caller (cast error)
+			panic(
+				fmt.Sprintf(
+					"Value for VM param %s update is an invalid type (%T)",
+					key,
+					value,
+				),
+			)
+		}
+
+		// Sanity check the param update
+		if err := validateValsetUpdate(changes); err != nil {
+			panic(err)
+		}
+	default:
+		// Allow setting arbitrary key-vals
+	}
+}
+
+// validateValsetUpdate validates the validator set updates,
+// which are serialized in the form:
+//   - <address>:<pub-key>:<voting-power>
+//   - voting power == 0 => validator removal
+//   - voting power != 0 => validator power update / validator addition
+func validateValsetUpdate(changes []string) error {
+	for _, change := range changes {
+		changeParts := strings.Split(change, ":")
+		if len(changeParts) != 3 {
+			return fmt.Errorf(
+				"valset update is not in the format <address>:<pub-key>:<voting-power>, but %s",
+				change,
+			)
+		}
+
+		// Grab the address
+		address, err := crypto.AddressFromBech32(changeParts[0])
+		if err != nil {
+			return fmt.Errorf("invalid validator address: %w", err)
+		}
+
+		// Grab the public key
+		pubKey, err := crypto.PubKeyFromBech32(changeParts[1])
+		if err != nil {
+			return fmt.Errorf("invalid validator pubkey: %w", err)
+		}
+
+		// Make sure the address matches the public key
+		if pubKey.Address().Compare(address) != 0 {
+			return fmt.Errorf(
+				"address (%s) does not match public key address (%s)",
+				address,
+				pubKey.Address(),
+			)
+		}
+
+		// Validate the voting power
+		_, err = strconv.ParseUint(changeParts[2], 10, 64)
+		if err != nil {
+			return fmt.Errorf("invalid voting power: %w", err)
+		}
+	}
+
+	return nil
 }
