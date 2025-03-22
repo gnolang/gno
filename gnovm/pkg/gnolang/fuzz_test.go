233f72856642f94e68d399f622182f6e01cb4833
Emmanuel T Odeke <emmanuel@orijtech.com>
2025-03-23 00:21:02 +0200

    test: fuzz gnolang.ConvertTo
    Adds a fuzzer for gnolang.ConvertTo.

--- gnovm/pkg/gnolang/fuzz_test.go
+++ gnovm/pkg/gnolang/fuzz_test.go
@@ -1,6 +1,8 @@
 package gnolang
 
 import (
+	"encoding/json"
+	"math/big"
 	"os"
 	"path/filepath"
 	"runtime"
@@ -8,6 +10,11 @@ import (
 	"testing"
 
 	"github.com/cockroachdb/apd/v3"
+
+	"github.com/gnolang/gno/tm2/pkg/db/memdb"
+	"github.com/gnolang/gno/tm2/pkg/store/dbadapter"
+	"github.com/gnolang/gno/tm2/pkg/store/iavl"
+	stypes "github.com/gnolang/gno/tm2/pkg/store/types"
 )
 
 func FuzzConvertUntypedBigdecToFloat(f *testing.F) {
@@ -85,5 +92,98 @@ func FuzzParseFile(f *testing.F) {
 	// 2. Now run the fuzzer.
 	f.Fuzz(func(t *testing.T, goFileContents string) {
 		_, _ = ParseFile("a.go", goFileContents)
+	})
+}
+
+type convertToCorpus struct {
+	TV  *TypedValue `json:"tv"`
+	Typ Type        `json:"typ"`
+}
+
+func mustDecFromStr(s string) *apd.Decimal {
+	dec, _, err := apd.NewFromString(s)
+	if err != nil {
+		panic(err)
+	}
+	return dec
+}
+
+func FuzzConvertTo(f *testing.F) {
+	seeds := []*convertToCorpus{
+		{
+			TV: &TypedValue{
+				T: Float32Type,
+				V: &BigdecValue{V: mustDecFromStr("10.999")},
+			},
+			Typ: Float64Type,
+		},
+		{
+			TV: &TypedValue{
+				T: Float64Type,
+				V: &BigdecValue{V: mustDecFromStr("10.999")},
+			},
+			Typ: Float64Type,
+		},
+		{
+			TV: &TypedValue{
+				T: Float64Type,
+				V: &BigdecValue{V: mustDecFromStr("10.999")},
+			},
+			Typ: Float32Type,
+		},
+		{
+			TV: &TypedValue{
+				T: Int64Type,
+				V: &BigintValue{V: big.NewInt(-9816)},
+			},
+			Typ: Float32Type,
+		},
+		{
+			TV: &TypedValue{
+				T: Int64Type,
+				V: &BigintValue{V: big.NewInt(9816)},
+			},
+			Typ: Float32Type,
+		},
+		{
+			TV: &TypedValue{
+				T: Uint64Type,
+				V: &BigintValue{V: big.NewInt(9816)},
+			},
+			Typ: Float32Type,
+		},
+		{
+			TV: &TypedValue{
+				T: Uint64Type,
+				V: &BigintValue{V: big.NewInt(9816)},
+			},
+			Typ: Float64Type,
+		},
+	}
+
+	for _, seed := range seeds {
+		blob, err := json.Marshal(seed)
+		if err != nil {
+			f.Fatal(err)
+		}
+		f.Add(blob)
+	}
+
+	f.Fuzz(func(t *testing.T, inputJSON []byte) {
+		cc := new(convertToCorpus)
+		if err := json.Unmarshal(inputJSON, cc); err != nil {
+			return
+		}
+		if cc.TV == nil || cc.TV.V == nil {
+			return
+		}
+
+		db := memdb.NewMemDB()
+		baseStore := dbadapter.StoreConstructor(db, stypes.StoreOptions{})
+		iavlStore := iavl.StoreConstructor(db, stypes.StoreOptions{})
+		store := NewStore(nil, baseStore, iavlStore)
+		allocator := NewAllocator(1 << 20)
+		ConvertTo(allocator, store, cc.TV, cc.Typ, true)
+		ConvertTo(allocator, store, cc.TV, cc.Typ, false)
 	})
 }
