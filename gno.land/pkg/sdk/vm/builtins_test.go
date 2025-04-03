cb9a43714ddbb0496027678e72c515ea163c3b8a
Lee ByeongJun <lbj199874@gmail.com>
2025-04-04 00:42:39 +0900

    fix

--- gno.land/pkg/sdk/vm/builtins_test.go
+++ gno.land/pkg/sdk/vm/builtins_test.go
@@ -3,10 +3,6 @@ package vm
 import (
 	"testing"
 
-	"github.com/gnolang/gno/tm2/pkg/crypto"
-	"github.com/gnolang/gno/tm2/pkg/std"
-
-	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
 
@@ -66,50 +62,6 @@ func TestParamsKeeper(t *testing.T) {
 	for _, tc := range testCases {
 		t.Run(tc.name, func(t *testing.T) {
 			require.PanicsWithValue(t, tc.expectedMsg, tc.setFunc, "The panic message did not match the expected value")
-		})
-	}
-}
-
-func TestSDKBankerTotalCoin(t *testing.T) {
-	env := setupTestEnv()
-	ctx := env.vmk.MakeGnoTransactionStore(env.ctx)
-	banker := NewSDKBanker(env.vmk, ctx)
-
-	// create test accounts and set coins
-	addr1 := crypto.AddressFromPreimage([]byte("addr1"))
-	addr2 := crypto.AddressFromPreimage([]byte("addr2"))
-
-	acc1 := env.acck.NewAccountWithAddress(ctx, addr1)
-	env.acck.SetAccount(ctx, acc1)
-	env.bankk.SetCoins(ctx, addr1, std.MustParseCoins("1000ugnot,500atom"))
-
-	acc2 := env.acck.NewAccountWithAddress(ctx, addr2)
-	env.acck.SetAccount(ctx, acc2)
-	env.bankk.SetCoins(ctx, addr2, std.MustParseCoins("2000ugnot,1500atom"))
-
-	tests := []struct {
-		name      string
-		denom     string
-		expected  int64
-		mustPanic bool
-	}{
-		{"ugnot total", "ugnot", 3000, false},
-		{"atom total", "atom", 2000, false},
-		{"non-existent denom", "foo", 0, false},
-		{"zero balance accounts included", "ugnot", 3000, false},
-		{"empty string denom", "", 0, true},
-	}
-
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			if tt.mustPanic {
-				assert.Panics(t, func() {
-					banker.TotalCoin(tt.denom)
-				})
-			} else {
-				total := banker.TotalCoin(tt.denom)
-				assert.Equal(t, tt.expected, total)
-			}
 		})
 	}
 }
