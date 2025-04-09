b7cf630442da390415fd9a291f1112ef9a3a0045
Mikael VALLENET <mikael.vallenetpro@gmail.com>
2025-04-09 10:20:31 +0200

    fix(stdlibs/time): update injected chainTZParamData function
    Co-authored-by: Morgan <git@howl.moe>

--- gnovm/stdlibs/time/timezoneinfo.go
+++ gnovm/stdlibs/time/timezoneinfo.go
@@ -4,7 +4,6 @@ import (
 	gno "github.com/gnolang/gno/gnovm/pkg/gnolang"
 )
 
-func X_chainTZParamData(m *gno.Machine) (data []byte) {
-	_ = m
-	return []byte{}
+func X_chainTZParamData() []byte {
+	return nil
 }
