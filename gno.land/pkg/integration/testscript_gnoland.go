d0bfc82008d19b4247ca050c1751b0122ea467d1
gfanton <8671905+gfanton@users.noreply.github.com>
2025-04-11 13:33:11 +0200

    fix: gnoland

--- gno.land/pkg/integration/testscript_gnoland.go
+++ gno.land/pkg/integration/testscript_gnoland.go
@@ -291,8 +291,10 @@ func gnolandCmd(t *testing.T, nodesManager *NodesManager, gnoRootDir string) fun
 			if *lockTransfer {
 				genesis.Bank.Params.RestrictedDenoms = []string{"ugnot"}
 			}
+
+			// XXX: temporary, we should set feecollector and other params whithin txtar
 			genesis.VM.RealmParams = append(genesis.VM.RealmParams, tsGenesis.VM.RealmParams...)
-			genesis.Auth.Params = tsGenesis.Auth.Params
+			genesis.Auth.Params.FeeCollector = tsGenesis.Auth.Params.FeeCollector
 
 			cfg.Genesis.AppState = genesis
 			if *nonVal {
