7b6f5aa4b0061aa23956de9f8cc06d933e7f6fc3
alexiscolin <alexis@jaunebleu.co>
2025-04-18 18:08:18 +0900

    fix: remove useless ts

--- gno.land/pkg/gnoweb/frontend/js/utils.ts
+++ gno.land/pkg/gnoweb/frontend/js/utils.ts
@@ -11,38 +11,6 @@ export function debounce<T extends (...args: any[]) => void>(func: T, delay: num
   };
 }
 
-export const throttle = (fn: Function, wait: number) => {
-  let lastTime = 0;
-  return (...args: any[]) => {
-    const now = Date.now();
-    if (now - lastTime >= wait) {
-      lastTime = now;
-      fn(...args);
-    }
-  };
-};
-
 export function escapeShellSpecialChars(arg: string): string {
   return arg.replace(/([$`"\\!|&;<>*?{}()])/g, "\\$1");
 }
-
-class ViewportObserver {
-  private width: number;
-
-  constructor() {
-    this.width = window.innerWidth;
-
-    window.addEventListener(
-      "resize",
-      throttle(() => {
-        this.width = window.innerWidth;
-      }, 100)
-    );
-  }
-
-  getWidth(): number {
-    return this.width;
-  }
-}
-export const viewport = new ViewportObserver();
-
