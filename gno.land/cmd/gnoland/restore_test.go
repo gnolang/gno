0f238dd1e5294037df685385f782ce7e1e41c73a
Norman <norman@samourai.coop>
2025-05-18 12:37:02 +0200

    chore: remove cumbersome golden backup
    Signed-off-by: Norman <norman@samourai.coop>

--- gno.land/cmd/gnoland/restore_test.go
+++ gno.land/cmd/gnoland/restore_test.go
@@ -2,28 +2,65 @@ package main
 
 import (
 	"context"
+	"flag"
 	"os"
 	"path/filepath"
 	"testing"
+	"time"
 
+	"github.com/gnolang/gno/tm2/pkg/bft/backup/v1"
 	"github.com/gnolang/gno/tm2/pkg/commands"
 	"github.com/stretchr/testify/require"
+	"go.uber.org/zap"
 )
 
 func TestRestore(t *testing.T) {
-	// XXX: generating the golden backup is cumbersome currently,
-	// we need to start a node with backup service enabled, wait for some blocks to be commited,
-	// run tm2backup, and copy files in the testdata dir, TODO: automate it
+	backupDir := t.TempDir()
+	generateBackup(t, backupDir, 2)
 
-	tmpDir := t.TempDir()
+	restoreDir := t.TempDir()
 	io := commands.NewTestIO()
 	io.SetOut(os.Stdout)
 	io.SetErr(os.Stderr)
 	err := newRestoreCmd(io).ParseAndRun(context.Background(), []string{
-		"--data-dir", filepath.Join(tmpDir, "chain-data"),
-		"--backup-dir", filepath.FromSlash("testdata/backup-2blocks"),
-		"--genesis", filepath.FromSlash("testdata/backup-2blocks/genesis.json"),
+		"--data-dir", filepath.Join(restoreDir, "chain-data"),
+		"--backup-dir", backupDir,
+		"--genesis", filepath.Join(backupDir, "genesis.json"),
 		"--lazy",
+	})
+	require.NoError(t, err)
+}
+
+func generateBackup(t *testing.T, backupDir string, height int64) {
+	t.Helper()
+
+	nodeDir := t.TempDir()
+
+	io := commands.NewTestIO()
+	io.SetOut(os.Stdout)
+	io.SetErr(os.Stderr)
+
+	cfg := &nodeCfg{}
+	fs := flag.NewFlagSet("", flag.PanicOnError)
+	cfg.RegisterFlags(fs)
+	require.NoError(t, fs.Parse([]string{"--lazy", "--data-dir", nodeDir, "--genesis", filepath.Join(backupDir, "genesis.json")}))
+
+	node, err := createNode(cfg, io)
+	require.NoError(t, err)
+
+	require.NoError(t, node.Start())
+
+	for node.BlockStore().Height() < height {
+		time.Sleep(1 * time.Second)
+	}
+
+	require.NoError(t, node.Stop())
+
+	err = backup.WithWriter(backupDir, 0, height, zap.NewNop(), func(startHeight int64, write backup.Writer) error {
+		for i := startHeight; i <= height; i++ {
+			require.NoError(t, write(node.BlockStore().LoadBlock(i)))
+		}
+		return nil
 	})
 	require.NoError(t, err)
 }
