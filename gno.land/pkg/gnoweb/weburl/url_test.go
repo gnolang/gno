gnolang/gno

commit 241b7ef39808237e79b35be0c1a1c5e2126a5b61

fix(gnoweb): pass encoded query strings to render (#4318)
see: https://github.com/gnolang/gno/pull/4084#discussion_r2090854152

gno.land/pkg/gnoweb/weburl/url.go
@@ -117,7 +117,7 @@ func escapeDollarSign(s string) string {
 // EncodeArgs encodes the arguments and query parameters into a string.
 // This function is intended to be passed as a realm `Render` argument.
 func (gnoURL GnoURL) EncodeArgs() string {
-	return gnoURL.Encode(EncodeArgs | EncodeQuery | EncodeNoEscape)
+	return gnoURL.Encode(EncodeArgs | EncodeQuery)
 }
 
 // EncodeURL encodes the path, arguments, and query parameters into a string.

gno.land/pkg/gnoweb/weburl/url_test.go
@@ -579,3 +579,18 @@ func TestEncode(t *testing.T) {
 		})
 	}
 }
+
+func TestGnoURL_EncodeArgs(t *testing.T) {
+	// EncodeArgs is used to pass the values to the render function in Gno.
+	// Thus, it should maintain values escaped as they are in the original URL
+	// where it matters - like =&
+	gurl, err := Parse("/r/demo/users:foo%3d%24bar%26%3fbaz?hey=1&a%2bb=b%2b&%26=%3d")
+	require.NoError(t, err)
+	assert.Equal(t, "foo=$bar&?baz", gurl.Args)
+	assert.Equal(t, url.Values{
+		"hey": {"1"},
+		"a+b": {"b+"},
+		"&":   {"="},
+	}, gurl.Query)
+	assert.Equal(t, "foo=%24bar&%3Fbaz?%26=%3D&a%2Bb=b%2B&hey=1", gurl.EncodeArgs())
+}

commit 54386d449d3a543401d0b6b6df5b9668ee157a53

fix(gnoweb): fixup path escaping (#4323)
There was a regression in #4318, whereby some paths like `/about` were
no longer working because the slash in `p/about` when passed to render
was being URL-encoded, and thus not understood by gnoweb.

This PR changes the behaviour in `weburl` so that the paths are more
adequately encoded in the different scenarios. cc @gfanton

gno.land/pkg/gnoweb/weburl/url.go
@@ -61,7 +61,7 @@ const (
 func (gnoURL GnoURL) Encode(encodeFlags EncodeFlag) string {
 	var urlstr strings.Builder
 
-	noEscape := encodeFlags.Has(EncodeNoEscape)
+	escape := !encodeFlags.Has(EncodeNoEscape)
 
 	if encodeFlags.Has(EncodeDomain) {
 		urlstr.WriteString(gnoURL.Domain)
@@ -82,42 +82,47 @@ func (gnoURL GnoURL) Encode(encodeFlags EncodeFlag) string {
 			urlstr.WriteRune(':')
 		}
 
-		// XXX: Arguments should ideally always be escaped,
-		// but this may require changes in some realms.
+		// web: $ url-encoded + normal path encoding except /
+		// qrender arg: ? url-encoded, rest is sent decoded.
 		args := gnoURL.Args
-		if !noEscape {
-			args = escapeDollarSign(url.PathEscape(args))
+		if escape {
+			args = webEscapedReplacer.Replace(url.PathEscape(args))
+		} else {
+			args = strings.ReplaceAll(args, "?", "%3F")
 		}
 
 		urlstr.WriteString(args)
 	}
 
+	// webquery & query should always be encoded, regardless.
+
 	if encodeFlags.Has(EncodeWebQuery) && len(gnoURL.WebQuery) > 0 {
 		urlstr.WriteRune('$')
-		urlstr.WriteString(EncodeValues(gnoURL.WebQuery, !noEscape))
+		urlstr.WriteString(EncodeValues(gnoURL.WebQuery, true))
 	}
 
 	if encodeFlags.Has(EncodeQuery) && len(gnoURL.Query) > 0 {
 		urlstr.WriteRune('?')
-		urlstr.WriteString(EncodeValues(gnoURL.Query, !noEscape))
+		urlstr.WriteString(EncodeValues(gnoURL.Query, true))
 	}
 
 	return urlstr.String()
 }
+
+var webEscapedReplacer = strings.NewReplacer(
+	"$", "%24",
+	"%2F", "/",
+)
 
 // Has checks if the EncodeFlag contains all the specified flags.
 func (f EncodeFlag) Has(flags EncodeFlag) bool {
 	return f&flags != 0
 }
 
-func escapeDollarSign(s string) string {
-	return strings.ReplaceAll(s, "$", "%24")
-}
-
 // EncodeArgs encodes the arguments and query parameters into a string.
 // This function is intended to be passed as a realm `Render` argument.
 func (gnoURL GnoURL) EncodeArgs() string {
-	return gnoURL.Encode(EncodeArgs | EncodeQuery)
+	return gnoURL.Encode(EncodeArgs | EncodeQuery | EncodeNoEscape)
 }
 
 // EncodeURL encodes the path, arguments, and query parameters into a string.
@@ -129,7 +134,7 @@ func (gnoURL GnoURL) EncodeURL() string {
 // EncodeWebURL encodes the path, package arguments, web query, and query into a string.
 // This function provides the full representation of the URL.
 func (gnoURL GnoURL) EncodeWebURL() string {
-	return gnoURL.Encode(EncodePath | EncodeArgs | EncodeWebQuery | EncodeQuery | EncodeNoEscape)
+	return gnoURL.Encode(EncodePath | EncodeArgs | EncodeWebQuery | EncodeQuery)
 }
 
 // IsPure checks if the URL path represents a pure path.

gno.land/pkg/gnoweb/weburl/url_test.go
@@ -337,7 +337,7 @@ func TestEncode(t *testing.T) {
 		Expected    string
 	}{
 		{
-			Name: "encode domain",
+			Name: "Encode domain",
 			GnoURL: GnoURL{
 				Domain: "gno.land",
 				Path:   "/r/demo/foo",
@@ -347,7 +347,7 @@ func TestEncode(t *testing.T) {
 		},
 
 		{
-			Name: "encode web query without escape",
+			Name: "Encode web query with NoEscape",
 			GnoURL: GnoURL{
 				Domain: "gno.land",
 				Path:   "/r/demo/foo",
@@ -357,7 +357,8 @@ func TestEncode(t *testing.T) {
 				},
 			},
 			EncodeFlags: EncodeWebQuery | EncodeNoEscape,
-			Expected:    "$fun$c=B$ ar&help",
+			// (still encoded; we always url-encode arguments after $ and ?)
+			Expected: "$fun%24c=B%24+ar&help",
 		},
 
 		{
@@ -580,17 +581,28 @@ func TestEncode(t *testing.T) {
 	}
 }
 
-func TestGnoURL_EncodeArgs(t *testing.T) {
-	// EncodeArgs is used to pass the values to the render function in Gno.
-	// Thus, it should maintain values escaped as they are in the original URL
-	// where it matters - like =&
-	gurl, err := Parse("/r/demo/users:foo%3d%24bar%26%3fbaz?hey=1&a%2bb=b%2b&%26=%3d")
+func TestGnoURL_Helpers(t *testing.T) {
+	gurl, err := Parse("/r/demo/users:p/foo%3d%24bar%26%3fbaz$func=foo?hey=1&a%2bb=b%2b&%26=%3d")
 	require.NoError(t, err)
-	assert.Equal(t, "foo=$bar&?baz", gurl.Args)
+	assert.Equal(t, "p/foo=$bar&?baz", gurl.Args)
 	assert.Equal(t, url.Values{
 		"hey": {"1"},
 		"a+b": {"b+"},
 		"&":   {"="},
 	}, gurl.Query)
-	assert.Equal(t, "foo=%24bar&%3Fbaz?%26=%3D&a%2Bb=b%2B&hey=1", gurl.EncodeArgs())
+
+	// EncodeArgs is used to pass the values to the render function in Gno.
+	// We are less restrictive about URLs in Gno, so it is only really important
+	// that ? in the path is encoded correctly, but we can pass in $ and other symbols
+	// regularly.
+	// The queries should always be encoded.
+	assert.Equal(t, "p/foo=$bar&%3Fbaz?%26=%3D&a%2Bb=b%2B&hey=1", gurl.EncodeArgs())
+	// EncodeURL, instead, should behave as the above but include the path.
+	// This should be used for display about the path to be used, but not used
+	// as an actual URL in <a> links.
+	assert.Equal(t, "/r/demo/users:p/foo=$bar&%3Fbaz?%26=%3D&a%2Bb=b%2B&hey=1", gurl.EncodeURL())
+	// EncodeWebURL is the representation meant to be used in links.
+	// WebQueries are included, and as such any webquery in the path should be escaped.
+	// The path is escaped too, except for / which is converted back for ease of reading.
+	assert.Equal(t, "/r/demo/users:p/foo=%24bar&%3Fbaz$func=foo?%26=%3D&a%2Bb=b%2B&hey=1", gurl.EncodeWebURL())
 }