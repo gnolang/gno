6e19189274d346e78449f696211507352a42863c
Morgan Bazalgette <morgan@morganbaz.com>
2025-05-28 11:06:16 +0200

    simplify SplitFilepath

--- tm2/pkg/std/memfile.go
+++ tm2/pkg/std/memfile.go
@@ -3,6 +3,7 @@ package std
 import (
 	"fmt"
 	"io/ioutil"
+	"path"
 	"path/filepath"
 	"regexp"
 	"slices"
@@ -244,24 +245,21 @@ func (mpkg *MemPackage) FileNames() (fnames []string) {
 
 // Splits a path into the dir and filename.
 func SplitFilepath(fpath string) (dir string, filename string) {
-	parts := strings.Split(fpath, "/")
-	if len(parts) == 1 {
-		return parts[0], ""
+	dir, filename = path.Split(fpath)
+	if dir == "" {
+		// assume that filename is actually a directory
+		return filename, ""
 	}
 
-	last := parts[len(parts)-1]
-
 	var (
-		isFileWithExtension = strings.Contains(last, ".")
-		isSpecialFile       = last == "LICENSE" || last == "README"
-		noFileSpecified     = last == ""
+		isFileWithExtension = strings.Contains(filename, ".")
+		isSpecialFile       = filename == "LICENSE" || filename == "README"
+		noFileSpecified     = filename == ""
 	)
 	if isFileWithExtension || isSpecialFile || noFileSpecified {
-		dir = strings.Join(parts[:len(parts)-1], "/")
 		dir = strings.TrimRight(dir, "/") // gno.land/r/path//a.gno -> dir=gno.land/r/path filename=a.gno
-		filename = last
 		return
 	}
 
-	return strings.Join(parts, "/"), ""
+	return dir + filename, ""
 }
