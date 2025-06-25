d752f797edba39c163b48fa6715241e5f9576195
alexiscolin <alexis@jaunebleu.co>
2025-06-26 01:43:46 +0900

    merge

--- gno.land/pkg/gnoweb/frontend/js/tooltip.ts
+++ gno.land/pkg/gnoweb/frontend/js/tooltip.ts
@@ -1,32 +1,38 @@
 class Tooltip {
-  private DOM = {
-    tooltip: [] as HTMLSpanElement[],
-  };
+	private DOM = {
+		tooltip: [] as HTMLSpanElement[],
+	};
 
-  private static SELECTORS = {
-    tooltip: "[data-tooltip]",
-  };
+	private static SELECTORS = {
+		tooltip: "[data-tooltip]",
+	};
 
-  constructor() {
-    this.DOM.tooltip = [
-      ...document.querySelectorAll<HTMLSpanElement>(Tooltip.SELECTORS.tooltip),
-    ];
-    
-    this.positionTooltip();
-    window.addEventListener("resize", this.positionTooltip.bind(this));
-  }
+	constructor() {
+		this.DOM.tooltip = [
+			...document.querySelectorAll<HTMLSpanElement>(Tooltip.SELECTORS.tooltip),
+		];
 
-  private positionTooltip(): void {
-    const screenWidth = window.innerWidth;
+		this.positionTooltip();
+		window.addEventListener("resize", this.positionTooltip.bind(this));
+	}
 
-    this.DOM.tooltip.forEach((tooltip) => {
-      const tooltipLeft = tooltip.getBoundingClientRect().left;
-      const isRightSide = tooltipLeft > screenWidth / 2;
+	private positionTooltip(): void {
+		const screenWidth = window.innerWidth;
 
-      tooltip.style.setProperty("--tooltip-left", isRightSide ? "initial" : "0");
-      tooltip.style.setProperty("--tooltip-right", isRightSide ? "0" : "initial");
-    });
-  }
+		this.DOM.tooltip.forEach((tooltip) => {
+			const tooltipLeft = tooltip.getBoundingClientRect().left;
+			const isRightSide = tooltipLeft > screenWidth / 2;
+
+			tooltip.style.setProperty(
+				"--tooltip-left",
+				isRightSide ? "initial" : "0",
+			);
+			tooltip.style.setProperty(
+				"--tooltip-right",
+				isRightSide ? "0" : "initial",
+			);
+		});
+	}
 }
 
 export default () => new Tooltip();
