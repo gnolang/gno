b074e2caa5197e87dfc962bf5de8984dd8263c90
alexiscolin <alexis@jaunebleu.co>
2025-07-02 19:12:13 +0900

    fix: remove loading warn

--- gno.land/pkg/gnoweb/frontend/js/index.ts
+++ gno.land/pkg/gnoweb/frontend/js/index.ts
@@ -6,24 +6,24 @@
 
   const modules: Record<string, Module> = {
     copy: {
-      selector: '.js-copy-btn',
-      path: '/public/js/copy.js',
+      selector: ".js-copy-btn",
+      path: "/public/js/copy.js",
     },
     help: {
-      selector: '.js-help-view',
-      path: '/public/js/realmhelp.js',
+      selector: ".js-help-view",
+      path: "/public/js/realmhelp.js",
     },
     searchBar: {
-      selector: '.js-header-searchbar',
-      path: '/public/js/searchbar.js',
+      selector: ".js-header-searchbar",
+      path: "/public/js/searchbar.js",
     },
     tooltip: {
-      selector: '.js-tooltip',
-      path: '/public/js/tooltip.js',
+      selector: ".js-tooltip",
+      path: "/public/js/tooltip.js",
     },
     list: {
-      selector: '.js-list',
-      path: '/public/js/list.js',
+      selector: ".js-list",
+      path: "/public/js/list.js",
     },
     breadcrumb: {
       selector: ".js-breadcrumb",
@@ -43,17 +43,15 @@
       } catch (err) {
         console.error(`Error while loading script ${path}:`, err);
       }
-    } else {
-      console.warn(
-        `Module not loaded: no element matches selector "${selector}"`
-      );
     }
   };
 
   const initModules = async (): Promise<void> => {
-    const promises = Object.values(modules).map(module => loadModuleIfExists(module));
+    const promises = Object.values(modules).map((module) =>
+      loadModuleIfExists(module)
+    );
     await Promise.all(promises);
   };
 
-  document.addEventListener('DOMContentLoaded', initModules);
+  document.addEventListener("DOMContentLoaded", initModules);
 })();
