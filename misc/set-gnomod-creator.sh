224cbae7d65cef25e94bac62f19d187449157fc4
Leon <33522493+leohhhn@users.noreply.github.com>
2025-07-09 23:46:47 +0200

    feat(examples): add svgbtn `p/` (#4445)
    ## Description
    
    Adds a cool svg button library.
    
    Also, sets the `leon` namespace creator to my address.

--- misc/set-gnomod-creator.sh
+++ misc/set-gnomod-creator.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # Set creator address in gnomod.toml files
 # Usage: ./set-gnomod-creator.sh <path> <address>
-# Example: ./set-gnomod-creator.sh examples/gno.land/p/demo g1jg8mtutu9khhfwc4nxmuhcpftf0pajdhfvsqf5
+# Example: ./set-gnomod-creator.sh ../examples/gno.land/p/demo g1jg8mtutu9khhfwc4nxmuhcpftf0pajdhfvsqf5
 
 [ $# -ne 2 ] && echo "Usage: $0 <path> <address>" && exit 1
 
