bc2deb5154ccfdcb02413548b9b5670648574ab3
ltzmaxwell <ltz.maxwell@gmail.com>
2025-07-11 18:09:53 +0800

    fix(gnovm): an alternative solution for mapkey attachment (#4385)
    alternative to #4312.

--- gnovm/pkg/gnolang/machine.go
+++ gnovm/pkg/gnolang/machine.go
@@ -2291,7 +2291,8 @@ func (m *Machine) PopAsPointer2(lx Expr) (pv PointerValue, ro bool) {
 	case *IndexExpr:
 		iv := m.PopValue()
 		xv := m.PopValue()
-		pv = xv.GetPointerAtIndex(m.Alloc, m.Store, iv)
+		pv = xv.GetPointerAtIndex(m.Realm, m.Alloc, m.Store, iv)
+
 		ro = m.IsReadonly(xv)
 	case *SelectorExpr:
 		xv := m.PopValue()
