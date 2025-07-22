adf953a0377a9e35310655483c77d69d6e7e16a4
alexiscolin <alexis@jaunebleu.co>
2025-07-22 17:59:25 +0900

    feat: add full source view

---
+++ gno.land/pkg/gnoweb/components/view_overview.go
@@ -0,0 +1,27 @@
+package components
+
+import (
+	"github.com/gnolang/gno/gnovm/pkg/doc"
+)
+
+const OverviewViewType ViewType = "overview-view"
+
+type OverviewData struct {
+	PkgPath     string
+	Readme      Component
+	Functions   []*doc.JSONFunc
+	Doc         string
+	Files       []string
+	FileCounter int
+}
+
+type overviewViewParams struct {
+	OverviewData
+}
+
+func OverviewView(data OverviewData) *View {
+	viewData := overviewViewParams{
+		OverviewData: data,
+	}
+	return NewTemplateView(OverviewViewType, "renderOverview", viewData)
+} 
