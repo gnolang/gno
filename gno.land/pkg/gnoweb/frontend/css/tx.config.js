8225bc71bd1cdff5e72ecae61316b06cdff6e308
alexiscolin <alexis@jaunebleu.co>
2025-07-23 01:04:48 +0900

    fix: font style

--- gno.land/pkg/gnoweb/frontend/css/tx.config.js
+++ gno.land/pkg/gnoweb/frontend/css/tx.config.js
@@ -83,7 +83,7 @@ export default {
 				'"Inter var"',
 				'-apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, "Noto Sans", "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji", sans-serif',
 			],
-			interSans: [
+			sans: [
 				'"Inter var"',
 				'-apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, "Noto Sans", sans-serif',
 			],
