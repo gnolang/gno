f76bdb2cc11c6d0541aaad5f94e39d8e131a0536
ltzMaxwell <ltz.maxwell@gmail.com>
2025-07-23 21:08:58 +0800

    reuse getObjectSafe

--- gnovm/pkg/gnolang/store.go
+++ gnovm/pkg/gnolang/store.go
@@ -287,18 +287,14 @@ func (ds *defaultStore) GetPackage(pkgPath string, isImport bool) *PackageValue 
 			ds.current = ds.current[:len(ds.current)-1]
 		}()
 	}
-	// first, check cache.
+
 	oid := ObjectIDFromPkgPath(pkgPath)
-	if oo, exists := ds.cacheObjects[oid]; exists {
-		pv := oo.(*PackageValue)
+	// Get package from cache or baseStore
+	oo := ds.GetObjectSafe(oid)
+	if pv, ok := oo.(*PackageValue); ok {
 		return pv
 	}
-	// else, load package.
-	if ds.baseStore != nil {
-		if oo := ds.loadObjectSafe(oid); oo != nil {
-			return oo.(*PackageValue)
-		}
-	}
+
 	// otherwise, fetch from pkgGetter.
 	if ds.pkgGetter != nil {
 		if pn, pv := ds.pkgGetter(pkgPath, ds); pv != nil {
