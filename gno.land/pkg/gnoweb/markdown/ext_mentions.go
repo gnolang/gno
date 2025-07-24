15893308269fb3e68a409941e98e1d23db94c061
alexiscolin <alexis@jaunebleu.co>
2025-07-24 21:54:44 +0900

    chore：order：order

--- gno.land/pkg/gnoweb/markdown/ext_mentions.go
+++ gno.land/pkg/gnoweb/markdown/ext_mentions.go
@@ -29,6 +29,12 @@ var (
 	mentionAddressPattern = regexp.MustCompile(`^(?: |)(g1[0-9A-Za-z]{37,90})(?:[^@0-9A-Za-z]|$)`)
 )
 
+// isValidBech32Address validates if the given string is a valid bech32 address
+func isValidBech32Address(address []byte) bool {
+	_, _, err := bech32.Decode(string(address))
+	return err == nil
+}
+
 func (p *mentionParser) Parse(parent ast.Node, block text.Reader, pc parser.Context) ast.Node {
 	current, before := rune(block.Peek()), block.PrecendingCharacter()
 
@@ -108,10 +114,4 @@ func (e *mentionExtension) Extend(m goldmark.Markdown) {
 	m.Parser().AddOptions(
 		parser.WithInlineParsers(util.Prioritized(NewMentionParser(), 500)),
 	)
-}
-
-// isValidBech32Address validates if the given string is a valid bech32 address
-func isValidBech32Address(address []byte) bool {
-	_, _, err := bech32.Decode(string(address))
-	return err == nil
 }
