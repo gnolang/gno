410c92d2ff640029af7c8b135ccd7bd1db56197b
Sacharbon <sacha.dujardin@epitech.eu>
2025-08-12 15:18:35 +0200

    test(gnokey): code review

--- tm2/pkg/crypto/keys/client/version_test.go
+++ tm2/pkg/crypto/keys/client/version_test.go
@@ -1,30 +1,50 @@
 package client
 
 import (
-	"testing"
+	"bytes"
 	"context"
+	"testing"
 
 	"github.com/gnolang/gno/tm2/pkg/commands"
 	"github.com/gnolang/gno/tm2/pkg/version"
+	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
 
 func TestClient_Version(t *testing.T) {
 	t.Parallel()
 
-	// Set current home
-	cfg := &BaseCfg{}
+	// Initialize stdout listener
+	mockOutput := bytes.NewBufferString("")
+	io := commands.NewTestIO()
+	io.SetOut(commands.WriteNopCloser(mockOutput))
 
 	// Initialize version command
-	versionCmd := NewVersionCmd(cfg, commands.NewTestIO())
-	versionValues := []string{"chain/test4.2", "develop", "master"}
+	versionCmd := NewVersionCmd(nil, io)
+	versionValues := []string{"develop", "chain/test4.2"}
 
 	// test: original version
-	err := versionCmd.ParseAndRun(context.Background(), []string{})
-	require.NoError(t, err)
+	require.NoError(t, versionCmd.ParseAndRun(context.Background(), nil))
+
+	output := mockOutput.String()
+	expected := "gnokey version: " + versionValues[0] + "\n"
+	assert.Equal(
+		t,
+		expected,
+		output,
+	)
+
+	mockOutput.Reset()
 
 	// test: version settled
-	version.Version = versionValues[0]
-	err = versionCmd.ParseAndRun(context.Background(), []string{})
-	require.NoError(t, err)
+	version.Version = versionValues[1]
+	require.NoError(t, versionCmd.ParseAndRun(context.Background(), nil))
+
+	output = mockOutput.String()
+	expected = "gnokey version: " + versionValues[1] + "\n"
+	assert.Equal(
+		t,
+		expected,
+		output,
+	)
 }
