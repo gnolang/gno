a121d34031df0398d76bcb189c0bed5a149a95e8
Sacharbon <sacha.dujardin@epitech.eu>
2025-08-22 13:09:04 +0200

    refactor(gnokey): fmt

--- tm2/pkg/crypto/keys/client/version.go
+++ tm2/pkg/crypto/keys/client/version.go
@@ -13,7 +13,7 @@ func NewVersionCmd(rootCfg *BaseCfg, io commands.IO) *commands.Command {
 			Name:       "version",
 			ShortUsage: "version",
 			ShortHelp:  "display the gnokey binary version",
-			LongHelp:  "Displays detailed version information for the gnokey binary, including the build number, commit hash, and build date. Useful for verifying the exact version of gnokey you are running.",
+			LongHelp:   "Displays detailed version information for the gnokey binary, including the build number, commit hash, and build date. Useful for verifying the exact version of gnokey you are running.",
 		},
 		nil,
 		func(_ context.Context, _ []string) error {
