35eab831869434ff57fc09a2f749bea8ae7359fb
Thomas <thomas.bruyelle@tendermint.com>
2025-09-29 16:53:12 +0200

    refactor!: replace `tm2/pkg/iavl` code with `cosmos/iavl` code (#4656)
    The IBC protocol is based on merkle proofs, and over the years a
    standard of merkle proof representation has emerged:
    [ICS23](https://github.com/cosmos/ics23). The simplest way for Gno to be
    compatible with this protocol is to migrate the iavl implementation to
    [`cosmos/iavl`](https://github.com/cosmos/iavl), which already supports
    ICS23.
    
    ```diff
    - Therefore, this PR deprecates `tm2/pkg/iavl` in favour of imported `cosmos/iavl`.
    + Therefore, this PR replaces the code of `tm2/pkg/iavl` with the code of `cosmos/iavl@v1.3.5`.
    ```
    
    Note that `cosmos/iavl` is just a maintened version of `tm2/pkg/iavl`,
    which has received numerous improvements and bug fixes over the years.
    
    
    In addition to the benefit of making Gno IBC-ready, this change also
    eliminates some drawbacks of `tm2/pkg/iavl`:
    - `tm2/pkg/iavl` uses the legacy `range-proof` under the hood which is
    considered unsafe. It has an exploited vulnerability (see DragonFruit
    [[1](https://github.com/cosmos/iavl/issues/584)][[2](https://forum.cosmos.network/t/cosmos-sdk-security-advisory-dragonfruit/7614)])
    - `tm2/pkg/iavl` proof generation is buggy: see
    https://github.com/gnolang/gno/pull/4369
    
    This change only concerns the proof system. The Gno light client realm
    which is the other important piece of the IBCv2 connection between
    Gnoland and AtomOne is still under progress
    [here](https://github.com/gnolang/gno/pull/4655).
    
    > [!IMPORTANT]
    > This
    [change](https://github.com/gnolang/gno/pull/4656/files#diff-4f1d80300014eb607e44892f6ebdcc31de2c0815669acd54f805b83501311b7fL419)
    to the commit info hashing is causing a app hash mismatch when
    restarting a node with a binary built from this branch.
    > ```
    > panic: state.AppHash does not match AppHash after replay. Got
    > 76BB396891B9F4415AC49FB72CF81933F407AE6A2C04551814719C9A908B2E8E,
    expected
    8EDAB60FFD0B2C6BB5AC398417CF0AFBFE6E7D491C830F831F9395DB7FAC522A.
    > ```
    > Unfortunately without it some tests are failing because ics23 does not
    expect to have hashed value. As a result, upgrading a mainnet launched
    using `tm2/pkg/iavl` to a new version with `cosmos/iavl` might be
    challenging. It's probably a good idea to merge this change for the
    initial mainnet.
    
    
    ## For reviewers
    
    - Most of the diff is related to the `DB` interface, because most of
    declared methods return an error in `comos/iavl`
    - When possible I added a comment for change related to the difference
    between `tm2/pkg/iavl` and `cosmos/iavl` implementation.
    - Lazy loading has been removed in `cosmos/iavl` so I had no other
    choice than removing the option from the Gno store. Anyway this feature
    was not used.
    
    ---------
    
    Co-authored-by: n0izn0iz <n0izn0iz@users.noreply.github.com>
    Co-authored-by: Miloš Živković <milos.zivkovic@tendermint.com>

--- tm2/pkg/store/cache/memiterator.go
+++ tm2/pkg/store/cache/memiterator.go
@@ -78,8 +78,13 @@ func (mi *memIterator) Value() []byte {
 	return mi.items[len(mi.items)-1].Value
 }
 
-func (mi *memIterator) Close() {
+func (mi *memIterator) Error() error {
+	return nil // famous last words
+}
+
+func (mi *memIterator) Close() error {
 	mi.start = nil
 	mi.end = nil
 	mi.items = nil
+	return nil
 }
