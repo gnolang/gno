ab12bd0bf16cf1ff0978a46ff0e3e436acf9486f
David <david.gozlan@epitech.eu>
2025-11-16 16:07:25 +0100

    refactor(tests): simplify UnmarshalAmino tests

--- gnovm/pkg/gnolang/ownership_test.go
+++ gnovm/pkg/gnolang/ownership_test.go
@@ -4,44 +4,21 @@ import "testing"
 
 func TestObjectID_UnmarshalAmino_InvalidPkgIDLength(t *testing.T) {
 	var oid ObjectID
-
-	// Too short
-	err := oid.UnmarshalAmino("abc:100")
-	if err == nil {
+	if oid.UnmarshalAmino("abc:100") == nil {
 		t.Error("expected error for short PkgID")
-	}
-
-	// Too long
-	err = oid.UnmarshalAmino("abcdef0123456789abcdef0123456789abcdef01234:100")
-	if err == nil {
-		t.Error("expected error for long PkgID")
-	}
-
-	// Valid length should work
-	err = oid.UnmarshalAmino("abcdef0123456789abcdef0123456789abcdef01:100")
-	if err != nil {
-		t.Errorf("unexpected error for valid PkgID: %v", err)
 	}
 }
 
-func TestObjectID_UnmarshalAmino_NewTimeZeroWithNonZeroPkgID(t *testing.T) {
+func TestObjectID_UnmarshalAmino_NewTimeZero(t *testing.T) {
 	var oid ObjectID
-
-	// NewTime zero with non-zero PkgID should fail
-	err := oid.UnmarshalAmino("abcdef0123456789abcdef0123456789abcdef01:0")
-	if err == nil {
+	if oid.UnmarshalAmino("abcdef0123456789abcdef0123456789abcdef01:0") == nil {
 		t.Error("expected error for NewTime=0 with non-zero PkgID")
 	}
+}
 
-	// NewTime zero with zero PkgID should work
-	err = oid.UnmarshalAmino("0000000000000000000000000000000000000000:0")
-	if err != nil {
-		t.Errorf("unexpected error for NewTime=0 with zero PkgID: %v", err)
-	}
-
-	// NewTime > 0 with non-zero PkgID should work
-	err = oid.UnmarshalAmino("abcdef0123456789abcdef0123456789abcdef01:1")
-	if err != nil {
-		t.Errorf("unexpected error for NewTime>0 with non-zero PkgID: %v", err)
+func TestObjectID_UnmarshalAmino_NegativeNewTime(t *testing.T) {
+	var oid ObjectID
+	if oid.UnmarshalAmino("abcdef0123456789abcdef0123456789abcdef01:-1") == nil {
+		t.Error("expected error for negative NewTime")
 	}
 }
