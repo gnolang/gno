76d9fc808c3a5b1505c33cdaa3361bf32fec581f
Miloš Živković <milos.zivkovic@tendermint.com>
2025-12-11 14:09:47 +0100

    Add health RPC endpoint

---
+++ tm2/pkg/bft/rpc/core/health/health.go
@@ -0,0 +1,19 @@
+package health
+
+import (
+	ctypes "github.com/gnolang/gno/tm2/pkg/bft/rpc/core/types"
+	"github.com/gnolang/gno/tm2/pkg/bft/rpc/lib/server/metadata"
+	"github.com/gnolang/gno/tm2/pkg/bft/rpc/lib/server/spec"
+)
+
+// HealthHandler fetches the node health.
+// Returns empty result (200 OK) on success, no response - in case of an error
+//
+//	No params
+func HealthHandler(_ *metadata.Metadata, p []any) (any, *spec.BaseJSONError) {
+	if len(p) > 0 {
+		return nil, spec.GenerateInvalidParamError(1)
+	}
+
+	return &ctypes.ResultHealth{}, nil
+}
