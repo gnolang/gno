8b3dcb05cca13a24aa18a721397a030187fd112c
David <david.gozlan@epitech.eu>
2026-01-12 16:48:48 +0100

    test(gnoweb): enhance TestHasRenderFunction with additional cases

--- gno.land/pkg/gnoweb/client_test.go
+++ gno.land/pkg/gnoweb/client_test.go
@@ -18,19 +18,69 @@ func TestHasRenderFunction(t *testing.T) {
 		{
 			name: "valid Render function",
 			jdoc: &doc.JSONDocumentation{
-				Funcs: []*doc.JSONFunc{
-					{
-						Name:    "Render",
-						Params:  []*doc.JSONField{{Name: "path", Type: "string"}},
-						Results: []*doc.JSONField{{Name: "", Type: "string"}},
-					},
-				},
+				Funcs: []*doc.JSONFunc{{
+					Name:    "Render",
+					Params:  []*doc.JSONField{{Type: "string"}},
+					Results: []*doc.JSONField{{Type: "string"}},
+				}},
 			},
 			expected: true,
 		},
+		{name: "nil doc", jdoc: nil, expected: false},
+		{name: "empty funcs", jdoc: &doc.JSONDocumentation{}, expected: false},
 		{
-			name:     "nil doc",
-			jdoc:     nil,
+			name: "wrong param type",
+			jdoc: &doc.JSONDocumentation{
+				Funcs: []*doc.JSONFunc{{
+					Name:    "Render",
+					Params:  []*doc.JSONField{{Type: "int"}},
+					Results: []*doc.JSONField{{Type: "string"}},
+				}},
+			},
+			expected: false,
+		},
+		{
+			name: "wrong result type",
+			jdoc: &doc.JSONDocumentation{
+				Funcs: []*doc.JSONFunc{{
+					Name:    "Render",
+					Params:  []*doc.JSONField{{Type: "string"}},
+					Results: []*doc.JSONField{{Type: "int"}},
+				}},
+			},
+			expected: false,
+		},
+		{
+			name: "too many params",
+			jdoc: &doc.JSONDocumentation{
+				Funcs: []*doc.JSONFunc{{
+					Name:    "Render",
+					Params:  []*doc.JSONField{{Type: "string"}, {Type: "string"}},
+					Results: []*doc.JSONField{{Type: "string"}},
+				}},
+			},
+			expected: false,
+		},
+		{
+			name: "no params",
+			jdoc: &doc.JSONDocumentation{
+				Funcs: []*doc.JSONFunc{{
+					Name:    "Render",
+					Params:  []*doc.JSONField{},
+					Results: []*doc.JSONField{{Type: "string"}},
+				}},
+			},
+			expected: false,
+		},
+		{
+			name: "wrong function name",
+			jdoc: &doc.JSONDocumentation{
+				Funcs: []*doc.JSONFunc{{
+					Name:    "NotRender",
+					Params:  []*doc.JSONField{{Type: "string"}},
+					Results: []*doc.JSONField{{Type: "string"}},
+				}},
+			},
 			expected: false,
 		},
 	}
@@ -38,8 +88,7 @@ func TestHasRenderFunction(t *testing.T) {
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			t.Parallel()
-			result := HasRenderFunction(tt.jdoc)
-			assert.Equal(t, tt.expected, result)
+			assert.Equal(t, tt.expected, HasRenderFunction(tt.jdoc))
 		})
 	}
 }
