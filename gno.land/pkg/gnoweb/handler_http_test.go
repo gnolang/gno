6dbe87c2b2812e24c53fdb785b9ff61c59136c52
David <david.gozlan@epitech.eu>
2026-01-13 17:35:38 +0100

    feat(gnoweb): add hasRenderMap in parameter of DirectoryView

--- gno.land/pkg/gnoweb/handler_http_test.go
+++ gno.land/pkg/gnoweb/handler_http_test.go
@@ -400,8 +400,8 @@ func TestHTTPHandler_RealmExplorerWithRender(t *testing.T) {
 	handler.ServeHTTP(rr, httptest.NewRequest(http.MethodGet, "/r/demo/", nil))
 
 	assert.Equal(t, http.StatusOK, rr.Code)
-	assert.Contains(t, rr.Body.String(), "#ico-realm")
 	assert.Contains(t, rr.Body.String(), "Source")
+	assert.Contains(t, rr.Body.String(), "Action")
 }
 
 // TestHTTPHandler_PathsListView_DocError tests Doc errors are handled gracefully.
