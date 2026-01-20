7b2c4f9d6530d03e208fed1d97249b02aaff1067
MikaelVallenet <mikael.vallenetpro@gmail.com>
2026-01-20 13:57:32 +0100

    feat(lint): clean code & merge gno code issue & lint issues

--- gnovm/pkg/lint/reporter.go
+++ gnovm/pkg/lint/reporter.go
@@ -1,5 +1,7 @@
 package lint
 
+// XXX: Consider moving Reporter to a standalone package (e.g., gnovm/pkg/report)
+// to allow reuse by other commands (run, test) without coupling them to the lint package.
 type Reporter interface {
 	Report(issue Issue)
 	Flush() error
