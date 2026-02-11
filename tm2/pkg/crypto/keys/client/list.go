535f4d2ada087dba20742d1b818aede7c8b1e773
D4ryl00 <d4ryl00@gmail.com>
2026-02-11 12:56:18 +0100

    feat: print bech32 pubkey for multisig key in gnokey list
    Signed-off-by: D4ryl00 <d4ryl00@gmail.com>

--- tm2/pkg/crypto/keys/client/list.go
+++ tm2/pkg/crypto/keys/client/list.go
@@ -5,49 +5,73 @@ import (
 	"flag"
 
 	"github.com/gnolang/gno/tm2/pkg/commands"
+	"github.com/gnolang/gno/tm2/pkg/crypto"
 	"github.com/gnolang/gno/tm2/pkg/crypto/keys"
 )
 
+type ListCfg struct {
+	RootCfg *BaseCfg
+
+	ShowMultisigMembers bool
+}
+
 func NewListCmd(rootCfg *BaseCfg, io commands.IO) *commands.Command {
+	cfg := &ListCfg{
+		RootCfg: rootCfg,
+	}
+
 	return commands.NewCommand(
 		commands.Metadata{
 			Name:       "list",
 			ShortUsage: "list",
 			ShortHelp:  "lists all keys in the keybase",
 		},
-		nil,
+		cfg,
 		func(_ context.Context, args []string) error {
-			return execList(rootCfg, args, io)
+			return execList(cfg, args, io)
 		},
 	)
 }
 
-func execList(cfg *BaseCfg, args []string, io commands.IO) error {
+func (c *ListCfg) RegisterFlags(fs *flag.FlagSet) {
+	fs.BoolVar(
+		&c.ShowMultisigMembers,
+		"multisig-members",
+		false,
+		"show multisig member public keys instead of the multisig public key",
+	)
+}
+
+func execList(cfg *ListCfg, args []string, io commands.IO) error {
 	if len(args) != 0 {
 		return flag.ErrHelp
 	}
 
-	kb, err := keys.NewKeyBaseFromDir(cfg.Home)
+	kb, err := keys.NewKeyBaseFromDir(cfg.RootCfg.Home)
 	if err != nil {
 		return err
 	}
 
 	infos, err := kb.List()
 	if err == nil {
-		printInfos(infos, io)
+		printInfos(infos, cfg.ShowMultisigMembers, io)
 	}
 
 	return err
 }
 
-func printInfos(infos []keys.Info, io commands.IO) {
+func printInfos(infos []keys.Info, showMultisigMembers bool, io commands.IO) {
 	for i, info := range infos {
 		keyname := info.GetName()
 		keytype := info.GetType()
 		keypub := info.GetPubKey()
+		keypubDisplay := keypub.String()
+		if keytype == keys.TypeMulti && !showMultisigMembers {
+			keypubDisplay = crypto.PubKeyToBech32(keypub)
+		}
 		keyaddr := info.GetAddress()
 		keypath, _ := info.GetPath()
 		io.Printfln("%d. %s (%s) - addr: %v pub: %v, path: %v",
-			i, keyname, keytype, keyaddr, keypub, keypath)
+			i, keyname, keytype, keyaddr, keypubDisplay, keypath)
 	}
 }
