b7c94155307ca97b76aa0baaf8f1164086b3cb55
David <david.gozlan@epitech.eu>
2026-03-04 08:36:46 +0100

    test(replay): enable parallel execution for TestReplayNilGuards

--- tm2/pkg/bft/consensus/replay_test.go
+++ tm2/pkg/bft/consensus/replay_test.go
@@ -1280,6 +1280,8 @@ func TestReplayNilGuards(t *testing.T) {
 
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
+
 			stateDB := memdb.NewMemDB()
 			state := genesisState
 			state.LastBlockHeight = tt.stateHeight
