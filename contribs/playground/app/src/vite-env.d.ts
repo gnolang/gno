8049779d5bd0d8d9af6282d53fec6a2d12342644
jeronimoalbi <894299+jeronimoalbi@users.noreply.github.com>
2026-03-27 16:31:25 +0100

    chore: add ".env.example" file to playground app

--- contribs/playground/app/src/vite-env.d.ts
+++ contribs/playground/app/src/vite-env.d.ts
@@ -5,10 +5,13 @@ interface ImportMetaEnv {
   readonly DEV: boolean
   readonly PROD: boolean
   readonly VITE_BACKEND_GIT_PROXY_URL: string
-  readonly VITE_BACKEND_GRPC_URL: string
   readonly VITE_DOMAIN_URL: string
   readonly VITE_DISABLE_BEFORE_UNLOAD?: string
   readonly VITE_LSP_DEBUG: string
+  readonly VITE_BACKEND_GIT_PROXY_URL?: string
+  readonly VITE_GNOVM_WASM_VERSION: string
+  readonly VITE_GNOVM_WASM_URL?: string
+  readonly VITE_GNOVM_ROOT_URL?: string
 }
 
 interface ImportMeta {
