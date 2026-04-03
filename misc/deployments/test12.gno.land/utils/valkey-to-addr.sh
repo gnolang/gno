71a588e558b8e80c59e4022e2af435daf83b6dcc
aeddi <antoine.e.b@gmail.com>
2026-04-03 14:46:34 +0200

    refactor: embed go script inside valkey-to-addr

--- misc/deployments/test12.gno.land/utils/valkey-to-addr.sh
+++ misc/deployments/test12.gno.land/utils/valkey-to-addr.sh
@@ -21,4 +21,36 @@ if [ $# -ne 1 ]; then
   exit 1
 fi
 
-go run -C "$REPO_ROOT" "$SCRIPT_DIR/valkey-to-addr/" "$1"
+tmpfile=$(mktemp "$REPO_ROOT/valkey-to-addr-XXXXXX.go")
+trap 'rm -f "$tmpfile"' EXIT
+
+cat > "$tmpfile" << 'EOF'
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/gnolang/gno/tm2/pkg/crypto"
+	// Register secp256k1 and ed25519 amino types so PubKeyFromBech32 can decode both.
+	_ "github.com/gnolang/gno/tm2/pkg/crypto/ed25519"
+	_ "github.com/gnolang/gno/tm2/pkg/crypto/secp256k1"
+)
+
+func main() {
+	if len(os.Args) != 2 {
+		fmt.Fprintf(os.Stderr, "Usage: valkey-to-addr <gpub1...>\n")
+		os.Exit(1)
+	}
+
+	pubKey, err := crypto.PubKeyFromBech32(os.Args[1])
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error: %v\n", err)
+		os.Exit(1)
+	}
+
+	fmt.Println(crypto.AddressToBech32(pubKey.Address()))
+}
+EOF
+
+go run "$tmpfile" "$1"
