96820086a8499615465b0fa7b6f8fcca9256192e
jeronimoalbi <894299+jeronimoalbi@users.noreply.github.com>
2026-06-08 12:34:23 +0200

    chore: remove template name when parsing

--- gno.land/pkg/gnoweb/feature/playground/template.go
+++ gno.land/pkg/gnoweb/feature/playground/template.go
@@ -11,10 +11,10 @@ var templateFS embed.FS
 // PageTemplate is the standalone playground page. Pre-parsed at init
 // so a misconfigured template surfaces immediately, not on the first
 // request.
-var PageTemplate = mustParse("renderPage", "templates/page.html")
+var PageTemplate = mustParse("templates/page.html")
 
-func mustParse(name string, paths ...string) *template.Template {
-	t, err := template.New(name).ParseFS(templateFS, paths...)
+func mustParse(paths ...string) *template.Template {
+	t, err := template.New("").ParseFS(templateFS, paths...)
 	if err != nil {
 		panic("playground: parse " + paths[0] + ": " + err.Error())
 	}
