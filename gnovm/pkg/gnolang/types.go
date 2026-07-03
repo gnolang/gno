8d5ab2e20cf1952e7c624d593ad902fdb7a550e6
ltzMaxwell <ltz.maxwell@gmail.com>
2026-07-03 14:16:16 +0800

    refactor(gnovm): shorten unflattened-interface panic message

--- gnovm/pkg/gnolang/types.go
+++ gnovm/pkg/gnolang/types.go
@@ -1087,7 +1087,7 @@ func (it *InterfaceType) FindEmbeddedFieldType(callerPath string, n Name, m map[
 // under -tags debugAssert.
 func (it *InterfaceType) panicUnflattened(im FieldType) {
 	panic(fmt.Sprintf(
-		"unflattened embedded interface %q in %s: state persisted before interface flattening requires regenesis or a re-flatten migration",
+		"unflattened embedded interface %q in %s: state persisted before interface flattening is unsupported",
 		im.Name, it.String()))
 }
 
