da54410460840ed494d77a61c3ef3676644e9abf
Morgan Bazalgette <morgan@morganbaz.com>
2026-07-07 14:03:57 +0200

    fixup

--- contribs/gnodev/pkg/packages/package_test.go
+++ contribs/gnodev/pkg/packages/package_test.go
@@ -40,7 +40,7 @@ func TestPackage_ToMemPackage_InMemory(t *testing.T) {
 func TestPackage_ToMemPackage_FS_RereadsOnEachCall(t *testing.T) {
 	dir := t.TempDir()
 	require.NoError(t, os.WriteFile(filepath.Join(dir, "gnomod.toml"),
-		[]byte(fmt.Sprintf("module = %q\n", "gno.land/p/demo/foo")), 0o644))
+		fmt.Appendf(nil, "module = %q\n", "gno.land/p/demo/foo"), 0o644))
 	foo := filepath.Join(dir, "foo.gno")
 	require.NoError(t, os.WriteFile(foo, []byte("package foo\nfunc A() {}\n"), 0o644))
 
